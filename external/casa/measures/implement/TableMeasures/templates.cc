545a7575911638bf7c916494e6cfa8a27d4b8d11
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-01-09 19:07:37 +0000

    Considered all template instantiations from _Reposfiller
    git-svn-id: http://usg.lofar.org/svn/code/trunk@86 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- external/casa/measures/implement/TableMeasures/templates.cc
+++ external/casa/measures/implement/TableMeasures/templates.cc
@@ -7,9 +7,9 @@
 #include <measures/Measures/MEarthMagnetic.h>
 #include <measures/Measures/MEpoch.h>
 #include <measures/Measures/MFrequency.h>
-#include <measures/Measures/MPosition.h 
-#include <measures/Measures/Muvw.h 
-#include <measures/Measures/MRadialVelocity.h 
+#include <measures/Measures/MPosition.h>
+#include <measures/Measures/Muvw.h>
+#include <measures/Measures/MRadialVelocity.h>
 
 // include of implementation files
 
@@ -34,39 +34,39 @@ namespace casa {
   template class ROArrayMeasColumn<MRadialVelocity>;
   template class ArrayMeasColumn<MRadialVelocity>;
   
-  template class ROArrayQuantColumn<Double>;
-  template class ArrayQuantColumn<Double>;
-  template class ROArrayQuantColumn<Float>;
-  template class ArrayQuantColumn<Float>;
+//   template class ROArrayQuantColumn<Double>;
+//   template class ArrayQuantColumn<Double>;
+//   template class ROArrayQuantColumn<Float>;
+//   template class ArrayQuantColumn<Float>;
 
-  template class ROScalarMeasColumn<MDirection>;
-  template class ScalarMeasColumn<MDirection>;
-  template class ROScalarMeasColumn<MDoppler>;
-  template class ScalarMeasColumn<MDoppler>;
-  template class ROScalarMeasColumn<MEpoch>;
-  template class ScalarMeasColumn<MEpoch>;
-  template class ROScalarMeasColumn<MFrequency>;
-  template class ScalarMeasColumn<MFrequency>;
-  template class ROScalarMeasColumn<MPosition>;
-  template class ScalarMeasColumn<MPosition>;
-  template class ROScalarMeasColumn<MRadialVelocity>;
-  template class ScalarMeasColumn<MRadialVelocity>;
-  template class ROScalarMeasColumn<Muvw>;
-  template class ScalarMeasColumn<Muvw>;
+//   template class ROScalarMeasColumn<MDirection>;
+//   template class ScalarMeasColumn<MDirection>;
+//   template class ROScalarMeasColumn<MDoppler>;
+//   template class ScalarMeasColumn<MDoppler>;
+//   template class ROScalarMeasColumn<MEpoch>;
+//   template class ScalarMeasColumn<MEpoch>;
+//   template class ROScalarMeasColumn<MFrequency>;
+//   template class ScalarMeasColumn<MFrequency>;
+//   template class ROScalarMeasColumn<MPosition>;
+//   template class ScalarMeasColumn<MPosition>;
+//   template class ROScalarMeasColumn<MRadialVelocity>;
+//   template class ScalarMeasColumn<MRadialVelocity>;
+//   template class ROScalarMeasColumn<Muvw>;
+//   template class ScalarMeasColumn<Muvw>;
 
-  template class ROScalarQuantColumn<Double>;
-  template class ROScalarQuantColumn<Float>;
-  template class ScalarQuantColumn<Double>;
-  template class ScalarQuantColumn<Float>;
+//   template class ROScalarQuantColumn<Double>;
+//   template class ROScalarQuantColumn<Float>;
+//   template class ScalarQuantColumn<Double>;
+//   template class ScalarQuantColumn<Float>;
 
-  template class TableMeasDesc<MBaseline>;
-  template class TableMeasDesc<MDirection>;
-  template class TableMeasDesc<MDoppler>;
-  template class TableMeasDesc<MEarthMagnetic>;
-  template class TableMeasDesc<MEpoch>;
-  template class TableMeasDesc<MFrequency>;
-  template class TableMeasDesc<MPosition>;
-  template class TableMeasDesc<MRadialVelocity>;
-  template class TableMeasDesc<Muvw>;
+//   template class TableMeasDesc<MBaseline>;
+//   template class TableMeasDesc<MDirection>;
+//   template class TableMeasDesc<MDoppler>;
+//   template class TableMeasDesc<MEarthMagnetic>;
+//   template class TableMeasDesc<MEpoch>;
+//   template class TableMeasDesc<MFrequency>;
+//   template class TableMeasDesc<MPosition>;
+//   template class TableMeasDesc<MRadialVelocity>;
+//   template class TableMeasDesc<Muvw>;
 
 }
