fce88c3119212c58905d1a54a5743ccac475f8a2
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-05-10 09:01:23 +0000

    Search script for SWIG
    git-svn-id: http://usg.lofar.org/svn/code/trunk@330 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ devel_common/cmake/FindSWIG.cmake
@@ -0,0 +1,71 @@
+# - Check for the presence of SWIG
+#
+# The following variables are set when SWIG is found:
+#  HAVE_SWIG       = Set to true, if all components of SWIG
+#                          have been found.
+#  SWIG_INCLUDES   = Include path for the header files of SWIG
+#  SWIG_LIBRARIES  = Link these to use SWIG
+#  SWIG_LFGLAS     = Linker flags (optional)
+
+## -----------------------------------------------------------------------------
+## Find executable
+
+find_program (SWIG_BIN swig
+  PATHS /bin /usr/bin /usr/local/bin /sw/bin
+  )
+
+## -----------------------------------------------------------------------------
+## Check for the header files
+
+#find_path (SWIG_INCLUDES <header file(s)>
+#  PATHS /usr/include /usr/local/include /sw/include
+#  PATH_SUFFIXES <optional path extension>
+#  NO_DEFAULT_PATH
+#  )
+
+## -----------------------------------------------------------------------------
+## Check for the library
+
+#find_library (SWIG_LIBRARIES <package name>
+#  PATHS /usr/lib /usr/local/lib /sw/lib
+#  NO_DEFAULT_PATH
+#  )
+
+## -----------------------------------------------------------------------------
+## Actions taken when all components have been found
+
+if (SWIG_BIN)
+  set (HAVE_SWIG TRUE)
+endif (SWIG_BIN)
+
+#if (SWIG_INCLUDES AND SWIG_LIBRARIES)
+#  set (HAVE_SWIG TRUE)
+#else (SWIG_INCLUDES AND SWIG_LIBRARIES)
+#  if (NOT SWIG_FIND_QUIETLY)
+#    if (NOT SWIG_INCLUDES)
+#      message (STATUS "Unable to find SWIG header files!")
+#    endif (NOT SWIG_INCLUDES)
+#    if (NOT SWIG_LIBRARIES)
+#      message (STATUS "Unable to find SWIG library files!")
+#    endif (NOT SWIG_LIBRARIES)
+#  endif (NOT SWIG_FIND_QUIETLY)
+#endif (SWIG_INCLUDES AND SWIG_LIBRARIES)
+
+if (HAVE_SWIG)
+  if (NOT SWIG_FIND_QUIETLY)
+    message (STATUS "Found components for SWIG")
+    message (STATUS "SWIG_BIN         = ${SWIG_BIN}")
+#    message (STATUS "SWIG_INCLUDES  = ${SWIG_INCLUDES}")
+#    message (STATUS "SWIG_LIBRARIES = ${SWIG_LIBRARIES}")
+  endif (NOT SWIG_FIND_QUIETLY)
+else (HAVE_SWIG)
+  if (SWIG_FIND_REQUIRED)
+    message (FATAL_ERROR "Could not find SWIG!")
+  endif (SWIG_FIND_REQUIRED)
+endif (HAVE_SWIG)
+
+## -----------------------------------------------------------------------------
+## Mark advanced variables
+
+#mark_as_advanced (
+#  )
