af95a3430def6b29d4acc8e743f10a61d30258bc
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-09-11 15:25:27 +0000

    Added further build targets
    git-svn-id: http://usg.lofar.org/svn/code/trunk@745 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- build/build.sh
+++ build/build.sh
@@ -15,6 +15,10 @@ basedir=`pwd`
 ## -----------------------------------------------------------------------------
 ## Helper functions
 
+# \param buidrDir  -- directory in which to perform the build; this is a 
+#                     sub-directory created below "build"
+# \param sourceDir -- directory in which the source code of the package can be
+#                     found; the path is given relativ to $LOFARSOFT
 build_package ()
 {
     buildDir=$1
@@ -49,6 +53,14 @@ case $1 in
     bison)
 	echo "[build] Selected package Bison"
 	build_package bison external/bison
+    ;;
+    casacore)
+	echo "[build] Selected package CASACORE"
+	build_package casacore external/casacore
+    ;;
+    cfitsio)
+	echo "[build] Selected package CFITSIO"
+	build_package cfitsio external/cfitsio
     ;;
     flex)
 	echo "[build] Selected package Flex"
