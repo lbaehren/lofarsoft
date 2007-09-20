c19e7b42a130201affc911da8fb86a578550d159
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-09-20 08:56:02 +0000

    [1] A number of fixes on the configuration for building the DAL. [2] Upgrade of the build shell script to handle multiple command line options; also added more extensive help information.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@798 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- build/build.sh
+++ build/build.sh
@@ -7,7 +7,7 @@
 ## A simple shell script to configure and install as many as possible of
 ## the packages in the LOFAR USG distribution. Simply run
 ## 
-##   ./build.sh <package>
+##   ./build.sh <package> [options]
 ##
 ## from within the build directory - and the script should take care of
 ## the rest.
@@ -16,41 +16,37 @@
 basedir=`pwd`
 
 ## -----------------------------------------------------------------------------
-## check command line parameters
-
-echo "[build]";
-
-## param 1 -- Name of the package to build
-
-if test -z $1 ; then
-    echo " -- Missing input parameters.";
-    echo "";
-    echo " Usage:  build.sh <package> [build-option]";
-    echo "";
-else
-    param_packageName=$1;
-    echo " -- Selected package: $param_packageName";
-fi
-
-## param 2 -- Force build of package from provided source?
-
-if test -z $2 ; then
-    echo " -- No further build options.";
-    param_forceBuild=0;
-else
-    case $2 in
-	--force-build)
-	    param_forceBuild=1;
-	    echo " -- Recognized build option; forcing build."; 
-	;;
-	*)
-	    echo " --> Unknown parameter $2"
-	;;
-    esac
-fi
-
-## -----------------------------------------------------------------------------
 ## Helper functions
+
+print_help ()
+{
+    echo "";
+    echo "NAME";
+    echo "    build.sh -- script for (batch) control of builds";
+    echo "";
+    echo "SYNOPSIS";
+    echo "    build.sh <package> [build-option]";
+    echo "    build.sh -clean";
+    echo "    build.sh -help";
+    echo "";
+    echo "DESCRIPTION";
+    echo "    A simple shell script to configure and install as many as possible of";
+    echo "    the packages in the LOFAR USG distribution.";
+    echo "";
+    echo "    <package> = bison blitz boost casacore cfitsio cmake cr dal flex hdf5";
+    echo "                pgplot plplot python wcslib";
+    echo "";
+    echo "    The following options are available:";
+    echo "";
+    echo " --force-build  = Force build from provided source code, even if system-wide";
+    echo "                  version exists; otherwise the system-wise version of a";
+    echo "                  library/package/binary will be used.";
+    echo "";
+    echo " --clean-build  = Clean up the build directory before the configuration step;";
+    echo "                  use this in order to erase previous configuration settings";
+    echo "                  and start fresh.";
+    echo "";
+}
 
 # \param buildDir  -- directory in which to perform the build; this is a 
 #                     sub-directory created below "build"
@@ -72,8 +68,6 @@ build_package ()
 	{
 	# change into the build directory
 	cd $buildDir
-	# clean up the directory before we do anything else
-	rm -rf *
 	# run cmake on the source directory
 	if test -z $configureOption ; then
 	    cmake $basedir/../$sourceDir
@@ -98,28 +92,99 @@ build_package ()
 }
 
 ## -----------------------------------------------------------------------------
+## check command line parameters
+
+## [1] Required parameter: Name of the package to build -- if now package name
+##                         is provided, we print the help information
+
+if test -z $1 ; then
+    print_help
+else
+    case $1 in
+	    -h)
+			print_help
+		;;
+		--h)
+			print_help
+		;;
+		-help)
+			print_help
+		;;
+		--help)
+			print_help
+		;;
+		help)
+			print_help
+		;;
+		-clean)
+			rm -f *~ *.log;
+			rm -rf bison blitz boost;
+			rm -rf casacore cfitsio cmake cr; 
+			rm -rf dal dsp;
+			rm -rf flex;
+			rm -rf hdf5;
+			rm -rf pgplot plplot python;
+			rm -rf vtk;
+			rm -rf wcslib;
+		;;
+		*)
+			param_packageName=$1;
+			echo " -- Selected package: $param_packageName";
+		;;
+	esac
+fi
+
+## [2] Optional parameters: Force build of package from provided source?
+
+param_forceBuild=0;
+param_cleanBuild=0;
+
+option_found=true
+while [ "$option_found" == "true" ]
+do
+  case $2 in
+    --force-build)
+	    param_forceBuild=1;
+		shift;
+	    echo " -- Recognized build option; forcing build."; 
+	;;
+    --clean-build)
+	    param_cleanBuild=1;
+		shift;
+	    echo " -- Recognized build option; forcing build."; 
+	;;
+	*)
+		option_found=false
+  esac
+done
+
+## -----------------------------------------------------------------------------
 ## Build individual/multiple packages
 
 case $param_packageName in 
     bison)
-	echo "[build] Selected package Bison"
-	build_package bison external/bison
+		echo "[build] Selected package Bison"
+		build_package bison external/bison -DBISON_FORCE_BUILD:BOOL=$param_forceBuild
     ;;
     blitz)
-	echo "[build] Selected package Blitz++"
-	build_package blitz external/blitz -DBLITZ_FORCE_BUILD:BOOL=$param_forceBuild
+		echo "[build] Selected package Blitz++"
+		build_package blitz external/blitz -DBLITZ_FORCE_BUILD:BOOL=$param_forceBuild
     ;;
     boost)
-	echo "[build] Selected package Boost"
-	build_package boost external/boost -DBOOST_FORCE_BUILD:BOOL=$param_forceBuild
+		echo "[build] Selected package Boost"
+		build_package boost external/boost -DBOOST_FORCE_BUILD:BOOL=$param_forceBuild
     ;;
     casacore)
-	echo "[build] Selected package CASACORE"
-	build_package casacore external/casacore
+		echo "[build] Selected package CASACORE"
+		## -- build required packages
+		./build.sh wcslib
+		./build.sh cfitsio
+		## -- build package
+		build_package casacore external/casacore
     ;;
     cfitsio)
-	echo "[build] Selected package CFITSIO"
-	build_package cfitsio external/cfitsio
+		echo "[build] Selected package CFITSIO"
+		build_package cfitsio external/cfitsio
     ;;
     cmake)
 	echo "[build] Selected package CMake"
@@ -137,10 +202,10 @@ case $param_packageName in
 	fi
 	## prepare to build cmake from its source
 	if test -d cmake ; then
-	    cd cmake
+	    cd cmake;
 	else
-	    mkdir cmake
-	    cd cmake
+	    mkdir cmake;
+	    cd cmake;
 	fi
 	## run the configure script
 	echo "[build] Running configure script for cmake ..."
@@ -159,7 +224,7 @@ case $param_packageName in
     ;;
     flex)
 	echo "[build] Selected package Flex"
-	build_package flex external/flex
+	build_package flex external/flex -DFLEX_FORCE_BUILD:BOOL=$param_forceBuild
     ;;
     hdf5)
 	echo "[build] Selected package Hdf5"
@@ -172,7 +237,7 @@ case $param_packageName in
     plplot)
 	echo "[build] Selected package Plplot"
 	if test -d $basedir/../external/plplot ; then
-	    build_package plplot external/plplot -DCMAKE_INSTALL_PREFIX:STRING=$basedir/../release;
+	    build_package plplot external/plplot "-DCMAKE_INSTALL_PREFIX:STRING=$basedir/../release -DCMAKE_INSTALL_BINDIR:STRING=bin -DCMAKE_INSTALL_DATADIR:STRING=share -DCMAKE_INSTALL_INCLUDEDIR:STRING=include -DPLD_aqt:BOOL=0";
 	else
 	    cd $basedir/../external
 	    ## download the source tar-ball from source forge
@@ -203,8 +268,6 @@ case $param_packageName in
 	echo "[build] Selected package DAL"
 	## external packages
 	./build.sh cmake
-	./build.sh wcslib
-	./build.sh cfitsio
 	./build.sh casacore
 	./build.sh blitz
 	./build.sh python
@@ -234,25 +297,5 @@ case $param_packageName in
 	echo "[build] Building all USG packsges";
 	build_package dal src/DAL;
 	build_package cr src/CR-Tools;
-    ;;
-    clean)
-    rm -f *~;
-    rm -f *.log;
-    rm -rf bison;
-    rm -rf blitz;
-    rm -rf boost;
-    rm -rf casacore;
-    rm -rf cfitsio;
-    rm -rf cmake;
-    rm -rf cr;
-    rm -rf dal;
-    rm -rf dsp;
-    rm -rf flex;
-    rm -rf hdf5;
-    rm -rf pgplot;
-    rm -rf plplot;
-    rm -rf python;
-    rm -rf vtk;
-    rm -rf wcslib;
     ;;
 esac
