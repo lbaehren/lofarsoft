35476a3a5cb663506228e8dbc7a511ddb7505e04
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-09-27 16:23:25 +0000

    [1] Activated keyword substitution for alle files [2] Minor adjustments in the Python script to drive animation with VisIt; also added parameter file used to control the behaviour of mpeg_encode, used to convert the frames to a movie.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@828 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/scripts/visit_animation.py
+++ src/CR-Tools/scripts/visit_animation.py
@@ -1,15 +1,19 @@
 
+## -------------------------------------------------------------------
+## $Id:: CMakeLists.txt 790 2007-09-19 14:12:22Z baehren             $
 ## ===================================================================
 ##
 ## [1] Run VisIt to create frames for the movie
 ##
-##     visit -cli -s event_animation.py
+##     visit -cli -s animation.py
 ##
 ## [2] Run mpeg_encode to combine frames into a movie
 ##
 ##     mpeg_encode movie.par
 ##
 ## ===================================================================
+
+## import additional Python modules
 
 from math import *
 
@@ -25,18 +29,23 @@ RestoreSession("visit0004.session",0);
 
 s = SaveWindowAttributes();
 s.format = s.PPM
+s.outputToCurrentDirectory = 0;
+s.outputDirectory = "frames";
 s.fileName = "frame";
+s.width  = 512
+s.height = 512
 SetSaveWindowAttributes(s);
 
 ## -------------------------------------------------------------------
 # Create control points for the view
 
-nofFrames=200
+nofFrames=360
 
 v = GetView3D();
 
 for i in range(nofFrames):
-    v.viewNormal = (-sin((i+20)*0.05),cos((i+20)*0.05),0.6);
+    v.viewNormal = (-sin(radians(i)),cos(radians(i)),0.6);
+#    v.viewNormal = (-sin((i+20)*0.05),cos((i+20)*0.05),0.6);
     ## rotation combined with moving upwards along the z-axis
 #    v.viewNormal = (-sin((i+20)*0.05),cos((i+20)*0.05),-0.6+i*0.005);
 #    v.viewNormal = (-0.552867-sin(i*0.05), -0.830462+cos(i*0.05), -0.0683444+i*0.001);
@@ -50,3 +59,6 @@ for i in range(nofFrames):
 ToggleCameraViewMode();
 SaveSession("keyframe.session");
 
+## exit when everything is done
+
+quit();
