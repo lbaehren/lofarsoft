76bd61c2e9fb8d180fafdbd80ce9c7361b29780d
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-09-27 12:32:13 +0000

    Python script to create set of movie frames, rendering the display of a data volume
    git-svn-id: http://usg.lofar.org/svn/code/trunk@827 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/CR-Tools/scripts/visit_animation.py
@@ -0,0 +1,52 @@
+
+## ===================================================================
+##
+## [1] Run VisIt to create frames for the movie
+##
+##     visit -cli -s event_animation.py
+##
+## [2] Run mpeg_encode to combine frames into a movie
+##
+##     mpeg_encode movie.par
+##
+## ===================================================================
+
+from math import *
+
+## -------------------------------------------------------------------
+## Import saved session to get the basic parameters correct
+
+RestoreSession("visit0004.session",0);
+
+## -------------------------------------------------------------------
+## Set attributes
+
+## set attributes for saving a display window
+
+s = SaveWindowAttributes();
+s.format = s.PPM
+s.fileName = "frame";
+SetSaveWindowAttributes(s);
+
+## -------------------------------------------------------------------
+# Create control points for the view
+
+nofFrames=200
+
+v = GetView3D();
+
+for i in range(nofFrames):
+    v.viewNormal = (-sin((i+20)*0.05),cos((i+20)*0.05),0.6);
+    ## rotation combined with moving upwards along the z-axis
+#    v.viewNormal = (-sin((i+20)*0.05),cos((i+20)*0.05),-0.6+i*0.005);
+#    v.viewNormal = (-0.552867-sin(i*0.05), -0.830462+cos(i*0.05), -0.0683444+i*0.001);
+    v.viewUp = (0,0,1);
+    v.viewAngle = 30;
+    SetView3D (v);
+    SaveWindow();
+
+## Store the session
+
+ToggleCameraViewMode();
+SaveSession("keyframe.session");
+
