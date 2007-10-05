cac5b251295947521e586b70a0fdf1339713e620
usov <usov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-10-05 14:48:25 +0000

    Add timing
    git-svn-id: http://usg.lofar.org/svn/code/trunk@845 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pybdsm/implement/bdsm/__init__.py
+++ src/pybdsm/implement/bdsm/__init__.py
@@ -1,3 +1,4 @@
+import time
 
 from Image import Image, Opts
 from FITS import Op_loadFITS
@@ -21,6 +22,16 @@ def execute(chain, opts):
     img = Image(bdsm_default_opts)
     img.opts._apply(opts)
     for op in chain:
+        op.__start_time = time.time()
         op(img)
+        op.__stop_time = time.time()
+
+    print "="*30
+    for op in chain:
+        print "%15s : %f" % (op.__class__.__name__, 
+                               (op.__stop_time - op.__start_time))
+    print "="*30
+    print "%15s : %f" % ("Total",
+                           (chain[-1].__stop_time - chain[0].__start_time))
 
     return img
