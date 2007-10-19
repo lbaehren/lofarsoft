1bf98f3f790672b9f5458dd6a4d7849f24892f89
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-10-19 19:14:25 +0000

    Additional settings for PLplot
    git-svn-id: http://usg.lofar.org/svn/code/trunk@925 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/SettingsPLplot.cmake
+++ devel_common/cmake/SettingsPLplot.cmake
@@ -3,3 +3,11 @@
 
 set (BUILD_SHARED_LIBS OFF)
 set (ENABLE_DYNDRIVERS OFF)
+
+## disable individual drivers
+
+set (PLD_aqt OFF)
+set (PLD_hp7470 OFF)
+set (PLD_hp7580 OFF)
+set (PLD_lj_hpgl OFF)
+set (PLD_svg OFF)
