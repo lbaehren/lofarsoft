7f75bb7f9de598e3e889a76c01343694ce1b8f3e
usov <usov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-11-06 13:36:57 +0000

    Switch pybdsm to really use new fitting routines.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1021 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pybdsm/implement/bdsm/gausfit.py
+++ src/pybdsm/implement/bdsm/gausfit.py
@@ -1,129 +1,52 @@
-import math
-import numpy as N
-import numpy.core.ma as M
-import scipy.optimize as opt
 from Image import Op
+from _cbdsm import *
 
 class Op_gausfit(Op):
     """Fit all islands with mutiple 2-d gaussians
     """
 
     def __call__(self, img):
+        self.beam = img.opts.beam
         for idx, isl in enumerate(img.islands):
             print "Fitting isl #", idx
             t = self.fit_island(isl, img.opts.thresh_isl)
             isl.gaus_fcn = t
-            isl.gaul = t.parameters()
+            self.store_parameters(isl, t.parameters)
 
         return img
+
+    def store_parameters(self, isl, par):
+        res = []
+        for p in par:
+            npar = list(p)
+            npar[1] += isl.bbox[0]
+            npar[2] += isl.bbox[2]
+            res.append(npar)
+
+        isl.gaul = res
 
     def fit_island(self, isl, thr):
-        fcn = gaus_fcn(isl)
+        dof = isl.size # number of active pixels
+        init_shape = list(self.beam)
+        fcn = MGFunction(isl.masked.data, isl.masked.mask, 1)
+        fit = lmder_fit
 
         while True:
-            peak, idx = find_peak(fcn.residuals())
-            
-            if peak <= thr: ## no good peaks left
+            peak, idx = fcn.find_peak()
+
+            if peak < thr: ## no good peaks left
                 break
 
-            if not fcn.add_gaussian(peak, idx): ## no more gaussians can be added
+            gtype = (Gtype.g3 if fcn.fitted_parameters() + 3 <= dof else None)
+            gtype = (Gtype.g6 if fcn.fitted_parameters() + 6 <= dof else gtype)
+
+            if gtype:
+                fcn.add_gaussian(gtype, [peak, idx[0], idx[1]] + init_shape)
+            else:
                 break
             
-            fcn.fit(final=0, verbose=1)
+            if not fit(fcn, final=0, verbose=1):
+                break
 
-        fcn.fit(final=1, verbose=1)
+        fit(fcn, final=1, verbose=1)
         return fcn
-
-class gaus_fcn:
-    """Gaussian-fitting"""
-    def __init__(self, isl):
-        self.gaul = []  ## list of gaussians (idx_par, n_par)
-        self.npar = 0   ## total number of parameters
-        self.dof  = isl.size
-        self.pars = []  ## parameters
-        self.opts = isl.opts
-        self.orig = isl.masked
-        self.img = M.array(isl.masked, dtype=N.float64, copy=True)
-        self.origin = (isl.bbox[0], isl.bbox[2])
-
-    def add_gaussian(self, peak, idx):
-        if self.npar + 6 < self.dof:  ## add 6-parameter gaussian
-            self.gaul.append((self.npar, 6))
-            self.npar += 6
-            self.pars.extend([peak, idx[0], idx[1]])
-            self.pars.extend(self.opts.beam)
-            return True
-        if self.npar + 3 < self.dof:  ## add 3-parameter gaussian
-            self.gaul.append((self.npar, 3))
-            self.npar += 3
-            self.pars.extend([peak, idx[0], idx[1]])
-            return True
-        return False
-
-    def residuals(self, pars = None):
-        img = self.img
-        img.data.flat = self.orig.data
-        if pars is None:
-            p = self.pars
-        else:
-            p = pars
-        for g in self.gaul:
-            img -= N.fromfunction(self.gauss2d, img.shape, params=p[g[0]:g[0]+g[1]])
-        return img
-
-    def fcn(self, x):
-        arr = self.residuals(x)
-        arr.data[arr.mask]=0
-        return arr.data.flat
-
-    def fit(self, final=0, verbose=0):
-        precision = {}
-        if not final:
-            precision = {'xtol': 1e-4, 'ftol': 1e-4}
-        res = opt.leastsq(self.fcn, self.pars,
-                          full_output=1, **precision)
-        if verbose == 1:
-            print 'Nfev %d  chisq %f' % \
-                (res[2]['nfev'], (res[2]['fvec']**2).sum())
-        elif verbose > 1:
-            print "================"
-            print "Par: ", res[0]
-            print "chi2:", (res[2]['fvec']**2).sum()
-            print "Nfev:", res[2]['nfev']
-            print "Err: ", res[4]
-            if res[4] != 1:
-                print "Err:", res[3]
-
-        self.opars = self.pars
-        self.pars = list(res[0])
-
-    def gauss2d(self, x1, x2, params):
-        if len(params) == 6:
-            amp, x1o, x2o, sx1, sx2, theta = params
-        elif len(params) == 3:
-            amp, x1o, x2o = params
-            sx1, sx2, theta = self.opts.beam
-        else:
-            raise RuntimeError("Don't know how to calculate this")
-        theta = math.radians(theta)
-        x1 -= x1o
-        x2 -= x2o
-        f1 = ( x1*math.cos(theta) + x2*math.sin(theta))/sx1
-        f2 = (-x1*math.sin(theta) + x2*math.cos(theta))/sx2
-        return amp * N.exp(-0.5*(f1**2 + f2**2))
-
-    def parameters(self):
-        l = []
-        for g in self.gaul:
-            p = self.pars[g[0]:g[0]+g[1]]
-            if g[1] == 3:
-                p.extend(self.opts.beam)
-            p[1] += self.origin[0]
-            p[2] += self.origin[1]
-            l.append(p)
-        return l
-
-def find_peak(arr):
-    idx = arr.argmax(axis=None)
-    return (arr.flat[idx], N.unravel_index(idx, arr.shape))
-
