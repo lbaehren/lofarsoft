1af9ce333cafd1fd24256ef6bec0d7b687da4067
huege <huege@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2007-11-16 16:51:52 +0000

    reas2event now takes command line arguments.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1057 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/apps/reas2event.cc
+++ src/CR-Tools/apps/reas2event.cc
@@ -42,23 +42,65 @@
 */
 
 #include <IO/simulation2event.h>
+#include <sstream>
 
 using CR::simulation2event;  // Namespace usage
 
 using namespace CR;
 
-int main(int argc, char *argv[]){
+int main(int argc, char** argv){
+
+  cout << "\nreas2event V1.0 by Andreas Horneffer & Tim Huege\n";
+  if ((argc < 6) || (argc >10))
+  {
+    cout << "\nUsage: reas2event <outfile> <simpath> <simname> <antlist> <date> [<caltabpath> <presync> <addnoise> <addtv>]<\n\n" 
+         << "outfile:     filename to write event to, e.g. newevent.event\n"
+         << "simpath:     path where REAS simulation lies, e.g. /home/huege/sims\n"
+         << "simname:     name of REAS simulation, e.g. 30deg1e17_all\n"
+         << "antlist:     name of antenna list, e.g. lopes30\n"
+	 << "date:        unique event date, e.g. 1136116800\n"
+	 << "caltabpath:  path to caltables, defaults to /home/huege/lopescasa/data/LOPES/LOPES-CalTable\n"
+	 << "presync:     number of samples in presync, defaults to 32768\n"
+	 << "addnoise:    set to 0 to switch off Galactic noise, defaults to 1\n"
+	 << "addtv:       set to 0 to switch off TV transmitter insertion, defaults to 1\n"
+         << endl;
+    return 10;
+  }
 
   int nofFailed=0;
-  String CalTablePath("/home/horneff/lopescasa/data/LOPES/LOPES-CalTable");
-  String outfile("reas2event.event");
-  String SimName("30deg1e17_shift");
-  String AntListName("lopes30");
-  String SimPath("/home/horneff/testreas");
-  uInt date=1136116800;
+  String outfile(argv[1]);
+  String SimPath(argv[2]);
+  String SimName(argv[3]);
+  String AntListName(argv[4]);
+  uInt date;
+  { // block to invalidate local variables
+    stringstream ss;
+    ss << argv[5];
+    ss >> date;
+  } // block to invalidate local variables
+  String CalTablePath("/home/huege/lopescasa/data/LOPES/LOPES-CalTable");
+  if (argc>6) { CalTablePath=argv[6]; }
   Int presync=32768;
-  Bool AddTV=True, AddNoise=True;
-    
+  if (argc>7)
+  {
+    stringstream ss;
+    ss << argv[7];
+    ss >> presync;
+  }
+  Bool AddNoise=True;
+  if (argc>8)
+  {
+    stringstream ss;
+    ss << argv[8];
+    ss >> AddNoise;
+  }
+  Bool AddTV=True;
+  if (argc>9)
+  {
+    stringstream ss;
+    ss << argv[9];
+    ss >> AddTV;
+  }
   try {
     simulation2event newsimulation2event;
     //
