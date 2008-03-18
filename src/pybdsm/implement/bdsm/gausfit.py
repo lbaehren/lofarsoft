32bf2e20e2b9dcd19e82976b6a96423b04b40f76
usov <usov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-03-18 14:42:28 +0000

    Implement normalization of gaussian parameters. We will need it soon.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1348 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pybdsm/implement/bdsm/gausfit.py
+++ src/pybdsm/implement/bdsm/gausfit.py
@@ -10,7 +10,7 @@ class Op_gausfit(Op):
                 print "Fitting isl #", idx
             t = self.fit_island(isl, img.opts.beam, img.opts.thresh_isl, img.opts.rms, img.opts.verbose_fitting)
             isl.gaus_fcn = t
-            isl.gaul = self._parameters_to_global(isl, t.parameters)
+            isl.gaul = self._normalize_parameters(isl, t.parameters)
 
         return img
 
@@ -43,12 +43,29 @@ class Op_gausfit(Op):
         fit(fcn, final=1, verbose=verbose)
         return fcn
 
-    def _parameters_to_global(self, isl, par):
-        """Update parameters by adjusting them to the proper image coordinates"""
+    def _normalize_parameters(self, isl, par):
+        """Normalize parameters by adjusting them to the proper image coordinates
+        and ensuring that all of the implicit conventions (such as bmaj >= bmin) are met.
+        """
         res = []
         for p in par:
-            npar = list(p)
-            npar[1] += isl.origin[0]
-            npar[2] += isl.origin[1]
-            res.append(npar)
+            np = list(p)
+
+            ### update to the image coordinates
+            np[1] += isl.origin[0]
+            np[2] += isl.origin[1]
+
+            ### shape values should be positive
+            np[3] = abs(np[3])
+            np[4] = abs(np[4])
+
+            ### first extent is major
+            if np[3] < np[4]:
+                np[3:5] = np[4:2:-1]
+                np[5] += 90
+
+            ### normalize position angle
+            np[5] = divmod(np[5], 180)[1]
+
+            res.append(np)
         return res
