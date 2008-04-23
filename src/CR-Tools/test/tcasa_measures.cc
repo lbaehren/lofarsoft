90dc94ce8b5ef1878b08d99a87e57a861dcd3d4f
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-04-23 10:04:15 +0000

    Debugging, to get file compiled
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1474 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/test/tcasa_measures.cc
+++ src/CR-Tools/test/tcasa_measures.cc
@@ -37,7 +37,7 @@
 #include <measures/Measures/MeasData.h>
 
 /*!
-  \file tUseMeasures.cc
+  \file tcasa_measures.cc
   
   \ingroup CR_test
 
@@ -51,7 +51,6 @@
 using std::cout;
 using std::endl;
 
-using casa::MEpoch;
 using casa::MVAngle;
 using casa::Quantity;
 using casa::MVTime;
@@ -59,16 +58,17 @@ using casa::MeasFrame;
 using casa::MDirection;
 using casa::MeasData;
 
+// ------------------------------------------------------------------------------
+
 /*!
-  \brief main routine
-  
-  \return nofFailedTests -- The number of failed tests
+  \brief Test working with a MVAngle measure
+
+  \return nofFailedTest -- The number of failed tests within this function
 */
-int main () 
+int test_MVAngle ()
 {
   int nofFailedTests (0);
   
-  cout << "[1] Testing MVAngle..." << std::endl;
   try {
     MVAngle mva1 = Quantity(190,"deg") + Quantity(59,"'") + Quantity(59.95,"\"");
     cout << " - Degrees:  " << mva1.degree()   << std::endl;
@@ -81,8 +81,16 @@ int main ()
     std::cerr << message << std::endl;
     nofFailedTests++;
   }
-  
-  cout << "[2] Testing MVTime..." << std::endl;
+
+  return nofFailedTests;
+}
+
+// ------------------------------------------------------------------------------
+
+int test_MVTime ()
+{
+  int nofFailedTests (0);
+
   try {
     MVTime mtim = 44362.6;
     
@@ -94,11 +102,19 @@ int main ()
     std::cerr << message << std::endl;
     nofFailedTests++;
   }
-  
-  cout << "[3] Testing MEpoch..." << std::endl;
+
+  return nofFailedTests;
+}
+
+// ------------------------------------------------------------------------------
+
+int test_MEpoch ()
+{
+  int nofFailedTests (0);
+
   try {
-    MEpoch today;
-    MEpoch ep (Quantity(50083.,"d"));
+    casa::MEpoch today;
+    casa::MEpoch ep (Quantity(50083.,"d"));
     cout << "-- Default epoch        = " << today << endl;
     cout << "-- User specified epoch = " << ep    << endl;
   } catch (std::string message) {
@@ -114,13 +130,13 @@ int main ()
 		      MDirection::J2000);
     // Zenith in GALACTIC coordinates
     MDirection galactic (Quantity(0,"deg"),
-		      Quantity(90,"deg"),
-		      MDirection::GALACTIC);
+			 Quantity(90,"deg"),
+			 MDirection::GALACTIC);
     // Zenith in AZEL coordinates
     MDirection azel (Quantity(0,"deg"),
-		      Quantity(90,"deg"),
-		      MDirection::AZEL);
-
+		     Quantity(90,"deg"),
+		     MDirection::AZEL);
+    
     cout << "-- J2000 zenith    = " << j2000    << endl;
     cout << "-- Galactic zenith = " << galactic << endl;
     cout << "-- AZEL zenith     = " << azel     << endl;
@@ -129,34 +145,21 @@ int main ()
     nofFailedTests++;
   }
 
-  cout << "[5] Testing conversion of direction between frames..." << endl;
-  try {
-    casa::MDirection::Ref refB1950 (casa::MDirection::B1950);
-    casa::MDirection::Ref refJ2000 (casa::MDirection::J2000);
+  return nofFailedTests;
+}
 
-    casa::MDirection::Convert conv (refB1950,
-				    refJ2000);
-    
-    cout << "-- Reference for source frame  = " << refB1950    << endl;
-    cout << "-- Reference for target frame  = " << refJ2000    << endl;
-    cout << "-- Coordinate conversion       = " << conv        << endl;
+// ------------------------------------------------------------------------------
 
-    // testwise conversion of some coordinates
-    casa::MVDirection coordB1950;
+/*!
+  \return nofFailedTests -- The number of failed tests
+*/
+int main () 
+{
+  int nofFailedTests (0);
 
-    for (int elevation (0); elevation<90; elevation+=10) {
-      coordB1950 = casa::MVDirection (casa::Quantity(0,"deg"),
-				      casa::Quantity(elevation,"deg"));
-      cout << "\t" << elevation
-	   << "\t" << coordB1950
-	   << "\t" << conv(coordB1950).getValue().getRecordValue()
-	   << endl;
-    }
-
-  } catch (std::string message) {
-    std::cerr << message << std::endl;
-    nofFailedTests++;
-  }
+  nofFailedTests += test_MVAngle ();
+  nofFailedTests +- test_MVTime ();
+  nofFailedTests +- test_MEpoch ();
   
   return nofFailedTests;
 }
