a411c0a18f70b2b2ffac2efa57a59b605b6c9fe7
masters <masters@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-04-25 15:38:16 +0000

    Added channel averaging to data2ascii script. (Casey)
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1482 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/contrib/data_inspection/data2ascii.py
+++ src/contrib/data_inspection/data2ascii.py
@@ -8,11 +8,17 @@ from pylab import *
 if len(sys.argv) != 7:
 	print "Usage:"
 	print "\tdata2ascii.py <msfile> <antenna1> <antenna2> " + \
-	      "<sub-band> <channel> <output ascii filename>"
+	      "<sub-band> <channel or range> <output ascii filename>"
 	print "\t<> required"
 	print "\t[] optional"
-	print "Writes data to ascii file.  Columns:  time (MJ seconds), xxr, xxi, xyr, xyi, yxr, yxi, yyr, yyi."
+	print "\tE.g., \t data2ascii.py SB3.MS 1 10 0 10:200 SB3.txt"
+	print "\tWrites data to ascii file.  Columns:  time (MJ seconds), xxr, xxi, xyr, xyi, yxr, yxi, yyr, yyi."
 	sys.exit(1)
+
+# set channels to average
+if len((sys.argv[5]).split(':')) > 1:
+	channels = range(int((sys.argv[5]).split(':')[0]),int((sys.argv[5]).split(':')[1]))
+else: channels = [int(sys.argv[5])]
 
 # open file
 msds= dal.dalDataset()
@@ -35,9 +41,12 @@ data_col = maintable.getColumn("DATA")
 data = data_col.data()
 nchannels = data.shape[1] # second element of the data shape i.e. (nrows,256,4) (after pydal change to numpy)
 
+# average data
+data_reduce = add.reduce(array=data[:,channels,:],axis=1)/len(channels)
+
 temp = sys.stdout
 sys.stdout = open(sys.argv[6],'a')
 for row in range(len(data)):
-	print time[row],data[row,int(sys.argv[5]),0].real,data[row,int(sys.argv[5]),0].imag,data[row,int(sys.argv[5]),1].real,data[row,int(sys.argv[5]),1].imag,data[row,int(sys.argv[5]),2].real,data[row,int(sys.argv[5]),2].imag,data[row,int(sys.argv[5]),3].real,data[row,int(sys.argv[5]),3].imag
+	print time[row],data_reduce[row,0].real,data_reduce[row,0].imag,data_reduce[row,1].real,data_reduce[row,1].imag,data_reduce[row,2].real,data_reduce[row,2].imag,data_reduce[row,3].real,data_reduce[row,3].imag
 
 sys.stdout = temp
