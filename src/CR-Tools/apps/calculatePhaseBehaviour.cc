79d27d7713040684c1fd57ba6c47b2e0488659fc
schroeder <schroeder@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-06-04 15:25:23 +0000

    added plotting of FFT and FX values
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1604 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/apps/calculatePhaseBehaviour.cc
+++ src/CR-Tools/apps/calculatePhaseBehaviour.cc
@@ -23,6 +23,9 @@
 #include <string>
 #include <Calibration/CalTableReader.h>
 #include <Data/LopesEventIn.h>
+#include <Display/SimplePlot.h>
+#include <startools/reconstruction.hh>
+
 
 using namespace CR;
 using CR::CalTableReader;
@@ -126,14 +129,24 @@ void readCalTableValues(void)
     // open the table
     table.AttachTable(CalTablePath);
 
-    //Vector<Double> frequencies;
+    Vector<Double> frequencies;
     Vector<DComplex> PhaseCalValues; 
 
     // set date for reading the values
     unsigned int date = time(NULL);	// default time: now
  
+    // Reading the frequency axis
+    cout << "\nThe value of the field \"" << "PhaseCalFreq" << "\" for the time " << date << " is: " << endl;
+    if (!table.GetData(date, antennaIDs[0], "PhaseCalFreq", &frequencies))
+    {
+       cerr << "Error while retrieving data" << endl;
+    } else
+    {
+      cout << "\nFrequency axis for antenna " << antennaIDs[0] << ":\n" << frequencies << endl;
+    }
+
     // Reading the values  
-    cout << "The value of the field \"" << "PhaseCal" << "\" for the time " << date << " is: " << endl;
+    cout << "\nThe value of the field \"" << "PhaseCal" << "\" for the time " << date << " is: " << endl;
 
     // Loop through antennas and get the data (depending of the type)
     for (int j = 0; j < maxAntennas ; j++)
@@ -145,7 +158,7 @@ void readCalTableValues(void)
        {
            cout << "\nfor antenna " << antennaIDs[j] << ":\n" << PhaseCalValues << endl;
            cout << "Absolute value:\n" << abs(PhaseCalValues) << endl;
-           cout << "Phase:\n" << phase(PhaseCalValues) << endl;
+           cout << "Phase:\n" << phase(PhaseCalValues)*180./3.1415926 << endl;
        }
     }
   }
@@ -156,13 +169,6 @@ void readCalTableValues(void)
   }
 
 }
-
-void show_data (Vector<Double> const &data)
-{
-  cout << "  Data shape : " << data.shape() << endl;
-  cout << "  Data point 100: " << data(100) << endl;
-}
-
 
 /*!
   \brief analyses one event for one antenna
@@ -192,12 +198,162 @@ void analyseEvent(int antenna, string eventName)
     }
 
     // get fx() data
-    Matrix<Double> data;
-    data = dr.fx();
-    show_data (data.column(antenna-1));
+    Matrix<Double> dataFX;
+    Matrix<DComplex> dataFFT;
+    Vector<Double> frequencies;
+    dataFX = dr.fx();
+    dataFFT = dr.fft();
+    frequencies = dr.frequencyValues();
 
     // cout << "Date: " << dr.header().asuInt("Date") << endl;
     // cout << "IDs: " << dr.header().asArrayInt("AntennaIDs") << endl;
+
+    // plot the data
+    SimplePlot plotter;    			// define plotter
+    Vector<Double> xaxis;			// xaxis
+    double xmax,xmin,ymin=0,ymax=0;		// Plotrange
+    Vector<Double> yValues;			// y-values
+    Vector<Double> upYvalues;			// upsampled y-values
+    int color = 9;				// starting color
+
+    // Get the (not upsampled) time axis
+    xaxis = static_cast< Vector<Double> >(dr.timeValues());
+    yValues = dataFX.column(antenna-1).copy();
+    upYvalues = dataFX.column(antenna-1).copy();
+
+    // Upsampled x-axis
+    Vector<Double> upxaxis = xaxis;
+
+    // check length of time axis and yValues traces for consistency
+    if (upxaxis.size() != upYvalues.size())
+      std::cerr << " WARNING: Length of time axis differs from length of the antenna traces!\n"
+           << std::endl;
+
+    // Define plotrange for not upsampled and upsampled data
+    const double plotStart = -2e-6;
+    const double plotStop = 0e-6;
+    int startsample = ntrue(xaxis<plotStart);			//number of elements smaller then starting value of plot range
+    int stopsample = ntrue(xaxis<plotStop);			//number of elements smaller then end of plot range
+    Slice plotRange(startsample,(stopsample-startsample));	// create Slice with plotRange
+    startsample = ntrue(upxaxis<plotStart);               	//number of elements smaller then starting value of plot range
+    stopsample = ntrue(upxaxis<plotStop);                 	//number of elements smaller then end of plot range
+    Slice upplotRange(startsample,(stopsample-startsample));	// create Slice with plotRange
+
+    // conversion to micro (no conversion for height of raw data)
+    xaxis *= 1e6;
+    upxaxis *= 1e6;
+
+    // define Plotrange
+    xmin = min(xaxis(plotRange));
+    xmax = max(xaxis(plotRange));
+
+    // find the minimal and maximal y values for the plot and take 105% to have some space
+    // do it with the upsampled data only as they are as least as heigh as the original ones
+    ymin = min(upYvalues(upplotRange)) * 1.05;
+    ymax = max(upYvalues(upplotRange)) * 1.05;
+
+
+    // create the plot filename
+    string plotfilename = eventName;
+    // delete ending
+    if (plotfilename.find(".event") != string::npos)
+        plotfilename.erase(plotfilename.find_last_of('.'));	
+    // deletes the file path, if it exists	
+    if (plotfilename.find("/") != string::npos)
+        plotfilename.erase(0,plotfilename.find_last_of('/')+1);
+
+
+    stringstream antennanumber;
+    antennanumber << antenna;
+ 
+    //set the plotfilename to filename + "-" + antennanumber.str() + ".ps";
+    if ( (antenna+1) < 10 )
+    {
+      plotfilename += "-FX-0" + antennanumber.str() + ".ps";
+    } else
+    {
+      plotfilename += "-FX-" + antennanumber.str() + ".ps";
+    }
+    cout << "Plotfilename: " << plotfilename << endl;
+
+    //set label "GT - Ant.Nr"
+    stringstream gtlabel;
+    uInt gtdate;
+    dr.header().get("Date",gtdate);
+    gtlabel << gtdate;
+    string label = "GT " + gtlabel.str() + " - Antenna " + antennanumber.str();
+
+    // Make the plot
+
+    // Create empty vector for not existing error bars 
+    Vector<Double> empty;
+
+    // Initialize the plot giving xmin, xmax, ymin and ymax
+    plotter.InitPlot(plotfilename, xmin, xmax, ymin, ymax);
+
+    // Add labels
+    plotter.AddLabels("Time t [#gmsec]", "Counts",label);
+
+    // Plot (upsampled) trace
+    plotter.PlotLine(upxaxis(upplotRange),upYvalues(upplotRange),color,1);
+    // Plot original data points (if upsampling was done).
+    //  plotter.PlotSymbols(xaxis(plotRange),yValues(plotRange),empty, empty, color, 2, 5);
+
+
+    // plot the FFT
+    yValues.resize(dataFFT.column(antenna-1).size());
+    xaxis.resize(frequencies.size());
+    xaxis = frequencies.copy();
+
+    // check length of time axis and yValues traces for consistency
+    if (xaxis.size() != yValues.size())
+      std::cerr << " WARNING: Length of frequency axis differs from length of the FFT!\n"
+           << std::endl;
+
+    // FFT-Power ~ square of absolute value; use log-plotting
+    for (unsigned int i=0; i < yValues.size(); i++)
+     yValues (i) = log( pow( abs(dataFFT.column(antenna-1)(i)), 2) );
+ 
+
+    // define Plotrange
+    xmin = min(xaxis);
+    xmax = max(xaxis);
+
+    // find the minimal and maximal y values for the plot and take 105% to have some space
+    ymin = min(yValues) * 1.05;
+    ymax = max(yValues) * 1.05;
+
+
+    // create the plot filename
+    plotfilename = eventName;
+    // delete ending
+    if (plotfilename.find(".event") != string::npos)
+        plotfilename.erase(plotfilename.find_last_of('.'));	
+    // deletes the file path, if it exists	
+    if (plotfilename.find("/") != string::npos)
+        plotfilename.erase(0,plotfilename.find_last_of('/')+1);
+
+    //set the plotfilename to filename + "-" + antennanumber.str() + ".ps";
+    if ( (antenna+1) < 10 )
+    {
+      plotfilename += "-FFT-0" + antennanumber.str() + ".ps";
+    } else
+    {
+      plotfilename += "-FFT-" + antennanumber.str() + ".ps";
+    }
+    cout << "Plotfilename: " << plotfilename << endl;
+
+
+    // Make the plot
+
+    // Initialize the plot giving xmin, xmax, ymin and ymax
+    plotter.InitPlot(plotfilename, xmin, xmax, ymin, ymax);
+
+    // Add labels
+    plotter.AddLabels("Frequency [MHz]", "log Power",label);
+
+    // Plot FFT
+    plotter.PlotLine(xaxis,yValues,color,1);
   } catch (AipsError x) 
   {
     cerr << "calculatePhaseBehaviour:analyseEvent: " << x.getMesg() << endl;
@@ -238,7 +394,7 @@ int main (int argc, char *argv[])
     }
 
     // process event
-    analyseEvent(antenna, "example.event");
+    analyseEvent(antenna, "/data/calibration/newCalib/080528/2008.05.28.08:19:39.599.event");
 
   }  catch (AipsError x) 
   {
