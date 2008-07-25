f1d9721d1d04ae77779efcbf810723aea21c6f38
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-07-25 13:42:09 +0000

    Explicit mentioning of libwcs in order to be able to build tanalyseLOPESevent2
    git-svn-id: http://usg.lofar.org/svn/code/trunk@1777 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Analysis/test/tanalyseLOPESevent2.cc
+++ src/CR-Tools/implement/Analysis/test/tanalyseLOPESevent2.cc
@@ -2,8 +2,8 @@
  | $Id::                                                                 $ |
  *-------------------------------------------------------------------------*
  ***************************************************************************
- *   Copyright (C) 2008                                                  *
- *   Frank Schroeder (<mail>)                                                     *
+ *   Copyright (C) 2008                                                    *
+ *   Frank Schroeder (<mail>)                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
