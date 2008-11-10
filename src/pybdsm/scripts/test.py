ae1dc70a815a7c63497f28e8bacfc95a76dc97ec
usov <usov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-11-10 09:11:11 +0000

    git-svn-id: http://usg.lofar.org/svn/code/trunk@2130 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pybdsm/scripts/test.py
+++ src/pybdsm/scripts/test.py
@@ -4,6 +4,6 @@ sys.path.append('')
 import bdsm
 
 
-bdsm.execute(bdsm.fits_chain,
-             {'fits_name': 'WN65341H.fits',
-              'beam': (.015, .015, 0)})
+img = bdsm.execute(bdsm.fits_chain,
+                   {'fits_name': 'WN65341H.fits',
+                    'beam': (.015, .015, 0)})
