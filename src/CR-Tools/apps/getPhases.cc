98febb26d32ed64bd579ff678d0e8ca6e118d56f
schroeder <schroeder@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2008-11-12 14:36:48 +0000

    added interpolation between frequency bins
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2147 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/apps/getPhases.cc
+++ src/CR-Tools/apps/getPhases.cc
@@ -59,6 +59,7 @@
     <li> --cal        CalTablePath        path to LOPES calibration tables (if the default doesn't work)
     <li> --refant     reference antenna   if no refAnt is given, phases instead of phase diffs will be calculated
     <li> --cutSample  number of samples   samples not used at the beginning and the end of the trace
+    <li> --inter                          linerly interpolates to the phase at the exact frequency
     <li> --help                           prints short help message
   </ul>
    
@@ -154,6 +155,7 @@ int main (int argc, char *argv[])
     unsigned int refAnt = 0;    // reference antenna for calculation of phase differences
     string CalTablePath("/home/schroeder/usg/data/lopes/LOPES-CalTable"); // default
     unsigned int cutSamples = 0;// unused samples at the beginning and the end of the trace
+    bool interpolate = false;	// interpolate phase to the exact frequency
     int i = 1;		//counter for arguments
     while (i < argc)
     {
@@ -167,14 +169,20 @@ int main (int argc, char *argv[])
              << "Possible options and arguments:\n" 
              << "option        argument\n"
              << "--help                           Displays this help message\n"
+             << "--inter                          Interpolate phase to the exact frequency (optional)\n"
              << "--in, -i      eventfile          The name of the input file (required)\n"
              << "--out, -o     outputfile         The name of the output file (optional)\n"
              << "--freq, -f    frequency in Hz    (Start)frequency to look at (required)\n"
              << "--stopfreq    frequency in Hz    (Stop)frequency for peak search in interval (optional)\n"
-             << "--cal         path               Path to calibration tables\n"
-             << "--refant      antenna number     Calculates phase differences to this antenna instead of phases\n"
-             << "--cutsamples  antenna number     Unused samples at the beginning and the end of the trace\n"
+             << "--cal         path               Path to calibration tables (optional)\n"
+             << "--refant      antenna number     Calculates phase differences to this antenna instead of phases (optional)\n"
+             << "--cutsamples  number of samples  Unused samples at the beginning and the end of the trace (optional)\n"
              << endl;
+        continue;
+      }
+
+      if ( (option == "--inter") || (option == "-inter") ) {
+        interpolate = true;
         continue;
       }
 
@@ -313,24 +321,42 @@ int main (int argc, char *argv[])
     // get the frequency Index of the nearest / peak frequency
     unsigned int freqIndex = getPeakPos(dr,startFreq, stopFreq);
 
-    /* old way:
-     // loop through frequencies and look for the next after the specified frequeny
-     for (unsigned int i = 0; i < freqValues.size(); i++)
-       if ( abs(startFreq - freqValues(i)) < abs(freqValues(freqIndex) - freqValues(i)) )
-       {
-         freqIndex = i;
-         break;
-       }
-   */
-
-    cout << "Used frequency: " 
-         << freqValues(freqIndex)/1e6 << " MHz at index " << freqIndex << endl;
-
-    // get phases of the spectra 
+    // get phases of the specta
     // if dispersion and delay correction are switched off (see above)
     // then phase(calfft) = phase(fft) (means raw data FFT)
     Matrix<DComplex> spectra = dr.fft(); // use fft() instead of calfft() to get faster results
     Vector<Double> phases = phase(spectra.row(freqIndex))*180./3.1415926;
+
+    // use frequency bin before and after the exact frequency and interpolate linearly
+    if (interpolate) {
+      cout << "Using linear interpolation between "
+           << freqValues(freqIndex-1)/1e6 << " MHz at index " << freqIndex-1 << " and "
+           << freqValues(freqIndex)/1e6   << " MHz at index " << freqIndex   << endl;
+
+      Vector<Double> phases2 = phase(spectra.row(freqIndex-1))*180./3.1415926;
+      double freqbin = freqValues(freqIndex-1) - freqValues(freqIndex);
+
+      // loop through the antennas and interpolate lineraly antenna by antenna
+      for (unsigned int i = 0; i < phases.size(); i++) {
+        double lowphase = phases2(i);
+        double highphase = phases(i);
+        // add +/- 360° if the difference between two phases is > 180°
+        // to bring the two phases closer together for interpolation
+        if ((lowphase - highphase) > 180) lowphase += 360 * ((highphase > 0) - (highphase < 0));
+
+        // print warning if phase difference is still big
+        if ( abs(lowphase - highphase) > 90)
+          cout << "WARNING: big phase difference between neighbour bins during interpolation. Ant: " << i+1 << endl;
+
+        // linear interpolation: 
+        // phase_int = phase1 - ( delta phase * delta freq / freq bin)
+        double interphase = lowphase - (highphase-lowphase) * (startFreq - freqValues(freqIndex-1)) / freqbin;
+        phases(i) = interphase;
+      }
+    } else {
+      cout << "Used frequency (without interpolation): " 
+           << freqValues(freqIndex)/1e6 << " MHz at index " << freqIndex << endl;
+    }
 
     // substract reference phase if refAnt is given (and not too big)
     if (refAnt > phases.size() ) {
@@ -340,6 +366,12 @@ int main (int argc, char *argv[])
     if (refAnt > 0) {
       double refPhase = phases(refAnt -1);
       phases = phases - refPhase;
+    }
+
+    // reduce phases to range of -180° until +180°
+    for (unsigned int i = 0; i < phases.size(); i++) {
+      while (phases(i) > 180) phases(i) -= 360;
+      while (phases(i) < -180) phases(i) += 360;
     }
 
     // if an output filename was given, write output
