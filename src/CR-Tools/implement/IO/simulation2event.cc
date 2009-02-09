620c6f0c747f8bb964d21a3d23ef09a804385a14
huege <huege@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-02-09 09:59:11 +0000

    Changes for reas2event.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2302 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/IO/simulation2event.cc
+++ src/CR-Tools/implement/IO/simulation2event.cc
@@ -87,40 +87,48 @@ namespace CR { // Namespace CR -- begin
       int i,nAnt;
      
       Sim.setWindowBoundaries(-3e-6,3e-6); //set the window boundaries to 2*MaxDelay
+      cout << "Importing simulation ... " << flush;
       success = success && Sim.openNewSimulation(SimulationName, AntListName, path);
       if (!success){
 	cerr << "simulation2event::generateEvent: " << "Error in Sim.openNewSimulation()!" << endl;
 	return False;
        };
+      cout << "done!" << endl;
 
       sim2fft.setCalTable(CTRead);
       ApplyInstEff.setCalTable(CTRead);
       sim2fft.setObsDate(date);
       ApplyInstEff.setObsDate(date);
+      cout << "Calculating FFT ... " << flush;
       success = success && sim2fft.SimulationImport(&Sim);
       if (!success){
 	cerr << "simulation2event::generateEvent: " << "Error in sim2fft.SimulationImport()!" << endl;
 	return False;
-       };
+      };
+      cout << "done!" << endl;
+
 
       AntIDs = sim2fft.getAntIDs();
       nAnt = AntIDs.nelements();
       ApplyInstEff.setAntennaIDs(AntIDs);
       ApplyInstEff.setFreqAxis(sim2fft.getFrequency());
+      cout << "Applying instrumental effects ... " << flush;
       FFT = ApplyInstEff.ApplyEffects(sim2fft.getfft());
-      //FFT *= DComplex(1e2,0); //use this to enhance the simulated pulse
       if ( FFT.nelements() <=  (uInt)nAnt){ //cast to get rid of a stupid waring...
 	cerr << "simulation2event::generateEvent: " << "Error in ApplyInstEff.ApplyEffects()!" << endl;
 	return False;
       };
+      cout << "done!" << endl;
 
       WindowStart = Sim.getSamplingWindowStart();
-      WindowStart -= 18.8e-6; //trial and error number, good for the test event...
+      WindowStart -= 1.85e-6; // move events to absolute position of -1.85 microseconds
+      cout << "Transforming to time series and sampling to adequate sampling rate ... " << flush;
       success = success && toGrid.AddFFTData(FFT,  sim2fft.getBlocklen(), sim2fft.getFrequency(), False, WindowStart);
       if (!success){
 	cerr << "simulation2event::generateEvent: " << "Error in toGrid.AddFFTData()!" << endl;
 	return False;
       };
+      cout << "done!" << endl;
 
       if (AddTV || AddNoise){
 	int blocksize=(131072*4),fftsize=blocksize/2+1;
@@ -133,11 +141,13 @@ namespace CR { // Namespace CR -- begin
 	  gTVspec.setCalTable(CTRead);
 	  gTVspec.setAntennaIDs(AntIDs);
 	  gTVspec.setFreqAxis(frequency);
+          cout << "Adding TV transmitter ... " << flush;
 	  success = success && toGrid.AddFFTData(gTVspec.GetTVSpectrum(date,1e3), blocksize, frequency );
 	  if (!success){
 	    cerr << "simulation2event::generateEvent: " << "Error while generating TV-Spectrum!" << endl;
 	    return False;
 	  };
+          cout << "done!" << endl;
 	};
 	if (AddNoise){
 	  FalckeGorhamGalacticNoise noiseGen;
@@ -145,6 +155,7 @@ namespace CR { // Namespace CR -- begin
 	  ApplyInstrumentEffects ApplyInstEff2Noise;
 	  Matrix<DComplex> nFFT,inFFT;
 	  nFFT.resize(fftsize,nAnt); inFFT.resize(fftsize,nAnt);
+          cout << "Adding galactic noise ... " << flush;
 	  for (i=0; i<nAnt; i++){
 	    noiseGen.getNoise(frequency, tmpnoise);
 	    nFFT.column(i) = tmpnoise;
@@ -154,15 +165,17 @@ namespace CR { // Namespace CR -- begin
 	  ApplyInstEff2Noise.setAntennaIDs(AntIDs);
 	  ApplyInstEff2Noise.setFreqAxis(frequency);
 	  inFFT = ApplyInstEff2Noise.ApplyEffects(nFFT);
-	  inFFT *= DComplex(1e-2); // ugly normalization hack!
+inFFT *= DComplex(1e-2); // ugly normalization hack!
 	  success = success && toGrid.AddFFTData(inFFT, blocksize, frequency );
 	  if (!success){
 	    cerr << "simulation2event::generateEvent: " << "Error while generating noise!" << endl;
 	    return False;
 	  };
+          cout << "done!" << endl;
 	};
       }
            
+      cout << "Generating event file ... " << flush;
       success = success && TS2Event.SetData(toGrid.GetData(), AntIDs, presync);
       success = success && TS2Event.SetDate(date);
       success = success && TS2Event.WriteEvent(outfile);
@@ -170,6 +183,7 @@ namespace CR { // Namespace CR -- begin
 	cerr << "simulation2event::generateEvent: " << "Error while generating the event-file!" << endl;
 	return False;
       };
+      cout << "done!" << endl;
       
 
     } catch (AipsError x) {
