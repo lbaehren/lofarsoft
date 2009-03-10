fa56ffb1cb12ef182b5413689236eb04a379e76f
sduscha <sduscha@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-03-10 23:56:16 +0000

    implemented test of casa::latticesIterator
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2435 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/RM/main.cpp
+++ src/RM/main.cpp
@@ -10,7 +10,8 @@
   \date 28.01.09.
 
   <h3>Synopsis</h3>
-
+  RM-synthesis command line tool. Using algorithms provided in librm.a library. I/O of FITS format
+  through casacore high-level functions.
 */
 
 #include <iostream>
@@ -26,11 +27,9 @@
 #include <tables/Tables/TiledFileAccess.h>
 #include <lattices/Lattices/TiledShape.h>
 #include <lattices/Lattices/LatticeBase.h>
+#include <lattices/Lattices/LatticeIterator.h>			// Iterator over lattices
 #include <images/Images/ImageOpener.h>					// wrapper class for Image type opening
 #include <images/Images/ImageFFT.h>
-//#include <fits/FITS/BasicFITS.h>
-//#include <images/Images/PagedImage.h>
-//#include <images/Images/ImageFITSConverter.h>
 #include <images/Images/ImageStatistics.h>
 #include <images/Images/ImageInterface.h>
 
@@ -42,12 +41,16 @@ using namespace casa;
 
 int main (int argc, char * const argv[]) {
 
-	Bool status;						// status of casa calls
+//	Bool status;						// status of casa calls
 	String casaerror;					// error message of casa calls
 	LatticeBase *lattice_Q;				// lattice for Q input image, why do we need LatticeBase here?
-	LatticeBase *itsLattice;			// lattice to determine type of lattice returned by imageOpen()
-	Lattice<Float> *itsImageFloat_Q;
-//	Array<double> image_Q;				// FITS for input image
+
+	Lattice<Float> *lattice_Q_float;		// lattice to determine type of lattice returned by imageOpen()
+	Lattice<Complex> *lattice_Q_complex;
+	Lattice<DComplex> *lattice_Q_dcomplex;
+
+	float min=0, max=0;		// DEBUG: using minMax() function on image
+
 
 	if(argc<2)				// if no filenames was given, display usage/help (MUST change to 3!)
 	{
@@ -57,8 +60,8 @@ int main (int argc, char * const argv[]) {
 		return(0);
 	}
 
-	const string filename_Q=argv[1];
-	char *filename_U=argv[2];
+	const string filename_Q=argv[1];	// get filename for Q image from command line
+	//	const string filename_U=argv[2];	// get filename for U image from command line
 	
 	#ifdef _debug
 	cout << "Filename_Q: " << filename_Q << endl;				// Debug output
@@ -66,10 +69,6 @@ int main (int argc, char * const argv[]) {
 
 	FITSImage::registerOpenFunction();				// Register the FITS and Miriad image types.
 
-//	MaskSpecifier::MaskSpecifier mask_Q;			// create a mask (needed for openFITSImage)
-//	lattice_Q=FITSImage::openFITSImage(filename_Q, mask_Q);
-
-	
 	lattice_Q=ImageOpener::openImage (filename_Q);	// try open the file with generic casa function
 	if(lattice_Q==NULL)								// on error	
 	{
@@ -77,36 +76,47 @@ int main (int argc, char * const argv[]) {
 		exit(0);
 	}
 		
-	// Lattice and iteration over line of sight
-	itsLattice=lattice_Q;
+	#ifdef _debug
+	cout << lattice_Q->dataType() << endl;
+	#endif
+
 	switch(lattice_Q->dataType()){
 		case TpFloat:
-			lattice_Q=dynamic_cast<ImageInterface<Float>*>(lattice_Q);
+			lattice_Q_float=dynamic_cast<ImageInterface<Float>*>(lattice_Q);
+			break;
 		case TpDouble:
-			lattice_Q=dynamic_cast<ImageInterface<Float>*>(lattice_Q);
+			lattice_Q_float=dynamic_cast<ImageInterface<Float>*>(lattice_Q);
+			break;
+		case TpComplex:
+			lattice_Q_complex=dynamic_cast<ImageInterface<Complex>*>(lattice_Q);
+			break;
+		case TpDComplex:
+			lattice_Q_dcomplex=dynamic_cast<ImageInterface<DComplex>*>(lattice_Q);
+			break;
 		default:
 			throw AipsError("Image has an invalid data type");
+			break;
 	}
 	
 	
-	//floatlattice_Q=dynamic_cast<ImageInterface<Float>*>(lattice_Q);
+	// Lattice and iteration over line of sight
+	// create Lattice shape and iterator
+//	const uInt cursorSize = lattice_Q_float->advisedMaxPixels();
+//	const IPosition cursorShape = lattice_Q_float->niceCursorShape(cursorSize);
+ 	const IPosition cursorShape(2, lattice_Q_float->shape()(0), lattice_Q_float->shape()(1));
+    
+	int i=0;
 	
-	cout << itsImageFloat_Q->dataType() << endl;
-	
-	// the shape function is forced upon us by the Lattice base class
-//	uInt rowLength = image_Q.shape()(0);
-//    IPosition rowShape(image_Q.ndim());
-//    rowShape = 1; rowShape(0) = rowLength;
-//    Float sumPix = 0;
- //   RO_LatticeIterator<Float> iter(image_Q, rowShape);
- //   while(!iter.atEnd()){
- //     sumPix += sum(iter.vectorCursor());
- //     iter++;
- //   }
+	RO_LatticeIterator<Float> iter(*lattice_Q_float, cursorShape);
+    for (iter.reset(); !iter.atEnd(); iter++) {
+		minMax(min, max, iter.cursor());
+		cout << i++ << " Min = " << min << " Max = " << max << iter.cursor() << endl; 
+	}	
 
 	cout << "Deleting lattice_Q" << endl;
 
-	delete lattice_Q;	// delete the lattice of image Q
+	delete lattice_Q_float;		// delete the lattice of image Q
+	delete &iter;				// delete iterator over image Q
 
 	#ifdef _debug
 	cout << "Finished: " << filename_Q << "!\n";	// Debug output
@@ -114,4 +124,3 @@ int main (int argc, char * const argv[]) {
 	
 	return 0;
 }
-
