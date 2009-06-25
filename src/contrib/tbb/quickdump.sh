37951dc48eb9bde931099cb3c906f891cbffe87a
terveen <terveen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-06-25 10:53:46 +0000

    added a help file for the tbbdump script
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2732 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/contrib/tbb/quickdump.sh
+++ src/contrib/tbb/quickdump.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #
 
-mkdir /home/eikelboom/data/hdf5data/sat06062009
-DireName="/home/eikelboom/data/hdf5data/sat06062009/"
+mkdir /home/veen/data/hdf5data/tue23062009
+DireName="/home/veen/data/hdf5data/tue23062009/"
 
 RCUNumber=0
 
-while :
- do
+#while :
+# do
 ssh CS001C tbbctl --free
-ssh CS001C tbbctl --alloc
-ssh CS001C tbbctl --rec
 sleep 0.2
+ssh CS001C tbbctl --alloc
+sleep 0.2
+ssh CS001C tbbctl --rec
+sleep 1.0
 ssh CS001C tbbctl --stop
 
 HourTag=$(date +%H)
@@ -25,13 +27,13 @@ FullName="$DireName""$FileName"
 
 if [ "$RCUNumber" -le 15 ]
  then
-nohup /bin/bash -e | ssh 10.181.0.3 /app64/usg/bin/tbb2h5 --ip 10.151.1.1 --port 31664 \
+nohup /bin/bash -e | ssh 10.181.0.1 /app64/usg/bin/tbb2h5 --ip 10.151.1.1 --port 31664 \
  --outfile "$FullName" & >> /dev/null
  else
-nohup /bin/bash -e | ssh 10.181.0.3 /app64/usg/bin/tbb2h5 --ip 10.151.1.1 --port 31665 \
+nohup /bin/bash -e | ssh 10.181.0.1 /app64/usg/bin/tbb2h5 --ip 10.151.1.1 --port 31665 \
  --outfile "$FullName" & >> /dev/null
 fi
 
 ssh CS001C --readall=2000 --select="$RCUNumber"
 
-done 
+#done 
