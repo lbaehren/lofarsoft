4f9a16ff701c2e76cc1dac7ff2f2afbdbf5f9d00
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-07-15 19:02:31 +0000

    [1] Enabled svn:keyword substitution for CMake modules and DAL source files. [2] Adjusted DAL coordinate classes to no longer use the rather cryptic FITS keywords when writing data to HDF5.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2818 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/InstallSystemPackages.cmake
+++ devel_common/cmake/InstallSystemPackages.cmake
@@ -28,7 +28,6 @@ find_program (yast_executable   yast    PATHS ${bin_locations})
 ## Determine platform details
 
 
-
 ## ==============================================================================
 ##
 ##  Package lists
@@ -39,34 +38,76 @@ if (UNIX)
   
   if (APPLE)
     
-    set (fink_packages
-      atlas
-      bison
-      flex
-      fftw3
-      fftw3-shlibs
-      gfortran-shlibs
-      numpy-py26
-      qt4-mac
-      sip-py26
-      pyqt4-py26
-      )
-    
-    if (NOT doxygen_executable)
-      list (APPEND fink_packages doxygen)
-    endif (NOT doxygen_executable)
-    
-    if (NOT HAVE_GSL)
-      list (APPEND fink_packages gsl)
-      list (APPEND fink_packages gsl-shlibs)
-    endif (NOT HAVE_GSL)
-    
-    if (NOT SWIG_FOUND)
-      list (APPEND fink_packages swig)
-    endif (NOT SWIG_FOUND)
+    if (fink_executable)
+      
+      ##______________________________________________________________
+      ## Set up the list of Fink packages
+      
+      set (fink_packages
+	atlas
+	bison
+	flex
+	fftw3
+	fftw3-shlibs
+	gfortran-shlibs
+	numpy-py26
+	qt4-mac
+	sip-py26
+	pyqt4-py26
+	)
+      
+      if (NOT doxygen_executable)
+	list (APPEND fink_packages doxygen)
+      endif (NOT doxygen_executable)
+      
+      if (NOT HAVE_GSL)
+	list (APPEND fink_packages gsl)
+	list (APPEND fink_packages gsl-shlibs)
+      endif (NOT HAVE_GSL)
+      
+      if (NOT SWIG_FOUND)
+	list (APPEND fink_packages swig)
+      endif (NOT SWIG_FOUND)
+      
+      if (NOT wget_executable)
+	list (APPEND fink_packages wget)
+      endif (NOT wget_executable)
+      
+      ##______________________________________________________________
+      ## Define installation targets for the packages
+      
+      foreach (fink_package ${fink_packages})
+	## define target for individual Fink package
+	add_custom_target (fink-${fink_package}
+	  COMMAND ${fink_executable} install ${fink_package}
+	  WORKING_DIRECTORY ${USG_BUILD}
+	  COMMENT "Installating Fink package ${fink_package} ..."
+	  )
+	## target dependencies
+	list (APPEND fink-packages-depends fink-${fink_package})
+      endforeach (fink_package)
+      
+      ## Master target to install all of the above Fink packages
+      add_custom_target (fink-packages
+	COMMAND ${fink_executable} remove hdf5 hdf5-shlibs
+	WORKING_DIRECTORY ${USG_BUILD}
+	COMMENT "Installating Fink packages ..."
+	)
+      add_dependencies (fink-packages ${fink-packages-depends})
+      
+    else (fink_executable)
+      message (STATUS "Unable to install Fink packages!")
+    endif (fink_executable)
     
   else (APPLE)
-    
+
+    ## Set up the list of Debian packages to install through apt-get
+
+    set (debian_packages
+      libfreetype6-dev
+      libpng12-dev
+    )
+
   endif (APPLE)
 
 endif (UNIX)
@@ -76,17 +117,3 @@ endif (UNIX)
 ##  Wrapper for system-dependent package managers
 ##
 ## ==============================================================================
-
-if (UNIX)
-  if (APPLE)
-    if (fink_executable)
-    else (fink_executable)
-      message (STATUS "Unable to install Fink packages - missing executable!")
-    endif (fink_executable)
-  else (APPLE)
-  endif (APPLE)
-else (UNIX)
-  if (WIN32)
-    message (STATUS "Installation of system packages not supported on Windows!")
-  else (WIN32)
-endif (UNIX)
