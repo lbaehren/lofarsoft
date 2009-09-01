060d4fef8eadba8d6b9086a44324e788a29fd8a3
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-09-01 12:49:36 +0000

    Added some additional feedback on the images before and after rebinning
    git-svn-id: http://usg.lofar.org/svn/code/trunk@2968 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/apps/imagetofits.cc
+++ src/CR-Tools/apps/imagetofits.cc
@@ -1,5 +1,29 @@
+/*-------------------------------------------------------------------------*
+ | $Id:: templates.h 391 2007-06-13 09:25:11Z baehren                    $ |
+ *-------------------------------------------------------------------------*
+ ***************************************************************************
+ *   Copyright (C) 2009                                                    *
+ *   Sef Welles                                                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include <crtools.h>
+#include <Utilities/TestsCommon.h>
+
 #include <images/Images/ImageFITSConverter.h>
 #include <images/Images/HDF5Image.h>
 #include <images/Images/PagedImage.h>
@@ -35,18 +59,24 @@ int main(int argc, char *argv[])
   String error;
   Bool ok;
   
-  								// Open the image and convert it to fits:
+  // Open the image and convert it to fits:
   if (datatype == "hdf5") {
     HDF5Image<Float> imageIn (infile);
-	    
-										// Regridding of input file:
-  	IPosition factors (imageIn.shape());
-  	factors    = 1;
-  	factors(3) = 513;
-   	RebinImage<Float> rb(imageIn, factors);
-   	IPosition shapeOut = rb.shape();
-   	TiledShape tShapeOut(shapeOut);
-	
+    
+    // Regridding of input file:
+    IPosition factors (imageIn.shape());
+    factors    = 1;
+    factors(3) = 513;
+    RebinImage<Float> rb(imageIn, factors);
+    IPosition shapeOut = rb.shape();
+    TiledShape tShapeOut(shapeOut);
+
+    /* Summary of image properties */
+    cout << "-- Summary of the original image:" << std::endl;
+    CR::summary (imageIn);
+    cout << "-- Summary of the image after rebinning:" << std::endl;
+    CR::summary (rb);
+    
 /* 	const LELCoordinates* pLatCoord = &(rb.lelCoordinates());
     const LELImageCoord* pImCoord =
     dynamic_cast<const LELImageCoord*>(pLatCoord);
