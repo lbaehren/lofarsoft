ae79d4823045069648a0057ac1ad1f74438b4c23
horneffer <horneffer@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-18 13:23:38 +0000

    changed the path to the CalTable in tFirstStagePipeline.cc
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3382 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Analysis/test/tFirstStagePipeline.cc
+++ src/CR-Tools/implement/Analysis/test/tFirstStagePipeline.cc
@@ -68,7 +68,7 @@ int test_FirstStagePipeline ()
     // Generate a record that points to the CalTable files 
     // Hardcoded path is not nice, but works...
     Record obsrec;
-    obsrec.define("LOPES","/home/horneff/usg/data/lopes/LOPES-CalTable");
+    obsrec.define("LOPES",caltable_lopes);
 
     // Put the record into the pipeline object.
     newObject.SetObsRecord(obsrec);
