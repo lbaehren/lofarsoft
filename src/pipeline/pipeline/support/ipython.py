fd2bed10970426ed5256f6ac5b944d7721a7c244
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:42 +0000

    Fixes to LOFARTask.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3476 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/support/ipython.py
+++ src/pipeline/pipeline/support/ipython.py
@@ -1,15 +1,19 @@
 from IPython.kernel.task import StringTask
 
 class LOFARTask(StringTask):
-    def __init__(self, expression, pull=None, push=None, 
-        clear_before=False, clear_after=False, retries=0, 
+    """
+    Extended version of IPython's StringTask, allowing external
+    arguments for dependency resolution.
+    """
+    def __init__(self, expression, pull=None, push=None,
+        clear_before=False, clear_after=False, retries=0,
         recovery_task=None, depend=None, dependargs=None
     ):
-    super(LOFARTask, self).__init__(
-        self, expression, pull=None, push=None, clear_before=False, 
-        clear_after=False, retries=0, recovery_task=None, depend=None
-    )
-    self.dependargs = dependargs
+        self.dependargs = dependargs
+        return super(LOFARTask, self).__init__(
+            expression, pull, push, clear_before, clear_after,
+            retries, recovery_task, depend
+        )
 
     def check_depend(self, properties):
         """
