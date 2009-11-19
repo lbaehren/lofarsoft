57c8dfbc7bda776f40c2a006747d2a65dadce86b
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:34 +0000

    Log timing information
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3648 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/nodes/vdsmaker.py
+++ src/pipeline/pipeline/nodes/vdsmaker.py
@@ -1,14 +1,9 @@
 # Python standard library
 from __future__ import with_statement
-from contextlib import closing
 from subprocess import Popen, CalledProcessError, PIPE, STDOUT
-from ConfigParser import SafeConfigParser as ConfigParser
-import os.path
 
 from pipeline.support.lofarexceptions import ExecutableMissing
-from pipeline.support.utilities import create_directory
-
-# Root directory for config file
+from pipeline.support.utilities import create_directory, log_time
 from pipeline.support.lofarnode import LOFARnode
 
 class makevds_node(LOFARnode):
@@ -16,22 +11,23 @@ class makevds_node(LOFARnode):
         # Make VDS file for input MS in specifed location
         # The dev version of makevds appears to make more comprehensive VDS files
         # (including the FileName field) than the stable version.
-        self.logger.info("Processing: %s" % (infile))
-        try:
-            if not os.access(executable, os.X_OK):
-                raise ExecutableMissing(executable)
-            cmd = [executable, clusterdesc, infile, outfile]
-            self.logger.debug("Running: %s" % (' '.join(cmd,)))
-            makevds_process = Popen(cmd, stdout=PIPE, stderr=STDOUT)
-            result = makevds_process.wait()
-            if result != 0:
-                raise CalledProcessError(result, cmd)
-            return result
-        except ExecutableMissing, e:
-            self.logger.error("%s not found" % (e.args[0]))
-            raise
-        except CalledProcessError, e:
-            # For CalledProcessError isn't properly propagated by IPython
-            # Temporary workaround...
-            self.logger.error(str(e))
-            raise Exception
+        with log_time(self.logger):
+            self.logger.info("Processing: %s" % (infile))
+            try:
+                if not os.access(executable, os.X_OK):
+                    raise ExecutableMissing(executable)
+                cmd = [executable, clusterdesc, infile, outfile]
+                self.logger.debug("Running: %s" % (' '.join(cmd,)))
+                makevds_process = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+                result = makevds_process.wait()
+                if result != 0:
+                    raise CalledProcessError(result, cmd)
+                return result
+            except ExecutableMissing, e:
+                self.logger.error("%s not found" % (e.args[0]))
+                raise
+            except CalledProcessError, e:
+                # For CalledProcessError isn't properly propagated by IPython
+                # Temporary workaround...
+                self.logger.error(str(e))
+                raise Exception
