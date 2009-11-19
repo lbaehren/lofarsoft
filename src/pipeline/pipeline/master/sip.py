4c6e8bab8baa6f41ac09aeb0f9ef96feac0cab37
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:41 +0000

    Bare-bones SIP recipe
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3474 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/pipeline/pipeline/master/sip.py
@@ -0,0 +1,34 @@
+from pipeline.support.lofarrecipe import LOFARrecipe
+from pipeline.support.lofaringredient import LOFARinput, LOFARoutput
+import pipeline.support.utilities as utilities
+
+class sip(LOFARrecipe):
+    """
+    The LOFAR Standard Imaging Pipeline.
+    """
+    def __init__(self):
+        super(sip, self).__init__()
+
+    def go(self):
+        self.logger.info("Standard Imaging Pipeline starting up")
+        super(sip, self).go()
+
+        self.logger.info("Calling DPPP")
+        inputs = LOFARinput(self.inputs)
+        outputs = LOFARoutput()
+        if self.cook_recipe('dppp', inputs, outputs):
+            self.logger.warn("DPPP reports failure")
+            return 1
+
+        self.logger.info("Calling MWImager")
+        inputs = LOFARinput(self.inputs)
+        outputs = LOFARoutput()
+        if self.cook_recipe('mwimager', inputs, outputs):
+            self.logger.warn("MWImager reports failure")
+            return 1
+
+        return 0
+
+
+if __name__ == '__main__':
+    sys.exit(sip().main())
