cb47f0b7706a44184d3bdcde99756abad9fc4852
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:18 +0000

    Use ssh to move images
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3598 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/master/collector.py
+++ src/pipeline/pipeline/master/collector.py
@@ -1,6 +1,7 @@
 # Local helpers
 from pipeline.support.lofarrecipe import LOFARrecipe
 from pipeline.support.ipython import LOFARTask
+from pipeline.support.clusterdesc import ClusterDesc
 import pipeline.support.utilities as utilities
 import os, os.path, glob, subprocess, sys, numpy, pyfits, shutil
 
@@ -22,6 +23,12 @@ class collector(LOFARrecipe):
             help="Clobber results directory if already exists",
             action="store_true"
         )
+        self.optionparser.add_option(
+            '--image-re',
+            dest="image_re",
+            help="Regular expression to match CASA image names"
+            default="image*"
+        )
 
     def go(self):
         self.logger.info("Starting data collector run")
@@ -33,52 +40,34 @@ class collector(LOFARrecipe):
                 exec_string = ['/bin/rm', '-rf', '%s' % file]
                 subprocess.check_call(exec_string)
 
-        image_names = self.inputs['args']
-        tc, mec = self._get_cluster()
-
-        self.logger.info("Copying images to results directory")
-        mec.push_function(
-            {
-                "build_available_list": utilities.build_available_list,
-                "clear_available_list": utilities.clear_available_list
-            }
+        clusterdesc = ClusterDesc(
+            self.config.get('cluster', 'clusterdesc')
         )
-        self.logger.info("Building list of data available on engines")
-        available_list = "%s%s" % (
-            self.inputs['job_name'], self.__class__.__name__
-        )
-        mec.push(dict(filenames=image_names))
-        mec.execute(
-            "build_available_list(\"%s\")" % (available_list,)
-        )
-        mec.execute("import shutil")
-        tasks = []
-        for image_name in image_names:
-            dest = os.path.join(
-                self.config.get('layout', 'results_directory'),
-                os.path.basename(image_name)
-            )
-            exec_string = "shutil.copytree(\"%s\", \"%s\")" % (image_name, dest)
-            self.logger.debug(exec_string)
-            task = LOFARTask(
-                exec_string,
-                depend=utilities.check_for_path,
-                dependargs=(image_name, available_list)
-            )
-            self.logger.info("Scheduling processing of %s" % (image_name,))
-            tasks.append(tc.run(task))
-        tc.barrier(tasks)
-        for task in tasks:
-            res = tc.get_task_result(task)
-            if res.failure:
-                print res.failure
-        mec.execute("clear_available_list(\"%s\")" % (available_list,))
+        results_dir = self.config.get('layout', 'results_directory'),
+        self.logger.debug("Copying CASA images to to %s"  % (results_dir))
+        for node in clusterdesc.get('ComputeNodes'):
+            self.logger.debug("Node: %s" % (node))
+            try:
+                check_call(
+                    [
+                        "ssh",
+                        node,
+                        "--",
+                        "mv",
+                        "%s/%s/%s" % (
+                            self._input_or_default('working_directory'),
+                            self.inputs['job_name'],
+                            self.inputs['image_re']
+                        ),
+                        results_dir
+                    ])
+            except CalledProcessError:
+                self.logger.warn("No images moved from %s" % (node))
         
         self.logger.info("Generating FITS files")
         fits_files = []
-        for filename in glob.glob(
-            "%s/image.i.*" % (self.config.get('layout', 'results_directory'))
-        ):
+        image_names = glob.glob("%s/%s" % (results_dir, self.inputs['image_re'])
+        for filename in image_names:
             self.logger.debug(filename)
             subband = os.path.basename(filename).split('.')[2]
             output = os.path.join(
