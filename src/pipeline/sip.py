2ad09431ee0e5d8fe8ebafe48324666cbde0e7b2
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:49 +0000

    log total sip time
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3694 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/sip.py
+++ src/pipeline/sip.py
@@ -8,19 +8,22 @@ hand.
 
 import sys, os
 from pipeline.master.control import control
+from pipeline.support.utilities import log_time
 
 class sip(control):
     def pipeline_logic(self):
-        datafiles = self.run_task("vdsreader")
-        datafiles = self.run_task("dppp", datafiles)
-        datafiles = self.run_task("exclude_DE001LBA", datafiles)
-        datafiles = self.run_task("trim_300", datafiles)
-        datafiles = self.run_task("bbs", datafiles)
-        datafiles = self.run_task("local_flag", datafiles)
-        self.outputs['images'] = self.run_task("mwimager", datafiles)
-        self.outputs['average'] = self.run_task("collector")
-        self.run_task("qcheck", self.outputs['images'])
-        self.run_task("sourcefinder", self.outputs['average'])
+        # Customise this to the requirements of a specific job.
+        with log_time(self.logger):
+            datafiles = self.run_task("vdsreader")
+            datafiles = self.run_task("dppp", datafiles)
+            datafiles = self.run_task("exclude_DE001LBA", datafiles)
+            datafiles = self.run_task("trim_300", datafiles)
+            datafiles = self.run_task("bbs", datafiles)
+            datafiles = self.run_task("local_flag", datafiles)
+            self.outputs['images'] = self.run_task("mwimager", datafiles)
+            self.outputs['average'] = self.run_task("collector")
+            self.run_task("qcheck", self.outputs['images'])
+            self.run_task("sourcefinder", self.outputs['average'])
        
 if __name__ == '__main__':
     sys.exit(eval(os.path.splitext(os.path.basename(sys.argv[0]))[0])().main())
