0491407f4790d154d442bfccf39ec4a41978cc0c
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:04 +0000

    Node-script to excluding stations from MS
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3549 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/pipeline/pipeline/nodes/excluder.py
@@ -0,0 +1,32 @@
+# Root directory for config file
+from pipeline import __path__ as config_path
+
+from twisted.python import log
+from pyrap.tables import table
+
+def exclude_stations(input, output, *stations):
+    # Remove data the given stations from the input MS.
+    # Output is clobbered.
+    try:
+        t = table(input)
+    except Exception, e:
+        log.msg(str(e))
+        raise
+    try:
+        a = table(t.getkeyword('ANTENNA').split()[1])
+        station_ids = [a.getcol('NAME').index(name) for name in stations]
+        selection = t.query(
+            "ANTENNA1 not in %s and ANTENNA2 not in %s" %
+            (str(station_ids), str_station_ids)
+        )
+        selection.copy(output, deep=True)
+    except Exception, e:
+        log.msg(str(e))
+        raise
+
+if __name__ == "__main__":
+    from sys import argv
+    try:
+        exclude_stations(*argv[1:])
+    except:
+        print "Usage: exclude_stations [input] [output] [station1...stationN]"
