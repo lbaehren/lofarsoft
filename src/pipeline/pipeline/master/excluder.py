84e9622729d9cd33673d69daa7946ecc49087d6d
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:04 +0000

    Master script for station excluder
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3550 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/master/excluder.py
+++ src/pipeline/pipeline/master/excluder.py
@@ -6,40 +6,33 @@ from pipeline.support.ipython import LOFARTask
 from pipeline.support.lofarrecipe import LOFARrecipe
 import pipeline.support.utilities as utilities
 
-def trim_ms(input, output, start_seconds, end_seconds):
-    from pipeline.nodes.trimmer import trim_ms
-    return trim_ms(input, output, start_seconds, end_seconds)
+def exclude_station(input, output, station):
+    from pipeline.nodes.excluder import exclude_stations
+    return exclude_station(input, output, station)
 
-class trimmer(LOFARrecipe):
+class excluder(LOFARrecipe):
     def __init__(self):
-        super(trimmer, self).__init__()
+        super(excluder, self).__init__()
         self.optionparser.add_option(
-            '--start-seconds',
-            dest="start_seconds",
-            type="float",
-            help="Seconds to trim from start of data"
-        )
-        self.optionparser.add_option(
-            '--end-seconds',
-            dest="end_seconds",
-            type="float",
-            help="Seconds to trim from end of data"
+            '--station',
+            dest="station",
+            help="Name of station to exclude (e.g. DE001LBA)"
         )
         self.optionparser.add_option(
             '--suffix',
             dest="suffix",
-            help="Suffix to add to trimmed data (default: overwrite existing)"
+            help="Suffix to add to output data (default: overwrite existing)"
         )
 
     def go(self):
-        super(trimmer, self).go()
+        super(excluder, self).go()
 
         ms_names = self.inputs['args']
 
         tc, mec = self._get_cluster()
         mec.push_function(
             dict(
-                trim_ms=trim_ms,
+                exclude_station=exclude_station,
                 build_available_list=utilities.build_available_list,
                 clear_available_list=utilities.clear_available_list
             )
@@ -47,7 +40,7 @@ class trimmer(LOFARrecipe):
         self.logger.info("Pushed functions to cluster")
 
         self.logger.info("Building list of data available on engines")
-        available_list = "%s%s" % (self.inputs['job_name'], "trimmer")
+        available_list = "%s%s" % (self.inputs['job_name'], "excluder")
         mec.push(dict(filenames=ms_names))
         mec.execute(
             "build_available_list(\"%s\")" % (available_list,)
@@ -56,10 +49,9 @@ class trimmer(LOFARrecipe):
         outnames = []
         for ms_name in ms_names:
             outnames.append(ms_name + self.inputs['suffix'])
-            execute_string = "result = trim_ms(ms_name, \"%s\", %f, %f)" % (
+            execute_string = "result = exclude_station(ms_name, \"%s\", %f, %f)" % (
                 outnames[-1],
-                self.inputs['start_seconds'],
-                self.inputs['end_seconds']
+                self.inputs['station'],
             )
             print "Executing: ", execute_string
             task = LOFARTask(
@@ -73,7 +65,7 @@ class trimmer(LOFARrecipe):
             )
             self.logger.info("Scheduling processing of %s" % (ms_name,))
             tasks.append(tc.run(task))
-        self.logger.info("Waiting for all trimmer tasks to complete")
+        self.logger.info("Waiting for all excluder tasks to complete")
         tc.barrier(tasks)
         for task in tasks:
             res = tc.get_task_result(task)
@@ -85,4 +77,4 @@ class trimmer(LOFARrecipe):
         mec.execute("clear_available_list(\"%s\")" % (available_list,))
 
 if __name__ == '__main__':
-    sys.exit(trimmer().main())
+    sys.exit(excluder().main())
