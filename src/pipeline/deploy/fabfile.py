e085276118c1930f31975d03d929d686861fccef
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:29:14 +0000

    Merge branch 'master' of ssh://gjw/home/jds/repositories/lofar_pipes
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3582 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/deploy/fabfile.py
+++ src/pipeline/deploy/fabfile.py
@@ -8,26 +8,26 @@ config.read("%s/pipeline.cfg" % (config_path[0],))
 
 script_path = config.get('deploy', 'script_path')
 
-def controller_host(clusterdesc=config.get('cluster', 'clusterdesc')):
+def head_node(clusterdesc=config.get('cluster', 'clusterdesc')):
     clusterdesc = ClusterDesc(clusterdesc)
     env.hosts = clusterdesc.get('HeadNode')
 
-def engine_hosts(clusterdesc=config.get('cluster', 'clusterdesc')):
+def compute_nodes(clusterdesc=config.get('cluster', 'clusterdesc')):
     clusterdesc = ClusterDesc(clusterdesc)
     env.hosts = clusterdesc.get('ComputeNodes')
 
 def start_controller(controlpath=config.get('DEFAULT', 'runtime_directory')):
-    require('hosts', provided_by=[controller_host])
+    require('hosts', provided_by=[head_node])
     run("bash %s/ipcontroller.sh %s start" % (script_path, controlpath))
 
 def stop_controller(controlpath=config.get('DEFAULT', 'runtime_directory')):
-    require('hosts', provided_by=[controller_host])
+    require('hosts', provided_by=[head_node])
     run("bash %s/ipcontroller.sh %s stop" % (script_path, controlpath))
 
 def start_engine(controlpath=config.get('DEFAULT', 'runtime_directory')):
-    require('hosts', provided_by=[engine_hosts])
+    require('hosts', provided_by=[compute_nodes])
     run("bash %s/ipengine.sh %s start" % (script_path, controlpath))
 
 def stop_engine(controlpath=config.get('DEFAULT', 'runtime_directory')):
-    require('hosts', provided_by=[engine_hosts])
+    require('hosts', provided_by=[compute_nodes])
     run("bash %s/ipengine.sh %s stop" % (script_path, controlpath))
