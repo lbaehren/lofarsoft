6291e24eca0e462d97f729e856e5d95137c46af0
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:29 +0000

    Rename methods
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3441 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/deploy/fabfile.py
+++ src/pipeline/deploy/fabfile.py
@@ -11,7 +11,7 @@ def engine_hosts(clusterdesc):
     clusterdesc = fabsupport.ClusterDesc(clusterdesc)
     env.hosts = clusterdesc['ComputeNodes']
 
-def run_controller(controlpath="/data/users/swinbank/pipeline_runtime"):
+def start_controller(controlpath="/data/users/swinbank/pipeline_runtime"):
     require('hosts', provided_by=[controller_host])
     run("bash %s/ipcontroller.sh %s start" % (script_path, controlpath))
 
@@ -19,7 +19,7 @@ def stop_controller(controlpath="/data/users/swinbank/pipeline_runtime"):
     require('hosts', provided_by=[controller_host])
     run("bash %s/ipcontroller.sh %s stop" % (script_path, controlpath))
 
-def run_engine(controlpath="/data/users/swinbank/pipeline_runtime"):
+def start_engine(controlpath="/data/users/swinbank/pipeline_runtime"):
     require('hosts', provided_by=[engine_hosts])
     run("bash %s/ipengine.sh %s start" % (script_path, controlpath))
 
