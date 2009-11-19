b5bd0a8571a96d681a426a0fecdea93492f0e570
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:40 +0000

    LOFAR ingredients provide some default values
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3471 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/pipeline/pipeline/support/lofaringredient.py
@@ -0,0 +1,22 @@
+from cuisine.ingredient import WSRTingredient
+
+class LOFARinput(WSRTingredient):
+    """
+    All LOFAR pipeline ingredients are required to provide a few basic
+    parameters: 
+
+    * job_name
+    * runtime_directory
+    * config
+    """
+    def __init__(self, defaults):
+        super(LOFARinput, self).__init__(self)
+        for param in ("job_name", "runtime_directory", "config"):
+            if defaults.has_key(param):
+                self[param] = defaults[param]
+            else:
+                self[param] = None
+
+class LOFARoutput(WSRTingredient):
+    pass
+        
