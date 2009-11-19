0fc2165694fbf98d9860f472803201bf0794fd2b
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:25 +0000

    Merge branch 'master' of ssh://gjw/home/jds/repositories/lofar_pipes
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3435 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/library/dppp.py
+++ src/pipeline/pipeline/library/dppp.py
@@ -7,7 +7,7 @@ from os import unlink
 from cuisine.parset import Parset
 
 def run_dppp(infile, outfile, parset):
-    executable = "CS1_IDPPP"
+    executable = "/app/lofar/dev/bin/CS1_IDPPP"
 
     # We need to patch the parset with the correct input/output MS names.
     temp_parset_filename = mkstemp()[1]
@@ -17,9 +17,11 @@ def run_dppp(infile, outfile, parset):
     temp_parset.writeToFile(temp_parset_filename)
 
     try:
-        check_call([executable, temp_parset_filename])
+        # What is the '1' for? Required by DP3...
+        check_call([executable, temp_parset_filename, '1'])
     finally:
         unlink(temp_parset_filename)
 
 if __name__ == "__main__":
-    run_dppp(ARGV[0], ARGV[1], ARGV[2])
+    from sys import argv
+    run_dppp(argv[1], argv[2], argv[3])
