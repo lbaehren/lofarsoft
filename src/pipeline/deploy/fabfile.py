c6be20916ae64dff193a5e337cbd524867db9f30
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-19 13:28:36 +0000

    configparser for fabfile
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3462 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/deploy/fabfile.py
+++ src/pipeline/deploy/fabfile.py
@@ -1,28 +1,33 @@
 from fabric.api import env, hosts, run, put, get, env, require
 import fabsupport
 
-script_path = '/home/swinbank/Work/lofar_pipes/deploy'
+from ConfigParser import SafeConfigParser as ConfigParser
+from pipeline import __path__ as config_path
+config = ConfigParser()
+config.read("%s/pipeline.cfg" % (config_path[0],))
 
-def controller_host(clusterdesc):
+script_path = config.get('deploy', 'script_path')
+
+def controller_host(clusterdesc=config.get('cluster', 'clusterdesc')):
     clusterdesc = fabsupport.ClusterDesc(clusterdesc)
     env.hosts = [clusterdesc['HeadNode']]
 
-def engine_hosts(clusterdesc):
+def engine_hosts(clusterdesc=config.get('cluster', 'clusterdesc')):
     clusterdesc = fabsupport.ClusterDesc(clusterdesc)
     env.hosts = clusterdesc['ComputeNodes']
 
-def start_controller(controlpath="/data/users/swinbank/pipeline_runtime"):
+def start_controller(controlpath=config.get('DEFAULT', 'runtime_directory')):
     require('hosts', provided_by=[controller_host])
     run("bash %s/ipcontroller.sh %s start" % (script_path, controlpath))
 
-def stop_controller(controlpath="/data/users/swinbank/pipeline_runtime"):
+def stop_controller(controlpath=config.get('DEFAULT', 'runtime_directory')):
     require('hosts', provided_by=[controller_host])
     run("bash %s/ipcontroller.sh %s stop" % (script_path, controlpath))
 
-def start_engine(controlpath="/data/users/swinbank/pipeline_runtime"):
+def start_engine(controlpath=config.get('DEFAULT', 'runtime_directory')):
     require('hosts', provided_by=[engine_hosts])
     run("bash %s/ipengine.sh %s start" % (script_path, controlpath))
 
-def stop_engine(controlpath="/data/users/swinbank/pipeline_runtime"):
+def stop_engine(controlpath=config.get('DEFAULT', 'runtime_directory')):
     require('hosts', provided_by=[engine_hosts])
     run("bash %s/ipengine.sh %s stop" % (script_path, controlpath))
