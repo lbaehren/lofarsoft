7f4d03af8efa1c9edd92a975cd9f18e15dfcb2ce
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-26 13:52:50 +0000

    Dump information on source<->catalogue associations
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3745 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/master/simple_se.py
+++ src/pipeline/pipeline/master/simple_se.py
@@ -8,12 +8,27 @@ import os, os.path, glob, subprocess, sys, numpy, pyfits, shutil, errno, re
 
 # SE tools
 from tkp_lib.dataset   import DataSet
-from tkp_lib.image    import ImageData
+from tkp_lib.image     import ImageData
 from tkp_lib.accessors import FitsFile
-from tkp_lib.database  import connection
+import tkp_lib.database as database
 
 region_local = "SELECT '# Region file format: DS9 version 4.0' UNION SELECT '# Filename: %s' UNION SELECT 'global color=green font=\"helvetica 10 normal\" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source' UNION SELECT 'fk5' UNION SELECT CONCAT('circle(', ra, ',', decl, ',0.025) #color=blue') FROM extractedsources WHERE image_id = %s;"
 region_wenss = "SELECT '# Region file format: DS9 version 4.0' UNION SELECT '# Filename: %s' UNION SELECT 'global color=green font=\"helvetica 10 normal\" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source' UNION SELECT 'fk5' UNION SELECT CONCAT('circle(', ra, ',', decl, ',0.025) #color=red') FROM catalogedsources,catalogs WHERE catname = 'WENSS' AND catid = cat_id AND ra BETWEEN %f AND %f AND decl BETWEEN %f AND %f;" 
+associations = """
+SELECT
+    x1.ra, x1.decl, x1.I_Peak, catalogs.catname, cs.ra, cs.decl, a.assoc_weight, a.assoc_distance_arcsec
+    FROM extractedsources x1
+        LEFT OUTER JOIN
+            assoccatsources a ON xtrsrc_id = xtrsrcid
+        LEFT OUTER JOIN
+            catalogedsources cs ON assoc_catsrc_id = catsrcid
+        LEFT OUTER JOIN
+            catalogs ON cat_id = catid
+    WHERE
+        x1.image_id = %d
+    ORDER BY
+        I_Peak;
+"""
 
 def image_region(image):
     """
@@ -44,6 +59,7 @@ class simple_se(LOFARrecipe):
     """
     Run source extraction on FITS images on the front-end.
     Dump ds9 region files of found sources & WENSS sources.
+    Dump text file of assocations with catalogue sources.
 
     Designed to be run e.g. on an averaged image at the end of a pipeline run.
     """
@@ -62,6 +78,12 @@ class simple_se(LOFARrecipe):
             help="Filename for region file of WENSS detections",
             default="wenss.reg"
         )
+        self.optionparser.add_option(
+            '--associations',
+            dest="associations",
+            help="Filename for association list",
+            default="assocation.list"
+        )
 
     def go(self):
         self.logger.info("Starting source identification")
@@ -74,10 +96,13 @@ class simple_se(LOFARrecipe):
             self.logger.info("Processing %s" % (file,))
             image = ImageData(FitsFile(file), dataset=dataset)
             self.logger.info("Running source finder")
-            sr = image.sextract()
-            with closing(connection()) as con:
+            sr = image.sextract(det=5, anl=2)
+            with closing(database.connection()) as con:
                 self.logger.debug("Saving results to database")
                 sr.savetoDB(con)
+                self.logger.info("Generating source associations")
+                database.assocXSrc2XSrc(image.id, con)
+                database.assocXSrc2Cat(image.id, con)
                 with closing(con.cursor()) as cur:
                     self.logger.info("Querying for local region file")
                     my_query = region_local % (file, int(image.id[0]))
@@ -106,6 +131,19 @@ class simple_se(LOFARrecipe):
                     ) as output_file:
                         for line in cur.fetchall():
                             output_file.write(line[0] + '\n')
+                    self.logger.info("Querying for association list")
+                    my_query = associations % (image.id)
+                    self.logger.debug(my_query)
+                    cur.execute(my_query)
+                    with open(
+                        os.path.join(
+                            os.path.dirname(file),
+                            self.inputs['associations']
+                        ),
+                        'w'
+                    ) as output_file:
+                        for line in cur.fetchall():
+                            output_file.write(str(line) + '\n')
 
         self.outputs['data'] = None
         return 0
