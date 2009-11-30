ab63cdd20035232a0888234ff3650cf2929d982d
falcke <falcke@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2009-11-30 23:35:12 +0000

    sdadas
    git-svn-id: http://usg.lofar.org/svn/code/trunk@3764 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/CR-Tools/implement/GUI/hfpp-undef.cc
@@ -0,0 +1,11 @@
+#undef HF_PP_FUNCNAME
+#undef HF_PP_FUNCTYPE
+#undef HF_PP_NVECS
+
+#undef HF_PP_NPAR
+#undef HF_PP_PAR0
+#undef HF_PP_PAR1
+#undef HF_PP_PAR2
+
+#undef HF_PP_VEC_WRAPPER_CODE_STL
+#undef HF_PP_VEC_WRAPPER_CODE_CASA
