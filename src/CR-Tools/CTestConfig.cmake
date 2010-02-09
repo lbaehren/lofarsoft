5867842e22d727238e5a147a84309d7eb47a2ef3
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-02-09 12:44:55 +0000

    New configuration to direct test results to the Dashboard at ASTRON.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4142 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/CTestConfig.cmake
+++ src/CR-Tools/CTestConfig.cmake
@@ -1,6 +1,13 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "CR-Tools")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-SET (CTEST_DROP_METHOD xmlrpc)
-SET (CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
-SET (CTEST_DROP_SITE "usg.lofar.org:8081")
-SET (CTEST_DROP_LOCATION "CR")
-#SET (CTEST_DROP_SITE_USER "lbaehren")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "svn.astron.nl")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CR-Tools")
+set(CTEST_DROP_SITE_CDASH TRUE)
