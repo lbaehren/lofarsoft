3761ca2c7e2d926b53ddc5ba05cbec18cfbfecd0
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-02-16 13:56:49 +0000

    Abstract file grouping for imager into a separate generator
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4166 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/master/mwimager.py
+++ src/pipeline/pipeline/master/mwimager.py
@@ -9,6 +9,7 @@ from cuisine.parset import Parset
 # Local helpers
 from pipeline.support.lofarrecipe import LOFARrecipe
 from pipeline.support.lofaringredient import LOFARinput, LOFARoutput
+from pipeline.support.group_data import group_files
 import pipeline.support.utilities as utilities
 
 class mwimager(LOFARrecipe):
@@ -83,43 +84,18 @@ class mwimager(LOFARrecipe):
         # Given a limited number of processes per node, the first task is to
         # partition up the data for processing.
         self.logger.debug('Listing data on nodes')
-        data = {}
-        for node in clusterdesc.get('ComputeNodes'):
-            self.logger.debug("Node: %s" % (node))
-            try:
-                exec_string = ["ssh", node, "--", "find",
-                    os.path.join(
-                        self.inputs['working_directory'],
-                        self.inputs['job_name']
-                        ),
-                    "-maxdepth 1",
-                    "-print0"
-                    ]
-                my_process = subprocess.Popen(exec_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                sout, serr = my_process.communicate()
-                data[node] = sout.split('\x00')
-            except:
-                pass
-            data[node] = utilities.group_iterable(
-                [element for element in data[node] if element in self.inputs['args']],
-                int(self.inputs['max_bands_per_node'])
-            )
-
-        # Now produce an iterator which steps through the various chunks of
-        # data to image, and image each chunk
-        data_iterator = utilities.izip_longest(*list(data.values()))
-        for data_chunk in data_iterator:
-            self.logger.info("Starting new imager pass")
-            to_process = []
-            for node_data in data_chunk:
-                if node_data: to_process.extend(node_data)
-            self.logger.debug("Processing: " +str(to_process))
-
+        for data_group in group_files(
+            self.logger,
+            clusterdesc,
+            os.path.join(self.inputs['working_directory'], self.inputs['job_name']),
+            int(self.inputs['max_bands_per_node'],
+            self.inputs['args']
+        ):
             self.logger.info("Calling vdsmaker")
             inputs = LOFARinput(self.inputs)
             inputs['directory'] = self.config.get('layout', 'vds_directory')
             inputs['gvds'] = self.inputs['gvds']
-            inputs['args'] = to_process
+            inputs['args'] = data_group
             inputs['makevds'] = self.inputs['makevds_exec']
             inputs['combinevds'] = self.inputs['combinevds_exec']
             outputs = LOFARoutput()
---
+++ src/pipeline/pipeline/support/group_data.py
@@ -0,0 +1,34 @@
+import subprocess
+import pipeline.support.utilities as utilities
+
+def group_files(logger, clusterdesc, node_directory, group_size, filenames):
+        # Given a limited number of processes per node, the first task is to
+        # partition up the data for processing.
+        logger.debug('Listing data on nodes')
+        data = {}
+        for node in clusterdesc.get('ComputeNodes'):
+            logger.debug("Node: %s" % (node))
+            try:
+                exec_string = ["ssh", node, "--", "find",
+                    node_directory,
+                    "-maxdepth 1",
+                    "-print0"
+                    ]
+                my_process = subprocess.Popen(exec_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                sout, serr = my_process.communicate()
+                data[node] = sout.split('\x00')
+            except: 
+                pass
+            data[node] = utilities.group_iterable(
+                [element for element in data[node] if element in filenames],
+                group_size,
+            )
+                    
+        # Now produce an iterator which steps through the various chunks of
+        # data to image, and image each chunk
+        data_iterator = utilities.izip_longest(*list(data.values()))
+        for data_chunk in data_iterator:
+            to_process = []
+            for node_data in data_chunk:
+                if node_data: to_process.extend(node_data) 
+            yield to_process
