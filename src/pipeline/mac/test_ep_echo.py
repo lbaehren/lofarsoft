06df9312da2b64f34c5215199b79c99915d4fd12
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-05 16:52:53 +0000

    Fix to work with new design
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4295 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/mac/test_ep_echo.py
+++ src/pipeline/mac/test_ep_echo.py
@@ -2,7 +2,7 @@
 import ep_echo
 
 if __name__ == "__main__":
-    ep_echo.ep_init("lfe001")
+    ep_interface = ep_echo.EP_Interface()
     for i in range(10):
-        print i, ": ", ep_echo.time_echo(), "seconds"
+        print i, ": ", ep_interface.time_echo(), "seconds"
 
