13b63af0b9e627ffcb0087acb9554d91d79bba0f
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-05 16:52:55 +0000

    snapshot wip on mac interface
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4298 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/mac/ep_echo.cc
+++ src/pipeline/mac/ep_echo.cc
@@ -1,103 +1,129 @@
-#include <lofar_config.h>
-#include <Common/LofarLogger.h>
-#include <MACIO/EventPort.h>
-#include <MACIO/MACServiceInfo.h>
-#include "Echo_Protocol.ph"
-
+#include "ep_echo.h"
 #include <boost/python.hpp>
-#include <string>
-#include <iostream>
-#include <sys/time.h>
-
         
-struct EP_Interface {
-    LOFAR::MACIO::EventPort* echoPort;
-    EP_Interface(std::string host = "") {
-        this->echoPort = new LOFAR::MACIO::EventPort("EchoServer:test", false, ECHO_PROTOCOL, host, true);
+EP_Interface::EP_Interface(std::string host = "") {
+    this->echoPort = new LOFAR::MACIO::EventPort("EchoServer:test", false, ECHO_PROTOCOL, host, true);
+}
+
+void EP_Interface::send_ping() {
+    EchoPingEvent pingEvent;
+    timeval pingTime;
+    gettimeofday(&pingTime, 0);
+    std::cout <<1.0 * pingTime.tv_sec + (pingTime.tv_usec / 1000000.0); 
+    pingEvent.ping_time = pingTime;
+
+    this->echoPort->send(&pingEvent);
+}
+
+GenericEventWrapper EP_Interface::receive_event()
+{
+    LOFAR::MACIO::GCFEvent* ackPtr;
+    ackPtr = echoPort->receive();
+//    EchoEchoEventWrapper event(ackPtr);
+    GenericEventWrapper event(ackPtr);
+    std::cout << event.get_signal();
+    return event;
+}
+
+double EP_Interface::time_echo()
+{
+    EchoPingEvent pingEvent;
+    timeval pingTime;
+    gettimeofday(&pingTime, 0);
+    pingEvent.ping_time = pingTime;
+
+    this->echoPort->send(&pingEvent);
+
+    LOFAR::MACIO::GCFEvent* ackPtr;
+    ackPtr = echoPort->receive();
+    if (ackPtr->signal == ECHO_ECHO) {
+        EchoEchoEvent ack(*ackPtr);
+
+        double  delta =  (1.0 * ack.echo_time.tv_sec + (ack.echo_time.tv_usec / 1000000.0));
+                delta -= (1.0 * ack.ping_time.tv_sec + (ack.ping_time.tv_usec / 1000000.0));
+
+        return delta;
     }
-    void send_ping()
-    {
-        EchoPingEvent pingEvent;
-        timeval pingTime;
-        gettimeofday(&pingTime, 0);
-        std::cout <<1.0 * pingTime.tv_sec + (pingTime.tv_usec / 1000000.0); 
-        pingEvent.ping_time = pingTime;
+}
 
-        this->echoPort->send(&pingEvent);
-    }
-//    EchoEvent receive_event()
-//    {
-//        LOFAR::MACIO::GCFEvent* ackPtr;
-//        ackPtr = echoPort->receive();
-//        EchoEvent event(ackPtr);
-//        return event;
-//    }
-    double time_echo()
-    {
-        EchoPingEvent pingEvent;
-        timeval pingTime;
-        gettimeofday(&pingTime, 0);
-        pingEvent.ping_time = pingTime;
+//void EP_Interface::send_event(const PythonEvent& pev) {
+//    this->echoPort->send(pev.evPtr);
+//}
 
-        this->echoPort->send(&pingEvent);
-
-        LOFAR::MACIO::GCFEvent* ackPtr;
-        ackPtr = echoPort->receive();
-        if (ackPtr->signal == ECHO_ECHO) {
-            EchoEchoEvent ack(*ackPtr);
-
-            double  delta =  (1.0 * ack.echo_time.tv_sec + (ack.echo_time.tv_usec / 1000000.0));
-                    delta -= (1.0 * ack.ping_time.tv_sec + (ack.ping_time.tv_usec / 1000000.0));
-
-            return delta;
-        }
-    }
+void EP_Interface::send_event(GenericEventWrapper* wrapped_event) {
+//    this->echoPort->send(wrapped_event->get_event());
+//    this->echoPort->send(&epe);
+//    std::cout << "foo";
+   this->echoPort->send(wrapped_event->get_event_ptr());
 };
 
-class PythonEvent {
-public:
-    void send(EP_Interface& send_interface) {
-        send_interface.echoPort->send(this->evPtr);
-    }
-    long get_signal() {
-        return this->evPtr->signal;
-    }
-protected:
-    LOFAR::MACIO::GCFEvent* evPtr;
-};
+//void PythonEvent::send(EP_Interface& send_interface) {
+//    send_interface.echoPort->send(this->evPtr);
+//}
 
-class EchoEvent: public PythonEvent {
-public:
-    EchoEvent(LOFAR::MACIO::GCFEvent* receivedPtr) {
-        this->evPtr = receivedPtr;
-    }
-    double get_pt() {
-        EchoEchoEvent my_event(*this->evPtr);
-        return 1.0 * my_event.ping_time.tv_sec + (my_event.ping_time.tv_usec / 1000000.0);
-    }
-    double get_et() {
-        EchoEchoEvent my_event(*this->evPtr);
-        return 1.0 * my_event.echo_time.tv_sec + (my_event.echo_time.tv_usec / 1000000.0);
-    }
-};
+/*long PythonEvent::get_signal() {
+    return this->evPtr->signal;
+}
 
-class PingEvent : public PythonEvent {
-public:
-    PingEvent() {
-        this->evPtr = new EchoPingEvent();
-        EchoPingEvent my_event(*this->evPtr);
-        timeval pingTime;
-        gettimeofday(&pingTime, 0);
-        my_event.ping_time = pingTime;
-    }
-    double get_pt() {
-        EchoPingEvent my_event(*this->evPtr);
-        return 1.0 * my_event.ping_time.tv_sec + (my_event.ping_time.tv_usec / 1000000.0);
-    }
-    //
-    // Copy constructor... probably doing the wrong thing!
-    PingEvent(const PingEvent&) {};
-};
+EchoEvent::EchoEvent(LOFAR::MACIO::GCFEvent* receivedPtr) {
+    this->evPtr = receivedPtr;
+}
+
+double EchoEvent::get_pt() {
+    EchoEchoEvent my_event(*this->evPtr);
+    return 1.0 * my_event.ping_time.tv_sec + (my_event.ping_time.tv_usec / 1000000.0);
+}
+
+double EchoEvent::get_et() {
+    EchoEchoEvent my_event(*this->evPtr);
+    return 1.0 * my_event.echo_time.tv_sec + (my_event.echo_time.tv_usec / 1000000.0);
+}
+
+PingEvent::PingEvent() {
+    this->evPtr = new EchoPingEvent();
+    EchoPingEvent my_event(*this->evPtr);
+    timeval pingTime;
+    gettimeofday(&pingTime, 0);
+    std::cout << pingTime.tv_sec << '\n';
+    my_event.ping_time = pingTime;
+    EchoPingEvent test(*this->evPtr);
+    std::cout << test.ping_time.tv_sec;
+}
+
+double PingEvent::get_pt() {
+    EchoPingEvent my_event(*this->evPtr);
+    return 1.0 * my_event.ping_time.tv_sec + (my_event.ping_time.tv_usec / 1000000.0);
+}*/
+
+double EchoPingEventWrapper::get_pt() {
+    return 1.0 * this->my_event->ping_time.tv_sec + (this->my_event->ping_time.tv_usec / 1000000.0); 
+}
+
+void EchoPingEventWrapper::set_pt(double my_time) {
+    this->my_event->ping_time.tv_sec = (int) my_time;
+    this->my_event->ping_time.tv_usec = 1000000.0 * (my_time - (int) my_time);
+}
+
+LOFAR::TYPES::uint16 EchoPingEventWrapper::get_seqnr() {
+    return this->my_event->seqnr;
+}
+
+void EchoPingEventWrapper::set_seqnr(LOFAR::TYPES::uint16 my_seqnr) {
+    this->my_event->seqnr = my_seqnr;
+}
+
+double EchoEchoEventWrapper::get_pt() {
+    return 1.0 * this->my_event->ping_time.tv_sec + (this->my_event->ping_time.tv_usec / 1000000.0); 
+}
+
+double EchoEchoEventWrapper::get_et() {
+    return 1.0 * this->my_event->echo_time.tv_sec + (this->my_event->echo_time.tv_usec / 1000000.0); 
+}
+
+LOFAR::TYPES::uint16 EchoEchoEventWrapper::get_seqnr() {
+    return this->my_event->seqnr;
+}
+
 
 BOOST_PYTHON_MODULE(ep_echo) {
     using namespace boost::python;
@@ -107,12 +133,37 @@ BOOST_PYTHON_MODULE(ep_echo) {
     scope().attr("ECHO") = ECHO_ECHO;
     scope().attr("CLOCK") = ECHO_CLOCK;
 
-    class_<PythonEvent>("PythonEvent", no_init)
+    class_<GenericEventWrapper>("GenericEvent")
+        .add_property("signal", &GenericEventWrapper::get_signal)
+    ;
+
+    class_<EchoPingEventWrapper, boost::noncopyable, bases<GenericEventWrapper> >("EchoPingEvent")
+        .add_property("ping_time", &EchoPingEventWrapper::get_pt, &EchoPingEventWrapper::set_pt)
+        .add_property("seqnr", &EchoPingEventWrapper::get_seqnr, &EchoPingEventWrapper::set_seqnr)
+    ;
+
+    class_<EchoEchoEventWrapper, bases<GenericEventWrapper> >("EchoEchoEvent", init<GenericEventWrapper&>())
+        .add_property("ping_time", &EchoEchoEventWrapper::get_pt)
+        .add_property("echo_time", &EchoEchoEventWrapper::get_et)
+        .add_property("seqnr", &EchoEchoEventWrapper::get_seqnr)
+    ;
+
+
+//    class_<EchoPingEvent, boost::noncopyable>("EchoPingEvent")
+//        .def_readonly("signal", &EchoPingEvent::signal)
+//        .def_readwrite("seqnr", &EchoPingEvent::seqnr)
+//        .def_readwrite("ping_time", &EchoPingEvent::ping_time)
+//    ;
+/*    class_<PythonEvent>("PythonEvent", no_init)
         .add_property("signal", &PingEvent::get_signal)
+    ;
+    class_<timeval>("timeval")
+        .def_readwrite("tv_sec", &timeval::tv_sec)
+        .def_readwrite("tv_usec", &timeval::tv_usec)
     ;
 
     class_<PingEvent, bases<PythonEvent> >("PingEvent")
-        .def("send", &PingEvent::send)
+//        .def("send", &PingEvent::send)
         .add_property("ping_time", &PingEvent::get_pt)
     ;
 
@@ -120,10 +171,12 @@ BOOST_PYTHON_MODULE(ep_echo) {
         .add_property("ping_time", &EchoEvent::get_pt)
         .add_property("echo_time", &EchoEvent::get_et)
     ;
+*/
 
     class_<EP_Interface>("EP_Interface", init<optional<std::string> >())
-        .def("time_echo", &EP_Interface::time_echo)
-        .def("send_ping", &EP_Interface::send_ping)
-//        .def("receive_event", &EP_Interface::receive_event)
+//        .def("time_echo", &EP_Interface::time_echo)
+//        .def("send_ping", &EP_Interface::send_ping)
+        .def("receive_event", &EP_Interface::receive_event)
+        .def("send_event", &EP_Interface::send_event)
     ;
 }
---
+++ src/pipeline/mac/ep_echo.h
@@ -0,0 +1,100 @@
+#ifndef EP_ECHO_H
+#define EP_ECHO_H
+
+#include <lofar_config.h>
+#include <Common/LofarLogger.h>
+#include <MACIO/EventPort.h>
+#include <MACIO/MACServiceInfo.h>
+#include "Echo_Protocol.ph"
+
+#include <boost/python.hpp>
+#include <string>
+#include <sys/time.h>
+#include <iostream>
+
+/*class PythonEvent {
+public:
+//    void send(EP_Interface&);
+    long get_signal();
+    LOFAR::MACIO::GCFEvent* evPtr;
+};
+
+class EchoEvent: public PythonEvent {
+public:
+    EchoEvent(LOFAR::MACIO::GCFEvent*);
+    double get_pt();
+    double get_et();
+};
+
+class PingEvent : public PythonEvent {
+public:
+    PingEvent();
+    double get_pt();
+    PingEvent(const PingEvent&) {};
+};*/
+
+class GenericEventWrapper {
+private:
+    LOFAR::MACIO::GCFEvent* my_event;
+public:
+    GenericEventWrapper() {
+        this->my_event = new LOFAR::MACIO::GCFEvent;
+    }
+    GenericEventWrapper(LOFAR::MACIO::GCFEvent* event_ptr) {
+        this->my_event = event_ptr;
+    }
+    virtual LOFAR::TYPES::uint16 get_signal() { return this->my_event->signal; }
+    virtual LOFAR::MACIO::GCFEvent* get_event_ptr() { return my_event;}
+};
+
+class EchoPingEventWrapper : public GenericEventWrapper {
+private:
+    EchoPingEvent* my_event;
+public:
+    EchoPingEventWrapper() {
+        this->my_event = new EchoPingEvent;
+    }
+    double get_pt();
+    void set_pt(double);
+    LOFAR::TYPES::uint16 get_seqnr();
+    void set_seqnr(LOFAR::TYPES::uint16);
+    virtual LOFAR::TYPES::uint16 get_signal() { return this->my_event->signal; }
+    virtual EchoPingEvent* get_event_ptr() { return this->my_event; }
+};
+
+class EchoEchoEventWrapper : public GenericEventWrapper {
+private:
+    EchoEchoEvent* my_event;
+public:
+    EchoEchoEventWrapper(LOFAR::MACIO::GCFEvent* my_event) {
+        this->my_event = new EchoEchoEvent(*my_event);
+    }
+    EchoEchoEventWrapper(GenericEventWrapper& my_gev) {
+        LOFAR::MACIO::GCFEvent* event_ptr;
+        event_ptr = my_gev.get_event_ptr();
+        this->my_event = new EchoEchoEvent(*event_ptr);
+    }
+    double get_pt();
+    double get_et();
+    LOFAR::TYPES::uint16 get_seqnr();
+    virtual LOFAR::TYPES::uint16 get_signal() { return this->my_event->signal; }
+    virtual EchoEchoEvent* get_event_ptr() { return this->my_event; }
+};
+        
+class EP_Interface {
+public:
+    LOFAR::MACIO::EventPort* echoPort;
+    EP_Interface(std::string);
+
+    GenericEventWrapper receive_event();
+//    void send_event(const PythonEvent&);
+    void send_event(GenericEventWrapper*);
+
+    void send_ping();
+    double time_echo();
+};
+
+
+
+
+#endif
