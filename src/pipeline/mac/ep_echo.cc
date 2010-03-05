a7aeae73f4eb82e52756376ebc1509db12c7e19c
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-05 16:52:54 +0000

    Split out separate send/receive event functions
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4296 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/mac/ep_echo.cc
+++ src/pipeline/mac/ep_echo.cc
@@ -7,14 +7,26 @@
 #include <boost/python.hpp>
 #include <string>
 
-using namespace LOFAR;
-using namespace LOFAR::MACIO;
-
 struct EP_Interface
 {
-    EventPort* echoPort;
+    LOFAR::MACIO::EventPort* echoPort;
     EP_Interface(std::string host = "") {
-        this->echoPort = new EventPort("EchoServer:test", false, ECHO_PROTOCOL, host, true); 
+        this->echoPort = new LOFAR::MACIO::EventPort("EchoServer:test", false, ECHO_PROTOCOL, host, true);
+    }
+    void send_ping()
+    {
+        EchoPingEvent pingEvent;
+        timeval pingTime;
+        gettimeofday(&pingTime, 0);
+        pingEvent.ping_time = pingTime;
+
+        this->echoPort->send(&pingEvent);
+    }
+    long receive_event()
+    {
+        LOFAR::MACIO::GCFEvent* ackPtr;
+        ackPtr = echoPort->receive();
+        return ackPtr->signal;
     }
     double time_echo()
     {
@@ -25,14 +37,16 @@ struct EP_Interface
 
         this->echoPort->send(&pingEvent);
 
-        GCFEvent* ackPtr;
+        LOFAR::MACIO::GCFEvent* ackPtr;
         ackPtr = echoPort->receive();
-        EchoEchoEvent ack(*ackPtr);
+        if (ackPtr->signal == ECHO_ECHO) {
+            EchoEchoEvent ack(*ackPtr);
 
-        double  delta =  (1.0 * ack.echo_time.tv_sec + (ack.echo_time.tv_usec / 1000000.0));
-                delta -= (1.0 * ack.ping_time.tv_sec + (ack.ping_time.tv_usec / 1000000.0));
+            double  delta =  (1.0 * ack.echo_time.tv_sec + (ack.echo_time.tv_usec / 1000000.0));
+                    delta -= (1.0 * ack.ping_time.tv_sec + (ack.ping_time.tv_usec / 1000000.0));
 
-        return delta;
+            return delta;
+        }
     }
 };
 
@@ -47,5 +61,7 @@ BOOST_PYTHON_MODULE(ep_echo)
 
     class_<EP_Interface>("EP_Interface", init<optional<std::string> >())
         .def("time_echo", &EP_Interface::time_echo)
+        .def("send_ping", &EP_Interface::send_ping)
+        .def("receive_event", &EP_Interface::receive_event)
     ;
 }
