2c64a0b434e86c84a9dde3d77e909c48c16892d0
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-09 13:51:02 +0000

    correct option
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4342 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/support/lofarrecipe.py
+++ src/pipeline/pipeline/support/lofarrecipe.py
@@ -33,7 +33,7 @@ class LOFARrecipe(WSRTrecipe):
             action="store_true"
         )
         self.optionparser.add_option(
-            '-st', '--start-time',
+            '--start-time',
             dest="start_time",
             help="[Expert use] Pipeline start time"
         )
