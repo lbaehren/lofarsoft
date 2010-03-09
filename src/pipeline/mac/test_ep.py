6f197bd258a3b73bb74a2e00b0598a8798b13acc
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-09 09:56:23 +0000

    test for controller protocol sendables
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4336 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/mac/test_ep.py
+++ src/pipeline/mac/test_ep.py
@@ -1,5 +1,6 @@
 import unittest
 import ep.echo
+import ep.control
 import time
 
 class TestPingEvent(unittest.TestCase):
@@ -33,6 +34,63 @@ class TestReceiveEcho(unittest.TestCase):
 
     def test_long_ping(self):
         self.assertTrue(self.eee.echo_time > self.eee.ping_time)
+
+class TestControllerSendables(unittest.TestCase):
+    def test_control_started(self):
+        event = ep.control.ControlStartedEvent("controller name", True)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertTrue(event.successful)
+        self.assertEqual(event.signal, ep.control.CONTROL_STARTED)
+
+    def test_control_connected(self):
+        event = ep.control.ControlConnectedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_CONNECTED)
+
+    def test_control_resynced(self):
+        event = ep.control.ControlResyncedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_RESYNCED)
+
+    def test_control_claimed(self):
+        event = ep.control.ControlClaimedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_CLAIMED)
+
+    def test_control_prepared(self):
+        event = ep.control.ControlPreparedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_PREPARED)
+
+    def test_control_resumed(self):
+        event = ep.control.ControlResumedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_RESUMED)
+
+    def test_control_suspended(self):
+        event = ep.control.ControlSuspendedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_SUSPENDED)
+
+    def test_control_released(self):
+        event = ep.control.ControlReleasedEvent("controller name", ep.control.OK)
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.signal, ep.control.CONTROL_RELEASED)
+
+    def test_control_quited(self):
+        event = ep.control.ControlQuitedEvent("controller name", 1, ep.control.OK, "no error")
+        self.assertEqual(event.controller_name, "controller name")
+        self.assertEqual(event.result, ep.control.OK)
+        self.assertEqual(event.treeID, 1)
+        self.assertEqual(event.error_message, "no error")
+        self.assertEqual(event.signal, ep.control.CONTROL_QUITED)
 
 if __name__ == '__main__':
     unittest.main()
