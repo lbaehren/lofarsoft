22d9b6df06bcf2d8ceba689de82277be47a64452
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-12 10:57:50 +0000

    added dummy shell script which can be used in a simple pipeline recipe;  takes a list of files and echo's them to stdout, else has an error message to STDERR
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4399 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/Pulsar/pipeline/dummy_echo.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This is a dummy shell script which accepts a list as input and 
+# echo's that input to STDOUT;  if a list of input is not given
+# an error message is echo'd to STDERR.
+
+echo "Starting script dummy_echo.sh at " `date`
+
+numfiles=0
+numfiles=$#
+
+# If there are no input arguments, then write an error message to STDERR (note, STDERR here!)
+if (( $numfiles < 1 )) 
+then
+   echo "Error: dummy_echo.sh requires a list of files as input (one file is the minumum set)" >&2 
+   exit 1
+fi
+
+filenames=$@
+
+## Turn on various echo statements depending on what you want to print to STDOUT
+echo "The number of files is: " $numfiles
+echo "The file names are: " $filenames
+echo "These same file name can be echo'd one by one to STDOUT:"
+for file in $filenames
+do
+   echo $file
+done
+
+echo "Ended script dummy_echo.sh successfully at " `date`
+
+exit 0
+
