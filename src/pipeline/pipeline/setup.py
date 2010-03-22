2b2c0db4d8cf3a3c6c9c2e2f746a093125015de5
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-22 18:15:06 +0000

    Install all modules
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4469 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/setup.py
+++ src/pipeline/pipeline/setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
     name="Pipeline",
     version="0.1.dev",
-    packages=['lofarpipe', 'lofarrecipe', 'qcheck'],
+    packages=[
+        'lofarpipe', 'lofarpipe.cuisine', 'lofarpipe.support',
+        'lofarrecipe', 'lofarrecipe.master', 'lofarrecipe.nodes',
+        'qcheck'
+    ],
     description="LOFAR Pipeline System",
     author="John Swinbank",
     author_email="swinbank@transientskp.org",
