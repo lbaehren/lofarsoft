447054e9b5c1754c676c0762316b3048b8c2c980
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-22 18:15:01 +0000

    clean up setup.py
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4467 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/setup.py
+++ src/pipeline/pipeline/setup.py
@@ -1,16 +1,11 @@
-#from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(
     name="Pipeline",
     version="0.1.dev",
-#    packages=find_packages(),
     packages=['lofarpipe', 'lofarrecipe', 'qcheck'],
-    scripts=['sip.py', 'mac-sip.py'],
-#    description="LOFAR Pipeline System",
-#    author="John Swinbank",
-#    author_email="swinbank@transientskp.org",
-#    url="http://www.transientskp.org/",
-#    package_dir={'cuisine': 'framework/cuisine'},
-#    packages=['cuisine', 'pipeline', 'pipeline.nodes', 'pipeline.master', 'pipeline.support'],
+    description="LOFAR Pipeline System",
+    author="John Swinbank",
+    author_email="swinbank@transientskp.org",
+    url="http://www.transientskp.org/",
 )
