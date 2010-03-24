21a034d494fab37e4f576e36dc15bd53a054bf50
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-24 16:28:30 +0000

    Example recipe
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4515 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/pipeline/pipeline/master/dummy_echo.py
@@ -0,0 +1,30 @@
+import subprocess
+from pipeline.support.lofarrecipe import LOFARrecipe
+
+class dummy_echo(LOFARrecipe):
+    def __init__(self):
+        super(dummy_echo, self).__init__()
+        self.optionparser.add_option(
+            '--executable',
+            dest="executable",
+            help="Executable to be run (ie, dummy_echo.sh)",
+            default="/home/swinbank/sw/bin/dummy_echo.sh"
+        )
+
+    def go(self):
+        self.logger.info("Starting dummy_echo run")
+        super(dummy_echo, self).go()
+
+        execute = [self.inputs['executable']]
+        execute.extend(self.inputs['args'])
+
+        my_process = subprocess.Popen(execute, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sout, serr = my_process.communicate()
+        self.logger.info("stdout: " + sout)
+        self.logger.info("stderr: " + serr)
+
+        return 0
+
+
+if __name__ == '__main__':
+    sys.exit(dummy_echo().main())
