55fcc46978f61718dbfcb019b46a4f2528b0ddb1
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-03-25 16:36:25 +0000

    Extended command line parameters.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4528 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/pycrinit.py
+++ src/CR-Tools/implement/Pypeline/pycrinit.py
@@ -1,11 +1,21 @@
 #A file to load the pycrtools run it with
-#python -i $LOFARSOFT/src/CR-Tools/implement/Pypeline/pycrinit.py 
+#python -i $LOFARSOFT/src/CR-Tools/implement/Pypeline/pycrinit.py
 #or with
 #python -i $LOFARSOFT/src/CR-Tools/implement/Pypeline/pycrinit.py -i myfile.py
 
 
 from pycrtools import *
+from getopt import *
 
-#execute yet another script if added in the parameter list
-if len(sys.argv)>=3:
-    if sys.argv[-2] == "-i": execfile(sys.argv[-1])
+try:
+    opts, args = getopt(sys.argv[1:],"hi:", ["help","include="])
+except GetoptError:
+    sys.exit(2)
+for opt, arg in opts:
+    # Display Help info
+    if opt in ("-h", "--help"):
+        help()
+    # Execute yet another script if added in the parameter list
+    elif opt in ("-i", "--include"):
+        execfile(arg)
+
