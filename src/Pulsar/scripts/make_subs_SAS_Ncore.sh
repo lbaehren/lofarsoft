f9e909c97544d684de5db37fd495d173d722cb3e
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-04-01 08:17:55 +0000

    typo in the usage instructions
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4594 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/make_subs_SAS_Ncore.sh
+++ src/Pulsar/scripts/make_subs_SAS_Ncore.sh
@@ -16,7 +16,7 @@ USAGE="\nusage : make_subs_SAS_Ncore.sh -id OBS_ID -p Pulsar_name -o Output_Proc
 "      [-rfi] ==> optional parameter perform Vlas's RFI checker and only use clean results (takes 7 extra min)\n"\
 "      [-C | -c] ==> optional parameter to switch on COLLAPSE\n"\
 "      [-del] ==> optional parameter to delete the previous Output_Processing_Location if it exists (override previous results!)\n"\
-"      [-core N] ==> optional parameter to change the number of cores (slipts) used for processing (default = 8)\n"
+"      [-core N] ==> optional parameter to change the number of cores (splits) used for processing (default = 8)\n"
 
 if [ $# -lt 6 ]                    # this script needs at least 6 args, including -switches
 then
