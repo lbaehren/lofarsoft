c1f4c027cdcf1102bfab0d33d8b55eec964a36f0
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-04-13 15:06:40 +0000

    CTest configuration file for Experimental and Nightly builds committed to the Dashboard.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4642 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/Pulsar/CTestConfig.cmake
@@ -0,0 +1,14 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "DAL")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "svn.astron.nl")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Pulsar-Tools")
+set(CTEST_DROP_SITE_CDASH TRUE)
