5261227da903b17a49c6ce57d5277f4d911e955f
horneffer <horneffer@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-10 18:02:32 +0000

    changed hardcoded path to the caltables to the "crtools.h" value
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4844 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Analysis/test/tSecondStagePipeline.cc
+++ src/CR-Tools/implement/Analysis/test/tSecondStagePipeline.cc
@@ -66,7 +66,7 @@ int test_SecondStagePipeline ()
     // Generate a record that points to the CalTable files 
     // Hardcoded path is not nice, but works...
     Record obsrec;
-    obsrec.define("LOPES","/home/horneff/usg/data/lopes/LOPES-CalTable");
+    obsrec.define("LOPES",caltable_lopes);
 
     // Put the record into the pipeline object.
     newObject.SetObsRecord(obsrec);
