00e527c7cf0d0034275fb7e5bccbdcb5ecd3ed0a
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-13 17:58:37 +0000

    Replaced package template.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4890 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/FindPython.cmake
+++ devel_common/cmake/FindPython.cmake
@@ -20,7 +20,7 @@
 # |   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 |
 # +-----------------------------------------------------------------------------+
 
-# - Check for the presence of <PACKAGE>
+# - Check for the presence of Python
 #
 # The following variables are set when PYTHON is found:
 #
