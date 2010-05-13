5c68a22b898797029192b830a9f7fc2c75e3be0f
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-13 17:58:30 +0000

    Generalized already introduced methods for detecting the locally used version control system to manage the working copy of the code base. As all version control systems utilize a configuration folder, search for it can be generalized, as is possible for the executable. Furthermore the already existing target _update_ now has been split into _make update_ and _make upgrade_, where the first one will only update the working copy and the second will update the CMake configuration as well.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4885 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/FindVersionControl.cmake
+++ devel_common/cmake/FindVersionControl.cmake
@@ -27,15 +27,18 @@
 # PARAMETERS:
 #  - VERSION_CONTROL_FIND_QUIETLY [in]
 #       Be quiet during search; of set "TRUE" search will be run in verbose mode.
+#  - VERSION_CONTROL_SYSTEM [out]
+#       Name of the version control system used for the local working copy.
+#       Returned value will be "bzr", "git" or "svn".
+#  - BZR_EXECUTABLE [out]
+#       Path to the bzr executable.
 #  - GIT_EXECUTABLE [out]
 #       Path to the git executable.
 #  - SVN_EXECUTABLE [out]
 #       Path to the SVN executable.
-#  - VERSION_CONTROL_GIT    [out]
-#  - VERSION_CONTROL_SVN    [out]
-#  - VERSION_CONTROL_SYSTEM [out]
-#       Name of the version control system used for the local working copy.
-#       Returned value will be "git" or "svn".
+#  - BZR_CONFIG_DIR [out]
+#  - GIT_CONFIG_DIR [out]
+#  - SVN_CONFIG_DIR [out]
 #
 
 ##_______________________________________________________________________________
@@ -44,42 +47,96 @@
 include (CMakeSettings)
 
 ##_______________________________________________________________________________
-##  Check for git
+##  Generic search to determine version control system 
 
-find_program (GIT_EXECUTABLE git PATHS ${bin_locations})
+foreach (_name bzr git svn)
 
-find_path (VERSION_CONTROL_DIR_GIT description hooks index
-  PATH .git
-  )
+  string (TOUPPER ${_name} _nameUpper)
 
-##_______________________________________________________________________________
-##  Check for Subversion
+  message (STATUS "Checking for version control system ${_name}")
 
-find_program (SVN_EXECUTABLE svn PATHS ${bin_locations})
+  ## Check for executable
+  find_program (${_nameUpper}_EXECUTABLE ${_name} PATHS ${bin_locations})
 
-find_path (VERSION_CONTROL_DIR_SVN entries
-  PATH .svn
-  )
+  ## Check for configuration directory
+  if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.${_name})
+    set (${_nameUpper}_CONFIG_DIR ${CMAKE_CURRENT_SOURCE_DIR}/.${_name}) 
+  endif (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.${_name})
+  
+  ## Feedback
+  if (${_nameUpper}_EXECUTABLE AND ${_nameUpper}_CONFIG_DIR)
+    message (STATUS "Checking for version control system ${_name} - found.")
+  else (${_nameUpper}_EXECUTABLE AND ${_nameUpper}_CONFIG_DIR)
+    message (STATUS "Checking for version control system ${_name} - not found.")
+  endif (${_nameUpper}_EXECUTABLE AND ${_nameUpper}_CONFIG_DIR)
+  
+endforeach (_name)
 
 ##_______________________________________________________________________________
 ##  Set up VERSION_CONTROL_SYSTEM variable
 
 set (VERSION_CONTROL_SYSTEM "")
 
-if (GIT_EXECUTABLE AND VERSION_CONTROL_DIR_GIT)
-  set (VERSION_CONTROL_SYSTEM "git")
-else (GIT_EXECUTABLE AND VERSION_CONTROL_DIR_GIT)
-  if (SVN_EXECUTABLE AND VERSION_CONTROL_DIR_SVN)
-    set (VERSION_CONTROL_SYSTEM "svn")
-  endif (SVN_EXECUTABLE AND VERSION_CONTROL_DIR_SVN)
-endif (GIT_EXECUTABLE AND VERSION_CONTROL_DIR_GIT)
+if (BZR_EXECUTABLE AND BZR_CONFIG_DIR) 
+  set (VERSION_CONTROL_SYSTEM "bzr")
+endif (BZR_EXECUTABLE AND BZR_CONFIG_DIR) 
 
+if (GIT_EXECUTABLE AND GIT_CONFIG_DIR)
+  set (VERSION_CONTROL_SYSTEM "git")
+else (GIT_EXECUTABLE AND GIT_CONFIG_DIR)
+  if (SVN_EXECUTABLE AND SVN_CONFIG_DIR)
+    set (VERSION_CONTROL_SYSTEM "svn")
+  endif (SVN_EXECUTABLE AND SVN_CONFIG_DIR)
+endif (GIT_EXECUTABLE AND GIT_CONFIG_DIR)
+
+##_______________________________________________________________________________
+##  Define target "make update"
+
+if (VERSION_CONTROL_SYSTEM STREQUAL "bzr")
+  add_custom_target (update
+    COMMAND ${BZR_EXECUTABLE} update
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT "Updating working version of the source code from repository ..."
+    )
+endif (VERSION_CONTROL_SYSTEM STREQUAL "bzr")
+
+if (VERSION_CONTROL_SYSTEM STREQUAL "git")
+  add_custom_target (update
+    COMMAND ${GIT_EXECUTABLE} stash
+    COMMAND ${GIT_EXECUTABLE} svn rebase
+    COMMAND ${GIT_EXECUTABLE} stash apply
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT "Updating working version of the source code from repository ..."
+    )
+endif (VERSION_CONTROL_SYSTEM STREQUAL "git")
+
+if (VERSION_CONTROL_SYSTEM STREQUAL "svn")
+  add_custom_target (update
+    COMMAND ${SVN_EXECUTABLE} cleanup
+    COMMAND ${SVN_EXECUTABLE} up
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT "Updating working version of the source code from repository ..."
+    )
+endif (VERSION_CONTROL_SYSTEM STREQUAL "svn")
+
+##_______________________________________________________________________________
+##  Define target "make upgrade"
+
+add_custom_target (upgrade
+  COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR}/build && rm -rf CMake* && ./bootstrap
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+  COMMENT "Updating working version of the source code from repository ..."
+  )
+
+add_dependencies (upgrade update)
+
+##_______________________________________________________________________________
 ##  Feedback
 
 if (NOT VERSION_CONTROL_FIND_QUIETLY)
   message (STATUS "VERSION_CONTROL_SYSTEM  = ${VERSION_CONTROL_SYSTEM}")
   message (STATUS "GIT_EXECUTABLE          = ${GIT_EXECUTABLE}")
-  message (STATUS "VERSION_CONTROL_DIR_GIT = ${VERSION_CONTROL_DIR_GIT}")
+  message (STATUS "GIT_CONFIG_DIR          = ${GIT_CONFIG_DIR}")
   message (STATUS "SVN_EXECUTABLE          = ${SVN_EXECUTABLE}")
-  message (STATUS "VERSION_CONTROL_DIR_SVN = ${VERSION_CONTROL_DIR_SVN}")
+  message (STATUS "SVN_CONFIG_DIR          = ${SVN_CONFIG_DIR}")
 endif (NOT VERSION_CONTROL_FIND_QUIETLY)
