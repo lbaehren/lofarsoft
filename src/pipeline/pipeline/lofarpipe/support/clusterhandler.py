bf294aa0a01fbe7deedc86748fe0828e9c764da3
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-17 14:54:38 +0000

    Simple class for managing an IPython cluster with SSH.
    Is this the beginning of the end for fabric?
    
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4908 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/pipeline/pipeline/lofarpipe/support/clusterhandler.py
@@ -0,0 +1,53 @@
+from lofarpipe.support.clusterdesc import ClusterDesc
+from lofarpipe.support.clusterdesc import get_compute_nodes, get_head_node
+
+class ClusterHandler(object):
+    def __init__(self, config):
+        clusterdesc = ClusterDesc(config.get('cluster', 'clusterdesc'))
+        self.head_node = get_head_node(clusterdesc)[0]
+        self.compute_nodes = get_compute_nodes(clusterdesc)
+        self.script_path = config.get('deploy', 'script_path')
+        self.config = config
+
+    def start_cluster(self):
+        self.__start_controller()
+        self.__start_engines()
+
+    def stop_cluster(self):
+        self.__stop_controller()
+        self.__stop_engines()
+
+    def __execute_ssh(self, host, command):
+        print "  *", host
+        ssh_cmd = shlex.split("ssh %s -- %s" % (host, command))
+        subprocess.check_call(ssh_cmd)
+
+    def __start_controller(self):
+        print "Starting controller:"
+        controlpath = self.config.get('DEFAULT', 'runtime_directory')
+        controller_ppath = self.config.get('deploy', 'controller_ppath')
+        self.__execute_ssh(self.head_node, "bash %s/ipcontroller.sh %s start %s" % (self.script_path, controlpath, controller_ppath))
+        print "done."
+
+    def __stop_controller(self):
+        print "Stopping controller:"
+        controlpath = self.config.get('DEFAULT', 'runtime_directory')
+        controller_ppath = self.config.get('deploy', 'controller_ppath')
+        self.__execute_ssh(self.head_node, "bash %s/ipcontroller.sh %s stop %s" % (self.script_path, controlpath, controller_ppath))
+        print "done."
+
+    def __start_engines(self):
+        print "Starting engines:"
+        controlpath = self.config.get('DEFAULT', 'runtime_directory')
+        engine_ppath = self.config.get('deploy', 'engine_ppath')
+        engine_lpath = self.config.get('deploy', 'engine_lpath')
+        for node in self.compute_nodes:
+            self.__execute_ssh(node, "bash %s/ipengine.sh %s start %s %s" % (self.script_path, controlpath, engine_ppath, engine_lpath))
+        print "done."
+
+    def __stop_engines(self):
+        print "Stopping engines:"
+        controlpath = self.config.get('DEFAULT', 'runtime_directory')
+        for node in self.compute_nodes:
+            self.__execute_ssh(node, "bash %s/ipengine.sh %s stop" % (self.script_path, controlpath))
+        print "done."
