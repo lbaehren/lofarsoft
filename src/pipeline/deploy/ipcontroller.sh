8e63c5fbfe247c4b836c8633b71c74b436b167a0
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-19 14:14:56 +0000

    Tweaks to scripts for starting & stopping ipython
    - don't start if pid file already exists
    - support specifying number of engines per node
    
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4924 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/deploy/ipcontroller.sh
+++ src/pipeline/deploy/ipcontroller.sh
@@ -3,17 +3,24 @@ export PYTHONPATH=$3
 
 mkdir -p $CONTROLPATH
 
+PIDFILE=$CONTROLPATH/ipc.pid
+
 case "$2" in
   start) 
-         /sbin/start-stop-daemon --start -b -m --pidfile $CONTROLPATH/ipc.pid \
-           --exec /opt/pipeline/dependencies/bin/ipcontroller -- -xy \
-           --engine-furl-file=$CONTROLPATH/engine.furl \
-           --task-furl-file=$CONTROLPATH/task.furl \
-           --multiengine-furl-file=$CONTROLPATH/multiengine.furl \
-           --logfile=$CONTROLPATH/ipc.log
+         if [ ! -f $PIDFILE ]; then
+             /sbin/start-stop-daemon --start -b -m --pidfile $PIDFILE \
+               --exec /opt/pipeline/dependencies/bin/ipcontroller -- -xy \
+               --engine-furl-file=$CONTROLPATH/engine.furl \
+               --task-furl-file=$CONTROLPATH/task.furl \
+               --multiengine-furl-file=$CONTROLPATH/multiengine.furl \
+               --logfile=$CONTROLPATH/ipc.log
+         else
+            echo "ipcontroller already running on `hostname`"
+         fi
          ;;
   stop)
-         /sbin/start-stop-daemon --stop --pidfile $CONTROLPATH/ipc.pid
+         /sbin/start-stop-daemon --stop --pidfile $PIDFILE
+         rm $PIDFILE
          ;;
   *)
          echo "Usage: $0 <controlpath> {start|stop}" >&2
--- src/pipeline/deploy/ipengine.sh
+++ src/pipeline/deploy/ipengine.sh
@@ -1,26 +1,39 @@
 CONTROLPATH=$1
 export PYTHONPATH=$3
 export LD_LIBRARY_PATH=$4
+NPROC=$5
 PIDPATH=$CONTROLPATH/engines/`hostname`
 
 mkdir -p $PIDPATH
 
 case "$2" in
   start) 
-         NCPU=`cat /proc/cpuinfo | grep ^processor | wc -l`
-         for CPU in `seq 1 $NCPU`
+         if [ $NPROC ] && [ $NPROC -eq $NPROC ]; then
+            NPROC=$NPROC
+         else
+            NPROC=`cat /proc/cpuinfo | grep ^processor | wc -l`
+         fi
+         for PROC in `seq 1 $NPROC`
             do
-                 /sbin/start-stop-daemon --start -b -m             \
-                   --pidfile $PIDPATH/ipengine$CPU.pid             \
-                   --exec /opt/pipeline/dependencies/bin/ipengine -- \
-                   --furl-file=$CONTROLPATH/engine.furl            \
-                   --logfile=$PIDPATH/log
+                 PIDFILE=$PIDPATH/ipengine$PROC.pid
+                 if [ ! -f $PIDFILE ]; then
+                     /sbin/start-stop-daemon --start -b -m               \
+                       --pidfile $PIDFILE                                \
+                       --exec /opt/pipeline/dependencies/bin/ipengine -- \
+                       --furl-file=$CONTROLPATH/engine.furl              \
+                       --logfile=$PIDPATH/log
+                     sleep 5
+                     ps -p `cat $PIDFILE` > /dev/null || echo "ipengine failed to start on `hostname`"
+                else
+                    echo "ipengine already running on `hostname`"
+                fi
             done
          ;;
   stop)
          for PIDFILE in $PIDPATH/ipengine*.pid
              do
-                 /sbin/start-stop-daemon --stop --pidfile $PIDFILE
+                 /sbin/start-stop-daemon --stop --pidfile $PIDFILE --oknodo
+                 rm $PIDFILE
              done
          ;;
   *)
