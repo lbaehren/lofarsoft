7ff20dfc9fc90ebb4dbdacd90475340f5b5444a8
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-05-28 09:16:45 +0000

    And added a description of the parameters.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@4972 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
+++ src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Parameters are:
+# - station name (e.g. RS205)
+# - date string (e.g. 2010-05-28)
+# - user name to log into portal.lofar.eu
+
 echo Starting live feed from $1 using file dated $2. User name is $3
 ssh $3@portal.lofar.eu "ssh lfe001 \"ssh $1\C tail -fn 10000 /opt/lofar/log/$2_TRIGGER.dat\"" | VHECRtest
 
