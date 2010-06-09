83538f42770849d1d8e34ad85175aeda1629f17c
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-09 12:30:02 +0000

    Feed script for event display: updated parameter passing, changed path.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5086 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
+++ src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
@@ -6,5 +6,12 @@
 # - user name to log into portal.lofar.eu
 
 echo Starting live feed from $1 using file dated $2. User name is $3
-ssh $3@portal.lofar.eu "ssh lfe001 \"ssh $1\C tail -fn 10000 /opt/lofar/log/$2_TRIGGER.dat\"" | VHECRtest
+echo VHECRtest parameters are:
+echo antenna selection $4
+echo nof channels $5
+echo fit type $6
+echo antenna positions file $7
+
+ssh $3@portal.lofar.eu "ssh lfe001 \"ssh $1\C tail -fn 10000 /opt/lofar/log/$2_TRIGGER.dat\"" | VHECRtest -S $4 -C $5 -D $6 -P $LOFARSOFT/usg/data/calibration/AntennaArrays/$1\-AntennaArrays.conf
+
 
