eb5b29082185be4c38beba59ea86cfc821d53ca9
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-09 12:25:22 +0000

    feedToVHECRtest.sh: kicked hardcoded path.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5085 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/scripts/led/test/feedToVHECRtest.sh
+++ src/CR-Tools/scripts/led/test/feedToVHECRtest.sh
@@ -7,5 +7,5 @@ echo nof channels $3
 echo fit type $4
 echo station name $5
 echo antenna pos file $5\-AntennaArrays.conf
-./test/feedfile.py $1 | VHECRtest -S $2 -C $3 -D $4 -P /Users/acorstanje/usg/data/calibration/AntennaPos/$5\-AntennaArrays.conf
+./test/feedfile.py $1 | VHECRtest -S $2 -C $3 -D $4 -P $LOFARSOFT/usg/data/calibration/AntennaPos/$5\-AntennaArrays.conf
 
