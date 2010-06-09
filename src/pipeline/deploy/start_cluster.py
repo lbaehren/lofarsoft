0d8323e4378b70ecb74b9f0f2f969050fbfebf8d
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-09 14:16:28 +0000

    Add proper logging, option parsing
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5089 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/deploy/start_cluster.py
+++ src/pipeline/deploy/start_cluster.py
@@ -1,13 +1,36 @@
-import sys
+#!/usr/bin/python
+
+"""
+Start IPython cluster.
+"""
+
+import sys, logging, os
+from optparse import OptionParser
 from ConfigParser import SafeConfigParser as ConfigParser
 from lofarpipe.support.clusterhandler import ClusterHandler
 
-config_file = sys.argv[1]
-num_engines = sys.argv[2]
+parser = OptionParser()
+parser.add_option(
+    "--config", help="Pipeline configuration file", default="~/.pipeline.cfg"
+)
+parser.add_option(
+    "--num_engines", help="Number of engines per node", default=8
+)
+options, args = parser.parse_args()
+
+my_logger = logging.getLogger()
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)-7s: %(message)s",
+    "%Y-%m-%d %H:%M:%S"
+)
+stream_handler.setFormatter(formatter)
+my_logger.addHandler(stream_handler)
+my_logger.setLevel(logging.DEBUG)
 
 config = ConfigParser()
-config.read(config_file)
+config.read(os.path.expanduser(options.config))
 
 clusterhandler = ClusterHandler(config)
-clusterhandler.start_cluster(num_engines)
+clusterhandler.start_cluster(options.num_engines)
 
--- src/pipeline/deploy/stop_cluster.py
+++ src/pipeline/deploy/stop_cluster.py
@@ -1,11 +1,32 @@
-import sys
+#!/usr/bin/python
+
+"""
+Stop IPython cluster.
+"""
+
+import sys, logging, os
+from optparse import OptionParser
 from ConfigParser import SafeConfigParser as ConfigParser
 from lofarpipe.support.clusterhandler import ClusterHandler
 
-config_file = sys.argv[1]
+parser = OptionParser()
+parser.add_option(
+    "--config", help="Pipeline configuration file", default="~/.pipeline.cfg"
+)
+options, args = parser.parse_args()
+
+my_logger = logging.getLogger()
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)-7s: %(message)s",
+    "%Y-%m-%d %H:%M:%S"
+)
+stream_handler.setFormatter(formatter)
+my_logger.addHandler(stream_handler)
+my_logger.setLevel(logging.DEBUG)
 
 config = ConfigParser()
-config.read(config_file)
+config.read(os.path.expanduser(options.config))
 
 clusterhandler = ClusterHandler(config)
 clusterhandler.stop_cluster()
