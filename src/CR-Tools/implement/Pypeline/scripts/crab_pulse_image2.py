16883fb92db812fee427e7aa5564cc09bc8d91d6
terveen <terveen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-17 11:41:45 +0000

    Updated version
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5116 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/scripts/crab_pulse_image2.py
+++ src/CR-Tools/implement/Pypeline/scripts/crab_pulse_image2.py
@@ -12,50 +12,78 @@ import pyfits
 # Data should show the Crab Nebula and a giant pulse in limited unknown bandwidth
 # The position of the Crab Nebula should be (pulse3) Az,El 206.229,57.0392
 #                                           (pulse1) Az,El 183.831, 59.0699
+#					    (pulse5) Az,El 224.908, 52.6475
+# Data can be obtained from: 
+# mkdir $LOFAR_DATA_DIR/Crab
+# scp -r <username>@helios:/mnt/lofar/CS1_tbb/CrabFirstTrigger/pulse1 $LOFAR_DATA_DIR/Crab/
+# scp -r <username>@helios:/mnt/lofar/CS1_tbb/CrabFirstTrigger/pulse3 $LOFAR_DATA_DIR/Crab/
+# scp -r <username>@helios:/mnt/lofar/CS1_tbb/CrabFirstTrigger/pulse5 $LOFAR_DATA_DIR/Crab/
 
 # Get environment variables
 
+# Files will be saved in your data directory with the name:
+# We may want to change this to an automatic name.
+SummedFitsFilename="imageCrabInt_100blocks.fits"
+SeparatedFitsFilename="imageCrab_100blocks.fits"
+
+# Get data files
 LOFARSOFT = os.environ["LOFARSOFT"].rstrip('/')+'/'
 LOFAR_DATA_DIR = os.environ['LOFAR_DATA_DIR'].rstrip('/')+'/'
-data_directory3 = LOFAR_DATA_DIR+"Crab/pulse3/"
 data_directory1 = LOFAR_DATA_DIR+"Crab/pulse1/"
-stationname="CS302"
-filenames3=["CS302C-B0T6:01:48.h5","CS302C-B1T6:01:48.h5","CS302C-B2T6:01:48.h5","CS302C-B3T6:01:48.h5","CS302C-B4T6:01:48.h5","CS302C-B5T6:01:48.h5"]
+data_directory3 = LOFAR_DATA_DIR+"Crab/pulse3/"
+data_directory5 = LOFAR_DATA_DIR+"Crab/pulse5/"
 filenames1=["CS302C-B0T5:00:39.h5","CS302C-B1T5:00:39.h5","CS302C-B2T5:00:39.h5","CS302C-B3T5:00:39.h5","CS302C-B4T5:00:39.h5","CS302C-B5T5:00:39.h5"]
-filenames1_ear1=["CS302C-B0T5:00:39.h5","CS302C-B1T5:00:39.h5","CS302C-B2T5:00:39.h5"]
-data_directory=data_directory1
-filenames=filenames1
+filenames3=["CS302C-B0T6:01:48.h5","CS302C-B1T6:01:48.h5","CS302C-B2T6:01:48.h5","CS302C-B3T6:01:48.h5","CS302C-B4T6:01:48.h5","CS302C-B5T6:01:48.h5"]
+filenames5=['CS302C-B0T6:50:02.h5', 'CS302C-B1T6:50:02.h5', 'CS302C-B2T6:50:02.h5', 'CS302C-B3T6:50:02.h5', 'CS302C-B4T6:50:02.h5', 'CS302C-B5T6:50:02.h5']
+filenames1_ear2=["CS302C-B3T5:00:39.h5","CS302C-B4T5:00:39.h5","CS302C-B5T5:00:39.h5"]
+data_directory=data_directory5
+filenames=filenames5
 #TBBboards=[0,1,2,3,4,5]
-nrfiles=3
-antennaset="HBA"
+nrfiles=6
+
+
+# Set calibration variables
+stationname="CS302" #This value should also be able to be obtained from the files, but then we have to open one first.
+antennaset="HBA" #Some files don't have the Antennaset set correctly in the hdf5 files yet.
 antennafilename=LOFARSOFT+"data/calibration/AntennaArrays/"+stationname+"-AntennaArrays.conf"
-blocksize=2**10
-nblocks=300
-startblock=5155
-NyquistZone=2
+NyquistZone=2    #Some files don't have the Nyquistzone set correctly in the hdf5 files yet
 caltablefilename=LOFARSOFT+"src/CR-Tools/data/calibrationCS302.dat" #(Not avaiable for LBA)
+
+# Set what part of the data you want to make an image from
+blocksize=2**10
+startblock=5155
+nblocks=100
 #selected_frequencies=range(268,292,1)
 #selected_frequencies=range(357,358,1)
 selected_frequencies=[80,110,155,230]
 FarField=True
 Xpol=True
-Ypol=False
+Ypol=True
+
+
+# Set what part of the sky you want to make an image from
+# This should be replaced by the new grid code
 azrange=(0.,365.,5)
 elrange=(0.,95.,5)
 #Cas A 342 24
-#if filenames==filenames1:
-#    azrange=(110.,262.5,2.5)
-#    elrange=(50.,76.5,1.5)
-#if filenames==filenames3:
-#    azrange=(205,232.5,2.5)
-#    elrange=(45,77.5,2.5)
-azrange=(183.831-30,183.831+30,3)
-elrange=(59.0699-30,59.0699+30,3)
+if filenames==filenames1:
+    azrange=(183.831-30,183.831+30,3)
+    elrange=(59.0699-30,59.0699+30,3)
+if filenames==filenames3:
+    azrange=(206.229-30,206.229+33,3)
+    elrange=(57.0392-30,57.0392+33,3)
+if filenames==filenames5:
+    azrange=(224.908-30,224.908+33,3)
+    elrange=(52.6475-30,52.6475+33,3)
 
-#muting channel 0:100 and 270:513 in the code now. Should make these variables
+
 # (end) configuration
 
 
+
+# Obtain the antenna positions from a calibration file
+# This should be changed to obtain the positions from the hdf5
+# This is limited to a single station set in the configuration variables
 antfile = open(antennafilename,'r') 
 antfile.seek(0)
 str = ''
@@ -76,6 +104,9 @@ for i in range(0,int(nrantennas)):
 antenna_positions=hArray(ant,[2*int(nrantennas),int(nrdir)])
 
 # Get gain calibration coefficients
+# At the moment only variables for CS302 HBA avaiable.
+# Maybe can also add them for RS503 
+# Add a check if the file correct file is avaiable?
 
 # These where obtained by correlation on the source for a dataset from an earlier time
 # De gegeven getallen zijn p0 en p1, waarbij fase =  p0+p1*freq. Om p1 om te zetten in delays moet je het nog delen door (2*pi)
@@ -121,11 +152,8 @@ for i in range(0,nrfiles):
     files[i]["blocksize"]=blocksize
     shifts.append(files[i]["SAMPLE_NUMBER"])
     files[i]["NyquistZone"]=NyquistZone
-    
-#totAntennas=file["nofSelectedAntennas"]
-#AntennasInFile=file["nofSelectedAntennas"]
 
-#search for maximum in shifts
+# search for maximum in shifts
 shiftmax=0
 for i in range(0,nrfiles):
     localmax=shifts[i].max()
@@ -133,7 +161,7 @@ for i in range(0,nrfiles):
         shiftmax=localmax
     
 
-#multiple number uses SAMPLE_NUMBER and TIME
+# multiple number uses SAMPLE_NUMBER and TIME
 for i in range(0,nrfiles):
     shifts2=[]
     for j in range(0,AntennasInFile[i]):
@@ -161,12 +189,14 @@ print "Antenna positions: ",used_antenna_pos
 # .... image part
 
 
+
+# Calculate number of pixels
 n_az=int((azrange[1]-azrange[0])/azrange[2])
 n_el=int((elrange[1]-elrange[0])/elrange[2])
 n_pixels=n_az*n_el
 
 
-
+# Initialize arrays that are used.
 file_time=files[0]["Time"].setUnit("\\mu","s")
 file_frequencies=files[0]["Frequency"][selected_frequencies]
 file_allfrequencies=files[0]["Frequency"]
@@ -181,8 +211,9 @@ phasecorr=hArray(float,[totAntennas,files[0]["fftLength"]])
 phasecorr.copy(file_frequencies)
 hanning=hArray(float,files[0]["blocksize"])
 hanning.gethanningfilter()
-#hanning.fill(1)
 
+
+#Calculate calibration phase corrections
 for i in range(totAntennas):
     phasecorr[i].mul(float(caltable[rcuIDs[i].val(),1].val()))
     phasecorr[i].add(float(caltable[rcuIDs[i].val(),0].val()))
@@ -190,9 +221,8 @@ for i in range(totAntennas):
 
 phasecorrweights=hArray(complex,[totAntennas,files[0]["fftLength"]])
 phasecorrweights.phasetocomplex(phasecorr)
-#THESE ARE NOT APPLIED YET
 
-
+# Initialize geographical delay arrays
 delays=hArray(float,dimensions=[n_pixels,totAntennas])
 
 print "t=",time.clock(),"s -","Initializing results arrays"
@@ -203,8 +233,13 @@ shifted_fft=hArray(complex,dimensions=weights,name="FFT times Weights")
 beamformed_fft=hArray(complex,dimensions=[n_pixels,len(file_frequencies)],name="Beamformed FFT")
 power=hArray(float,dimensions=beamformed_fft,name="Spectral Power",xvalues=file_frequencies,fill=0.0)
 
-#phase_center=hArray(antenna_positions[0].vec())
-#antenna_positions -= phase_center
+
+
+# Calculate grid.
+# Maybe should have a common phase_center to substract from the antennas is different files are processed
+# in parallel or if a non-local coordinate system is used.
+# phase_center=hArray(antenna_positions[0].vec())
+# antenna_positions -= phase_center
 
 azel=hArray(float,dimensions=[n_pixels,3])
 cartesian=azel.new()
@@ -226,7 +261,6 @@ hGeometricDelays(delays,used_antenna_pos,cartesian,FarField)
 phases.delaytophase(file_frequencies,delays)
 weights.phasetocomplex(phases)
 
-# At the moment we don't know how much data there is for each file.
 
 
 #shut down history tracking for the arrays as it's a big "memory leak"
@@ -242,8 +276,8 @@ power.setHistory(False)
 selected_fft.setHistory(False)
 
 
+# Process blocks of data
 t0=time.clock()
-#file["block"]=startblock
 for block in range(startblock,startblock+nblocks):
     antennastart=0
     antennaend=0
@@ -256,14 +290,12 @@ for block in range(startblock,startblock+nblocks):
         files[i]["block"]=block
         file_efieldtemp[i].read(files[i],"Fx").none()
         file_efield[antennastart:antennaend].copy(file_efieldtemp[i])
-    #file_efield[...].plot()
     print "t=",time.clock()-t0,"s -","Applying hanning filter",block
     file_efield*=hanning
     print "t=",time.clock()-t0,"s -","Performing fft",block
     file_fft[...].fftcasa(file_efield[...],NyquistZone)
     file_fft.mul(phasecorrweights)
     print "t=",time.clock()-t0,"s -","Copying to grid",block
-    #file_fft[...].plot()
     #copies the fft to all positions in shifted_fft
     for i in range(totAntennas):
         selected_fft[i].copy(file_fft[i,selected_frequencies])
@@ -272,6 +304,8 @@ for block in range(startblock,startblock+nblocks):
     print "t=",time.clock()-t0,"s -","Applying weights ",block
     shifted_fft[...].mul(weights[...])
     print "t=",time.clock()-t0,"s -","Beamforming ",block
+    # Process both polarizations separately. Actually this should check if the RCU numbers is odd or even for HBAs
+    # And check this for LBA as well. At the moment it only works for continuous datasets.
     for n in range(n_pixels):
         if Xpol:
             beamformed_fft[n] = shifted_fft[n,0]
@@ -287,7 +321,8 @@ for block in range(startblock,startblock+nblocks):
 
 print "t=",time.clock(),"s -","Binning and Normalizing"
 
-#weight the frequency channels of power
+# weight the frequency channels in power
+# This should be replaced by a proper gain calibration beforehand.
 for j in range(len(selected_frequencies)):
     sum2=0
     for k in range(totAntennas):
@@ -297,8 +332,6 @@ for j in range(len(selected_frequencies)):
 
 
 intpower=np.array(power[...].sum())
-#maxval=intpower.max()
-#intpower /= maxval
 intpower.resize([n_el,n_az])
 
 print "t=",time.clock(),"s -","Plotting"
@@ -345,25 +378,15 @@ prihdr.update('CUNIT2','deg')
 #prihdr.update('CRPIX3',1.)
 #prihdr.update('CUNIT3','s')
 
-hdulist.writeto(data_directory+"/imageCrabInt15.fits")
+hdulist.writeto(data_directory+SummedFitsFilename)
 
-#For images where frequency or time are also a dimension, the array has to be rearranged
-
+# For images where frequency or time are also a dimension, the array has to be rearranged
+# Still have to do this part
+# 
 
 hdu2 = pyfits.PrimaryHDU(freqpower)
 hdulist2 = pyfits.HDUList([hdu2])
-hdulist2.writeto(data_directory+"/imageCrab15.fits")
+hdulist2.writeto(data_directory+SeparatedFitsFilename)
 
 
-
-# RFI mitigation and baseline calculation
-
-# Beamforming section
-
-
-
-#for block in range(200000,200300):
-#    file["block"]=block
-#    file_efield.read(file,"Fx").none()
-#    print block," ",file_efield[0].max()
-
+d
