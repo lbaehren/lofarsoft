f3f5a4c28ec5d7df1ad28aa3c599e55c7337c5a6
mohan <mohan@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-17 12:27:26 +0000

    Removed obsolete source tree; everything BDSM-related now is collected inside a single directory.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5122 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pybdsm/scripts/pybdsm_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('')
-
-import bdsm
-
-
-img = bdsm.execute(bdsm.fits_chain,
-                   {'fits_name': 'WN65341H.fits',
-                    'beam': (.015, .015, 0)})
