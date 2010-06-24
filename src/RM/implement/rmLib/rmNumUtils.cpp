4502808685f149916be48f6906ee6f52d2c8c56b
thriller <thriller@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-06-24 11:12:26 +0000

    Add a utilities class for use inside the Wiener Filter
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5175 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/RM/implement/rmLib/rmNumUtils.cpp
@@ -0,0 +1,505 @@
+//  	ITPP Test program and Wiener Filter test implementation
+//
+//	File:			wienerfilter.cpp
+//	Author:			Sven Duscha (sduscha@mpa-garching.mpg.de)
+//	Date:			09-06-2009
+//	Last change:		18-10-2009
+
+/* Standard header files */
+#include <complex>
+#include <vector>
+#include <stack>
+#include <fstream>
+#include <algorithm>			// for minimum element in vector
+#include <limits>			// limit for S infinite
+#include <iomanip>			// to set precision in cout
+#include "rmNumUtils.h"
+/* RM header files */
+
+using namespace std;
+
+namespace RM {
+
+
+
+/*! procedure finds gaps inside the ordered double valued vector 
+    \param freqs ordered double valued vector to find the gaps in */
+void findGaps(vector<double> &freqs,vector<uint> &result) {
+  vector<double> mean(2) ;
+  double epsilon = 1+5E-8 ;
+  calcMeanVarDist(freqs,mean) ;
+  double meanDist = mean[0] ;
+  double varDist = mean[1] ;
+  result.push_back(0) ; // always put the first element into the gaps 
+  /* loop over all elements of the given vector, to look at the distances
+    and to find the gaps, where the positions are stored into result */
+  for (uint i=0; i<freqs.size()-1; i++) {
+    /* a gap is detected for intervallength more than mean length plus 3 times 
+       the variance of the intervallenths */
+    if ((freqs[i+1]-freqs[i]) > (epsilon*meanDist+3*varDist)) {
+      result.push_back(i+1) ; 
+    }
+  }
+  result.push_back(freqs.size()) ; // always put the first element into the gaps 
+}
+
+
+/*! Procedure to calculate the mean separation and varinace of the  
+    separations of the element of an ordered  real valued vector 
+    \param freqs ordered real valued vector (double precission ) 
+    \param result vector conatains the mean [0] and the variance[1] */
+void calcMeanVarDist(vector<double> &freqs, vector<double> &result) {
+  double mean = 0 ;
+  double var = 0 ;
+  int num = freqs.size()-1 ;
+  for (int i=0; i<num ; i++) {
+    double diff = freqs[i+1]-freqs[i] ;
+    mean = mean+diff ;
+    var = var+diff*diff ;
+  }
+  result[0] = mean/num ;
+  result[1] = sqrt((var-mean*mean/num)/num);
+}
+
+
+
+complex<double> cmat::prod(vector<complex<double> > vek1, vector<complex<double> > vek2) {
+  uint s1 = vek1.size() ;
+  uint s2 = vek2.size() ;
+  complex<double> erg = 0 ;
+  if (s1 != s2) {
+   throw "imcomplatible dimensions \n"; 
+  }
+  else {
+    for (uint i=0; i<s1; i++) {
+      erg = erg + vek1[i]*vek2[i] ;
+    }
+  }
+  return erg ;
+}
+
+cmat cmat::operator* (mat &matr) {
+  uint nrow = rows() ;
+  uint ncol = matr.cols() ;
+  mat temp = matr.H() ;
+  cmat erg(nrow,ncol) ;	
+  for (uint i=0; i<nrow;i++) {
+      vector<complex<double> > row = data[i] ;
+      for (uint j=0;j<ncol;j++) {
+        vector<double> col = temp.data[j] ;
+        erg.set(i,j,mult(row,col)) ;
+	
+      }
+  }   
+  return erg ;
+}
+
+/*! procedure writing out the martix in rectangular way 
+    \param fileName Name of the file to write the data into */
+void cmat::write(string fileName) {
+  ofstream outfile (fileName.c_str());
+  /* loop over all rows of the current signal covariance matrix */
+  for (int i=0; i<rows(); i++) {
+    vector<complex<double> > aus = data[i] ;
+    for (int j=0; j<aus.size() ; j++) {
+      outfile << aus[j] << " " ;
+    }
+    outfile << endl ;
+  }
+  outfile.close();
+
+}
+/*! writing out absolute values of a Matrix with row and collumn index.
+    Each value gets a own line, one line skip for a new row 
+   \param fileName Name of the file to write the data into */
+void cmat::absOut(string fileName) {
+  ofstream outfile (fileName.c_str());
+  /* loop over all rows of the current signal covariance matrix */
+  for (int i=0; i<rows(); i++) {
+    vector<complex<double> > aus = data[i] ;
+    for (int j=0; j<aus.size() ; j++) {
+      outfile << i << "\t" << j << "\t" << abs(aus[j]) << endl  ;
+    }
+    outfile << endl ;
+  }
+  outfile.close();
+
+}
+cmat cmat::inv() {
+  cmat zw ;
+  zw.data = data ;
+  return zw.GaussEle() ;
+}
+
+complex<double> cmat::mult(vector<complex<double> > v1,vector<complex<double> > v2) {
+  complex<double> erg(0,0) ;
+  for (uint i=0; i<v1.size(); i++) {
+    erg = erg+v1[i]*v2[i] ;
+  }
+  return erg ;
+}
+
+complex<double> cmat::mult(vector<complex<double> > v1,vector<double> v2) {
+  complex<double> erg(0,0) ;
+  for (uint i=0; i<v1.size(); i++) {
+    erg = erg+v1[i]*v2[i] ;
+  }
+  return erg ;
+}
+
+double cmat::maxi() {
+  double erg = 0 ;
+  for (uint i=0; i<rows();i++) {
+    vector<complex<double> > row = data[i] ;
+    for (uint j=0; j< cols() ; j++) {
+      if (erg < abs(row[j])) erg = abs(row[j]) ;
+    }
+  } 
+  return erg;
+}
+
+void cmat::swapCols(uint ind1, uint ind2) {
+  if (ind1 != ind2) {
+    if(max(ind1,ind2)>rows()) {
+	 throw "index out of Range in swapCols\n" ;
+    }
+    for (uint i=0; i<rows(); i++) {
+	swap(data[i][ind1],data[i][ind2]);
+    }
+  }
+}
+
+/*! Procedure performs an complete elimination schema including the following steps:
+    1. Forward elimination
+    2. backward elimination
+    3. normalisation */
+
+cmat cmat::GaussEle() {
+  if (rows() != cols()) {
+    throw "no Elimination implemented for non quadratic matrixes\n" ;
+  }
+  stack<permEntry> perm ;
+  cmat unity(rows(),rows()) ;
+  unity.fillUnit() ;
+  /* forward elemination */
+  for (uint i=0; i<rows()-1; i++) {
+    uint ro = getMaxPivot(i) ;
+    if (ro != i) {
+      data[ro].swap(data[i]);
+      permEntry eint(ro,i) ;
+      perm.push(eint) ;
+    }
+    if (data[i][i] == 0.0 ) {
+      cout << "index " << i << "  diagonal " << data[i][i] << endl ;
+      throw "Matrix is not invertible \n";
+    }
+    for (uint j= i+1; j< rows(); j++ ) {
+      complex<double> fak = -data[j][i]/data[i][i];
+      add(j,i,fak);
+      unity.add(j,i,fak);
+    }
+  }
+  /* backward elemination */
+    for (uint i=rows()-1; i>0; i--) {    
+      for (uint j= 0; j< i; j++ ) {
+        complex<double> fak = -data[j][i]/data[i][i];
+        add(j,i,fak);
+        unity.add(j,i,fak);
+      }
+    }
+    /* normalisation */
+    for (uint i=0; i<rows(); i++) {
+	complex<double> fak = 1.0/data[i][i] ;
+	unity.mult(fak,i) ;
+    }
+    /* swap collumns of result */
+    while(!perm.empty()) {
+	    
+      permEntry per = perm.top() ;
+      perm.pop() ;
+      unity.swapCols(per.pos1,per.pos2) ;
+    }
+    return unity;
+}
+
+void cmat::mult(complex<double> fak, uint row) {
+  for (uint i=0; i<data.size(); i++) {
+    data[row][i] = fak*data[row][i] ;
+  }
+}
+
+void mat::mult(double fak, uint row) {
+  for (uint i=0; i<data.size(); i++) {
+    data[row][i] = fak*data[row][i] ;
+  }
+}
+
+void mat::swapCols(uint ind1, uint ind2) {
+  if (ind1 != ind2) {
+    if(max(ind1,ind2)>rows()) {
+	 throw "index out of Range in swapCols\n" ;
+    }
+    for (uint i=0; i<rows(); i++) {
+	swap(data[i][ind1],data[i][ind2]);
+    }
+  }
+}
+
+/*! writing out absolute values of a Matrix with row and collumn index.
+    Each value gets a own line, one line skip for a new row 
+   \param fileName Name of the file to write the data into */
+void mat::absOut(string fileName) {
+  ofstream outfile (fileName.c_str());
+  /* loop over all rows of the current signal covariance matrix */
+  for (int i=0; i<rows(); i++) {
+    vector<double> aus = data[i] ;
+    for (int j=0; j<aus.size() ; j++) {
+      outfile << i << "\t" << j << "\t" << fabs(aus[j]) << endl  ;
+    }
+    outfile << endl ;
+  }
+  outfile.close();
+
+}
+
+void cmat::fillUnit() {
+  uint nCol = cols() ;
+  uint nRow = rows() ;
+  if (nRow != nCol) {
+    throw "no unit for non quadratic matrixes\n" ;
+  }
+  for (uint i=0; i< nRow ; i++) {
+     for (uint j=0; j<nRow; j++ ) {
+	 data[i][j] =(i == j) ? 1 : 0; // kronecer delta
+     }
+  }
+}
+
+void cmat::add(uint addto, uint addfrom, complex<double> fak) {
+  for (uint i=0; i<data.size(); i++) {
+    data[addto][i] = data[addto][i]+fak*data[addfrom][i] ;
+  }	
+}
+
+void mat::add(uint addto, uint addfrom, double fak) {
+  for (uint i=0; i<data.size(); i++) {
+    data[addto][i] = data[addto][i]+fak*data[addfrom][i] ;
+  }	
+}
+
+cmat cmat::H() {
+  cmat erg(cols(),rows()) ;
+  for (uint i =0; i<rows(); i++) {
+    vector<complex<double> > u = data[i] ;
+    for (uint j=0; j<cols(); j++) {
+       erg.set(j,i,conj(u[j])) ;
+    }
+  }
+  return erg ;
+}
+
+cmat cmat::t() {
+  cmat erg(cols(),rows()) ;
+  for (uint i =0; i<rows(); i++) {
+    vector<complex<double> > u = data[i] ;
+    for (uint j=0; j<cols(); j++) {
+       erg.set(j,i,u[j]) ;
+    }
+  }
+  return erg ;
+}
+
+complex<double> cmat::det() {
+  GaussEle() ;
+  complex<double> erg = 1.0 ;
+  for (uint i=0; i<rows();i++) {
+     erg = erg*get(i,i) ;
+  }
+  return erg ;
+}
+
+mat mat::inv() {
+  mat zw ;
+  zw.data = data ;
+  return zw.GaussEle() ;
+}
+
+double mat::det() {
+  GaussEle() ;
+  double erg = 1.0 ;
+  for (uint i=0; i<rows();i++) {
+     erg = erg*get(i,i) ;
+  }
+  return erg ;
+}
+
+/*! Procedure performs an complete elimination schema including the following steps:
+    1. Forward elimination
+    2. backward elimination
+    3. normalisation */
+mat mat::GaussEle() {
+  if (rows() != cols()) {
+    throw "no Elimination implemented for non quadratic matrixes\n" ;
+  }
+  stack<permEntry> perm ;
+  mat unity(rows(),rows()) ;
+  unity.fillUnit() ;
+  /* forward elemination */
+  for (uint i=0; i<rows()-1; i++) {
+    uint ro = getMaxPivot(i) ;
+    if (ro != i) {
+	data[ro].swap(data[i]);
+	permEntry eint(ro,i) ;
+	perm.push(eint) ;
+    }    
+    if (data[i][i] == 0 ) {
+      throw "Matrix is not invertible \n";
+    }
+    for (uint j= i+1; j< rows(); j++ ) {
+      double fak = -data[j][i]/data[i][i];
+      add(j,i,fak);
+      unity.add(j,i,fak);
+    }
+  }
+  /* backward elemination */
+    for (uint i=rows()-1; i>0; i--) {    
+      for (uint j= 0; j< i; j++ ) {
+        double fak = -data[j][i]/data[i][i];
+        add(j,i,fak);
+        unity.add(j,i,fak);
+      }
+    }
+    /* noralisation */
+    for (uint i=0; i<rows(); i++) {
+	double fak = 1.0/data[i][i] ;
+	unity.mult(fak,i) ;
+    }
+    /* swap collumns of result */
+    while(!perm.empty()) {
+      permEntry per = perm.top() ;
+      perm.pop() ;
+      unity.swapCols(per.pos1,per.pos2) ;
+    }
+  return unity;
+}
+
+void mat::write(string fileName) {
+  ofstream outfile (fileName.c_str());
+  /* loop over all rows of the current signal covariance matrix */
+  for (int i=0; i<rows(); i++) {
+    vector<double> aus = data[i] ;
+    for (int j=0; j<aus.size() ; j++) {
+      outfile << aus[j] << " " ;
+    }
+    outfile << endl ;
+  }
+  outfile.close();
+
+}
+
+void mat::fillUnit() {
+  uint nCol = cols() ;
+  uint nRow = rows() ;
+  if (nRow != nCol) {
+    throw "no unit for non quadratic matrixes\n" ;
+  }
+  for (uint i=0; i< nRow ; i++) {
+     for (uint j=0; j<nRow; j++ ) {
+	 data[i][j] =(i == j) ? 1 : 0; // kronecer delta
+     }
+  }
+}
+
+double mat::maxi() {
+  double erg = 0 ;
+  for (uint i=0; i<rows();i++) {
+    vector<double>  row = data[i] ;
+    for (uint j=0; j< cols() ; j++) {
+      if (erg < fabs(row[j])) erg = fabs(row[j]) ;
+    }
+  } 
+  return erg;
+}
+
+mat mat::H() {
+  mat erg(cols(),rows()) ;
+  for (uint i =0; i<rows(); i++) {
+    vector<double>  u = data[i] ;
+    for (uint j=0; j<cols(); j++) {
+       erg.set(j,i,u[j]) ;
+    }
+  }
+  return erg ;
+}
+
+double mat::prod(vector<double> vek1, vector<double> vek2) {
+  uint s1 = vek1.size() ;
+  uint s2 = vek2.size() ;
+  double erg = 0 ;
+  if (s1 != s2) {
+   throw "imcomplatible dimensions \n"; 
+  }
+  else {
+    for (uint i=0; i<s1; i++) {
+      erg = erg + vek1[i]*vek2[i] ;
+    }
+  }
+  return erg ;
+}
+
+double mat::getPivot(vector<double> &vek, uint start) {
+  double erg = fabs(vek[start]) ;
+  
+  erg = erg/abs(vek,start) ;
+  return erg ;
+}
+
+uint mat::getMaxPivot(uint start) {
+  double pivot = 0 ;
+  uint erg = start ;
+  for (uint i=start; i<data.size(); i++) {
+    double test = getPivot(data[i],start) ;
+    if (test>pivot) {
+	pivot = test ;
+	erg = i ;
+    }
+  }
+  return erg ;
+}
+
+uint cmat::getMaxPivot(uint start) {
+  double pivot = 0 ;
+  uint erg = start ;  
+  for (uint i=start; i<data.size(); i++) {
+    double test = getPivot(data[i],start) ;
+    if (test>pivot) {
+	pivot = test ;
+	erg = i ;
+    }
+  }
+  return erg ;
+}
+
+double mat::abs(vector<double> vek, uint anf) {
+  double erg = 0 ;
+  for (uint i=anf; i<vek.size(); i++) {
+    erg = erg + vek[i]*vek[i] ;
+  }
+  return sqrt(erg) ;
+}
+
+double cmat::abs(vector<complex<double> > vek, uint anf) {
+  double erg = 0 ;
+  for (uint i=anf; i<vek.size(); i++) {
+    erg = erg + real(vek[i]*conj(vek[i])) ;
+  }
+  return sqrt(erg) ;
+}
+
+double cmat::getPivot(vector<complex<double> > &vek, uint start) {
+  double erg = abs(vek[start]) ;
+  erg = erg/abs(vek,start) ;
+  return erg ;
+}
+
+}
---
+++ src/RM/implement/rmLib/rmNumUtils.h
@@ -0,0 +1,391 @@
+
+
+// Standard header files
+#include <iostream>
+#include <string>
+#include <vector>
+// IT++ header files
+#ifdef HAVE_ITPP
+#include <itpp/itbase.h>
+#include <itpp/itcomm.h>
+#endif
+// RM header files
+#include "rmNoise.h"
+
+using namespace std;
+
+#define DEBUG 1
+//! statistical limit for number of line of sights before attempting a S(i,j) iteration
+#define LOSLIMIT 100
+
+namespace RM {  //  namespace RM
+
+void calcMeanVarDist(vector<double> &freqs, vector<double> &result) ;
+void findGaps(vector<double> &freqs, vector<uint> &result) ;
+
+  class mat ;
+  class permEntry {
+     public:
+     uint pos1;
+     uint pos2;     
+     permEntry(uint w1,uint w2) {
+	 pos1 = w1 ;
+	 pos2 = w2 ;
+     }
+  } ;
+  /*! class for a complex vector */
+  class cvec {
+    public :
+      vector<complex<double> > data ;  // the actual data
+      cvec(void ) {
+      } ;
+      /*! constructor which reservese the memory for size elements */
+      cvec(uint size) {
+	 data.resize(size) ;
+      };
+      /*! constructor makes a cvec from a stl vector */
+      cvec(vector<complex<double> > &vek) {
+        data=vek ;
+      } ;
+      /*! resizes the dimension of the current vector */
+      void set_size(uint size) {
+	 data.resize(size);
+      } ;
+      /*! set the value of the element index ind to value val */
+      void set(uint ind, complex<double> val) {
+	data[ind] = val ;
+      }
+      /*! returns the size of the vector */
+      uint size() {
+	return data.size() ;
+      } ;
+      /*! returns the size of the vector */
+      uint length() {
+	return data.size() ;
+      } ;
+      /*! returns a stl vector of the same data as the current vector */
+      vector<complex<double> > getData() {
+        return data ;
+      }
+      /*! returns the n-th element of the vector */
+      complex<double> &operator[](uint n) { 
+	  return data[n] ;
+      }
+      /*! returns the n-th element of the vector */
+      complex<double> operator()(uint n) {
+	  return data[n] ;
+      }
+      /*! scalar multiplication of two vectors */
+      const complex<double> operator* (const cvec &vec) const {
+	  return vec*data ;
+      } ;
+      /*! scalat multiplication of a cvec whith a stl complex vector */
+      const complex<double> operator* (const vector<complex<double> > &vec) const {
+	 complex<double> erg = 0 ;
+	 if (data.size() != vec.size()) {
+	   throw "imcomplatible dimensions \n"; 
+	 }
+	 else {
+	   for (uint i=0; i<data.size(); i++) {
+		erg = erg + data[i]*vec[i] ;
+	   }
+	 }
+	 return erg ;
+      };   
+	/*! scalat multiplication of a cvec whith a stl double vector */
+      const complex<double> operator* (const vector<double> &vec) const {
+	 complex<double> erg = 0 ;
+	 if (data.size() != vec.size()) {
+	   throw "imcomplatible dimensions \n"; 
+	 }
+	 else {
+	   for (uint i=0; i<data.size(); i++) {
+		erg = erg + data[i]*vec[i] ;
+	   }
+	 }
+	 return erg ;
+      };    
+  } ;
+  /*! class for handling a complex matrix */
+  class cmat {     
+     public:
+        bool inverted ;
+        vector<vector<complex<double> > > data ; // actual data 
+        cmat() {
+	   inverted = false ;
+        } ;
+	/*! constructor for a row x col matrix */
+        cmat(uint row, uint col) {
+	   inverted = false ;
+	   data.resize(row) ;
+	   for (uint i=0; i<row; i++) {
+	     data[i].resize(col) ;
+	   }
+        } 
+	/*! preset the matrix to a unity matrix */
+	void fillUnit() ;
+	/*! returns the absolute maimal value of the matrix */
+	double maxi() ;
+	/*! returns the squaroot of the absolute squared values form anf to the end of the stl vector vek
+	    \param vek vector to calculate the value for
+            \param anf start index from where the calculation starts */
+	double abs(vector<complex<double> > vek, uint anf) ;
+	/*! performs a gauss elimination on the matrix */
+	cmat GaussEle() ;
+	/*! returns the row index for the maximal pivot element (not a complete pivot search)
+	    \param start start index for the search searching at mat[i,start] i=start .. dimension of the matrix */
+	uint getMaxPivot(uint start) ;
+	/*! performs a multiplication of a row of the matrix an a complex factor
+	    \param fak factor to be multiplied to the row
+	    \param row index of the rwo to be treated */
+	void mult(complex<double> fak, uint row) ;
+	/*! performs a adding of a product of one row(index addfrom) of the matrix to another row (addto) inside this seccond one
+	    \param addto index of the goal row 
+	    \param addfrom index of the added row
+	    \param fak factor to multiply the row addfrom before adding */ 
+	void add(uint addto, uint addfrom, complex<double> fak) ;
+	/*! calculate determinant of the matrix */
+	complex<double> det() ;
+	/*! swap two collumns of the matrix 
+	    \param ind1 index of the first collumn 
+	    \param ind2 index of the seccond collumn */
+	void swapCols(uint ind1, uint ind2);	
+	/*! scalar product of two stl complex vectors */
+	complex<double> prod(vector<complex<double> > vek1, vector<complex<double> > vek2) ;
+	/*! number of rows of the matrix */
+	uint rows() {
+	  return data.size() ;
+        }
+	/*! calculates the absolute value of a complex number */
+	double abs(complex<double> val) {
+	  return sqrt(real(val*conj(val))) ;
+	}
+	/*! number of collumns of the matrix */
+	uint cols() {
+	  return data[0].size() ;
+	}
+	/*! get the value of the rowindex r and collumnindex c*/
+	complex<double> get(int r, int c) {
+	  vector<complex<double> > u = data[r] ;
+	  complex<double> erg = u[c] ;
+	  return erg ;
+	}
+	uint size() {
+	  return data[0].size() ;
+	}
+	/*! resize the dimension of the matrix to 
+	    \param srow new number of rows
+	    \param scol new number of collumns */
+	void set_size(uint srow, uint scol) {
+	  data.resize(srow) ;
+	  vector<vector<complex<double> > >::iterator it = data.begin() ;
+	  while (it != data.end()) {
+	    it->resize(scol) ;
+	    it++ ;
+	  }
+	};
+	/*! set an element of the matrix 
+	  \param posR row index of the element 
+	  \param posC col index of the element 
+	  \param val new value of the element */
+	void set(uint posR, uint posC,complex<double> val) {
+	  data[posR][posC] = val ;
+	} ;
+	/*! product of the matrix with an cvek object result also an cvec object */
+	cvec operator* (cvec &vec) {
+	  uint nCol = rows() ;
+	  cvec erg(nCol) ;
+	  for (uint i=0; i<nCol; i++) {
+	    vector<complex<double> > zw = data[i] ;
+	    erg.set(i,vec*zw) ;
+	  }
+	  return erg ;
+	} ;
+
+        /*! product of the matrix with an cvek object result also an cvec object */
+	vector<complex<double> > operator* (vector<complex<double> > &vec) {
+	  uint nrows = rows() ;
+	  uint ncols = cols() ;
+	  vector<complex<double> > erg(nrows) ;
+	  for (uint i=0; i<nrows; i++) {
+	    vector<complex<double> > zw = data[i] ;
+ 	    complex<double> res(0,0) ;
+	    for (int j=0; j<ncols;j++) {
+		res += zw[j]*vec[j] ;
+	    }
+	    erg[i]=res;
+	  }
+	  return erg ;
+	} ;
+	/*! matrix poduct of two complex matrices */
+	cmat operator* (cmat &matr) {
+   	  uint nrow = rows() ;
+          uint ncol = matr.cols() ;
+	  cmat temp = matr.t() ;
+	  cmat erg(nrow,ncol) ;	
+	  for (uint i=0; i<nrow;i++) {
+	    vector<complex<double> > row = data[i] ;
+	    for (uint j=0;j<ncol;j++) {
+	      vector<complex<double> > col = temp.data[j] ;
+	      erg.set(i,j,mult(row,col)) ;	      
+	    }
+	  }	
+	  return erg ;
+	}
+	/*! Sum of two complex matrices */
+	cmat operator+ (cmat &matr) {
+   	  uint nrow = rows() ;
+          uint ncol = cols() ;	  
+	  cmat erg(nrow,ncol) ;
+	  for (uint i=0; i<nrow; i++) {
+	    vector<complex<double> > u1 = data[i] ;
+	    vector<complex<double> > u2 = matr.data[i] ;
+	    for (uint j=0; j<u1.size() ; j++) {
+		erg.set(i,j,u1[j]+u2[j]) ;
+	    }
+	  }		  	
+	  return erg ;
+	}
+	/*! procedure returns the pivot value of a
+	    complex vector from a specified index */
+	double getPivot(vector<complex<double> > &vek, uint start) ;
+	cmat operator* (mat &matr) ;
+	complex<double> operator()(uint row, uint col) {
+           vector<complex<double> >  u = data[row] ;
+	   return u[col] ;
+	}
+        void absOut(string fileName) ;
+	void write(string fileName) ;
+        /*! inverse of the current matrix */
+	cmat inv() ;
+	/*! complex adjunct of the current matrix */
+	cmat H() ;
+	cmat t() ;
+	complex<double> mult(vector<complex<double> > v1,vector<complex<double> > v2) ;
+	complex<double> mult(vector<complex<double> > v1,vector<double> v2) ;
+	cmat* inverse ;
+  };
+  /*! class for a real matrix */
+  class mat {    
+    public:
+	bool inverted ;
+        vector<vector<double> > data ; // actual data 
+        mat() {
+	    inverted = false ;
+        } ;
+	/*! constructor for a row x col matrix */
+        mat(uint row, uint col) {
+	   inverted = false ;
+	   data.resize(row) ;
+	   for (uint i=0; i<row; i++) {
+	     data[i].resize(col) ;
+	   }
+        } 
+
+	/*! returns the squaroot of the absolute squared values form anf to the end of the stl vector vek
+	    \param vek vector to calculate the value for
+            \param anf start index from where the calculation starts */
+	double abs(vector<double> vek, uint anf) ;
+	double prod(vector<double> vek1, vector<double>  vek2) ;
+	double getPivot(vector<double> &vek, uint start) ;
+	void add(uint addto, uint addfrom, double fak) ;
+	void swapCols(uint ind1, uint ind2) ;
+	double maxi() ;
+	mat GaussEle() ;
+	void write(string fileName) ;
+	void fillUnit() ;
+	uint getMaxPivot(uint start);
+	void mult(double fak, uint row) ;
+	uint rows() {
+	  return data.size() ;
+        }		    	     
+	uint cols() {
+	  return data[0].size() ;
+	}
+	void set_size(uint srow, uint scol) {
+	  data.resize(srow) ;
+	  vector<vector<double> >::iterator it = data.begin() ;
+	  while (it != data.end()) {
+	    it->resize(scol) ;
+	    it++ ;
+	  }
+	}
+	void zeros() {
+	  vector<vector<double> >::iterator it1 = data.begin() ;
+	  while (it1 != data.end()) {
+	    vector<double>  uv = *it1 ;
+	    vector<double>::iterator it2 = uv.begin() ;
+	    while (it2 != uv.end()) {
+	 	*it2 = 0.0 ;
+		it2++;
+	    }
+	    it1++ ;
+	  }	
+	}
+	
+	double &operator() (uint row, uint col) {
+	  vector<double> r = data[row];
+	  return r[col];
+	}
+	
+	cvec operator* (cvec &vec) {
+	  uint nCol = cols() ;
+	  cvec erg(nCol) ;
+	  for (uint i=0; i<nCol; i++) {
+	    vector<double> zw = data[i] ;
+	    erg[i] = vec*zw ;
+	  }
+	  return erg ;
+	} ;
+
+        /*! matrix poduct of two complex matrices */
+	mat operator* (mat &matr) {
+   	  uint nrow = rows() ;
+          uint ncol = matr.cols() ;
+	  mat temp = matr.H() ;
+	  mat erg(nrow,ncol) ;	
+	  for (uint i=0; i<nrow;i++) {
+	    vector<double>  row = data[i] ;
+	    for (uint j=0;j<ncol;j++) {
+	      vector<double>  col = temp.data[j] ;
+	      erg.set(i,j,prod(row,col)) ;
+	    }
+	  }	
+	  return erg ;
+	}
+	
+	void set(uint posR, uint posC,double val) {
+	  data[posR][posC] = val ;
+	}
+	double get(int r, int c) {
+	  vector<double> u = data[r] ;
+	  double erg = u[c] ;
+	  return erg ;
+	}
+	mat inv() ;
+	void absOut(string fileName) ;
+	mat H() ;
+	double det() ;
+	mat* inverse;
+  };
+  
+  class vec {
+    vector<double> data ;
+    public :
+      void set_size(uint size) {
+	 data.resize(size);
+      } ;
+      double &operator[](uint n) { 
+	  return data[n] ;
+      }
+      double operator()(uint n) {
+	  return data[n] ;
+      }
+      uint size() {
+	return data.size() ;
+      }
+      uint length() {
+	return data.size() ;
+      }
+  };
+}  // end of the namespace RM
