d8719007cbbbdf5ffb8480fd11135b606d664a94
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-07-08 17:00:35 +0000

    Added from ttl import * option that imports submodules.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5281 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/ttl/bindings/__init__.py
+++ src/CR-Tools/implement/ttl/bindings/__init__.py
@@ -16,3 +16,32 @@
 #  along with this library. If not, see <http://www.gnu.org/licenses/>.  #
 ##########################################################################
 
+# Make subpackages available when doing from ttl import *
+__all__ = ['beamforming', 'coordinates', 'fft']
+
+# Make content of subpackages available when calling from ttl import *
+def extend_all(module):
+    adict = {}
+    for a in __all__:
+        adict[a] = 1
+    try:
+        mall = getattr(module, '__all__')
+    except AttributeError:
+        mall = [k for k in module.__dict__.keys() if not k.startswith('_')]
+    for a in mall:
+        if a not in adict:
+            __all__.append(a)
+
+import beamforming
+import coordinates
+import fft
+
+extend_all(beamforming)
+extend_all(coordinates)
+extend_all(fft)
+
+# Make content of subpackages available when calling import ttl
+from beamforming import *
+from coordinates import *
+from fft import *
+
