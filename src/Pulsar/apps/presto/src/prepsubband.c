cdecb62897395bdb0c6cfdb9edd72e5e621ba7a7
hassall <hassall@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-07-20 10:13:21 +0000

    Added LOFAR observatory
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5345 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/apps/presto/src/prepsubband.c
+++ src/Pulsar/apps/presto/src/prepsubband.c
@@ -311,7 +311,9 @@ int main(int argc, char *argv[])
       } else if (!strcmp(idata.telescope, "GBT")) {
          strcpy(obs, "GB");
       } else if (!strcmp(idata.telescope, "GMRT")) {
-         strcpy(obs, "GM");
+         strcpy(obs, "GM");} 
+      else if (!strcmp(idata.telescope, "LOFAR")) {
+         strcpy(obs, "LF");
       } else {
          printf("\nYou need to choose a telescope whose data is in\n");
          printf("$TEMPO/obsys.dat.  Exiting.\n\n");
@@ -386,6 +388,8 @@ int main(int argc, char *argv[])
             strcpy(obs, "EF");
          } else if (!strcmp(idata.telescope, "GBT")) {
             strcpy(obs, "GB");
+	 } else if (!strcmp(idata.telescope, "LOFAR")) {
+	   strcpy(obs, "LF");
          } else {
             printf("\nYou need to choose a telescope whose data is in\n");
             printf("$TEMPO/obsys.dat.  Exiting.\n\n");
