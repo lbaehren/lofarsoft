dd4498108d369ca478901cb6fbc82883ccba3c33
terveen <terveen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-07-26 08:25:08 +0000

    Added reading station data to bfdata.py
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5381 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/modules/bfdata.py
+++ src/CR-Tools/implement/Pypeline/modules/bfdata.py
@@ -120,3 +120,139 @@ def get_block_rawvoltagedata_old(file, stations, channels=64, samples=3056, npol
 
     # return sequence number, data
     return t0,np.asarray(t1).reshape(stations,channels,samples|2,npol,2)
+
+def read_header_raw_station_data(file):
+    """Get the header of a raw station data file.
+    Returns a tuple of (bitsPerSample,nrPolarizations,nrSubbands,
+    nrSamplesPerSubband,station,sampleRate,subbandFrequencies,beamDirections,
+    subbandToBeamMapping)"""
+
+    file.seek(0)
+    maxNrSubbands=62
+    # Check endiannes. This is done by a magic number at the beginning of the file
+    endiannes='>'
+    fmt=endiannes+'I'
+    encoded_data=file.read(struct.calcsize(fmt))
+    magic_nr=struct.unpack(fmt,encoded_data)[0]
+    
+    # Check if this is the right endiannes
+    if magic_nr != 0x3F8304EC:
+        # If not change the endiannes.
+        endiannes='<'
+        fmt=endiannes+'I'
+        magic_nr=struct.unpack(fmt,encoded_data)
+        
+    # Check if the magic nr is now correct. If not, quit the operation.
+    assert magic_nr == 0x3F8304EC
+
+
+    # Now we can do the same reading and unpacking for the other data formats
+
+    # The number of bits per sample
+    fmt=endiannes+'B'
+    bitsPerSample = struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0]
+
+    # The number of polarizations
+    fmt=endiannes+'B'
+    nrPolarizations= struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0]
+
+    # The number of subbands
+    fmt=endiannes+'H'
+    nrSubbands= struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0]
+
+    # 155648 (160MHz) or 196608 (200MHz)
+    fmt=endiannes+'I'
+    nrSamplesPerSubband = struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0]
+
+    # Name of the station
+    fmt=endiannes+'20s'
+    station = struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0].strip('\x00')
+
+    # The sample rate: 156260.0 or 195312.5 .. double (number of samples per second for each subband
+    fmt=endiannes+'d'
+    sampleRate = struct.unpack(fmt,file.read(struct.calcsize(fmt)))[0]
+
+    # The frequencies within a subband
+    fmt=endiannes+str(maxNrSubbands)+'d'
+    subbandFrequencies = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+
+    # The beam pointing directions
+    fmt=endiannes+'16d'
+    beamDirections = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+
+    # mapping from subbands to beams
+    fmt=endiannes+str(maxNrSubbands)+'h'
+    subbandToBeamMapping = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    # Padding
+    fmt=endiannes+'I'
+    padding = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+
+    # Return the parameters
+    return (bitsPerSample,nrPolarizations,nrSubbands,nrSamplesPerSubband,station,sampleRate,subbandFrequencies[0:nrSubbands],beamDirections,subbandToBeamMapping[0:nrSubbands])
+
+
+def get_blockheader_raw_station_data(file,nrSamplesPerSubband,nrSubbands):
+    """Get the header of a raw station data block
+    returns (coarseDelayApplied,fineDelayRemainingAtBegin, fineDelayRemainingAfterEnd, time, flags)"""
+    
+    
+
+    # Check endiannes. This is done by a magic number at the beginning of the file
+    endiannes='>'
+    fmt=endiannes+'I'
+    encoded_data=file.read(struct.calcsize(fmt))
+    magic_nr=struct.unpack(fmt,encoded_data)[0]
+    
+    # Check if this is the right endiannes
+    if magic_nr != 0x2913D852:
+        # If not change the endiannes.
+        endiannes='<'
+        fmt=endiannes+'I'
+        magic_nr=struct.unpack(fmt,encoded_data)
+        
+    # Check if the magic nr is now correct. If not, quit the operation.
+    assert magic_nr == 0x2913D852
+    
+    
+    # 
+    fmt=endiannes+'8i'
+    coarseDelayApplied= struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    # for alignment
+    fmt=endiannes+'4B'
+    padding = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    #
+    fmt=endiannes+'8d'
+    fineDelayRemainingAtBegin = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    #
+    fmt=endiannes+'8d'
+    fineDelayRemainingAfterEnd = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    #
+    fmt=endiannes+'8q'
+    time= struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    
+    # This has the structure: ( nrFlagsRanges, (range begin,range end)[16] )  [8]  
+    fmt=endiannes+'264I'
+    flags = struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+   
+
+    
+    # Read data
+    fmt=endiannes+str(nrSamplesPerSubband*nrSubbands*4)+'h' 
+    data= struct.unpack(fmt,file.read(struct.calcsize(fmt)))
+    data=np.array(data)
+    data.resize(nrSubbands,nrSamplesPerSubband,2,2)
+
+    # Return the values
+    return (coarseDelayApplied,fineDelayRemainingAtBegin, fineDelayRemainingAfterEnd, time, flags,data)
+
+
+    #  sizeof(blockheader)
+    #  +
+    #  ( header.nrSamplesPerSubband * header.nrSubbands * sizeof(Sample) );
+    #nrOfBlocks = (file_byte_size - sizeof(header)) / oneSecondBlockSize;
+    #std::cout << "Number of one second blocks in file: " << nrOfBlocks << std::endl;
