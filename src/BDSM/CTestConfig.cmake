56d3e9a4b6b7b79824c1c584be97cc3e9459a40e
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-10 13:16:13 +0000

    Enable testing.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5524 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/BDSM/CTestConfig.cmake
+++ src/BDSM/CTestConfig.cmake
@@ -1,11 +1,13 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-
-set(CTEST_PROJECT_NAME "BDSM")
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "LUS BDSM")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "svn.astron.nl")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=BDSM")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=LUS+BDSM")
 set(CTEST_DROP_SITE_CDASH TRUE)
