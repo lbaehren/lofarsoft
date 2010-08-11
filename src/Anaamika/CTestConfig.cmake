329541c61a79eae8654c7c53f1b9848a060ab187
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-11 15:18:31 +0000

    Added Anaamika to the list of packages for which automated build and testing is done.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5554 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Anaamika/CTestConfig.cmake
+++ src/Anaamika/CTestConfig.cmake
@@ -4,10 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "LUS BDSM")
+set(CTEST_PROJECT_NAME "LUS Anaamika")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=LUS+BDSM")
+set(CTEST_DROP_LOCATION "/submit.php?project=LUS+Anaamika")
 set(CTEST_DROP_SITE_CDASH TRUE)
