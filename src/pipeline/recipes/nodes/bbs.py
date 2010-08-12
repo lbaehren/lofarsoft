1cb6daa086a009b7c008ccd6cb0129a4a9108f87
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-12 16:54:58 +0000

    Log directly from node, rather than slurping stdout
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5563 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/recipes/nodes/bbs.py
+++ src/pipeline/recipes/nodes/bbs.py
@@ -7,8 +7,23 @@ from tempfile import mkstemp, mkdtemp
 import os
 import sys
 import shutil
+import threading
+import time
 
 from subprocess import Popen, CalledProcessError, PIPE, STDOUT
+
+log_prop = """
+log4cplus.rootLogger=DEBUG, FILE
+log4cplus.logger.TRC=TRACE9
+
+log4cplus.appender.FILE=log4cplus::RollingFileAppender
+log4cplus.appender.FILE.File=KernelControl.log
+log4cplus.appender.FILE.ImmediateFlush=true
+log4cplus.appender.FILE.MaxFileSize=10MB
+log4cplus.appender.FILE.MaxBackupIndex=1
+log4cplus.appender.FILE.layout=log4cplus::PatternLayout
+log4cplus.appender.FILE.layout.ConversionPattern=%l [%-3p] - %m%n
+"""
 
 def get_mountpoint(path):
     return path if os.path.ismount(path) else get_mountpoint(os.path.abspath(os.path.join(path, os.pardir)))
@@ -43,8 +58,17 @@ class bbs(LOFARnode):
             )
             with open(log_prop_filename, 'w') as log_prop_file:
                 log_prop_file.write(log_prop)
+            log_filename = (os.path.join(working_dir, 'KernelControl.log'))
+            open(log_filename, 'w').close()
+            logging_thread = threading.Thread(
+                target=self._log_file,
+                args=(log_filename,)
+            )
+            logging_thread.setDaemon(True)
+            logging_thread.start()
 
             env = read_initscript(initscript)
+
             try:
                 cmd = [executable, parset_filename, "0"]
                 self.logger.debug("Executing BBS kernel")
@@ -62,6 +86,16 @@ class bbs(LOFARnode):
                 shutil.rmtree(working_dir)
 
             return 0
+
+    def _log_file(self, filename):
+        with open(filename, 'r') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    f.seek(0, 2)
+                    time.sleep(1)
+                else:
+                    self.logger.debug(line.strip())
 
 if __name__ == "__main__":
     loghost, logport = sys.argv[1:3]
