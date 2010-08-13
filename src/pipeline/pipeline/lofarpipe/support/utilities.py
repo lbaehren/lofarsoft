ac4042de797b73f81498cb10736bc7fd940778cc
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-13 21:02:45 +0000

    comments and cleanups
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5581 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/pipeline/pipeline/lofarpipe/support/utilities.py
+++ src/pipeline/pipeline/lofarpipe/support/utilities.py
@@ -1,11 +1,30 @@
+#                                                       LOFAR PIPELINE FRAMEWORK
+#
+#                                                               Utility routines
+#                                                         John Swinbank, 2009-10
+#                                                      swinbank@transientskp.org
+# ------------------------------------------------------------------------------
+
 from __future__ import with_statement
-from contextlib import closing, contextmanager
-from lofarpipe.cuisine.parset import Parset
-from tempfile import mkstemp
-import os, errno, shutil, subprocess, time, resource, threading, logging
+
 from itertools import islice, repeat, chain, izip
+from contextlib import closing, contextmanager
+from tempfile import mkstemp
 from string import Template
 
+import os
+import errno
+import shutil
+import subprocess
+import time
+import resource
+import threading
+import logging
+
+from lofarpipe.cuisine.parset import Parset
+
+#                                                                        Logging
+# ------------------------------------------------------------------------------
 log_prop = Template("""
 log4cplus.rootLogger=DEBUG, FILE
 log4cplus.logger.TRC=TRACE9
@@ -23,6 +42,9 @@ def log_file(filename, logger, killswitch):
     """
     Do the equivalent of tail -f on filename -- ie, watch it for updates --
     and send any lines written to the file to the logger.
+
+    killswitch is an instance of threading.Event: when set, we bail out of the
+    loop.
     """
     if not os.path.exists(filename):
         open(filename, 'w').close()
@@ -37,6 +59,14 @@ def log_file(filename, logger, killswitch):
 
 @contextmanager
 def catch_log4cplus(working_dir, logger_name, executable_name):
+        """
+        Set up a log4cplus log_prop file for executable_name in working dir.
+
+        Set up a context in which we monitor the resulting log file in a
+        separate thread, and send the reuslts to a logger called logger_name.
+
+        When the context finishes, terminate the monitoring thread.
+        """
         log_filename = os.path.join(
             working_dir, "pipeline_process.log"
         )
@@ -56,84 +86,12 @@ def catch_log4cplus(working_dir, logger_name, executable_name):
         killswitch.set()
         logging_thread.join()
 
-def get_parset(parset):
-    p = Parset()
-    p.readFromFile(parset)
-    return p
-
-def build_available_list(listname):
-    # Run on engine to construct list of locally-stored data
-    import os
-    from IPython.kernel.engineservice import get_engine
-    available = [
-        filename for filename in filenames if os.access(filename, os.R_OK)
-    ]
-    properties = get_engine(id).properties
-    properties[listname] = available
-
-def clear_available_list(listname):
-    from IPython.kernel.engineservice import get_engine
-    del(get_engine(id).properties[listname])
-
-def check_for_path(properties, dependargs):
-    try:
-        return dependargs[0] in properties[dependargs[1]]
-    except NameError:
-        return False
-
-def patch_parset(parset, data, output_dir=None):
-    # We want to be able to update parsets on the fly, for instance to add the
-    # names of data files which need processing into a standard configuration
-    temp_parset_filename = mkstemp(dir=output_dir)[1]
-    temp_parset = get_parset(parset)
-    for k, v in data.items():
-        temp_parset[k] = v
-    temp_parset.writeToFile(temp_parset_filename)
-    return temp_parset_filename
-
-def create_directory(dirname):
-    # Recursively create a directory, without failing if it already exists
-    try:
-        os.makedirs(dirname)
-    except OSError, failure:
-        if failure.errno != errno.EEXIST:
-            raise
-
-def move_log(log_file, destination):
-    create_directory(destination)
-    shutil.move(log_file, destination)
-
-def read_initscript(filename, shell="/bin/sh"):
-    # Return a dict of the environment after sourcing
-    # the given script in a shell.
-    p = subprocess.Popen(
-        ['. %s ; env' % (filename)],
-        shell=True,
-        executable=shell,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        close_fds=True
-    )
-    so, se = p.communicate()
-    return dict([x.split('=', 1) for x in so.strip().split('\n')])
-
-@contextmanager
-def locked_ms(ms_name):
-    # If this fails, we'll raise an OSError 17 (EEXIST)
-    lock_extension = "_lock"
-    lock_name = os.path.join(
-        ms_name,
-        lock_extension
-    )
-    try:
-        my_fd = os.open(lock_name, os.O_EXCL | os.O_CREAT)
-        yield ms_name
-        os.close(my_fd)
-    finally:
-        os.unlink(lock_name)
-
 @contextmanager
 def log_time(logger):
+    """
+    Send information about the processing time used by code in this context to
+    the specified logger.
+    """
     def get_rusage():
         return [
             x + y for x, y in zip(
@@ -159,20 +117,121 @@ def log_time(logger):
             )
         )
 
-def string_to_list(my_string):
+#                                                            Parset Manipulation
+# ------------------------------------------------------------------------------
+
+def get_parset(parset_filename):
     """
-    Convert a list-like string (as in pipeline.cfg) to a list of values.
+    Read a parset from a parset_filename and return the result as an instance
+    of lofarpipe.cuisine.Parset to the caller.
     """
-    return [x.strip() for x in my_string.strip('[] ').split(',')]
+    p = Parset()
+    p.readFromFile(parset_filename)
+    return p
+
+def patch_parset(parset, data, output_dir=None):
+    """
+    Generate a parset file by adding the contents of the data dictionary to
+    the specified parset object. Write it to file, and return the filename.
+    """
+    # We want to be able to update parsets on the fly, for instance to add the
+    # names of data files which need processing into a standard configuration
+    temp_parset_filename = mkstemp(dir=output_dir)[1]
+    temp_parset = get_parset(parset)
+    for k, v in data.items():
+        temp_parset[k] = v
+    temp_parset.writeToFile(temp_parset_filename)
+    return temp_parset_filename
+
+#                                                File and Directory Manipulation
+# ------------------------------------------------------------------------------
+
+def create_directory(dirname):
+    """
+    Recursively create a directory, without failing if it already exists.
+    """
+    try:
+        os.makedirs(dirname)
+    except OSError, failure:
+        if failure.errno != errno.EEXIST:
+            raise
+
+def move_log(log_file, destination):
+    """
+    Move a log file into the directory destination, creating that directory
+    if required.
+    """
+    create_directory(destination)
+    shutil.move(log_file, destination)
+
+@contextmanager
+def locked_ms(ms_name):
+    """
+    Probably dangerous method for "locking" a MeasurementSet for the scope of
+    the given context.
+
+    Deprecated.
+    """
+    # If this fails, we'll raise an OSError 17 (EEXIST)
+    lock_extension = "_lock"
+    lock_name = os.path.join(
+        ms_name,
+        lock_extension
+    )
+    try:
+        my_fd = os.open(lock_name, os.O_EXCL | os.O_CREAT)
+        yield ms_name
+        os.close(my_fd)
+    finally:
+        os.unlink(lock_name)
+
+#                                                    IPython Dependency Checking
+# ------------------------------------------------------------------------------
+
+def build_available_list(listname):
+    """
+    This can be pushed to an IPython engine and run there to generate a list
+    of data which is locally stored (and hence available for processing).
+    """
+    import os
+    from IPython.kernel.engineservice import get_engine
+    available = [
+        filename for filename in filenames if os.access(filename, os.R_OK)
+    ]
+    properties = get_engine(id).properties
+    properties[listname] = available
+
+def clear_available_list(listname):
+    """
+    Delete lists of locally stored data to free up resources on the engine.
+    """
+    from IPython.kernel.engineservice import get_engine
+    del(get_engine(id).properties[listname])
+
+def check_for_path(properties, dependargs):
+    """
+    Run on IPython engine to check for the existence of a given path in the
+    locally available data, as recorded by build_available_list().
+
+    Used for dependency checking when scheduling jobs.
+    """
+    try:
+        return dependargs[0] in properties[dependargs[1]]
+    except NameError:
+        return False
+
+#                                                       Iterators and Generators
+# ------------------------------------------------------------------------------
 
 try:
     from itertools import izip_longest
 except ImportError:
     def izip_longest(*args, **kwds):
-        # This code is lifted from the Python 2.6 manual, since izip_longest
-        # isn't available in the 2.5 standard library.
-        # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
-
+        """
+        This code is lifted from the Python 2.6 manual, since izip_longest
+        isn't available in the 2.5 standard library.
+        izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+        """
         fillvalue = None
         def sentinel(counter = ([fillvalue]*(len(args)-1)).pop):
             yield counter()         # yields the fillvalue, or raises IndexError
@@ -194,3 +253,28 @@ def group_iterable(iterable, size):
     (4, 5)
     """
     return (filter(lambda x: x is not None, x) for x in izip_longest(*[islice(iterable, n, None, size) for n in xrange(size)]))
+
+#                                                                  Miscellaneous
+# ------------------------------------------------------------------------------
+
+def read_initscript(filename, shell="/bin/sh"):
+    """
+    Return a dict of the environment after sourcing the given script in a shell.
+    """
+    p = subprocess.Popen(
+        ['. %s ; env' % (filename)],
+        shell=True,
+        executable=shell,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True
+    )
+    so, se = p.communicate()
+    return dict([x.split('=', 1) for x in so.strip().split('\n')])
+
+def string_to_list(my_string):
+    """
+    Convert a list-like string (as in pipeline.cfg) to a list of values.
+    """
+    return [x.strip() for x in my_string.strip('[] ').split(',')]
+
