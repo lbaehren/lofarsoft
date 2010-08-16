28d213925b1fc3ce120ab006018b5e45ca4d7384
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-16 08:54:28 +0000

    Options for handling test builds.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5594 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -2,6 +2,12 @@
 set (CMAKE_INSTALL_PREFIX ${USG_INSTALL_PREFIX} CACHE PATH
   "CMake installation area."
   )
+set (LUS_ENABLE_TESTING ${LUS_ENABLE_TESTING} CACHE BOOL
+  "Enable testing?"
+  )
+set (LUS_ENABLE_DASHBOARD ${LUS_ENABLE_DASHBOARD} CACHE BOOL
+  "Enable Experimental/Nightly builds with results send to Dashboard?"
+  )
 
 ## === Anaamika =================================================================
 
