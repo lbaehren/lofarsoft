597198014b17e3f2ccf7a9b9a9dc5d3f6a469e83
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-08-16 12:36:55 +0000

    deleting previous version of psrchive
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5613 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/external/psrchive/psrchive-13.0-patched/Base/Formats/WAPP/vanvleck.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VANVLECK_H
-#define _VANVLECK_H
-void vanvleck9lev(float *rho,int npts);
-int vanvleck3lev(float *rho,int npts);
-//double inv_cerf(double input);
-#endif
--- src/Pulsar/external/psrchive/psrchive-13.0-patched/config/sla.m4
+++
@@ -1,2 +0,0 @@
-# SWIN_LIB_SLA
-# This empty file exists to keep make dependencies happy in the interim
