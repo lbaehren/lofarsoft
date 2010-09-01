ecbfabe84d4e0459572af2764f7d08b6b42bf82c
terveen <terveen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-09-01 12:36:58 +0000

    minor changes
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5818 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/scripts/pipeline_start.py
+++ src/CR-Tools/implement/Pypeline/scripts/pipeline_start.py
@@ -16,12 +16,12 @@ for i in range(len(filenames)):
     filenames[i]=datadir+filenames[i]
 
 #start block and nrblocks to process
-startblock=0
-nblocks=20
-blocksize=1024
+startblock=30
+nblocks=1
+blocksize=1024*1024
 
 # Filter type for the FFT: Hanning or PPF
-filtertype="PPF"
+filtertype="Hanning"
 if filtertype is "PPF":
     #Check if PPF is possible
     assert blocksize == 1024
@@ -33,9 +33,9 @@ antennaset="HBA"
 
 #antenna selection (on number or by IDs)
 #Examples:
-#selection=[142000001,142000005,142003018]
+selection=[142000001]#,142000005,142002018]
 #selection=[0,5,8]
-selection=None
+#selection=None
 
 #Initialisation of arrays
 crfile=IO.open(filenames,blocksize,selection)
@@ -44,16 +44,15 @@ fftdata=crfile["emptyFFT"]
 crfile.setAntennaset(antennaset)
 
 # Apply calibration shift for clockdelays
-crfile.applyCalibrationShift()
+#crfile.applyCalibrationShift()
 
 # Gets Calibration object for weight and gain calibration
-MyCal=crfile.getCalibrator()
+#MyCal=crfile.getCalibrator()
 
 nyquistZone=crfile["nyquistZone"]
 
 # Obtain antenna positions. Last value True=hArray, False=np.array
-AntPos=md.get("RelativeAntennaPositions",crfile["antennaIDs"],crfile.antennaset,True)
-
+#AntPos=md.get("RelativeAntennaPositions",crfile["antennaIDs"],crfile.antennaset,True)
 for block in range(startblock,startblock+nblocks):
 
     crfile.readdata(fxdata,block)
@@ -62,16 +61,16 @@ for block in range(startblock,startblock+nblocks):
 
     # Apply filter before the FFT
     if filtertype is "Hanning":
-        fxdata.applyhanningfilter()
+        fxdata[...].applyhanningfilter()
     elif filtertype is "PPF":
+        fxdata.div(32768*2)
         myppf.add(fxdata)
-
     fftdata[...].fftcasa(fxdata[...],nyquistZone)
     # Apply calibration for station clock, antenna cables and station calibration (now only for LBA_OUTER, blocksize = 1024 )
-    MyCal.applyCalibration(fftdata)
+    #MyCal.applyCalibration(fftdata)
     
 #plot data:
 
 fftdata.abs()
 fftdata.log()
-fftdata[...].plot()
+fftdata[...].plot(clf=False)
