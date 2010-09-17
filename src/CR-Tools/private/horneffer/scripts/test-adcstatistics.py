df0e98c6588f255eee7381f27825acc58a07289b
horneffer <horneffer@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-09-17 12:10:01 +0000

    added datafile output to adcstatistics script
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5929 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/private/horneffer/scripts/test-adcstatistics.py
+++ src/CR-Tools/private/horneffer/scripts/test-adcstatistics.py
@@ -11,13 +11,17 @@ import matplotlib.pyplot as plt
 ---------------
 """
 #-------
-#datafiles = '/mnt/lofar/triggered-data/2010-07-07-CS003-CS005-CS006/trigger-dumps-2010-07-07-cs003/*'
+datafiles = '/mnt/lofar/triggered-data/2010-07-07-CS003-CS005-CS006/trigger-dumps-2010-07-07-cs003/*'
 #datafiles = '/mnt/lofar/triggered-data/2010-07-07-CS003-CS005-CS006/trigger-dumps-2010-07-07-cs005/*'
 #datafiles = '/mnt/lofar/triggered-data/2010-07-07-CS003-CS005-CS006/trigger-dumps-2010-07-07-cs006/*'
-datafiles = '/home/driss/usg/data/lofar/trigger-2010-02-11/*h5'
+#datafiles = '/home/driss/usg/data/lofar/trigger-2010-02-11/*h5'
 #-------
 sortstring = 'sort -rn --field-separator="-" --key=18'
 outfile = 'test-adcstatistics-out.txt'
+blocksize = 1024*63
+normalize = True
+blocks = [0]
+maxants = 96
 #------------------------------------------------------------------------
 
 fd = os.popen('ls '+ datafiles+' | ' + sortstring)
@@ -26,7 +30,8 @@ nofiles = len(files)
 print "Number of files to process:", nofiles
 fd.close()
 
-outfd = open(outfile, mode='w')
+summedhist = np.zeros((maxants,4096))
+
 for fileind in range(nofiles):
   print 'Processing file', fileind, 'out of', nofiles
   filename = files[fileind].strip()
@@ -35,21 +40,31 @@ for fileind in range(nofiles):
       dr = cr.open(filename,'LOFAR_TBB')
       #select the data before the triggered pulse
       # ***this implements a bias in the data!***
-      dr.blocksize = 1024*63  
-      dr.block = 0
+      dr.blocksize = blocksize
       ants = dr["nofantennas"]
       blocksize = dr["blocksize"]
       cr_efield = np.empty((ants, blocksize))
-      dr.read("Fx",cr_efield)
-      (histo, bins) = np.histogram(cr_efield[0], bins=4096, range=(-2048.5,2047.5))
-      plt.plot((bins[0:4096]+0.5),histo)
-      print bins
-      print histo
-      raw_input("Plotted a histogram - press Enter to continue...")
-
-      outfd.flush()
+      for blockno in blocks:
+        dr.block = blockno
+        dr.read("Fx",cr_efield)
+        for antno in range(min(ants,maxants)):
+          (histo, bins) = np.histogram(cr_efield[antno], bins=4096, range=(-2048.5,2047.5), normed=normalize)
+          summedhist[antno] += histo
+      
+      #plt.plot((bins[0:4096]+0.5),histo)
+      #print bins
+      #print histo
+      #raw_input("Plotted a histogram - press Enter to continue...")
     except (ZeroDivisionError, IndexError):
       pass
   else:
     print 'file \'',filename,'\' does not exist!'    
+outfd = open(outfile, mode='w')
+outfd.flush()
+for binno in range(4096):
+  outstring = '%5u '%(binno-2048)
+  for antno in range(min(ants,maxants)):
+    outstring += '%f '%(summedhist[antno,binno])
+  outstring += '\n'
+  outfd.write(outstring)
 outfd.close()
