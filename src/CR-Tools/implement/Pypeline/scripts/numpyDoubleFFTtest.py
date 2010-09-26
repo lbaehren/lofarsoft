9c6a1e8aaaf1ab636df77cf213efa84791b1280f
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-09-26 19:15:52 +0000

    Large Fourier transform script: now works on general factorizations of N = M K samples. Updated names and descriptions to describe correctly what happens.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@5982 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/scripts/numpyDoubleFFTtest.py
+++ src/CR-Tools/implement/Pypeline/scripts/numpyDoubleFFTtest.py
@@ -2,8 +2,8 @@
 #
 # Copyright (c) Arthur Corstanje, Sep 2010
 #
-# This implements and tests a size-N FFT, with N = M^2,
-# by splitting up the FFT in a radix-M decimation-in-frequency style
+# This implements and tests a size-N FFT, with N = M K,
+# by splitting up the FFT in a radix-M (or radix-K) splitting step.
 # When N is too large to fit in memory, like when we'll take 256 * 10^6 Samples from an entire TBB buffer,
 # the calculation can be done by doing 2 M FFTs of size-M, plus N complex multiplications (phase factors). 
 # In this case we'd have M = 16384 which is relatively small.
@@ -11,10 +11,10 @@
 #
 # So the procedure is:
 #
-# make a MxM matrix-shape out of the length-N signal
-# do FFT on all the rows in the matrix
+# make a MxK matrix-shape out of the length-N signal
+# do FFT on all the columns in the matrix (or: transpose first, then operate on rows)
 # multiply element-wise by a set of phase factors (see below)
-# do FFT on all the columns (or: transpose first, then again on rows)
+# do FFT on all the rows 
 # result is in the columns of this FFT.
 #
 # Therefore:
@@ -29,41 +29,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 pi = np.pi; cos = np.cos; sin = np.sin; sqrt = np.sqrt
 
-bigFFTblocksize = 1024        # small enough to be done in memory (for this test)
+bigFFTblocksize = 2048        # small enough to be done in memory (for this test)
 w0 = 2 * pi / bigFFTblocksize # fundamental frequency of the 'big' signal
-blocksize = 32                # = sqrt(bigFFTblocksize)
+rowBlocksize = 16                
+columnBlocksize = 128         # = bigFFTblocksize / rowBlocksize
 
-fftsize = blocksize           # / 2 + 1 for real input... first do all of it
+#fftsize = blocksize           # / 2 + 1 for real input... first do all of it
 
 t = np.arange(0, bigFFTblocksize)
 #x = 1 + sin((32+5) * w0 * t) 
 x = cos((100 + 20 * sin(w0*t)) * w0 * t) # The FFT loves frequency-modulated signals... 
 bigFFT = np.fft.fft(x)                   # done in one step, this is what we'll do below in two steps
 
-x = x.reshape(blocksize, blocksize) # put into M x M matrix
+x = x.reshape(columnBlocksize, rowBlocksize) # put into M x K matrix with M = size of row, K = size of column. And, # rows = N / columnBlocksize and vice versa.
 
 phaseFactorMatrix = (2*pi/bigFFTblocksize)*np.ones(bigFFTblocksize)
-phaseFactorMatrix = phaseFactorMatrix.reshape(blocksize, blocksize)
-for i in range(blocksize):
-    for k in range(blocksize):
-        phaseFactorMatrix[i, k] *= -(i*k)
+phaseFactorMatrix = phaseFactorMatrix.reshape(columnBlocksize, rowBlocksize)
+for i in range(columnBlocksize):
+    for k in range(rowBlocksize):
+        phaseFactorMatrix[i, k] *= -(i*k) 
 
-# We'll multiply the result of the FFT-by-rows by Phi_ik = exp( - j 2pi/N (i*k))
+# We'll multiply the result of the FFT-by-columns by Phi_ik = exp( - j 2pi/N (i*k))
 # Explanation follows elsewhere...
 
 phaseFactorMatrix = np.exp(phaseFactorMatrix * 1j) # complex exponential
 
-fftOverRows = np.fft.fft(x, axis=0)
+# in a large FFT, operations over columns mean you have to transpose first, then operate over rows... Not done here.
+fftOverColumns = np.fft.fft(x, axis=0)
     
-fftOverRows *= phaseFactorMatrix
+fftOverColumns *= phaseFactorMatrix
 
-fftOverColumns = np.fft.fft(fftOverRows, axis=1)
+fftOverRows = np.fft.fft(fftOverColumns, axis=1)
 
-testFFT=fftOverColumns.transpose()
+testFFT=fftOverRows.transpose()
 testFFT=testFFT.reshape(bigFFTblocksize)
 
 plt.plot(t, np.abs(bigFFT))
 plt.plot(t, np.abs(testFFT))
+print 'The mean squared difference between one-step and two-step FFTs is: %e' % np.linalg.norm(bigFFT - testFFT)
+print 'while the norm of the FFT is: %e' % np.linalg.norm(bigFFT)
+
 plt.show()
 
 
