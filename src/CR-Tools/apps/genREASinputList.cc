035ab20f1b9da30de8e050e4de6a5f930410b26b
schroeder <schroeder@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-10-01 10:18:38 +0000

    Change of genREASinputList (not backwards compatible!): All cuts have to be set in config file
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6017 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/apps/genREASinputList.cc
+++ src/CR-Tools/apps/genREASinputList.cc
@@ -60,22 +60,11 @@
 
   Example:
   \verbatim
-  # use default radio cut for EW, NS or BOTH polarizations
-
-  useEWpol
-  #useNSpol
-  #useBOTHpol
-
-  # Define minimum number of the antennas for the lateral distribution 
-  Nant_lopes30_min        15
-  Nant_lopesPol_min       10
-
-  # minimum CC height for KASCADE and Grande triggered events
-  CCcutA 9.5
-  CCcutG 7
-
   # Name of the different files that should be created ( .txt and .root )
-  namebase		REASinpuiList
+  namebase		REASinputList
+  
+  # Write all cuts (e.g. time + CC beam cuts into this config file)
+  TCut    ((goodReconstructed_EW)&&(EfieldMaxAbs<3000)&&((Xheight_EW/CCheight_EW)>0.8)&&((Xheight_EW/CCheight_EW)<1.5)&&(CCheight_EW/rmsCCbeam_EW>14*sqrt(NCCbeamAntennas_EW/30)))
   \endverbatim
 */
 
@@ -113,7 +102,7 @@ int main(int argc, char* argv[])
   char KRETAver[1024];
   Int_t Nant_lopes30_min=10,Nant_lopesPol_min=5;
   // default cuts for CC beam
-  double CCcutA = 9.5, CCcutG = 7;
+  //double CCcutA = 9.5, CCcutG = 7;
 
   TCut cut;
 
@@ -129,10 +118,10 @@ int main(int argc, char* argv[])
       opt >> Nant_lopesPol_min;
     if(buf.compare("TCut")==0)
       opt>>cut_str;
-    if(buf.compare("CCcutA")==0) 
-      opt >> CCcutA;
-    if(buf.compare("CCcutG")==0) 
-      opt >> CCcutG;
+    //if(buf.compare("CCcutA")==0) 
+    //  opt >> CCcutA;
+    //if(buf.compare("CCcutG")==0) 
+    //  opt >> CCcutG;
 
     if(buf.compare("useEWpol")==0) 
       cut = TCut("(Xheight_EW/CCheight_EW)>=-0.6&&(Xheight_EW/CCheight_EW)<=1.4");
@@ -165,22 +154,21 @@ int main(int argc, char* argv[])
   TFile *f = new TFile( argv[1], "READ");
   TTree *t1 =  (TTree*)f->Get("T;1");
 
-  TFile *ftemp = new TFile ("tempfile.root", "RECREATE");
   TTree *t2 = t1->CopyTree(cut);
 
-  UInt_t Gt;
-  Float_t Age,Ageg,Az,Ze,Xc,Yc,Xcg,Ycg,Azg,Zeg,Nmu,Lmuo,Size,Sizeg,Sizmg;
+  UInt_t Gt = 0;
+  Float_t Age=0,Ageg=0,Az=0,Ze=0,Xc=0,Yc=0,Xcg=0,Ycg=0,Azg=0,Zeg=0,Nmu=0,Lmuo=0,Size=0,Sizeg=0,Sizmg=0;
   //char Eventname[64];
-  Int_t CutSNR_EW,CutSNR_NS;
-  Int_t NlateralAntennas_EW,NlateralAntennas_NS,NCCbeamAntennas_EW,NCCbeamAntennas_NS;
-  Double_t lgE, lgEg, lnA, lnAg, err_lgE, err_lgEg, err_lnA, err_lnAg;
-  Double_t err_core, err_coreg, err_Az, err_Azg, err_Ze, err_Zeg;
-  Double_t geomag_angle, geomag_angleg;
-  Double_t chi2NDF_EW,chi2NDF_NS,eps_EW,eps_NS;
-  Double_t Xheight_EW,Xheight_NS,Xheight_error_EW,Xheight_error_NS;
-  Double_t CCheight_EW,CCheight_NS,CCheight_error_EW,CCheight_error_NS,R_0_EW,R_0_NS;
-  Double_t rmsCCbeam_EW,rmsCCbeam_NS,sigR_0_EW,sigR_0_NS,sigeps_EW,sigeps_NS;
-  Char_t reconstruction;
+  Int_t CutSNR_EW=0,CutSNR_NS=0;
+  Int_t NlateralAntennas_EW=0,NlateralAntennas_NS=0,NCCbeamAntennas_EW=0,NCCbeamAntennas_NS=0;
+  Double_t lgE=0, lgEg=0, lnA=0, lnAg=0, err_lgE=0, err_lgEg=0, err_lnA=0, err_lnAg=0;
+  Double_t err_core=0, err_coreg=0, err_Az=0, err_Azg=0, err_Ze=0, err_Zeg=0;
+  Double_t geomag_angle=0, geomag_angleg=0;
+  Double_t chi2NDF_EW=0,chi2NDF_NS=0,eps_EW=0,eps_NS=0;
+  Double_t Xheight_EW=0,Xheight_NS=0,Xheight_error_EW=0,Xheight_error_NS=0;
+  Double_t CCheight_EW=0,CCheight_NS=0,CCheight_error_EW=0,CCheight_error_NS=0,R_0_EW=0,R_0_NS=0;
+  Double_t rmsCCbeam_EW=0,rmsCCbeam_NS=0,sigR_0_EW=0,sigR_0_NS=0,sigeps_EW=0,sigeps_NS=0;
+  Char_t reconstruction=0;
 
   t2->SetBranchAddress("Age",&Age);
   t2->SetBranchAddress("Ageg",&Ageg);
@@ -283,7 +271,7 @@ int main(int argc, char* argv[])
        <<"TCut = "<<cut<<endl
      //  <<"Nant_lopes30_min required for lat. distr. = " <<Nant_lopes30_min <<endl
       // <<"Nant_lopesPol_min required for lat. distr.= " <<Nant_lopesPol_min <<endl
-       <<"LOPES-GT	Source	KRETA_ver	az_in(deg)	errAz_in(deg)	ze_in(deg)	errZe_in(deg)	coreN_in(m)	coreE_in(m)	errCore(m)	Ne_in	Nmu_in	Nmu^tr_in	E_in(eV)	errlgE_in	Age_in\n"
+       <<"LOPES-GT \tSource \tKRETA \taz(deg)\t errAz  \tze(deg)\terrZe  \tcoreN(m) \tcoreE(m)\terrCor \tNe_in \tNmu_in \tNmu^tr \tE_in(eV) \terrlgE_in(GeV) \tAge_in\n"
        <<"======================================================================================================================================================================================================================================================================\n";
 
    int count=0;
@@ -295,69 +283,8 @@ int main(int argc, char* argv[])
   TTree *k = new TTree("k","KASCADE-Information for eventlist");
   //recreated same root file after the cuts
 
-  //k->Branch("Age",&Age,"Age/F");
-  //k->Branch("Ageg",&Ageg,"Age/F");
-  //k->Branch("Az",&Az,"Az/F");
-  //k->Branch("Azg",&Az,"Azg/F");
-  //k->Branch("CCheight_EW",&CCheight_EW,"CCheight_EW/D");
-  //k->Branch("CCheight_NS",&CCheight_NS,"CCheight_NS/D");
-  //k->Branch("CCheight_error_EW",&CCheight_error_EW,"CCheight_error_EW/D");
-  //k->Branch("CCheight_error_NS",&CCheight_error_NS,"CCheight_error_NS/D");
-  //k->Branch("CutSNR_EW",&CutSNR_EW,"CutSNR_EW/I");
-  //k->Branch("CutSNR_NS",&CutSNR_NS,"CutSNR_NS/I");
-  //k->Branch("Eventname",&Eventname,"Eventname/C");
   k->Branch("Gt",&Gt,"Gt/i");
   k->Branch("KRETAver",&KRETAver,"KRETAver/C");
-  //k->Branch("Lmuo",&Lmuo,"Lmuo/F");
-  //k->Branch("NCCbeamAntennas_EW",&NCCbeamAntennas_EW,"NCCbeamAntennas_EW/I");
-  //k->Branch("NCCbeamAntennas_NS",&NCCbeamAntennas_NS,"NCCbeamAntennas_NS/I");
-  //k->Branch("NlateralAntennas_EW",&NlateralAntennas_EW,"NlateralAntennas_EW/I");
-  //k->Branch("NlateralAntennas_NS",&NlateralAntennas_NS,"NlateralAntennas_NS/I");
-  //k->Branch("Nmu",&Nmu,"Nmu/F");
-  //k->Branch("R_0_EW",&R_0_EW,"R_0_EW/D");
-  //k->Branch("R_0_NS",&R_0_NS,"R_0_NS/D");
-  //k->Branch("Size",&Size,"Size/F");
-  //k->Branch("Sizeg",&Sizeg,"Sizeg/F");
-  //k->Branch("Sizmg",&Sizmg,"Sizmg/F");
-  //k->Branch("Xc",&Xc,"Xc/F");
-  //k->Branch("Yc",&Yc,"Yc/F");
-  //k->Branch("Xcg",&Xcg,"Xcg/F");
-  //k->Branch("Ycg",&Ycg,"Ycg/F");
-  //k->Branch("Xheight_EW",&Xheight_EW,"Xheight_EW/D");
-  //k->Branch("Xheight_NS",&Xheight_NS,"Xheight_NS/D");
-  //k->Branch("Xheight_error_EW",&Xheight_error_EW,"Xheight_error_EW/D");
-  //k->Branch("Xheight_error_NS",&Xheight_error_NS,"Xheight_error_NS/D");
-  //k->Branch("Ze",&Ze,"Ze/F");
-  //k->Branch("Zeg",&Zeg,"Zeg/F");
-  //k->Branch("chi2NDF_EW",&chi2NDF_EW,"chi2NDF_EW/D");
-  //k->Branch("chi2NDF_NS",&chi2NDF_NS,"chi2NDF_NS/D");
-  //k->Branch("eps_EW",&eps_EW,"eps_EW/D");
-  //k->Branch("eps_NS",&eps_NS,"eps_NS/D");
-  //k->Branch("err_Az",&err_Az,"err_Az/D");
-  //k->Branch("err_Azg",&err_Azg,"err_Azg/D");
-  //k->Branch("err_Ze",&err_Ze,"err_Ze/D");
-  //k->Branch("err_Zeg",&err_Zeg,"err_Zeg/D");
-  //k->Branch("err_core",&err_core,"err_core/D");
-  //k->Branch("err_coreg",&err_coreg,"err_coreg/D");
-  //k->Branch("err_lgE",&err_lgE,"err_lgE/D");
-  //k->Branch("err_lgEg",&err_lgEg,"err_lgEg/D");
-  //k->Branch("err_lnA",&err_lnA,"err_lnA/D");
-  //k->Branch("err_lnAg",&err_lnAg,"err_lnAg/D");
-  //k->Branch("geomag_angle",&geomag_angle,"geomag_angle/D");
-  //k->Branch("geomag_angleg",&geomag_angleg,"geomag_angleg/D");
-  //k->Branch("lgE",&lgE,"lgE/D");
-  //k->Branch("lgEg",&lgEg,"lgEg/D");
-  //k->Branch("lnA",&lnA,"lnA/D");
-  //k->Branch("lnAg",&lnAg,"lnAg/D");
-  //k->Branch("rmsCCbeam_EW",&rmsCCbeam_EW,"rmsCCbeam_EW/D");
-  //k->Branch("rmsCCbeam_NS",&rmsCCbeam_NS,"rmsCCbeam_NS/D");
-  //k->Branch("sigR_0_EW",&sigR_0_EW,"sigR_0_EW/D");
-  //k->Branch("sigR_0_NS",&sigR_0_NS,"sigR_0_NS/D");
-  //k->Branch("sigeps_EW",&sigeps_EW,"sigeps_EW/D");
-  //k->Branch("sigeps_NS",&sigeps_NS,"sigeps_NS/D");
-
-
-
 
   // Additional eventlist info file *******************************************************************
   ofstream f1;
@@ -369,7 +296,7 @@ int main(int argc, char* argv[])
         <<"TCut = "<<cut<<endl
        // <<"Nant_lopes30_min required for lat. distr. = " <<Nant_lopes30_min <<endl
        // <<"Nant_lopesPol_min required for lat. distr.= " <<Nant_lopesPol_min <<endl
-        <<"LOPES-GT	Source	KRETA_ver	az_in(deg)	errAz_in(deg)	ze_in(deg)	errZe_in(deg)	coreN_in(	coreE_in(m)	errCore(m)	Ne_in	Nmu_in	Nmu^tr_in	E_in(eV)	errlgE_in(GeV)	Age_in\n"
+        <<"LOPES-GT \tSource \tKRETA \taz(deg)\t errAz  \tze(deg)\terrZe  \tcoreN(m) \tcoreE(m)\terrCor \tNe_in \tNmu_in \tNmu^tr \tE(eV) \terrlgE_in(GeV) \tAge_in\n"
         <<"======================================================================================================================================================================================================================================================================\n";
 
   //some variables declaration
@@ -402,66 +329,15 @@ int main(int argc, char* argv[])
     err_Zeg=err_Zeg*(180./TMath::Pi());
 
     if( reconstruction=='A' ) {// KASCADE reconstruction!!!!
-      if(string(cut).find("(Xheight_EW/CCheight_EW)")!=string::npos) {
-        cout << "\nUsing EW cut..." << endl;
-        if((Gt <= 1165389469 && NlateralAntennas_EW>Nant_lopes30_min) || (Gt >= 1165738743 && NlateralAntennas_EW>Nant_lopesPol_min) ) {
-          if((CCheight_EW/rmsCCbeam_EW)>CCcutA) {
-            f1<<Gt<<"\t"<<"A"<<"\t"<<"KRETAver"<<"\t"<<Az<<"\t"<<err_Az<<"\t"<<Ze<<"\t"<<err_Ze<<"\t"<<Ylopes<<"\t"<<Xlopes<<"\t"<<err_core<<"\t"<<Size<<"\t"<<Nmu<<"\t"<<Lmuo<<"\t"<<E*pow(10,9)<<"\t"<<err_lgE<<"\t"<<Age<<endl;
-            count++;
-            k->Fill();
-          }
-        }
-      } else if (string(cut).find("(Xheight_NS/CCheight_NS)")!=string::npos) {
-        cout << "\nUsing NS cut..." << endl;
-        if((Gt <= 1165389469 && NlateralAntennas_NS>Nant_lopes30_min) || (Gt >= 1165738743 && NlateralAntennas_NS>Nant_lopesPol_min) ) {
-          if((CCheight_EW/rmsCCbeam_EW)>CCcutA) {
-             f1<<Gt<<"\t"<<"A"<<"\t"<<"KRETAver"<<"\t"<<Az<<"\t"<<err_Az<<"\t"<<Ze<<"\t"<<err_Ze<<"\t"<<Ylopes<<"\t"<<Xlopes<<"\t"<<err_core<<"\t"<<Size<<"\t"<<Nmu<<"\t"<<Lmuo<<"\t"<<E*pow(10,9)<<"\t"<<err_lgE<<"\t"<<Age<<endl;
-            count++;
-            k->Fill();
-          }
-        }
-      } else if (string(cut).find("(Xheight_EW/CCheight_EW)<=1.4)&&((Xheight_NS/CCheight_NS)")!=string::npos) {
-        cout << "\nUsing BOTH pol. cut..." << endl;
-        if(   (Gt <= 1165389469 && NlateralAntennas_EW>Nant_lopes30_min && NlateralAntennas_NS>Nant_lopes30_min) 
-            || (Gt >= 1165738743 && NlateralAntennas_EW>Nant_lopesPol_min && NlateralAntennas_NS>Nant_lopesPol_min) ) {
-          if(sqrt(pow(CCheight_EW/rmsCCbeam_EW,2)+pow(CCheight_NS/rmsCCbeam_NS,2))>CCcutA) {
-              f1<<Gt<<"\t"<<"A"<<"\t"<<"KRETAver"<<"\t"<<Az<<"\t"<<err_Az<<"\t"<<Ze<<"\t"<<err_Ze<<"\t"<<Ylopes<<"\t"<<Xlopes<<"\t"<<err_core<<"\t"<<Size<<"\t"<<Nmu<<"\t"<<Lmuo<<"\t"<<E*pow(10,9)<<"\t"<<err_lgE<<"\t"<<Age<<endl;
-              count++;
-              k->Fill();
-          }
-        }
-      }
-      
+      f1<<Gt<<"\t"<<"A"<<"\t"<<KRETAver<<"\t"<<Az<<"\t"<<err_Az<<"\t"<<Ze<<"\t"<<err_Ze<<"\t"<<Ylopes<<"\t"<<Xlopes<<"\t"<<err_core<<"\t"<<Size<<"\t"<<Nmu<<"\t"<<Lmuo<<"\t"<<E*pow(10,9)<<"\t"<<err_lgE<<"\t"<<Age<<endl;
+      cout<<Gt<<"\t"<<"A"<<"\t"<<KRETAver<<"\t"<<Az<<"\t"<<err_Az<<"\t"<<Ze<<"\t"<<err_Ze<<"\t"<<Ylopes<<"\t"<<Xlopes<<"\t"<<err_core<<"\t"<<Size<<"\t"<<Nmu<<"\t"<<Lmuo<<"\t"<<E*pow(10,9)<<"\t"<<err_lgE<<"\t"<<Age<<endl;
+      ++count;
+      k->Fill();
     } else if(reconstruction=='G') {
-      if(string(cut).find("(Xheight_EW/CCheight_EW)")!=string::npos) {
-        cout << "\nUsing EW cut..." << endl;
-        if((Gt <= 1165389469 && NlateralAntennas_EW>Nant_lopes30_min) || (Gt >= 1165738743 && NlateralAntennas_EW>Nant_lopesPol_min) ) {
-          if((CCheight_EW/rmsCCbeam_EW)>CCcutG) {
-            f1<<Gt<<"\t"<<"G"<<"\t"<<"KRETAver"<<"\t"<<Azg<<"\t"<<err_Azg<<"\t"<<Zeg<<"\t"<<err_Zeg<<"\t"<<Ylopesg<<"\t"<<Xlopesg<<"\t"<<err_coreg<<"\t"<<Sizeg<<"\t"<<Sizmg<<"\t"<<0.<<"\t"<<Eg*pow(10,9)<<"\t"<<err_lgEg<<"\t"<<Ageg<<endl;
-            count++;
-            k->Fill();
-          }
-        }
-      } else if (string(cut).find("(Xheight_NS/CCheight_NS)")!=string::npos) {
-        cout << "\nUsing NS cut..." << endl;
-        if((Gt <= 1165389469 && NlateralAntennas_NS>Nant_lopes30_min) || (Gt >= 1165738743 && NlateralAntennas_NS>Nant_lopesPol_min) ) {
-          if((CCheight_EW/rmsCCbeam_EW)>CCcutG) {
-            f1<<Gt<<"\t"<<"G"<<"\t"<<"KRETAver"<<"\t"<<Azg<<"\t"<<err_Azg<<"\t"<<Zeg<<"\t"<<err_Zeg<<"\t"<<Ylopesg<<"\t"<<Xlopesg<<"\t"<<err_coreg<<"\t"<<Sizeg<<"\t"<<Sizmg<<"\t"<<0.<<"\t"<<Eg*pow(10,9)<<"\t"<<err_lgEg<<"\t"<<Ageg<<endl;
-            count++;
-            k->Fill();
-          }
-        }
-      } else if (string(cut).find("(Xheight_EW/CCheight_EW)<=1.4)&&((Xheight_NS/CCheight_NS)")!=string::npos) {
-        cout << "\nUsing BOTH pol. cut..." << endl;
-        if(   (Gt <= 1165389469 && NlateralAntennas_EW>Nant_lopes30_min && NlateralAntennas_NS>Nant_lopes30_min) 
-            || (Gt >= 1165738743 && NlateralAntennas_EW>Nant_lopesPol_min && NlateralAntennas_NS>Nant_lopesPol_min) ) {
-          if(sqrt(pow(CCheight_EW/rmsCCbeam_EW,2)+pow(CCheight_NS/rmsCCbeam_NS,2))>CCcutG) {
-            f1<<Gt<<"\t"<<"G"<<"\t"<<"KRETAver"<<"\t"<<Azg<<"\t"<<err_Azg<<"\t"<<Zeg<<"\t"<<err_Zeg<<"\t"<<Ylopesg<<"\t"<<Xlopesg<<"\t"<<err_coreg<<"\t"<<Sizeg<<"\t"<<Sizmg<<"\t"<<0.<<"\t"<<Eg*pow(10,9)<<"\t"<<err_lgEg<<"\t"<<Ageg<<endl;
-            count++;
-            k->Fill();
-          }
-        }
-      }
+      f1<<Gt<<"\t"<<"G"<<"\t"<<KRETAver<<"\t"<<Azg<<"\t"<<err_Azg<<"\t"<<Zeg<<"\t"<<err_Zeg<<"\t"<<Ylopesg<<"\t"<<Xlopesg<<"\t"<<err_coreg<<"\t"<<Sizeg<<"\t"<<Sizmg<<"\t"<<0.<<"\t"<<Eg*pow(10,9)<<"\t"<<err_lgEg<<"\t"<<Ageg<<endl;
+      cout<<Gt<<"\t"<<"G"<<"\t"<<KRETAver<<"\t"<<Azg<<"\t"<<err_Azg<<"\t"<<Zeg<<"\t"<<err_Zeg<<"\t"<<Ylopesg<<"\t"<<Xlopesg<<"\t"<<err_coreg<<"\t"<<Sizeg<<"\t"<<Sizmg<<"\t"<<0.<<"\t"<<Eg*pow(10,9)<<"\t"<<err_lgEg<<"\t"<<Ageg<<endl;
+      ++count;
+      k->Fill();
     } else {
       cerr << "\n\nERROR: Event reconstruction is neither Array (KASCADE) nor Grande!\n" << endl;
     }
@@ -471,8 +347,7 @@ int main(int argc, char* argv[])
   fout->Close();
   f1.close();
 
-  ftemp->Close();
-  ftemp->Delete();
+  cout<<count<<" events written in REAS input list:"<<endl;
   cout<<namebase<<".txt written"<<endl;
   cout<<namebase<<".root written"<<endl;
 
