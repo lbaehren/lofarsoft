ca727231958b00034e33460157af72de15768621
kenneth <kenneth@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-10-31 21:32:05 +0000

    new interface
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6199 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/pipeline/pulp.py
+++ src/Pulsar/pipeline/pulp.py
@@ -9,23 +9,65 @@ hand.
 """
 
 from __future__ import with_statement
-import sys, os
+
 from lofarpipe.support.control import control
 from lofarpipe.support.utilities import log_time
 from lofarpipe.support.clusterhandler import ipython_cluster
+import lofarpipe.support.lofaringredient as ingredient
 
+import sys, os
 
 class pulp(control):
-    def __init__(self):
-        super(pulp, self).__init__()
+
+    """
+    Provides a convenient, pipeline-based mechanism of running pulp
+    on a dataset.
+    """
+
+    inputs = {
+        'obsid' : ingredient.StringField(
+            '--obsid',
+            dest="obsid",
+            help="Observation identifier"
+        ),
+        'pulsar' : ingredient.StringField(
+            '--pulsar',
+            dest="pulsar",
+            help="Pulsar name"
+        ),
+        'filefactor' : ingredient.IntField(
+            '--filefactor',
+             dest="filefactor",
+             help="factor by which obsid subbands will be RSP split."
+        ),
+        'arch' : ingredient.StringField(
+            '--arch',
+            dest="arch",
+            help="Destination archive for output, pick one of arch134, etc."
+        )
+    }
+
+    outputs = {
+        'data': ingredient.ListField()
+    }
 
     def pipeline_logic(self):
+
+        obsid      = self.inputs['obsid']
+        pulsar     = self.inputs['pulsar']
+        arch       = self.inputs['arch']
+        filefactor = self.inputs['filefactor']
+
         with log_time(self.logger):
             with ipython_cluster(self.config, self.logger):
-                self.run_task("buildPulsArch", obsid="L2010_06160", pulsar="B0329+54", filefactor=4)
-                self.run_task("bf2presto",     obsid="L2010_06160", pulsar="B0329+54", filefactor=4)
-                self.run_task("prepfold",      obsid="L2010_06160", pulsar="B0329+54", filefactor=4)
+		self.run_task("buildPulsArch",obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
+                self.run_task("bf2presto",    obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
+                self.run_task("buildRSPAll",  obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
+                self.run_task("prepareInf",   obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
+                self.run_task("prepfold",     obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
+                self.run_task("rfiplot",      obsid=obsid,pulsar=pulsar,filefactor=filefactor,arch=arch)
         return
+
 
 if __name__ == '__main__':
     sys.exit(pulp().main())
