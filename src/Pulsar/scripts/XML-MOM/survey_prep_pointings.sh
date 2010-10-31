cdbb9da643d3d2312b6009c8fc4609de91ba40b9
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-10-31 17:43:27 +0000

    added ability to input a list of row numbers instead of a range of rows
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6186 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/XML-MOM/survey_prep_pointings.sh
+++ src/Pulsar/scripts/XML-MOM/survey_prep_pointings.sh
@@ -6,8 +6,11 @@ USAGE="\nusage : survey_prep_pointings.sh pointing_range pointings_file xml_scri
 "      pointings_file  ==> Input pointings table (i.e. LOFAR_HBA_pointings_cluster7_101021.txt) \n"\
 "      xml_script_options  ==> All required arguments to run the multi-templatesNbeams.sh script (except -in infile) \n"\
 "\n"\
-"      Example:\n"\
-"      survey_prep_pointings.sh 75-139 LOFAR_HBA_catalog_cluster7_101031.txt -inswitch BF -intype position -project Pulsars -out output.xml -start 2010-07-22T09:40:00 -time 25 -stations CS001,CS002 -antenna HBA +multi -namecol \n"
+"      Example using range:\n"\
+"      survey_prep_pointings.sh 75-139 LOFAR_HBA_catalog_cluster7_101031.txt -inswitch BF -intype position -project Pulsars -out output.xml -start 2010-07-22T09:40:00 -time 25 -stations CS001,CS002 -antenna HBA +multi -namecol \n"\
+"\n"\
+"      Example using row list:\n"\
+"      survey_prep_pointings.sh 75,79,93,106 LOFAR_HBA_catalog_cluster7_101031.txt -inswitch BF -intype position -project Pulsars -out output.xml -start 2010-07-22T09:40:00 -time 25 -stations CS001,CS002 -antenna HBA +multi -namecol \n"
 
 
 range=""
@@ -44,11 +47,31 @@ then
    exit 1
 fi
 
-min_range=`echo $range | sed 's/-.*//g'`
-max_range=`echo $range | sed 's/.*-//g'`
+is_range=`echo $range | grep "-"`
+is_comma=`echo $range | grep ","`
+if [[ $is_range != "" ]]
+then
+   min_range=`echo $range | sed 's/-.*//g'`
+   max_range=`echo $range | sed 's/.*-//g'`
+   RANGE=1
+elif [[ $is_comma != "" ]]
+then
+   RANGE=0
+   tmp_list=`echo $range | sed 's/,/ /g'`
+   range=$tmp_list
+else #assuming single row
+   min_range=$range
+   max_range=$range
+   RANGE=1
+fi
 
 echo "Using input file $infile"
-echo "Start at pointing $min_range and go to $max_range"
+if (( $RANGE == 1 ))
+then
+   echo "Start at pointing $min_range and go to $max_range"
+else
+   echo "Using a comma separated list of row numbers"
+fi
 tmp=/tmp/$$survey.txt
 # delete all lines with #
 # get all pointings w/o reduntant central pointing
@@ -59,7 +82,17 @@ tmp=/tmp/$$survey.txt
 # 1st version does not name the NAMECOL in results:
 # grep -v "#" $infile | grep "_[0-9]" | awk '{print $1 " " $4 " " $5}' | sed '/_0[0-5]/s/$/ ;/g' | sed -e :a -e '/;$/N; s/;\n/ ; /; ta' | sed -n "$min_range,$max_range p" | sed 's/LOFAR_HBA....._.. //g' > $tmp
 # 2nd version does have the NAMECOL in 1st beam results:
-grep -v "#" $infile | grep "_[0-9]" | awk '{print $1 " " $4 " " $5}' | sed '/_0[0-5]/s/$/ ;/g' | sed -e :a -e '/;$/N; s/;\n/ ; /; ta' | sed -n "$min_range,$max_range p" | sed 's/LOFAR_HBA....._0[1-9] //g' | sed 's/_00//g' > $tmp
+
+if (( $RANGE == 1 ))
+then
+   grep -v "#" $infile | grep "_[0-9]" | awk '{print $1 " " $4 " " $5}' | sed '/_0[0-5]/s/$/ ;/g' | sed -e :a -e '/;$/N; s/;\n/ ; /; ta' | sed -n "$min_range,$max_range p" | sed 's/LOFAR_HBA....._0[1-9] //g' | sed 's/_00//g' > $tmp
+else 
+   touch $tmp
+   for ii in $range
+   do
+      grep -v "#" $infile | grep "_[0-9]" | awk '{print $1 " " $4 " " $5}' | sed '/_0[0-5]/s/$/ ;/g' | sed -e :a -e '/;$/N; s/;\n/ ; /; ta' | sed -n "$ii,$ii p" | sed 's/LOFAR_HBA....._0[1-9] //g' | sed 's/_00//g' >> $tmp
+   done 
+fi
 
 # run the xml script with the tmp file as input and rest of the arguments
 echo "Running command:"
