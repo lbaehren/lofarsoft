b96b53af7a05181f26ae21d6eaf06bb465776e5b
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-11-02 21:46:26 +0000

    changed the pipeline version number for the above changes just put back
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6214 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
+++ src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
@@ -4,7 +4,7 @@
 # N core defaul is = 8 (cores)
 
 #PLEASE increment the version number when you edit this file!!!
-VERSION=1.29
+VERSION=1.30
 
 #Check the usage
 USAGE="\nusage : make_subs_SAS_Ncore_Mmodes.sh -id OBS_ID -p Pulsar_names -o Output_Processing_Location [-core N] [-all] [-all_pproc] [-rfi] [-rfi_ppoc] [-C] [-del] [-incoh_only] [-coh_only] [-incoh_redo] [-coh_redo] [-transpose] [-help]\n\n"\
