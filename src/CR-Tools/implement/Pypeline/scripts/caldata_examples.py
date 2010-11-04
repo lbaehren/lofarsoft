299d3538740c554eb6206fd7d299ac1acfb89a70
cwjames <cwjames@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-11-04 15:06:01 +0000

    Modified 'caldata_examples'
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6233 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/CR-Tools/implement/Pypeline/scripts/caldata_examples.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python 
+
+
+######## A Modified Pipeline Start Script by Clancy ##########
+
+# this is intended to show off the module 'caldata'
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pyfits
+import pycrtools as cr
+from IOpkg import calData as cal
+import time as time
+
+reload(cal)
+
+
+LOFARSOFT=os.environ["LOFARSOFT"]
+datadir=LOFARSOFT+'/data/lofar/trigger-2010-04-15/'
+filename=["triggered-pulse-2010-04-15-RS205.h5"]
+
+filename[0]=datadir+filename[0]
+
+#########
+
+# initiates a calibration data object with default values
+print 'Initiating class with default values only'
+caldata = cal.calData(filename, "LBA_OUTER")
+# normally we perform a calibration, but this data is crap
+print 'Default for the fft is ', caldata.fftMethod
+print 'Default for the filter is: ', caldata.filterType
+print 'Default block size is: ', caldata.blockSize
+print 'Calibrate? ', caldata.calMethod
+
+print 'We have not set any antenna selection: ', caldata.antennaSelection
+print 'so do not have any o/p-related quantities: '
+print '   nAntennas = ', caldata.nAntennas
+print '   op array = ', caldata.timeData
+print '   op array = ', caldata.freqData
+print '\nand hence can not get data back: '
+
+data=caldata.getFFTData()
+print ' the data is: ', data
+
+print '\n\n But when we DO select the antennas: '
+selection=range(20,38)
+caldata.setAntennaSelection(selection)
+
+caldata.setFrequencyRange(3e7,5e7)
+
+print 'We DO have these quantities!'
+print '   nAntennas = ', caldata.nAntennas
+print '   op array = ', caldata.timeData
+print '   op array = ', caldata.freqData
+print '\nand CAN get frequency-domain data back! '
+print caldata.getFFTData()
+print 'from block: ', caldata.currentBlock, 'and again:'
+print caldata.getFFTData()
+print 'from ', caldata.currentBlock
+print '\n\n'
+
+print 'we can also ask to get back various quantities ourselves'
+freqData,timeData, allfrequencies=caldata.getFFTData()
+print 'which can be plotted in the usual way'
+print freqData, '\n', timeData, '\n', allfrequencies
+print '\n\n\n'
+
+print '\nIf we want to plot, we can ask caldata what frequencies it is returning'
+print 'frequencies in Hz are: ', caldata.frequencies
+
+print 'It will also give us the antenna positions'
+print 'Antenna positions are: ', caldata.getAntennaPositions()
+
+print '\n\n If we want to use our own arrays which will ',\
+'not be over-written for future calls, we can get the right dimensions:'
+freqdata=cr.hArray(complex,caldata.getOPDim())
+allFreqData=cr.hArray(complex, caldata.getAllFreqDim())
+timeData=cr.hArray(float, caldata.getTimeDim())
+print 'and just send these arrays to the get routine: '
+freqData=caldata.getFFTData(allFreqData=allFreqData, timeData=timeData)
+print 'and these will be used, while the internal arrays will remain unaltered'
+
--- src/CR-Tools/implement/Pypeline/scripts/pipeline_start.py
+++ src/CR-Tools/implement/Pypeline/scripts/pipeline_start.py
@@ -10,17 +10,31 @@ import IO as IO
 
 
 #Configuration
-datadir="/Users/STV/Astro/data/TBB/Crab/pulse1/"
-filenames=["CS302C-B0T5:00:39.h5","CS302C-B1T5:00:39.h5","CS302C-B2T5:00:39.h5","CS302C-B3T5:00:39.h5","CS302C-B4T5:00:39.h5","CS302C-B5T5:00:39.h5"]
+# we need to have a more sensible way of getting to the file/filenames themselves
+LOFARSOFT=os.environ["LOFARSOFT"]
+
+######### Now we define what data we want to work on and get it ##############
+
+datadir=LOFARSOFT+'/data/lofar/trigger-2010-04-15/'
+filenames=["triggered-pulse-2010-04-15-RS205.h5"]
+
 for i in range(len(filenames)):
     filenames[i]=datadir+filenames[i]
 
 #start block and nrblocks to process
-startblock=30
-nblocks=1
-blocksize=1024*1024
+# if ppf operates on 16 blocks at once,
+# then really we only have 4 useful blocks
+# here before edge effects make us go insane
+# we need to mke this more logical!
+
+startblock=7
+nblocks=100
+blocksize=1024
+
+######## Now we define what we want to do with the data ###########
 
 # Filter type for the FFT: Hanning or PPF
+#filtertype="PPF"
 filtertype="Hanning"
 if filtertype is "PPF":
     #Check if PPF is possible
@@ -28,14 +42,20 @@ if filtertype is "PPF":
     import PPF
     myppf=PPF.PPF()
 
+########## Now we again look at subsets of the data ###########
+
 #Antenna set
-antennaset="HBA"
+antennaset="LBA_OUTER"
 
 #antenna selection (on number or by IDs)
 #Examples:
-selection=[142000001]#,142000005,142002018]
+#selection=[142000001]#,142000005,142002018]
 #selection=[0,5,8]
+selection=[8]
 #selection=None
+
+####### Now we read in the actual data and perform checks to ensure ########
+####### that we will be able to do to it what we want to do with it ########
 
 #Initialisation of arrays
 crfile=IO.open(filenames,blocksize,selection)
@@ -50,13 +70,21 @@ crfile.setAntennaset(antennaset)
 #MyCal=crfile.getCalibrator()
 
 nyquistZone=crfile["nyquistZone"]
+sin_freq=20.
+#sweights0=np.linspace(0.,1023., 1024)
+#sweights=hf.hArray(sweights0)
+#sweights.mul(2.*hf.pi/sin_freq)
+#sweights.sin()
 
 # Obtain antenna positions. Last value True=hArray, False=np.array
 #AntPos=md.get("RelativeAntennaPositions",crfile["antennaIDs"],crfile.antennaset,True)
+# this should be much more efficient. We can use fftw in the advanced format to create
+# one long fft over the whole block. At the very least do this for a bunch of stuff
 for block in range(startblock,startblock+nblocks):
 
     crfile.readdata(fxdata,block)
     
+#    fxdata.mul(sweights)
     #Quality check, to be made
 
     # Apply filter before the FFT
