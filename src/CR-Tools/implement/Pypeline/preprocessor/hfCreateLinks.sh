67ab0d1412c880caeb9a103c9428613246810ff5
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-11-05 11:59:30 +0000

    - [hftools] remove python links before reinstalling them.\n- Module gets rebuild if *any* of the header files changes.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6244 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/preprocessor/hfCreateLinks.sh
+++ src/CR-Tools/implement/Pypeline/preprocessor/hfCreateLinks.sh
@@ -3,7 +3,7 @@
 src=$(cd $1 ; pwd)
 dst=$(cd $2 ; pwd)
 
-echo "Building new directories in $dest..."
+echo "Building new directories in $dst..."
 for dir in $(find $src -type d); do
 	mkdir -p $dst${dir#$src}
 done
@@ -11,8 +11,9 @@ done
 echo "Symlinking files..."
 for src_f in $(find $src -type f); do
 	dst_f=$dst${src_f#$src}
+	rm -rf $dst_f
 	ln -s $src_f $dst_f
 done
 
-echo "Success"
+
 
