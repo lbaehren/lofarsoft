102dad344b5c6e1b54409b6d529cca6cfa0ba186
kondratiev <kondratiev@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-11-13 20:07:11 +0000

    polishing ...
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6291 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/Pulsar_Search_LOFAR.py
+++ src/Pulsar/scripts/Pulsar_Search_LOFAR.py
@@ -680,7 +680,7 @@ if __name__ == "__main__":
 	else:
 		rfi_time = timed_execute(cmd,1)
 
-	print "RFI masking time (s) : %.2f   [%.1f h]" % (rfi_time, rfi_time/3600.))
+	print "RFI masking time (s) : %.2f   [%.1f h]" % (rfi_time, rfi_time/3600.)
 	rfp.write("RFI masking time (s) : %.2f   [%.1f h]\n" % (rfi_time, rfi_time/3600.))
         totime += rfi_time
 	maskfile=outfile+"_rfifind.mask"
