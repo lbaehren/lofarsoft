ff6b1b9af9b4d08048f095aaddad905ef4edc64b
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2010-12-14 10:23:07 +0000

    added warning to delete previous results if they already exist
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6536 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/thumbnail_combine.sh
+++ src/Pulsar/scripts/thumbnail_combine.sh
@@ -2,11 +2,18 @@
 
 #find all the th.png files and convert them into a list to paste together using "montage".
 
+if [ -f combined.th.png ]
+then 
+   echo "WARNING: deleting previous version of results: combined.th.png"
+   rm combined.th.png
+fi
+
 find ./ -name "*.th.png" -print  > /tmp/$$_combine_col1.txt
 find ./ -name "*.th.png" -print  | sed -e 's/\// /g' -e 's/^.* //g' -e 's/.*_RSP/RSP/g' -e 's/\..*//g'  -e 's/_PSR//g' > /tmp/$$_combine_col2.txt
 paste /tmp/$$_combine_col1.txt /tmp/$$_combine_col2.txt | awk '{print "-label "$2" "$1" "}' | tr -d '\n' | awk '{print "montage "$0" combined.th.png"}' > combine_png.sh
 rm /tmp/$$_combine_col1.txt /tmp/$$_combine_col2.txt
 wc_convert=`wc -l combine_png.sh | awk '{print $1}'`
+
 if [[ $wc_convert > 0 ]]
 then
    chmod 777 combine_png.sh
