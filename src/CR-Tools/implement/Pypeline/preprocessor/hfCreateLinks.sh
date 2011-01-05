e661cdbac9c8caefbbc47bc3ff309381755d9d1a
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-01-05 12:46:56 +0000

    Fixed adding links of pycrtools scripts/modules located in subdirectories.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6677 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/preprocessor/hfCreateLinks.sh
+++ src/CR-Tools/implement/Pypeline/preprocessor/hfCreateLinks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-src=$1
-dst=$2
+src_d=$1
+dst_d=$2
 
 # Overwrite command line values for testing
 #src_d=${LOFARSOFT}/src/CR-Tools/implement/Pypeline/modules/
@@ -13,18 +13,20 @@ if test -d $src_d ; then
     #echo "src: $src_d"
 
     # Check if destination directory exists
-    if test -d $dst_d ; then
-	echo "OK: Source and destination directories exist."
-    else
+    if ! test -d $dst_d ; then
 	echo "Creating destination directory..."
 	mkdir -p $dst_d
     fi
+
+    for dir in $(find $src_d -type d); do
+	mkdir -p $dst_d${dir#$src_d}
+    done
 
     dst_d=$(cd $dst_d ; pwd)
     #echo "dst: $dst_d"
 
     # Loop over files in source directory and create symlinks
-    for src_f in $(find $src_d -type f); do
+    for src_f in $(find $src_d/*py -type f); do
 	dst_f=$dst_d${src_f#$src_d}
 	#echo a $src_f
 	#echo b $dst_f
