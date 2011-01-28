e17501f05803f43ce5996f30916034069d2b1007
kenneth <kenneth@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-01-28 11:15:02 +0000

    svn keys on...
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6878 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/pipeline/recipes/master/prepfold.py
+++ src/Pulsar/pipeline/recipes/master/prepfold.py
@@ -1,3 +1,5 @@
+#           $Id$
+
 #                                                          LOFAR PULSAR PIPELINE
 #
 #                                        Executed data folding (prepfold) recipe
@@ -19,12 +21,23 @@ from lofarpipe.support.clusterlogger import clusterlogger
 from lofarpipe.support.clusterdesc   import ClusterDesc, get_compute_nodes
 from lofarpipe.support.lofarnode     import run_node
 
+# Repository info ...
+__svn_revision__ = ('$Rev$').split()[1]
+__svn_revdate__  = ('$Date$')[7:26]
+__svn_author__   = ('$Author$').split()[1]
+
 
 class prepfold(LOFARrecipe):
 
     """
-    Provides a convenient, pipeline-based mechanism of running bf2presto
-    on a dataset.
+    Pipeline-based mechanism of running the PRESTO code,
+    prefold on a dataset.
+
+    Parser processes all arguments passed by the framework
+    through cli arguments and any arguments specified in 
+    tasks configuration files.
+
+    Command line arguments override defaults set in the task.cfg.
 
     """
 
