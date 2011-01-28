22aa4b74c4034f3f41344c74e8616788fbecaf57
kenneth <kenneth@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-01-28 10:37:10 +0000

    added keywords
    git-svn-id: http://usg.lofar.org/svn/code/trunk@6871 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/pipeline/dynspec.py
+++ src/Pulsar/pipeline/dynspec.py
@@ -40,9 +40,9 @@ import pulpVersion
 
 __version__      = pulpVersion.version
 __version_date__ = pulpVersion.version_date
-__svn_revision__ = ('$Rev: 6868 $').split()[1]
-__svn_revdate__  = ('$Date: 2011-01-28 11:14:49 +0100 (Fri, 28 Jan 2011) $')[7:26]
-__svn_author__   = ('$Author: kenneth $').split()[1]
+__svn_revision__ = ('$Rev$').split()[1]
+__svn_revdate__  = ('$Date$')[7:26]
+__svn_author__   = ('$Author$').split()[1]
 
 
 class dynspec(control):
