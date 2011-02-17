81fd5e8a8d53d0ac89f47ec2b4681377ba925012
kenneth <kenneth@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-02-17 10:16:37 +0000

    rfi stitching update
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7110 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/pipeline/support/stitchReports.py
+++ src/Pulsar/pipeline/support/stitchReports.py
@@ -14,9 +14,9 @@ import os, glob
 import pulpEnv
 
 # Repository info ...
-__svn_revision__ = ('$Rev: 6880 $').split()[1]
-__svn_revdate__  = ('$Date: 2011-01-28 14:51:09 +0100 (Fri, 28 Jan 2011) $')[7:26]
-__svn_author__   = ('$Author: kenneth $').split()[1]
+__svn_revision__ = ('$Rev$').split()[1]
+__svn_revdate__  = ('$Date$')[7:26]
+__svn_author__   = ('$Author$').split()[1]
 
 
 class StitchReports():
