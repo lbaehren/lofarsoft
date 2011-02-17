64ff4b39fa399203932915a2649930b127c44330
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-02-17 13:38:23 +0000

    Added CMake script to detect a CUDA capable GPU.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7116 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ devel_common/cmake/FindCUDAGPU.cmake
@@ -0,0 +1,38 @@
+#
+# - Check for the presence of a CUDA enabled GPU
+#
+# The following variables are set when a CUDA enabled GPU is found:
+#
+#  CUDA_HAVE_GPU            = Set to true, if a CUDA enabled GPU is found
+#
+
+## ------------------------------------------------------------------------------
+## include guard
+
+if (NOT CUDA_HAVE_GPU)
+
+    cmake_minimum_required (VERSION 2.8)
+    
+    find_package (cuda)
+    
+    if (CUDA_FOUND)
+        try_run (RUN_RESULT_VAR COMPILE_RESULT_VAR
+            ${CMAKE_BINARY_DIR} 
+            ${CMAKE_MODULE_PATH}/TestCUDAGPU.c
+            CMAKE_FLAGS 
+                -DINCLUDE_DIRECTORIES:STRING=${CUDA_TOOLKIT_INCLUDE}
+                -DLINK_LIBRARIES:STRING=${CUDA_CUDART_LIBRARY}
+            COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_VAR
+            RUN_OUTPUT_VARIABLE RUN_OUTPUT_VAR)
+        message ("${RUN_OUTPUT_VAR}") # Display number of GPUs found
+        # COMPILE_RESULT_VAR is TRUE when compile succeeds
+        # RUN_RESULT_VAR is zero when a GPU is found
+        if (COMPILE_RESULT_VAR AND NOT RUN_RESULT_VAR)
+            set (CUDA_HAVE_GPU TRUE CACHE BOOL "Whether CUDA-capable GPU is present")
+        else (COMPILE_RESULT_VAR AND NOT RUN_RESULT_VAR)
+            set (CUDA_HAVE_GPU FALSE CACHE BOOL "Whether CUDA-capable GPU is present")
+        endif (COMPILE_RESULT_VAR AND NOT RUN_RESULT_VAR)
+    endif (CUDA_FOUND)
+    
+endif (NOT CUDA_HAVE_GPU)
+
