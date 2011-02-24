de4d7b56d2c09c58e9768a8a625b3dc0d399ad78
baehren <baehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-02-24 21:06:04 +0000

    Update CMake script to find installation of DAL.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7167 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/FindDAL.cmake
+++ devel_common/cmake/FindDAL.cmake
@@ -20,49 +20,137 @@
 # |   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 |
 # +-----------------------------------------------------------------------------+
 
-# Check for the presence of Data Access Library (DAL).
+# - Check for the presence of DAL
 #
 # The following variables are set when DAL is found:
-#  HAVE_DAL       = Set to true, if all components of DAL have been found.
-#  DAL_INCLUDES   = Include path for the header files of DAL
-#  DAL_LIBRARIES  = Link these to use DAL
-#  DAL_LFGLAS     = Linker flags (optional)
+#
+#   DAL_FOUND      = Set to true, if all components of DAL have been found.
+#   DAL_INCLUDES   = Include path for the header files of DAL
+#   DAL_LIBRARIES  = Link these to use DAL
+#   DAL_LFLAGS     = Linker flags (optional)
+#
+# In order to maintain compatibility with the Filesystem Hierarchy Standard (FHS)
+# the following default installation layout has been defined:
+#
+# /opt
+# └── dal                          DAL_INSTALL_PREFIX
+#     ├── bin                      DAL_INSTALL_BINDIR
+#     ├── include                  DAL_INSTALL_INCLUDEDIR
+#     │   ├── coordinates
+#     │   ├── core
+#     │   ├── data_common
+#     │   └── data_hl
+#     ├── lib                      DAL_INSTALL_INCLUDEDIR
+#     └── share                    DAL_INSTALL_DATAROOTDIR
+#         └── doc                  DAL_INSTALL_DOCDIR
+#             └── html
+#
 
-if (NOT FIND_DAL_CMAKE)
-  
-  set (FIND_DAL_CMAKE TRUE)
-  
-  ##_____________________________________________________________________________
-  ## Search locations
-  
-  include (CMakeSettings)
+if (NOT DAL_FOUND)
+
+  if (NOT DAL_ROOT_DIR)
+    set (DAL_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
+  endif (NOT DAL_ROOT_DIR)
   
   ##_____________________________________________________________________________
   ## Check for the header files
   
-  find_path (DAL_INCLUDES dalCommon.h dalData.h dalFilter.h
-    PATHS ${include_locations}
-    PATH_SUFFIXES dal
-    NO_DEFAULT_PATH
+  find_path (DAL_INCLUDES dal_config.h
+    HINTS ${DAL_ROOT_DIR}
+    PATHS /sw /usr /usr/local /opt/local ${include_locations}
+    PATH_SUFFIXES include include/dal
     )
+
+  ## core/dalDataset.h
+
+  find_path (DAL_DALDATASET_H core/dalDataset.h
+    HINTS ${DAL_ROOT_DIR}
+    PATHS /sw /usr /usr/local /opt/local ${include_locations}
+    PATH_SUFFIXES include include/dal
+    )
+  if (DAL_DALDATASET_H)
+    list (APPEND DAL_INCLUDES ${DAL_DALDATASET_H})
+  endif (DAL_DALDATASET_H)
   
-  get_filename_component (DAL_INCLUDES ${DAL_INCLUDES} ABSOLUTE)
+  ## core/dalDataset.h
+
+  find_path (DAL_COORDINATE_H coordinates/Coordinate.h
+    HINTS ${DAL_ROOT_DIR}
+    PATHS /sw /usr /usr/local /opt/local ${include_locations}
+    PATH_SUFFIXES include include/dal
+    )
+  if (DAL_COORDINATE_H)
+    list (APPEND DAL_INCLUDES ${DAL_COORDINATE_H})
+  endif (DAL_COORDINATE_H)
+
+  ## Remove duplicate entries
+
+  list (REMOVE_DUPLICATES DAL_INCLUDES)
   
   ##_____________________________________________________________________________
   ## Check for the library
+
+  set (DAL_LIBRARIES "")
   
-  find_library (DAL_LIBRARIES dal
-    PATHS ${lib_locations}
-    NO_DEFAULT_PATH
+  find_library (DAL_DAL_LIBRARY dal
+    HINTS ${DAL_ROOT_DIR}
+    PATHS /sw /usr /usr/local /opt/local ${lib_locations}
+    PATH_SUFFIXES lib lib/dal
     )
+  if (DAL_DAL_LIBRARY)
+    list (APPEND DAL_LIBRARIES ${DAL_DAL_LIBRARY})
+  endif (DAL_DAL_LIBRARY)
+  
+  ##_____________________________________________________________________________
+  ## Check for the executable(s)
+  
+  foreach (_dal_executable
+      lopes2h5
+      msread
+      ms2h5
+      tbb2h5
+      TBBraw2h5
+      )
+
+    ## try to locate the executable
+    find_program (DAL_${_dal_executable}_EXECUTABLE ${_dal_executable}
+      HINTS ${DAL_ROOT_DIR}
+      PATHS /sw /usr /usr/local /opt/local ${bin_locations}
+      PATH_SUFFIXES bin bin/dal
+      )
+    
+  endforeach (_dal_executable)
+  
+  ##_____________________________________________________________________________
+  ## Test DAL library for:
+  ##  - library version <major.minor.release>
+  ##  - registered external packages (e.g. casacore, HDF5, etc.)
+  
+  if (DAL_INCLUDES AND DAL_LIBRARIES)
+    ## Locate test program
+    find_file (HAVE_TestDALLibrary TestDALLibrary.cc
+      PATHS ${PROJECT_SOURCE_DIR}
+      PATH_SUFFIXES cmake devel_common/cmake Modules
+      )
+    ## Build and run test program
+    if (HAVE_TestDALLibrary)
+      try_run(DAL_VERSION_RUN_RESULT DAL_VERSION_COMPILE_RESULT
+	${PROJECT_BINARY_DIR}
+	${HAVE_TestDALLibrary}
+#	CMAKE_FLAGS -DLINK_LIBRARIES:STRING=${DAL_LIBRARIES}
+	COMPILE_DEFINITIONS -I${DAL_INCLUDES}
+	RUN_OUTPUT_VARIABLE DAL_VERSION_OUTPUT
+	)
+    endif (HAVE_TestDALLibrary)
+  endif (DAL_INCLUDES AND DAL_LIBRARIES)
   
   ##_____________________________________________________________________________
   ## Actions taken when all components have been found
   
   if (DAL_INCLUDES AND DAL_LIBRARIES)
-    set (HAVE_DAL TRUE)
+    set (DAL_FOUND TRUE)
   else (DAL_INCLUDES AND DAL_LIBRARIES)
-    set (HAVE_DAL FALSE)
+    set (DAL_FOUND FALSE)
     if (NOT DAL_FIND_QUIETLY)
       if (NOT DAL_INCLUDES)
 	message (STATUS "Unable to find DAL header files!")
@@ -73,17 +161,22 @@ if (NOT FIND_DAL_CMAKE)
     endif (NOT DAL_FIND_QUIETLY)
   endif (DAL_INCLUDES AND DAL_LIBRARIES)
   
-  if (HAVE_DAL)
+  if (DAL_FOUND)
     if (NOT DAL_FIND_QUIETLY)
       message (STATUS "Found components for DAL")
+      message (STATUS "DAL_ROOT_DIR  = ${DAL_ROOT_DIR}")
       message (STATUS "DAL_INCLUDES  = ${DAL_INCLUDES}")
       message (STATUS "DAL_LIBRARIES = ${DAL_LIBRARIES}")
     endif (NOT DAL_FIND_QUIETLY)
-  else (HAVE_DAL)
+  else (DAL_FOUND)
     if (DAL_FIND_REQUIRED)
       message (FATAL_ERROR "Could not find DAL!")
     endif (DAL_FIND_REQUIRED)
-  endif (HAVE_DAL)
+  endif (DAL_FOUND)
+
+  ## Compatibility settings
+
+  set (HAVE_DAL ${DAL_FOUND})
   
   ##_____________________________________________________________________________
   ## Mark advanced variables
@@ -93,4 +186,4 @@ if (NOT FIND_DAL_CMAKE)
     DAL_LIBRARIES
     )
   
-endif (NOT FIND_DAL_CMAKE)
+endif (NOT DAL_FOUND)
