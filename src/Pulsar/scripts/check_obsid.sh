64958f00155ae0c0504f78b858edcdaa0d35d7ba
kondratiev <kondratiev@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-02-25 14:29:11 +0000

    new script to look for raw and _red data for chosen ObsID
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7175 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/Pulsar/scripts/check_obsid.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Check the RAW and Reduced data for chosen ObsID in sub4, sub5, and sub6
+# Also, script is looking for the Parset file in /globalhome/lofarsystem
+# (be careful, though, only the suffix of ObsID is checked in the parset file name,
+# so, there maybe other "wrong" parset files found)
+#
+
+if [ $# -eq 0 ]; then
+ echo "Usage: $0 <ObsID>"
+ exit 0
+fi
+
+obsid=$1
+echo "OBSID: $obsid"
+echo "*******************************************************"
+echo "Look for RAW data ..."
+cexec lse:9-17 "find /data? -maxdepth 1 -type d -name '$obsid' -exec du -sh {} \;"
+echo "*******************************************************"
+echo "Look for REDUCED data in Pulsar Archive Area ..."
+cexec lse:9-17 "find /data4/LOFAR_PULSAR_ARCHIVE_lse0?? -type d -name '$obsid*' -exec du -sh {} \;" | grep -v Permission
+echo "*******************************************************"
+echo "Look for Parset file in /globalhome/lofarsystem ..."
+obsid_suffix=`echo $obsid | cut -f 2 -d _`
+find /globalhome/lofarsystem -name "*$obsid_suffix*" -print 2>&1 | grep -v Permission
