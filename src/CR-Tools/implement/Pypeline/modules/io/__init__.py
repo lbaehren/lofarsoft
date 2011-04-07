ee99af2698fa78312ba84c0da15118e8efafabb4
falcke <falcke@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-04-07 20:38:43 +0000

    dito
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7513 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/modules/io/__init__.py
+++ src/CR-Tools/implement/Pypeline/modules/io/__init__.py
@@ -9,6 +9,7 @@ __all__ = ['tbb', 'interfaces']
 
 import tbb
 import pyfits
+import pycrtools as cr
 import __builtin__ # Needed because from io import * will cause overloading
 
 def open(filename, *args, **kwargs):
@@ -22,6 +23,9 @@ def open(filename, *args, **kwargs):
     if ext == "h5":
         # Open file with LOFAR TBB data
         return tbb.open(filename, *args, **kwargs)
+    elif ext == "event":
+        # Open LOPES Event file with pyfits
+        return cr.crfile(filename, *args, **kwargs)
     elif ext == "fits":
         # Open FITS file with pyfits
         return pyfits.open(filename, *args, **kwargs)
