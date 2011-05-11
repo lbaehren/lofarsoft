347010215cfdf538444873b0042a4023e204205a
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-05-11 19:07:46 +0000

    added cep2 capabilities of taking a finished template and writing scripts to run on the cluster to process data with cexec
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7685 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/XML-MOM/xml_pipeline_kickoff.sh
+++ src/Pulsar/scripts/XML-MOM/xml_pipeline_kickoff.sh
@@ -6,6 +6,7 @@ USAGE="\nusage : xml_pipeline_kickoff.sh -infile obs_finished.xml -prefix prefix
 "      [-splits N]  ==> Number of output split files to contain the processing commands (i.e. 4) \n"\
 "      [-survey]    ==> Indicates that all the observations are in survey mode;  changes pipeline args to:\n"\
 "                       pulp.sh -id OBSID -o OBSID_red -p position -rfi   [without -all] \n"\
+"      [-cep2]      ==> Indicates that processing will be on CEP2 cluster, distributed processing separates scripts.\n"\
 "\n"\
 "      Example:\n"\
 "      xml_pipeline_kickoff.sh -infile Obs_B1254-10_HBA.xml -prefix Obs_20100730 -splits 4 \n"\
@@ -16,6 +17,7 @@ infile=""
 prefix=""
 splits=1
 survey=0
+cep2=0
 
 if [ $# -lt 4 ]                    
 then
@@ -30,6 +32,7 @@ do
 	-prefix)    prefix=$2; shift;;
 	-splits)    splits=$2; shift;;
 	-survey)    survey=1;;
+	-cep2)      cep2=1;;
 	-*)
 	    echo >&2 \
 	    "$USAGE"
@@ -66,63 +69,106 @@ fi
 # previous version to extract the OBSID and Target name
 #egrep "observationId|\(.BA" $infile | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/\<observationId\>//' | sed 's/<>//' | sed 's/<.*Obs / /g' | sed 's/(.BA.*//g' | awk '{printf("pulp.sh -id L2011_%05d -p %s -o L2011_%05d_red -all -rfi\n", $1, $2, $1)}' > $outfile
 
-if [[ $survey == 0 ]]
+if [[ $survey == 0 ]] && [[ $cep2 == 0 ]]
 then
-   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ if ( $3 == "Pos" ) printf("pulp.sh -id L2011_%05d -p position -o L2011_%05d_red -all -rfi\n", $1, $1); else if ( $2 == "Obs" ) printf("pulp.sh -id L2011_%05d -p %s -o L2011_%05d_red -all -rfi\n", $1, $3, $1) ; else printf("pulp.sh -id L2011_%05d -p %s -o L2011_%05d_red -all -rfi\n", $1, $2, $1)}' > $outfile
-else
+   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ if ( $3 == "Pos" ) printf("pulp.sh -id L%05d -p position -o L%05d_red -all -rfi\n", $1, $1); else if ( $2 == "Obs" ) printf("pulp.sh -id L2011_%05d -p %s -o L2011_%05d_red -all -rfi\n", $1, $3, $1) ; else printf("pulp.sh -id L2011_%05d -p %s -o L2011_%05d_red -all -rfi\n", $1, $2, $1)}' > $outfile
+elif [[ $survey == 1 ]] && [[ $cep2 == 0 ]]
+then
    egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ printf("pulp.sh -id L2011_%05d -p position -o L2011_%05d_red -rfi\n", $1, $1)}' > $outfile
+elif [[ $survey == 1 ]] && [[ $cep2 == 1 ]]
+then
+   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ printf("pulp.sh -id L%05d -p position -o L%05d_red -coh_only -raw /data/ -rfi\n", $1, $1)}' > $outfile
+   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ printf("pulp.sh -id L%05d -p position -o L%05d_redIS -incoh_only -all -raw /data/ -rfi\n", $1, $1)}' >> $outfile
+elif [[ $survey == 0 ]] && [[ $cep2 == 1 ]]
+then
+   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ if ( $3 == "Pos" ) printf("pulp.sh -id L%05d -p position -o L%05d_red -raw /data/ -coh_only -rfi\n", $1, $1); else if ( $2 == "Obs" ) printf("pulp.sh -id L%05d -p %s -o L%05d_red -all -raw /data/ -coh_only -rfi\n", $1, $3, $1) ; else printf("pulp.sh -id L%05d -p %s -o L%05d_red -raw /data/ -coh_only -rfi\n", $1, $2, $1)}' > $outfile
+   egrep "description|observationId" $infile  | sed 's/\/observationId./observationId\>\\/g' | sed -e :a -e '/\\$/N; s/\\\n//; ta' | grep observationId | sed 's/<observationId>//' | sed 's/<>//' | sed 's/</ </g' | sed 's/>/> /g' | sed 's/(.*//g' | sed 's/<.*>//g' | awk '{ if ( $3 == "Pos" ) printf("pulp.sh -id L%05d -p position -o L%05d_red -raw /data/ -incoh_only -rfi -all\n", $1, $1); else if ( $2 == "Obs" ) printf("pulp.sh -id L%05d -p %s -o L%05d_red -all -raw /data/ -incoh_only -rfi -all\n", $1, $3, $1) ; else printf("pulp.sh -id L%05d -p %s -o L%05d_red -raw /data/ -incoh_only -rfi -all\n", $1, $2, $1)}' >> $outfile
 fi
 
 cp $outfile $outfile.all
 
-# perform the splitting up of the all files into multiples
-ii=0
-splits_list=""
-jj=$splits
-last=$jj
-while (( $jj > 0 ))
-do 
-    splits_list=`echo $jj $splits_list`
-    jj=`expr $jj - 1`
-done
-
-num_lines=`wc -l $outfile | awk '{print $1}'`
-
-modulo_files=`echo $num_lines $splits | awk '{print ($1 % $2)}'`
-if (( modulo_files != 0 ))
+if [[ $cep2 == 0 ]]
 then
-   echo "Warning: this is an uneven split; one file will contain more OBSIDs than the rest."
-fi
 
-div_files=`echo $num_lines $splits | awk '{printf("%d"), $1 / $2}'`
-
-# split the single file into multiples with the same number per split
-if (( $splits > 1 ))
-then
-	for ii in $splits_list
-	do
-		head -$div_files $outfile > $outfile.$ii
-		c=$div_files
-		sed "1,$c d" $outfile > $outfile.tmp
-		mv $outfile.tmp $outfile
+	# perform the splitting up of the all files into multiples
+	ii=0
+	splits_list=""
+	jj=$splits
+	last=$jj
+	while (( $jj > 0 ))
+	do 
+	    splits_list=`echo $jj $splits_list`
+	    jj=`expr $jj - 1`
 	done
-fi
-
-# when there's an uneven split, concatinate the rest to the last split file
-if (( modulo_files != 0 ))
-then
-   cat $outfile >> $outfile.$ii
-   rm $outfile
-fi
+	
+	num_lines=`wc -l $outfile | awk '{print $1}'`
+	
+	modulo_files=`echo $num_lines $splits | awk '{print ($1 % $2)}'`
+	if (( modulo_files != 0 ))
+	then
+	   echo "Warning: this is an uneven split; one file will contain more OBSIDs than the rest."
+	fi
+	
+	div_files=`echo $num_lines $splits | awk '{printf("%d"), $1 / $2}'`
+	
+	# split the single file into multiples with the same number per split
+	if (( $splits > 1 ))
+	then
+		for ii in $splits_list
+		do
+			head -$div_files $outfile > $outfile.$ii
+			c=$div_files
+			sed "1,$c d" $outfile > $outfile.tmp
+			mv $outfile.tmp $outfile
+		done
+	fi
+	
+	# when there's an uneven split, concatinate the rest to the last split file
+	if (( modulo_files != 0 ))
+	then
+	   cat $outfile >> $outfile.$ii
+	   rm $outfile
+	fi
+else # if [[ $cep2 == 1 ]]
+    ii=0
+    rm -rf $outfile.all.sh
+    while read line 
+    do
+        rm -rf $outfile.$ii
+        obsid=`echo $line | sed 's/^.*-id //g' | awk '{print $1}'`  
+        #create a shell script per line to run on locus nodes
+        incoherent=`echo $line | grep incoh_only`
+        if [[ $incoherent == "" ]]
+        then
+           echo cexec locus:0-99 "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; use LUS; ~alexov/$line"  >> $outfile.$ii
+           echo cexec locus:67 "mkdir -p /data/LOFAR_PULSAR_ARCHIVE_locus*/${obsid}_plots" >> $outfile.$ii
+           echo cexec locus:0-99  "scp /data/scratch/alexov/${obsid}_red/stokes/*/*th.png /data/scratch/alexov/${obsid}_red/stokes/*/*prepout /data/scratch/alexov/${obsid}_red/stokes/*/*/*th.png /data/scratch/alexov/${obsid}_red/stokes/*/*/*prepout `whoami`@locus068:/data/LOFAR_PULSAR_ARCHIVE_locus*/${obsid}_plots/"   >> $outfile.$ii
+           echo cexec locus:67 "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/${obsid}_plots/; use LUS; ~alexov/thumbnail_combine.sh"  >> $outfile.$ii
+        else
+           echo cexec locus:67 "mkdir -p /data/scratch/pulsar/${obsid}" >> $outfile.$ii
+           echo cexec locus:0-99 "scp /data/${obsid}/*stokes `whoami`@locus068:/data/scratch/pulsar/${obsid}"  >> $outfile.$ii
+           echo cexec locus:67 "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; use LUS; ~alexov/$line" >> $outfile.$ii
+        fi
+	    echo "./$outfile.$ii >& $outfile.$ii.log &" >> $outfile.all.sh
+	    ii=`expr $ii + 1`
+    done < $outfile.all
+fi #end if [[ $cep2 == 0 ]]
 
 rm $outfile
 
-chmod 777 $outfile.*
+chmod 777 $outfile.*  $outfile.all.sh
 
-echo "Single script '$outfile.all' contains all the processing commands in one file."
-if (( $splits > 1 ))
+if [[ $cep2 == 0 ]]
 then
-   echo "Run that file or, run the N split files:"
-   echo "`ls $outfile.[0-9]*`"
-   echo "in the processing locations."
+    echo "Single script '$outfile.all' contains all the processing commands in one file."
+	if (( $splits > 1 ))
+	then
+	   echo "Run that file or, run the N split files:"
+	   echo "`ls $outfile.[0-9]*`"
+	   echo "in the processing locations."
+	fi
+else
+   echo "Single script '$outfile.all.sh' contains all the processing commands in one file."
+   echo "Copy that file and all the sub-scripts ($outfile.*) to activate processing."
 fi
+
