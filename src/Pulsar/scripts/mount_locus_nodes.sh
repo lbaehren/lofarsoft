ce32602c848db579f49f367cc8ad71e327abda02
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-05-15 15:16:00 +0000

    added a small script to mount all the locus nodes on the hoover nodes - works for 10 min
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7709 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/mount_locus_nodes.sh
+++ src/Pulsar/scripts/mount_locus_nodes.sh
@@ -3,10 +3,10 @@
 # cross mount all the locus disks for the next 10 minutes
 
 ii=1
-
+echo "Opening the mount points for locus nodes 1->100; please wait."
 while (( $ii <= 100 ))
 do
-     `echo $ii | awk '{ printf("ls /cep2/locus%3.3d_data\n",$1)}'`
+     `echo $ii | awk '{ printf("ls /cep2/locus%3.3d_data\n",$1)}'` >> /dev/null
      ii=`expr $ii + 1`
 
 done
