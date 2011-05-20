08b45319b906145a6d208f7a1c0cacea81aec67e
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-05-20 08:18:51 +0000

    added node name for each beam processed, into its own file;  fixed bug with removal of tmp directory
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7736 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
+++ src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
@@ -4,7 +4,7 @@
 # N core defaul is = 8 (cores)
 
 #PLEASE increment the version number when you edit this file!!!
-VERSION=2.8
+VERSION=2.9
 
 #Check the usage
 USAGE1="\nusage : make_subs_SAS_Ncore_Mmodes.sh -id OBS_ID -p Pulsar_names -o Output_Processing_Location [-raw input_raw_data_location] [-par parset_location] [-core N] [-all] [-all_pproc] [-rfi] [-rfi_ppoc] [-C] [-del] [-incoh_only] [-coh_only] [-incoh_redo] [-coh_redo] [-transpose] [-nofold] [-help] [-test] [-debug]\n\n"\
@@ -2410,6 +2410,7 @@ do
 		done
 		cd ${location}/${STOKES}/tmp$$/
 		mv * ../
+		cd ..
 		rmdir tmp$$
 		cd ${location}	
     fi
@@ -2417,6 +2418,10 @@ do
 	cd ${location}	
 
     echo $core > $location/${STOKES}/nof_cores.txt
+    if [[ $STOKES == "stokes" ]] 
+    then 
+       cat $master_list | awk -v NODE_NAME=`uname -n` '{print NODE_NAME " " $1}' > $location/${STOKES}/beam_process_node.txt
+    fi
 done # for loop over modes in $mode_str 
 
 if [[ $proc != 0 ]]
