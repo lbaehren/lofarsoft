4352847438913f5be8e3baa5d563c8d9a2858538
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-05-25 12:34:17 +0000

    added hoover_only switch properly to pass to next script, not pipeline
    git-svn-id: http://usg.lofar.org/svn/code/trunk@7776 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/pulp_cep2.sh
+++ src/Pulsar/scripts/pulp_cep2.sh
@@ -4,6 +4,7 @@ USAGE="\nusage : pulp_cep2.sh <args identical to pulp.sh> \n\n"\
 "      NOTE: If '-incoh_only' or '-coh_only' is not used, then both will be run."\
 "      NOTE: There is no need to specify the raw data location becuase cep2 is assumed."\
 "      NOTE: There is little flexibility with the output directory name;  use _red for everything."\
+"      To run just the hoover/collection part of coh_only, using switch '-hoover_only'."\
 "\n"\
 "      Example:\n"\
 "      pulp_cep2.sh -id L26170 -p B1254-10 -o L26170_red -incoh_only -rfi -all \n"\
@@ -29,6 +30,15 @@ done
 
 # clean out the -del option if it's given
 args=`echo $args | sed 's/-del//g'`
+hoover_only=`echo $args | grep hoover_only`
+hoover=0
+if [[ $hoover_only != "" ]]
+then
+    args=`echo $args | sed 's/-hoover_only//g'`
+    hoover=1
+else
+    hoover=0
+fi
 incoh=`echo $args | grep "\-incoh_only"`
 coh=`echo $args | grep "\-coh_only"`
 
@@ -47,4 +57,9 @@ else
 fi
 
 #feed the input into xml_pipeline_kickoff.sh
-xml_pipeline_kickoff.sh -infile pulp_$$tmp -cep2 -noxml -prefix pulp_$$tmp
+if [[ $hoover == 0 ]]
+then
+   xml_pipeline_kickoff.sh -infile pulp_$$tmp -cep2 -noxml -prefix pulp_$$tmp
+else
+   xml_pipeline_kickoff.sh -infile pulp_$$tmp -cep2 -noxml -hoover_only -prefix pulp_$$tmp
+fi
