0dedd12d364ddd4e9f86c3f267b4eebe2f73c46e
terveen <terveen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-07-05 11:16:20 +0000

    Adjusted version plotlora, works now from commandline and as import
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8037 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/implement/Pypeline/scripts/plotlora.py
+++ src/CR-Tools/implement/Pypeline/scripts/plotlora.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
 import pycrtools as cr
 from pycrtools import lora
 from optparse import OptionParser
+
 
 def main():
     usage = "usage: %prog [options] arg"
@@ -14,6 +16,8 @@ def main():
     parser.add_option("--blocksize", type="int", default=2**16)
     parser.add_option("--before", type="int", default=0)
     parser.add_option("--after", type="int", default=0)
+    parser.add_option("--block", type="int", default=None)
+    parser.add_option("--output", type="string", default=None)
 
     (options, args) = parser.parse_args()
 
@@ -22,25 +26,36 @@ def main():
 
     filename = args[0]
 
+    makeplot(filename,options.lora_seconds,options.lora_nanoseconds,options.block,options.blocksize,options.antenna,options.output)
+
+def makeplot(filename,lora_seconds=0,lora_nanoseconds=0,blocksize=2**16,block=None,antenna=None,output=None):
     if not filename.endswith(".h5"):
         parser.error("not an HDF5 file")
 
-    f = cr.open(parser, options.blocksize)
+    print filename
 
-    if options.antenna:
-        f.setAntennaSelection(options.antenna)
+    f = cr.open(filename, blocksize)
+
+    if antenna:
+        f.setAntennaSelection(antenna)
 
     data = f.empty("TIMESERIES_DATA")
 
     print f["TIME"][0]
-    block = lora.loraTimestampToBlocknumber(options.lora_seconds, options.lora_nanoseconds, f["TIME"][0], f["SAMPLE_NUMBER"][0], blocksize = options.blocksize)
+    if not block:
+        block = lora.loraTimestampToBlocknumber(lora_seconds, lora_nanoseconds, f["TIME"][0], f["SAMPLE_NUMBER"][0], blocksize = blocksize)
+    else:
+        block=block
     
-    f.getTimeSeriesData(data, block)
+    print block
+    f.getTimeseriesData(data, block)
 
-    data = data.toNumpy()
 
-    plt.plot(data)
-    plt.show()
+    data[...].plot()
+    if not output:
+        plt.show()
+    else:
+        plt.savefig(output)
 
 if __name__ == "__main__":
     main()
