a1b4bf1d74791bbe39ab45511c599a24604d59c3
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-07-18 18:48:32 +0000

    Cleanup of shortcuts module.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8142 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/shortcuts.py
+++ src/PyCRTools/modules/tasks/shortcuts.py
@@ -3,12 +3,19 @@ This file contains a few simple shortcuts that make programming a little easier
 
 """
 
-current_task=None
+current_task = None
 
 #Shortcuts used for Workspaces in task
-doc="doc"; default="default";unit="unit";workarray="workarray";dependencies="dependencies"; export="export"; output="output"; positional="positional"
+default = "default"
+dependencies = "dependencies"
+doc = "doc"
+export = "export"
+output = "output"
+positional = "positional"
+unit = "unit"
+workarray = "workarray"
 
-def p_(default=None,doc="",unit="",**args):
+def p_(default=None, doc="", unit="", **args):
     """
     Turn a tuple into a parameter definition used in WorkSpace.
 
@@ -19,8 +26,12 @@ def p_(default=None,doc="",unit="",**args):
             "lofarmode":{default:"LBA_OUTER",doc:"Which LOFAR mode was used (HBA/LBA_OUTER/LBA_INNER)"},
             "stride_n":p_(0,"if >0 then divide the FFT processing in n=2**stride_n blocks. This is slower but uses less memory.")
             }
-
     """
-    d={"default":default,"doc":doc,"unit":unit}
-    if len(args)>0: d.update(args)
+    d = {"default":default,
+         "doc":doc,
+         "unit":unit}
+
+    if len(args)>0:
+        d.update(args)
+
     return d
