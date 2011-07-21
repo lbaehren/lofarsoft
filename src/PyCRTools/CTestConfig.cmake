99818862d0ca12d99ba0a2442794b2b5484c8607
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-07-21 10:49:30 +0000

    Added CTest testing of PyCRTools.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8177 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/PyCRTools/CTestConfig.cmake
@@ -0,0 +1,8 @@
+set (CTEST_PROJECT_NAME       "PyCRTools")
+set (CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set (CTEST_TESTING_TIMEOUT    "500")
+
+set (CTEST_DROP_METHOD "http")
+set (CTEST_DROP_SITE "svn.astron.nl")
+set (CTEST_DROP_LOCATION "/CDash/submit.php?project=PyCRTools")
+set (CTEST_DROP_SITE_CDASH TRUE)
