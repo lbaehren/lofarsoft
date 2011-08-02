b829ac86195d7e5d15e62af7aa68d1a453c4696d
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-08-02 09:24:23 +0000

    Updated data feed scripts for led.py for directory of antenna positions etc.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8221 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
+++ src/CR-Tools/scripts/led/test/liveFeedFromLofarStation.sh
@@ -15,7 +15,7 @@ echo antenna positions file $7
 #ssh $3@portal.lofar.eu "ssh lfe001 \"ssh $1\C tail -fn 10000 /localhome/data/$2_TRIGGER.dat\"" | VHECRtest -S $4 -C $5 -D $6 -P $LOFARSOFT/data/calibration/AntennaArrays/$1\-AntennaArrays.conf
 # This seems to be no longer possible, looks like someone restricted the portal access even more to prevent ssh-chaining :(
 
-ssh $1\C tail -fn 10000 /localhome/data/$2_TRIGGER.dat | VHECRtest -S $4 -C $5 -D $6 -P $LOFARSOFT/data/calibration/AntennaArrays/$1\-AntennaArrays.conf
+ssh $1\C tail -fn 10000 /localhome/data/$2_TRIGGER.dat | VHECRtest -S $4 -C $5 -D $6 -P $LOFARSOFT/data/lofar/StaticMetaData/AntennaArrays/$1\-AntennaArrays.conf
 # This needs the user to have stuff inside .ssh/config to get direct access to the station (ask me).
 
 
