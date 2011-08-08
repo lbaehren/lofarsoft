0d5442b3b175e276d06fb86d8bc80b5331b44c89
falcke <falcke@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-08-08 16:16:13 +0000

    bugfibugfixx
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8268 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/pipelines/cr_event.py
+++ src/PyCRTools/pipelines/cr_event.py
@@ -552,8 +552,8 @@ for full_filename in files:
         print "Note: You can get all antenna properties from ``antennacharacteristics=avspectrum.power.getHeader('antennacharacteristics')``"
             
         results.update(dict(
-            antennas_timeseries_rms=list(avspectrum.power.getHeader("rms_antenna")),
-            antennas_timeseries_npeaks=list(avspectrum.power.getHeader("npeaks_antenna")),
+            antennas_timeseries_rms=list(avspectrum.power.getHeader("rms_antenna")) if avspectrum.power.getHeader("rms_antenna") else None,
+            antennas_timeseries_npeaks=list(avspectrum.power.getHeader("npeaks_antenna")) if avspectrum.power.getHeader("npeaks_antenna") else None),
             station_timeseries_rms=avspectrum.power.getHeader("rms"),
             station_timeseries_npeaks=avspectrum.power.getHeader("npeaks"),
             station_antennas_homogeneity_factor=avspectrum.power.getHeader("homogeneity_factor") #how similar are the antennas
