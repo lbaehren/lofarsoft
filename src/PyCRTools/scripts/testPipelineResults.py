46b29063d63b1116911a307ee5e6b17e93bdaadb
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-08-29 11:00:18 +0000

    Test all results in a given event directory (stations, pols). Correct for 3-sigma outliers in power.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8321 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/testPipelineResults.py
+++ src/PyCRTools/scripts/testPipelineResults.py
@@ -5,20 +5,38 @@
 
 import os
 import numpy as np
+import pycrtools as cr
 
-datadir = '/Users/acorstanje/triggering/CR/results4/lora-event-0-station/pol0/2'
-res = {}
-execfile(os.path.join(datadir,"results.py"),res)
-res = res["results"]
+eventdir = '/Users/acorstanje/triggering/CR/results4/lora-event-0-station'
 
-trms = res["timeseries_rms"]
-avg = np.mean(trms)
-spread = np.std(trms)
+datadirs=cr.listFiles(os.path.join(os.path.join(eventdir,"pol?"),"*"))
 
-print "RMS power in timeseries_rms (= the data in the block with the pulse, from 0:pulse.start)"
-print "mean = %f" % avg
-print "sdev = %f" % spread
-print "relative error = %f" % (spread / avg)
+for datadir in datadirs:
+    if not os.path.isfile(os.path.join(datadir,"results.py")):
+        continue
+    resfile=open(os.path.join(datadir,"results.py"))
+    
+    res = {}
+    execfile(os.path.join(datadir,"results.py"),res)
+    res = res["results"]
 
+    trms = np.array(res["timeseries_rms"])
+    avg = np.mean(trms)
+    spread = np.std(trms)
+    print datadir
+#    print "RMS power in timeseries_rms (= the data in the block with the pulse, from 0:pulse.start)"
+#    print "mean = %f" % avg
+#    print "sdev = %f" % spread
+    print "relative error = %f" % (spread / avg)
+    outliers = np.where(abs(trms - avg) > 3 * spread)
+    outlierCount = len(outliers[0])
+    if outlierCount > 0:
+        print 'There are %d outliers above 3 sigma' % outlierCount
+        ok_index = np.where(abs(trms - avg) < 3 * spread)
+        corrected = trms[ok_index]
+        newavg = np.mean(corrected)
+        newspread = np.std(corrected)
+        print '--> Corrected relative error = %f' % (newspread / newavg)
+#    print ' '
 
 
