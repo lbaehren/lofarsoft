cf634058a15698fa449737a08c23dfc60ca7d458
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-09-28 09:33:24 +0000

    Enabled dashboard for PyCRTools.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8421 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/CTestConfig.cmake
+++ src/PyCRTools/CTestConfig.cmake
@@ -1,8 +1,13 @@
-set (CTEST_PROJECT_NAME       "PyCRTools")
-set (CTEST_NIGHTLY_START_TIME "00:00:00 EST")
-set (CTEST_TESTING_TIMEOUT    "500")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "PyCRTools")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
 
-set (CTEST_DROP_METHOD "http")
-set (CTEST_DROP_SITE "svn.astron.nl")
-set (CTEST_DROP_LOCATION "/CDash/submit.php?project=PyCRTools")
-set (CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "svn.astron.nl")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=PyCRTools")
+set(CTEST_DROP_SITE_CDASH TRUE)
