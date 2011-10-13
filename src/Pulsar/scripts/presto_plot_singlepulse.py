44d001d7151ff30154c3594e855b0884ae70dcdc
coenen <coenen@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-13 14:46:20 +0000

    Added a fix to ad hoc LPPS single pulse plotter.
    Note : works around the way PRESTO single_pulse_search.py names .ps files.
    
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8481 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/presto_plot_singlepulse.py
+++ src/Pulsar/scripts/presto_plot_singlepulse.py
@@ -4,6 +4,7 @@ import os
 import sys
 from lpps_search.util import symlink_matching, remove_matching
 from lpps_search.crawler import get_basename
+from lpps_search import inf
 
 # Hardcoded time intervals to plot (in minutes!):
 TIMES = [(0, 10), (5, 15), (10, 20), (15, 25), (20, 30), (25, 35), (30, 40),
@@ -34,6 +35,19 @@ if __name__ == '__main__':
     symlink_matching(os.path.join(sod, 'INF'), os.path.join(sod, 'SINGLEPULSE'), r'\S+\.inf$')
     cwd = os.getcwd()
     os.chdir(os.path.join(sod, 'SINGLEPULSE'))
+    # Need to read a .inf file to calculate length of the observation that
+    # is needed to predict the filenames that PRESTO single_pulse_search.py
+    # produces.
+    for f in os.listdir('.'):
+        if f[-4:] == '.inf':
+            # This is safe to do because the search script set the length such that
+            # even downsampled .inf files will yield the same observation time.
+            inf_file = inf.inf_reader(f)
+            t_end = inf_file.n_bins * inf_file.bin_width
+            break
+    else:
+        print 'No .inf files present, exiting!'
+        sys.exit(1)
     for ts, te in TIMES:
         for dm_lo, dm_hi, glob_chunks in DM_RANGES:
             globpattern = ' '.join(['%s%s.singlepulse' % (basename, g) for g in glob_chunks])
@@ -41,13 +55,21 @@ if __name__ == '__main__':
                 {'t' : 5.5, 'g' : globpattern, 's' : ts, 'e' : te,}
             print 'Running command :', cmd
             os.system(cmd)
-            tempname = '%s_%d-%ds_singlepulse.ps' % (basename, ts, te)
-            psname = '%s_%d-%ds_DM%d_%d_singlepulse.ps' % (basename, ts, te, dm_lo, dm_hi)
-            pngname = '%s_%d-%ds_DM%d_%d_singlepulse.png' % (basename, ts, te, dm_lo, dm_hi)
+            if te > t_end:
+                TMP1 = t_end
+            else:
+                TMP1 = te
+            tempname = '%s_%d-%0.fs_singlepulse.ps' % (basename, ts, TMP1)
+            psname = '%s_%d-%ds_DM%d_%d_singlepulse.ps' % (basename, ts, TMP1, dm_lo, dm_hi)
             cmd = 'mv %s %s' % (tempname, psname)
             print 'Running command :', cmd
             os.system(cmd)
+    for f in os.listdir('.'):
+        if f[-3:] == '.ps':
+            psname = f
+            pngname = psname[:-3] + '.png'
             cmd = 'convert -matte %s %s' % (psname, pngname)
+            print 'Running command :', cmd
             os.system(cmd)
     os.chdir(cwd)
     remove_matching(os.path.join(sod, 'SINGLEPULSE'), r'\S+\.inf$')
