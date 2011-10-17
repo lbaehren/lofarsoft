e5f340470288a08ae3fae17a373bdc90f78b3322
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-17 14:13:19 +0000

    Catch for files containing an unfinished reconstruction and do not contain useable results.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8500 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/make_ldf_automated.py
+++ src/PyCRTools/scripts/make_ldf_automated.py
@@ -10,5 +10,8 @@ dirList=os.listdir(datalocation)
 
 for fname in dirList:
     if "VHECR" in fname:
-        Ev = [str(fname)]
-        ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True)
+        try:
+            Ev = [str(fname)]
+            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True)
+        except:
+            continue
