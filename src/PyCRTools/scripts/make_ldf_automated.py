b076e0be9a132292d1ffef59a559a148e7dd6f9e
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-17 12:51:08 +0000

    Catch for non VHECR files in folder.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8499 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/make_ldf_automated.py
+++ src/PyCRTools/scripts/make_ldf_automated.py
@@ -4,10 +4,11 @@ import pycrtools as cr
 import os
 
 
-datalocation="/data/VHECR/LORAtriggered/results"
-
+#datalocation="/data/VHECR/LORAtriggered/results"
+datalocation = "/Users/annanelles/Uni/LOFAR/data/GOOD"
 dirList=os.listdir(datalocation)
 
 for fname in dirList:
-    Ev = [str(fname)]
-    ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True)
+    if "VHECR" in fname:
+        Ev = [str(fname)]
+        ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True)
