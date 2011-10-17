42c54800dbfcb8e30083a55b2a225cd6d2221776
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-17 16:04:03 +0000

    Adding plotting of footprint in automation.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8506 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/make_ldf_automated.py
+++ src/PyCRTools/scripts/make_ldf_automated.py
@@ -4,14 +4,21 @@ import pycrtools as cr
 import os
 
 
-#datalocation="/data/VHECR/LORAtriggered/results"
-datalocation = "/Users/annanelles/Uni/LOFAR/data/GOOD"
+datalocation="/data/VHECR/LORAtriggered/results"
 dirList=os.listdir(datalocation)
 
 for fname in dirList:
     if "VHECR" in fname:
+        Ev = str(fname)
         try:
-            Ev = [str(fname)]
-            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True)
+            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True,generate_html=True)
+        except:
+            continue
+            
+        pollist=[0,1]
+        filefilter=datalocation+"/"+Ev+"/"
+        try:
+            for polarization in pollist:
+                crfootprint=cr.trun("plotfootprint",filefilter=filefilter,pol=polarization,plotlora=True,save_images=True,generate_html=True)
         except:
             continue
