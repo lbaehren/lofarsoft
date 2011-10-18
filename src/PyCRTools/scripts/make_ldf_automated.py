14b7908001b485c25f6efb0d8f852299adf23fb2
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-18 11:19:30 +0000

    Update of automated ldf generator: turned of HTML generation.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8508 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/make_ldf_automated.py
+++ src/PyCRTools/scripts/make_ldf_automated.py
@@ -6,19 +6,21 @@ import os
 
 datalocation="/data/VHECR/LORAtriggered/results"
 dirList=os.listdir(datalocation)
+sav_images = True
+gen_html = False
 
 for fname in dirList:
     if "VHECR" in fname:
         Ev = str(fname)
         try:
-            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=True,generate_html=True)
+            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=sav_images,generate_html=gen_html)
         except:
             continue
-            
+
         pollist=[0,1]
         filefilter=datalocation+"/"+Ev+"/"
         try:
             for polarization in pollist:
-                crfootprint=cr.trun("plotfootprint",filefilter=filefilter,pol=polarization,plotlora=True,save_images=True,generate_html=True)
+                crfootprint=cr.trun("plotfootprint",filefilter=filefilter,pol=polarization,plotlora=True,save_images=sav_images,generate_html=gen_html)
         except:
             continue
