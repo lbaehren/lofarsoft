7a5385d77226f8f2bafed06acff1bc6da1dab60a
martin <martin@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-28 12:12:24 +0000

    Updated default settings for generating LDF plots.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8584 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/make_ldf_automated.py
+++ src/PyCRTools/scripts/make_ldf_automated.py
@@ -3,17 +3,23 @@ nogui=True
 import pycrtools as cr
 import os
 
-
-datalocation="/data/VHECR/LORAtriggered/results"
-dirList=os.listdir(datalocation)
-sav_images = True
-gen_html = False
+datalocation = "/data/VHECR/LORAtriggered/results"
+dirList = os.listdir(datalocation)
+sav_images = True                       # Save images
+gen_html = True                         # Generate html page
 
 for fname in dirList:
     if "VHECR" in fname:
         Ev = str(fname)
         try:
-            ldf=cr.trun("ldf",topdir=datalocation,events=Ev, plot_xmax=300,draw_global = False,Draw3D = False,save_images=sav_images,generate_html=gen_html)
+            ldf=cr.trun("ldf",
+                        topdir=datalocation,
+                        events=Ev,
+                        plot_xmax=300,
+                        draw_global=False,
+                        Draw3D=False,
+                        save_images=sav_images,
+                        generate_html=gen_html)
         except:
             continue
 
@@ -21,6 +27,11 @@ for fname in dirList:
         filefilter=datalocation+"/"+Ev+"/"
         try:
             for polarization in pollist:
-                crfootprint=cr.trun("plotfootprint",filefilter=filefilter,pol=polarization,plotlora=True,save_images=sav_images,generate_html=gen_html)
+                crfootprint=cr.trun("plotfootprint",
+                                    filefilter=filefilter,
+                                    pol=polarization,
+                                    plotlora=True,
+                                    save_images=sav_images,
+                                    generate_html=gen_html)
         except:
             continue
