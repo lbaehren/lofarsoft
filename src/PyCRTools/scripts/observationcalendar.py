4d65abccd56505251a9d97d86e1eed567e0ffe47
acorstanje <acorstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-10-31 13:36:47 +0000

    extract start and stop times of observations for calendar display
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8594 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/observationcalendar.py
+++ src/PyCRTools/scripts/observationcalendar.py
@@ -4,11 +4,50 @@
 #  Created by Arthur Corstanje on Oct 24, 2011.
 #  Copyright (c) 2011. All rights reserved.
 
+import subprocess
+import os
+import time
+
+def getFileList(criterion, dir):
+# get all parset filenames which match 'criterion'
+    proc = subprocess.Popen(['grep', '-lr', criterion, dir], stdout=subprocess.PIPE)
+    output = proc.communicate()[0]
+    print output
+    filelist = output.splitlines()
+    return filelist
+
+def getStartStopTime(filename):
+    infile = open(filename, 'r')
+    startTime = -1
+    stopTime = -1
+    for line in infile:
+        if 'Observation.startTime' in line:
+            startTime = line.split('=')[1]
+            print startTime
+        if 'Observation.stopTime' in line:
+            stopTime = line.split('=')[1]
+            print stopTime
+    infile.close()
+    return (startTime, stopTime)
+
 def hexstr(i):
     s = hex(i)[2:]
     if len(s) < 2:
         s = '0' + s
     return s
+
+parsetDir = '/Users/acorstanje/triggering/CR/observations'
+criterion = 'NoCoincChann=1000' # for including TBBs - without VHECR radio trigger (i.e. LORA)
+
+print 'Getting the latest Observationxxxxx parset files...'
+proc = subprocess.Popen(['rsync', '-avv', 'mcu001:/opt/lofar/share/Observation*', parsetDir])
+proc.wait()
+
+filelist = getFileList(criterion, parsetDir)
+for file in filelist:
+    (startTime, stopTime) = getStartStopTime(filelist[0])
+    print time.strptime(startTime, "%Y-%b-%d %H:%M:%S\n")
+
 
 htmlfilename = '/Users/acorstanje/usg/src/PyCRTools/scripts/htmltabletest.html'
 
@@ -24,14 +63,14 @@ for i in range(24): # columns (header)
 html.write("</tr>\n")
 
 for i in range(30): # rows
-    bgcolor = 64 + ((255-64) * i) / 30
+    bgcolor = 255 # 64 + ((255-64) * i) / 30
     colorstr = hexstr(bgcolor)
 
-    html.write("<tr bgcolor=#00"+colorstr+"00>\n")
+    html.write("<tr bgcolor=#FF"+colorstr+"FF>\n")
     html.write("<td>" + str(i) + "</td>") # day of month
     for j in range(24):
         if (j + i) % 5 != 0:
-            html.write("<td> X </td>")
+            html.write("<td>  </td>")
         else:
             html.write('<td bgcolor=#FF0000>  </td>')
     html.write("</tr>\n")
