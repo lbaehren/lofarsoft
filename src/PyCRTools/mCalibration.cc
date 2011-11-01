7eaf652d90a6b81d9edb81b3e911d52e8c8ac151
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-01 09:35:13 +0000

    Added LBA polarization calibration routines for regular on-sky polarization and Cosmic Ray N-S, E-W zenith coordinate frames.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8606 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/mCalibration.cc
+++ src/PyCRTools/mCalibration.cc
@@ -66,7 +66,7 @@ using namespace casa;
 
 //$DOCSTRING: Calibrate polarization
 //$COPY_TO HFILE START --------------------------------------------------
-#define HFPP_FUNC_NAME hCalibratePolarization
+#define HFPP_FUNC_NAME hCalibratePolarizationLBA
 //-----------------------------------------------------------------------
 #define HFPP_FUNCDEF  (HFPP_VOID)(HFPP_FUNC_NAME)("$DOCSTRING")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
 #define HFPP_PARDEF_0 (HComplex)(pol0)()("Polarization 0.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
@@ -80,16 +80,12 @@ using namespace casa;
   $PARDOCSTRING
 
   Given the signals of both LOFAR dipoles in frequency space, the corresponding frequencies and a direction
-  (in azimuth - elevation with respect to the antenna frame) this routine corrects for the antenna response (beam pattern)
-  and mixes the two signals so that the first *pol0* channel array contains the signal in N-S polarization and the second
-  *pol1* channel array contains the signal in E-W polarization (or the other way arround, need to check this).
+  (in azimuth - elevation with respect to the antenna frame) this routine corrects for the antenna response (beam pattern).
 
   For each frequency the Jones matrix containing the antenna response is calculated using the LOFAR provided ElementResponse
   library.
   This matrix contains the beam pattern and multiplying by the inverse of this matrix gives the polarized signal in the on-sky
   frame.
-  For the Cosmic-Rays application one is interrested in the polarization in N-S and E-W respectively and so a projection and
-  rotation is needed to get the response in this frame.
 */
 
 template <class CIter, class NIter>
@@ -100,13 +96,7 @@ void HFPP_FUNC_NAME (const CIter pol0, const CIter pol0_end,
 {
   std::complex<double> J[2][2]; // Jones matrix for element response
   std::complex<double> Jinv[2][2]; // Inverse Jones matrix used for beam correction in on sky polarization
-  std::complex<double> result[2][2]; // Final matrix used for beam correction
   std::complex<double> det; // Determinant of Jones matrix (used for matrix inversion)
-  double R[2][2]; // Rotation + Projection matrix
-  double phi, theta;
-
-  const double pi_2 = M_PI / 2;
-  const double pi_4 = M_PI / 4;
 
   // Get lengths
   const int N = std::distance(frequencies, frequencies_end);
@@ -121,16 +111,6 @@ void HFPP_FUNC_NAME (const CIter pol0, const CIter pol0_end,
   CIter pol0_it = pol0;
   CIter pol1_it = pol1;
   NIter freq_it = frequencies;
-
-  // Calculate angles
-  phi = az;
-  theta = pi_2 - el;
-
-  // Calculate rotation matrix (for going to N-S, E-W polarization frame)
-  R[0][0] = cos(phi - pi_4) * cos(theta);
-  R[0][1] = -1.0 * sin(phi - pi_4);
-  R[1][0] = sin(phi - pi_4) * cos(theta);
-  R[1][1] = cos(phi - pi_4);
 
   for (int i=0; i<N; i++)
   {
@@ -147,18 +127,101 @@ void HFPP_FUNC_NAME (const CIter pol0, const CIter pol0_end,
     Jinv[1][0] = -1.0 * J[1][0] / det;
     Jinv[1][1] = J[0][0] / det;
 
-    // Get the final multiplication matrix
-    result[0][0] = R[0][0] * Jinv[0][0] + R[0][1] * Jinv[1][0];
-    result[0][1] = R[0][0] * Jinv[0][1] + R[0][1] * Jinv[1][1];
-    result[1][0] = R[1][0] * Jinv[0][0] + R[1][1] * Jinv[1][0];
-    result[1][1] = R[1][0] * Jinv[0][1] + R[1][1] * Jinv[1][1];
-
     // Correct polarizations for antenna gain pattern
-    *pol0_it = result[0][0] * *pol0_it + result[0][1] * *pol1_it;
-    *pol1_it = result[1][0] * *pol0_it + result[1][1] * *pol1_it;
+    *pol0_it = Jinv[0][0] * *pol0_it + Jinv[0][1] * *pol1_it;
+    *pol1_it = Jinv[1][0] * *pol0_it + Jinv[1][1] * *pol1_it;
 
     ++pol0_it;
     ++pol1_it;
+    ++freq_it;
+  }
+}
+//$COPY_TO HFILE: #include "hfppnew-generatewrappers.def"
+
+//$DOCSTRING: Calibrate polarization and obtain x,y,z components for Cosmic Rays application
+//$COPY_TO HFILE START --------------------------------------------------
+#define HFPP_FUNC_NAME hCalibratePolarizationLBA
+//-----------------------------------------------------------------------
+#define HFPP_FUNCDEF  (HFPP_VOID)(HFPP_FUNC_NAME)("$DOCSTRING")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+#define HFPP_PARDEF_0 (HComplex)(polx)()("Polarization X.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_1 (HComplex)(poly)()("Polarization Y.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_2 (HComplex)(polz)()("Polarization Z.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_3 (HNumber)(frequencies)()("Frequencies in Hz.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_4 (HNumber)(az)()("Azimuth with respect to antenna frame.")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+#define HFPP_PARDEF_5 (HNumber)(el)()("Elevation with respect to antenna frame.")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+//$COPY_TO END --------------------------------------------------
+/*!
+  \brief $DOCSTRING
+  $PARDOCSTRING
+
+  Given the signals of both LOFAR dipoles in frequency space, the corresponding frequencies and a direction
+  (in azimuth - elevation with respect to the antenna frame) this routine corrects for the antenna response (beam pattern).
+
+  For each frequency the Jones matrix containing the antenna response is calculated using the LOFAR provided ElementResponse
+  library.
+  This matrix contains the beam pattern and multiplying by the inverse of this matrix gives the polarized signal in the on-sky
+  frame.
+
+  The corrected signal is then projected onto the X (N-S), Y (E-W), Z (zenith) frame as needed for the Cosmic Rays application.
+*/
+
+template <class CIter, class NIter>
+void HFPP_FUNC_NAME (const CIter polx, const CIter polx_end,
+    const CIter poly, const CIter poly_end,
+    const CIter polz, const CIter polz_end,
+    const NIter frequencies, const NIter frequencies_end,
+    const HNumber az, const HNumber el)
+{
+  std::complex<double> J[2][2]; // Jones matrix for element response
+  std::complex<double> Jinv[2][2]; // Inverse Jones matrix used for beam correction in on sky polarization
+  std::complex<double> det; // Determinant of Jones matrix (used for matrix inversion)
+  std::complex<double> p_theta, p_phi; // Polarization in on-sky theta phi frame
+
+  // Direction in spherical coordinates with +x direction along N-S
+  const double theta = (M_PI / 2) - el;
+  const double phi = az - (M_PI / 4);
+
+  // Get lengths
+  const int N = std::distance(frequencies, frequencies_end);
+
+  // Sanity checks
+  if (N != std::distance(polx, polx_end) || N != std::distance(poly, poly_end) || N != std::distance(polz, polz_end))
+  {
+    throw PyCR::ValueError("[hCalibratePolarization] input vectors have incompatible sizes.");
+  }
+
+  // Get iterators
+  CIter polx_it = polx;
+  CIter poly_it = poly;
+  CIter polz_it = polz;
+  NIter freq_it = frequencies;
+
+  for (int i=0; i<N; i++)
+  {
+    // Get element response
+    LOFAR::element_response_lba(*freq_it, az, el, J);
+
+    // Invert the Jones matrix (J contains beam pattern so J^-1 is the correction)
+    det = (J[0][0] * J[1][1]) - (J[0][1] * J[1][0]);
+
+    if (det == std::complex<double>(0,0)) throw PyCR::ValueError("[hCalibratePolarization] Jones matrix is singular.");
+
+    Jinv[0][0] = J[1][1] / det;
+    Jinv[0][1] = -1.0 * J[0][1] / det;
+    Jinv[1][0] = -1.0 * J[1][0] / det;
+    Jinv[1][1] = J[0][0] / det;
+
+    // Correct polarizations for antenna gain pattern
+    p_theta = Jinv[0][0] * *polx_it + Jinv[0][1] * *poly_it;
+    p_phi = Jinv[1][0] * *polx_it + Jinv[1][1] * *poly_it;
+
+    // Project onto new coordinate frame
+    *polx_it = cos(theta) * cos(phi) * p_theta - sin(phi) * p_phi;
+    *poly_it = cos(theta) * sin(phi) * p_theta + cos(phi) * p_phi;
+    *polz_it = sin(theta) * p_theta;
+
+    ++polx_it;
+    ++poly_it;
     ++freq_it;
   }
 }
