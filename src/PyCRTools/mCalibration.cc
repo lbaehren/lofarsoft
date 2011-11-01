b17cb58bfae48c91e4fb263a57b86c5ad535fcd8
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-01 09:35:10 +0000

    Inserted all necessary components for the polarization/gain calibration except the actual beam pattern which is provided by an external LOFAR library.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8604 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/mCalibration.cc
+++ src/PyCRTools/mCalibration.cc
@@ -85,6 +85,63 @@ void HFPP_FUNC_NAME (const CIter pol0, const CIter pol0_end,
     const NIter frequencies, const NIter frequencies_end,
     const HNumber az, const HNumber el)
 {
+  std::complex<double> J[2][2]; // Jones matrix for element response
+  std::complex<double> Jinv[2][2]; // Inverse Jones matrix used for beam correction in on sky polarization
+  std::complex<double> result[2][2]; // Final matrix used for beam correction
+  std::complex<double> det; // Determinant of Jones matrix (used for matrix inversion)
+  double R[2][2]; // Rotation + Projection matrix
+  double phi, theta;
+
+  const double pi_2 = M_PI / 2;
+  const double pi_4 = M_PI / 4;
+
+  // Get lengths
+  const int N = std::distance(frequencies, frequencies_end);
+
+  // Sanity checks
+  if (N != std::distance(pol0, pol0_end) || N != std::distance(pol1, pol1_end))
+  {
+    throw PyCR::ValueError("[hCalibratePolarization] input vectors have incompatible sizes.");
+  }
+
+  // Calculate angles
+  phi = az;
+  theta = pi_2 - el;
+
+  // Calculate rotation matrix
+  R[0][0] = cos(phi - pi_4) * cos(theta);
+  R[0][1] = -1.0 * sin(phi - pi_4);
+  R[1][0] = sin(phi - pi_4) * cos(theta);
+  R[1][1] = cos(phi - pi_4);
+
+  // Get iterators
+  CIter pol0_it = pol0;
+  CIter pol1_it = pol1;
+  NIter frequencies_it = frequencies;
+
+  for (int i=0; i<N; i++)
+  {
+    // Get element response
+//    LOFAR::element_response_lba(*freq_it, az, el, J);
+
+    // Invert the Jones matrix
+    det = (J[0][0] * J[1][1]) - (J[0][1] * J[1][0]);
+
+    if (det == std::complex<double>(0,0)) throw PyCR::ValueError("[hCalibratePolarization] Jones matrix is singular.");
+
+    Jinv[0][0] = J[1][1] / det;
+    Jinv[0][1] = -1.0 * J[0][1] / det;
+    Jinv[1][0] = -1.0 * J[1][0] / det;
+    Jinv[1][1] = J[0][0] / det;
+
+    // Get the final multiplication matrix
+    result[0][0] = R[0][0] * Jinv[0][0] + R[0][1] * Jinv[1][0];
+    result[0][1] = R[0][0] * Jinv[0][1] + R[0][1] * Jinv[1][1];
+    result[1][0] = R[1][0] * Jinv[0][0] + R[1][1] * Jinv[1][0];
+    result[1][1] = R[1][0] * Jinv[0][1] + R[1][1] * Jinv[1][1];
+
+    // Correct polarizations for antenna gain pattern
+  }
 }
 //$COPY_TO HFILE: #include "hfppnew-generatewrappers.def"
 
