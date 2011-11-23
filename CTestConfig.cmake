3616da28a3b3253444d195ff1eb91413c44e3f4c
lbaehren <lbaehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-23 21:15:42 +0000

    Increase timeout for running test, as some of the larger external packages (such as casacore) take quite a while to finish compiling.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8802 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,8 @@
 
-set(CTEST_PROJECT_NAME "LUSCommon")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
-
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "svn.astron.nl")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=LUSCommon")
-set(CTEST_DROP_SITE_CDASH TRUE)
+set (CTEST_PROJECT_NAME       "LUSCommon"     )
+set (CTEST_NIGHTLY_START_TIME "00:00:00 CET"  )
+set (CTEST_TESTING_TIMEOUT    "3600"          )
+set (CTEST_DROP_METHOD        "http"          )
+set (CTEST_DROP_SITE          "svn.astron.nl" )
+set (CTEST_DROP_LOCATION "/CDash/submit.php?project=LUSCommon")
+set (CTEST_DROP_SITE_CDASH TRUE)
