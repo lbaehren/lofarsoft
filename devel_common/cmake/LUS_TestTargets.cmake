2f8e8e544300ab350f8247b4735197c3ef6037fe
lbaehren <lbaehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-23 21:15:47 +0000

    Additional check before defining test targets.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8805 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/LUS_TestTargets.cmake
+++ devel_common/cmake/LUS_TestTargets.cmake
@@ -95,29 +95,56 @@ ${CMAKE_CTEST_COMMAND} -D ExperimentalSubmit"
   
   ## Test building the packages from within the LUS framework
   
-  add_test (test_build_anaamika   ${CMAKE_BUILD_TOOL} anaamika  )
-  add_test (test_build_dal        ${CMAKE_BUILD_TOOL} dal       )
   add_test (test_build_contrib    ${CMAKE_BUILD_TOOL} contrib   )
-  add_test (test_build_rm         ${CMAKE_BUILD_TOOL} rm        )
-  add_test (test_build_pulsar     ${CMAKE_BUILD_TOOL} pulsar    )
+  add_test (test_build_dal        ${CMAKE_BUILD_TOOL} dal       )
   add_test (test_build_pycrtools  ${CMAKE_BUILD_TOOL} pycrtools )
 
-  ## Run the tests defined as part of the individual packages
+  ## Anaamika
   
-  if (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
-    add_test (
-      NAME test_tests_anaamika
-      WORKING_DIRECTORY ${LUS_BINARY_DIR}/src/Anaamika/src/anaamika-build
-      COMMAND ${CMAKE_BUILD_TOOL} Experimental
-      )
-  endif (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
+  if (TARGET anaamika)
+    
+    add_test (test_build_anaamika ${CMAKE_BUILD_TOOL} anaamika )
+    
+    if (EXISTS ${LUS_BINARY_DIR}/src/Anaamika/src/anaamika-build)
+      add_test (
+	NAME test_tests_anaamika
+	WORKING_DIRECTORY ${LUS_BINARY_DIR}/src/Anaamika/src/anaamika-build
+	COMMAND ${CMAKE_CTEST_COMMAND} -D Experimental
+	)
+    endif (EXISTS ${LUS_BINARY_DIR}/src/Anaamika/src/anaamika-build)
+    
+  endif (TARGET anaamika)
+
+  ## Pulsar Tools
   
-  if (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
-    add_test (
-      NAME test_tests_rm
-      WORKING_DIRECTORY ${LUS_BINARY_DIR}/src/RM/src/rm-build
-      COMMAND ${CMAKE_BUILD_TOOL} Experimental
-      )
-  endif (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
+  if (TARGET pulsar)
+    
+    add_test (test_build_pulsar ${CMAKE_BUILD_TOOL} pulsar )
+    
+    if (EXISTS ${LUS_BINARY_DIR}/pulsar)
+      add_test (
+	NAME test_tests_pulsar
+	WORKING_DIRECTORY ${LUS_BINARY_DIR}/pulsar
+	COMMAND ${CMAKE_CTEST_COMMAND} -D Experimental
+	)
+    endif (EXISTS ${LUS_BINARY_DIR}/pulsar)
+    
+  endif (TARGET pulsar)
+  
+  ## RM-Synthesis tools
+  
+  if (TARGET rm)
+    
+    add_test (test_build_rm ${CMAKE_BUILD_TOOL} rm )
+    
+    if (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
+      add_test (
+	NAME test_tests_rm
+	WORKING_DIRECTORY ${LUS_BINARY_DIR}/src/RM/src/rm-build
+	COMMAND ${CMAKE_CTEST_COMMAND} -D Experimental
+	)
+    endif (EXISTS ${LUS_BINARY_DIR}/src/RM/src/rm-build)
+    
+  endif (TARGET rm)
   
 endif (NOT LUS_TESTTARGETS_CMAKE)
