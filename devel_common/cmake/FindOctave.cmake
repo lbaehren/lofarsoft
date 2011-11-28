430ca9301f386f1addad197af48a465eb6034a1d
lbaehren <lbaehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-28 20:20:55 +0000

    Update of CMake module to find components of Octave installation.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8842 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/FindOctave.cmake
+++ devel_common/cmake/FindOctave.cmake
@@ -1,8 +1,6 @@
 # +-----------------------------------------------------------------------------+
-# | $Id::                                                                     $ |
-# +-----------------------------------------------------------------------------+
-# |   Copyright (C) 2007                                                        |
-# |   Lars B"ahren (bahren@astron.nl)                                           |
+# |   Copyright (C) 2011                                                        |
+# |   Lars B"ahren (lbaehren@gmail.com)                                         |
 # |                                                                             |
 # |   This program is free software; you can redistribute it and/or modify      |
 # |   it under the terms of the GNU General Public License as published by      |
@@ -20,71 +18,86 @@
 # |   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 |
 # +-----------------------------------------------------------------------------+
 
-# - Check for the presence of Octave
+# - Check for the presence of OCTAVE
 #
-# The following variables are set when Octave is found:
-#  HAVE_OCTAVE       = Set to true, if all components of Octave
-#                          have been found.
-#  OCTAVE_INCLUDES   = Include path for the header files of Octave
-#  OCTAVE_LIBRARIES  = Link these to use Octave
+# The following variables are set when OCTAVE is found:
+#  OCTAVE_FOUND      = Set to true, if all components of OCTAVE have been found.
+#  OCTAVE_INCLUDES   = Include path for the header files of OCTAVE
+#  OCTAVE_LIBRARIES  = Link these to use OCTAVE
 #  OCTAVE_LFLAGS     = Linker flags (optional)
 
-## -----------------------------------------------------------------------------
-## Search locations
+if (NOT OCTAVE_FOUND)
 
-include (CMakeSettings)
-
-## -----------------------------------------------------------------------------
-## Check for the header files
-
-find_path (OCTAVE_INCLUDES octave/oct.h
-  PATHS ${include_locations}
-  PATH_SUFFIXES octave2.9 octave-2.9 octave-2.9.9 octave2.1 octave-2.1.73 octave-2.1.73
-  NO_DEFAULT_PATH
-  )
-
-## -----------------------------------------------------------------------------
-## Check for the library
-
-find_library (OCTAVE_LIBRARIES octave
-  PATHS ${lib_locations}
-  PATH_SUFFIXES octave2.9 octave-2.9 octave-2.9.9 octave2.1 octave-2.1.73 octave-2.1.73
-  NO_DEFAULT_PATH
-  )
-
-## -----------------------------------------------------------------------------
-## Actions taken when all components have been found
-
-if (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
-  set (HAVE_OCTAVE TRUE)
-else (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
-  set (HAVE_OCTAVE FALSE)
-  if (NOT OCTAVE_FIND_QUIETLY)
-    if (NOT OCTAVE_INCLUDES)
-      message (STATUS "Unable to find Octave header files!")
-    endif (NOT OCTAVE_INCLUDES)
-    if (NOT OCTAVE_LIBRARIES)
-      message (STATUS "Unable to find Octave library files!")
-    endif (NOT OCTAVE_LIBRARIES)
-  endif (NOT OCTAVE_FIND_QUIETLY)
-endif (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
-
-if (HAVE_OCTAVE)
-  if (NOT OCTAVE_FIND_QUIETLY)
-    message (STATUS "Found components for Octave")
-    message (STATUS "OCTAVE_INCLUDES  = ${OCTAVE_INCLUDES}")
-    message (STATUS "OCTAVE_LIBRARIES = ${OCTAVE_LIBRARIES}")
-  endif (NOT OCTAVE_FIND_QUIETLY)
-else (HAVE_OCTAVE)
-  if (OCTAVE_FIND_REQUIRED)
-    message (FATAL_ERROR "Could not find Octave!")
-  endif (OCTAVE_FIND_REQUIRED)
-endif (HAVE_OCTAVE)
-
-## -----------------------------------------------------------------------------
-## Mark advanced variables
-
-mark_as_advanced (
-  OCTAVE_INCLUDES
-  OCTAVE_LIBRARIES
-  )
+  ## Include common CMake settings
+  if (EXISTS LUS_CMakeSettings.cmake)
+    include (LUS_CMakeSettings)
+  endif (EXISTS LUS_CMakeSettings.cmake)
+  
+  if (NOT OCTAVE_ROOT_DIR)
+    set (OCTAVE_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
+  endif (NOT OCTAVE_ROOT_DIR)
+  
+  set (OCTAVE_VERSIONS
+    octave2.9
+    octave-2.9
+    octave-2.9.9
+    octave2.1
+    octave-2.1.73
+    octave-2.1.73
+    )
+  
+  ##_____________________________________________________________________________
+  ## Check for the header files
+  
+  find_path (OCTAVE_INCLUDES octave/oct.h
+    HINTS ${OCTAVE_ROOT_DIR}
+    PATH_SUFFIXES ${OCTAVE_VERSIONS}
+    )
+  
+  ##_____________________________________________________________________________
+  ## Check for the library
+  
+  find_library (OCTAVE_LIBRARIES octave
+    HINTS ${OCTAVE_ROOT_DIR}
+    PATH_SUFFIXES ${OCTAVE_VERSIONS}
+    )
+  
+  ##_____________________________________________________________________________
+  ## Actions taken when all components have been found
+  
+  if (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
+    set (OCTAVE_FOUND TRUE)
+  else (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
+    set (OCTAVE_FOUND FALSE)
+    if (NOT OCTAVE_FIND_QUIETLY)
+      if (NOT OCTAVE_INCLUDES)
+	message (STATUS "Unable to find OCTAVE header files!")
+      endif (NOT OCTAVE_INCLUDES)
+      if (NOT OCTAVE_LIBRARIES)
+	message (STATUS "Unable to find OCTAVE library files!")
+      endif (NOT OCTAVE_LIBRARIES)
+    endif (NOT OCTAVE_FIND_QUIETLY)
+  endif (OCTAVE_INCLUDES AND OCTAVE_LIBRARIES)
+  
+  if (OCTAVE_FOUND)
+    if (NOT OCTAVE_FIND_QUIETLY)
+      message (STATUS "Found components for OCTAVE")
+      message (STATUS "OCTAVE_ROOT_DIR  = ${OCTAVE_ROOT_DIR}")
+      message (STATUS "OCTAVE_INCLUDES  = ${OCTAVE_INCLUDES}")
+      message (STATUS "OCTAVE_LIBRARIES = ${OCTAVE_LIBRARIES}")
+    endif (NOT OCTAVE_FIND_QUIETLY)
+  else (OCTAVE_FOUND)
+    if (OCTAVE_FIND_REQUIRED)
+      message (FATAL_ERROR "Could not find OCTAVE!")
+    endif (OCTAVE_FIND_REQUIRED)
+  endif (OCTAVE_FOUND)
+  
+  ##_____________________________________________________________________________
+  ## Mark advanced variables
+  
+  mark_as_advanced (
+    OCTAVE_INCLUDES
+    OCTAVE_LIBRARIES
+    )
+  
+endif (NOT OCTAVE_FOUND)
