db8bb9f75ade46394edbb8b15ef218652c8ae2ce
lbaehren <lbaehren@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-29 15:50:13 +0000

    Updating test driver script to run the various higher-level tests.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8874 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/LUS_TestTargets.cmake
+++ devel_common/cmake/LUS_TestTargets.cmake
@@ -23,25 +23,25 @@ if (NOT LUS_TESTTARGETS_CMAKE)
   ## Custom target to run the collection of tests and submit the
   ## results to the Dashboard server.
 
-  file (WRITE ${PROJECT_BINARY_DIR}/testbuild "## LUS test driver script\n")
-
-  foreach (_name
-      anaamika
-      dal
-      pulsar
-      pycrtools
-      rm
-      )
-    file (APPEND ${PROJECT_BINARY_DIR}/testbuild "${CMAKE_BUILD_TOOL} ${_name}\n")
-    file (APPEND ${PROJECT_BINARY_DIR}/testbuild "${CMAKE_BUILD_TOOL} ${_name}_Experimental\n")
-  endforeach (_name)
-
-  add_custom_target (DashboardBuild
-    COMMAND sh testbuild
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    COMMENT "Starting test builds to be submitted to Dashboard ..."
-    SOURCES ${PROJECT_SOURCE_DIR}/CTestConfig.cmake
+  find_file (LUS_TEST_DRIVER_IN LUS_TestDriver.in
+    PATHS ${PROJECT_SOURCE_DIR} ${LUS_ROOT_DIR}
+    PATH_SUFFIXES devel_common/cmake cmake
     )
+
+  if (LUS_TEST_DRIVER_IN)
+
+    configure_file (
+      ${LUS_TEST_DRIVER_IN}
+      ${PROJECT_BINARY_DIR}/LUS_TestDriver
+      )
+    
+    add_custom_target (DashboardBuild
+      COMMAND sh LUS_TestDriver
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+      COMMENT "Starting test builds to be submitted to Dashboard ..."
+      SOURCES ${PROJECT_SOURCE_DIR}/CTestConfig.cmake
+      )
+  endif (LUS_TEST_DRIVER_IN)
   
   ## ============================================================================
   ##
