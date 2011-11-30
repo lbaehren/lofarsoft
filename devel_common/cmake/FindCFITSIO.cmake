61e60b1b9635ec5b36a7535cd2c32f541f823bc1
swinbank <swinbank@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-11-30 13:43:45 +0000

    Tidy up FindCFITSIO.cmake
    - If CFITSIO is *not* found, set CFITSIO_FOUND to false so it is correctly
      shown in the bootstrap summary;
    - Remove NO_DEFAULT_PATH, so that system installations of CFITSIO can be found
      correctly (is there ever a situation where NO_DEFAULT_PATH is desirable?).
    
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8898 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- devel_common/cmake/FindCFITSIO.cmake
+++ devel_common/cmake/FindCFITSIO.cmake
@@ -23,61 +23,61 @@
 #  CFITSIO_FOUND          = Do we have CFITSIO?
 #  CFITSIO_LIBRARIES     = Set of libraries required for linking against CFITSIO
 #  CFITSIO_INCLUDES      = Directory where to find fitsio.h
-#  CFITSIO_VERSION       = Full version of the CFITSIO library (as deduced from 
+#  CFITSIO_VERSION       = Full version of the CFITSIO library (as deduced from
 #                          fitsio.h)
 #  CFITSIO_MAJOR_VERSION = Major version of the CFITSIO library
 #  CFITSIO_MINOR_VERSION = Minor version of the CFITSIO library
 
 if (NOT FIND_CFITSIO_CMAKE)
-  
+
   set (FIND_CFITSIO_CMAKE TRUE)
-  
+
   ##_____________________________________________________________________________
   ## Search locations
-  
+
   include (CMakeSettings)
-  
+
   ##_____________________________________________________________________________
   ## Check for the header files
-  
+
   set (CFITSIO_INCLUDES "")
-  
+
   FIND_PATH (HAVE_FITSIO_H fitsio.h
     HINTS ${CFITSIO_ROOT_DIR}
     PATHS ${include_locations}
     PATH_SUFFIXES include include/cfitsio cfitsio cfitsio/include
-    NO_DEFAULT_PATH
+#    NO_DEFAULT_PATH
     )
-  
+
   if (HAVE_FITSIO_H)
     list (APPEND CFITSIO_INCLUDES ${HAVE_FITSIO_H})
   endif (HAVE_FITSIO_H)
-  
+
   ##_____________________________________________________________________________
   ## Check for the parts of the library
-  
+
   FIND_LIBRARY (CFITSIO_CFITSIO_LIBRARY
     NAMES cfitsio
     HINTS ${CFITSIO_ROOT_DIR}
     PATHS ${lib_locations}
     PATH_SUFFIXES cfitsio cfitsio/lib
-    NO_DEFAULT_PATH
+#    NO_DEFAULT_PATH
     )
-  
+
   if (CFITSIO_CFITSIO_LIBRARY)
     set (CFITSIO_LIBRARIES ${CFITSIO_CFITSIO_LIBRARY})
   endif (CFITSIO_CFITSIO_LIBRARY)
-  
+
   ##_____________________________________________________________________________
   ## Determine library version
-  
+
   if (HAVE_FITSIO_H)
-    
+
     ## parse the header file for version string
     file (STRINGS ${HAVE_FITSIO_H}/fitsio.h CFITSIO_VERSION
       REGEX "CFITSIO_VERSION"
       )
-    
+
     if (CFITSIO_VERSION)
       ## extract full library version
       string (REGEX REPLACE "#define CFITSIO_VERSION " "" CFITSIO_VERSION ${CFITSIO_VERSION})
@@ -88,16 +88,16 @@ if (NOT FIND_CFITSIO_CMAKE)
     else (CFITSIO_VERSION)
       message (STATUS "Unable to extract CFITSIO version!")
     endif (CFITSIO_VERSION)
-    
+
   endif (HAVE_FITSIO_H)
-  
+
   ##_____________________________________________________________________________
   ## Check libcfitsio for required symbols
-  
+
   include (CheckLibraryExists)
-  
+
   if (CFITSIO_CFITSIO_LIBRARY)
-    foreach (libsymbol file_open ftp_open file_openfile ffopen ffopentest ffreopen)    
+    foreach (libsymbol file_open ftp_open file_openfile ffopen ffopentest ffreopen)
       check_library_exists (
 	${CFITSIO_CFITSIO_LIBRARY}
 	${libsymbol}
@@ -106,23 +106,24 @@ if (NOT FIND_CFITSIO_CMAKE)
 	)
     endforeach (libsymbol)
   endif (CFITSIO_CFITSIO_LIBRARY)
-  
+
   ##_____________________________________________________________________________
   ## Actions taken when all components have been found
-  
+
   IF (CFITSIO_INCLUDES AND CFITSIO_LIBRARIES)
     SET (CFITSIO_FOUND TRUE)
   ELSE (CFITSIO_INCLUDES AND CFITSIO_LIBRARIES)
+    SET (CFITSIO_FOUND FALSE)
     IF (NOT CFITSIO_FIND_QUIETLY)
       IF (NOT CFITSIO_INCLUDES)
-	MESSAGE (STATUS "Unable to find CFITSIO header files!")
+        MESSAGE (STATUS "Unable to find CFITSIO header files!")
       ENDIF (NOT CFITSIO_INCLUDES)
       IF (NOT CFITSIO_LIBRARIES)
-	MESSAGE (STATUS "Unable to find CFITSIO library files!")
+        MESSAGE (STATUS "Unable to find CFITSIO library files!")
       ENDIF (NOT CFITSIO_LIBRARIES)
     ENDIF (NOT CFITSIO_FIND_QUIETLY)
   ENDIF (CFITSIO_INCLUDES AND CFITSIO_LIBRARIES)
-  
+
   IF (CFITSIO_FOUND)
     IF (NOT CFITSIO_FIND_QUIETLY)
       MESSAGE (STATUS "Found components for CFITSIO")
@@ -139,10 +140,10 @@ if (NOT FIND_CFITSIO_CMAKE)
   ENDIF (CFITSIO_FOUND)
 
   set (HAVE_CFITSIO ${CFITSIO_FOUND})
-  
+
   ##_____________________________________________________________________________
   ## Mark as advanced ...
-  
+
   MARK_AS_ADVANCED (
     CFITSIO_CFITSIO_LIBRARY
     CFITSIO_INCLUDES
@@ -152,11 +153,11 @@ if (NOT FIND_CFITSIO_CMAKE)
     CFITSIO_MINOR_VERSION
     HAVE_FITSIO_H
     )
-  
+
   if (CFITSIO_libnsl)
     mark_as_advanced (
       CFITSIO_libnsl
       )
   endif (CFITSIO_libnsl)
-  
+
 endif (NOT FIND_CFITSIO_CMAKE)
