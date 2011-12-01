874cd04b0dea7e1a2b04e3d1aa6d7ffedc316179
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-12-01 15:29:15 +0000

    added pdmp to the tarball of results
    git-svn-id: http://usg.lofar.org/svn/code/trunk@8912 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
+++ src/Pulsar/scripts/make_subs_SAS_Ncore_Mmodes.sh
@@ -3076,7 +3076,7 @@ then
 	date
 	date >> $log
 	#tar_list="*/*profiles.pdf */RSP*/*pfd.ps */RSP*/*pfd.pdf */RSP*/*pfd.png */RSP*/*pfd.th.png */RSP*/*pfd.bestprof */RSP*/*.sub.inf */*.rfirep"
-	tar_list=`find ./ -type f \( -name "*.pdf" -o -name "*.ps" -o -name "*.pfd" -o -name "*.inf" -o -name "*.rfirep" -o -name "*png" -o -name "*out" -o -name "*parset" -o -name "*.par" -o -name "*.ar" \)`
+	tar_list=`find ./ -type f \( -name "*.pdf" -o -name "*.ps" -o -name "*.pfd" -o -name "*.inf" -o -name "*.rfirep" -o -name "*png" -o -name "*out" -o -name "*parset" -o -name "*.par" -o -name "*.ar" -o -name "*pdmp*" \)`
 	echo "tar cvzf ${PULSAR_ARRAY_PRIMARY[0]}_${OBSID}_plots.tar.gz  $tar_list" >> $log
 	tar cvzf ${PULSAR_ARRAY_PRIMARY[0]}_${OBSID}_plots.tar.gz $tar_list
 	
