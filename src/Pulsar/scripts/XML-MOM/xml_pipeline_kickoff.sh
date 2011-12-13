e2931fa6f187b5a0f7118d5909efbaa4132ef546
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2011-12-13 15:41:09 +0000

    print the locus node list to STDOUT;  fix problem with escaping the quotes for the cexec step
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9049 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/XML-MOM/xml_pipeline_kickoff.sh
+++ src/Pulsar/scripts/XML-MOM/xml_pipeline_kickoff.sh
@@ -184,14 +184,16 @@ else # if [[ $cep2 == 1 ]]
               then
                  echo "WARNING: Unable to determine correct list of locus nodes for CS data processing; using all nodes"
                  locus_list="0-99"
+              else 
+                 echo "Using the following locus nodes for CS processing: cexec locus:$locus_list"
               fi
            fi
           
            if [[ $hoover_only == 0 ]]
            then
-              echo 'cexec locus:${locus_list} "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; '$line' -del"'  >> $outfile.$obsid.CS.sh
+              echo 'cexec locus:'${locus_list}' "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; '$line' -del"'  >> $outfile.$obsid.CS.sh
            else
-              echo '#cexec locus:${locus_list} "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; '$line' -del"'  >> $outfile.$obsid.CS.sh
+              echo '#cexec locus:'${locus_list}' "cd /data/LOFAR_PULSAR_ARCHIVE_locus*/; '$line' -del"'  >> $outfile.$obsid.CS.sh
            fi
            echo 'cexec hoover:0 cd /data/LOFAR_PULSAR_ARCHIVE_locus101/; rm -rf '${obsid}'_CSplots;  mkdir -p '${obsid}'_CSplots ; cd '${obsid}'_CSplots ; mount_locus_nodes.sh;  cat /cep2/locus???_data/LOFAR_PULSAR_ARCHIVE_locus???/'${obsid}'_red/*log >> '${obsid}'_combined.log  ;  cat /cep2/locus???_data/LOFAR_PULSAR_ARCHIVE_locus???/'${obsid}'_red/stokes/beam_process_node.txt >> beam_process_node.txt ; ls /cep2/locus???_data/LOFAR_PULSAR_ARCHIVE_locus???/'${obsid}'_red/*tar.gz | grep tar | sed -e "s/^/tar xvzf /g" > untar.sh; chmod 777 untar.sh ; ./untar.sh ; rm untar.sh' |  sed -e "s/:0 /:0 \'/" -e "s/rm untar.sh/rm untar.sh\'/"  >> $outfile.$obsid.CS.sh      
            echo 'cexec hoover:0 "cd /data/LOFAR_PULSAR_ARCHIVE_locus101/'${obsid}'_CSplots/; thumbnail_combine.sh; lofar_status_map.py; cp /cep2/locus???_data/LOFAR_PULSAR_ARCHIVE_locus???/'${obsid}'_red/TA_heatmap.sh .; ./TA_heatmap.sh; convert -append *_core_status.png  *_remote_status.png  *_intl_status.png status.png; convert -append *TA_heatmap_log.png  *TA_heatmap_linear.png status.png; convert -scale 200x140-0-0 status.png status.th.png; mv status_diag.png status.png"'  >> $outfile.$obsid.CS.sh
