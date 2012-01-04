1e4058e1d339a2483f511630c0239cc37cdeeb65
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-01-04 13:40:43 +0000

    Added simple short script to run crevent pipeline in subprocesses.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9295 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/PyCRTools/pipelines/run_pipeline.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+"""This script runs the automatic pipeline (cr_event) on the LORA triggered LOFAR VHECR data.
+"""
+
+import os
+import glob
+import subprocess
+
+input_directory = "/data/VHECR/LORAtriggered/data"
+processed_files = "/data/VHECR/LORAtriggered/processed.log"
+
+# Read already processed files from list
+done = []
+if os.path.isfile(processed_files):
+    f = open(processed_files, "r")
+    done = [l.strip() for l in f.readlines()]
+    f.close()
+
+# Open processed files list for appending
+f = open(processed_files, "a")
+
+for file in glob.glob(input_directory+"/*.h5"):
+
+    if file not in done:
+        print "processing", file
+        status = subprocess.call("/opt/lofarsoft/src/PyCRTools/pipelines/cr_event.py "+file+" --datadir=/data/VHECR/LORAtriggered/data --outputdir=/data/VHECR/LORAtriggered/results --loradir /data/VHECR/LORAtriggered/LORA --lora_logfile LORAtime4 --max_data_length=12289024 --min_data_length=1 --search_window_width=5000 --nsigma=3 -R >& /data/VHECR/LORAtriggered/log_pipeline_run.txt", shell=True)
+
+        if status == 0:
+            f.write(file + "\n")
+
+    else:
+        print "skipping already processed", file
+
+# Close processed files list
+f.close()
+
