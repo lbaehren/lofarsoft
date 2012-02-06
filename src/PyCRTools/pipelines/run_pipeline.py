b2180205a083e6f0c536eb2a80c66a3150be8cc0
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-02-06 09:29:32 +0000

    Run multiple instances of CR pipeline in parallel.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9499 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/pipelines/run_pipeline.py
+++ src/PyCRTools/pipelines/run_pipeline.py
@@ -5,12 +5,35 @@
 
 import os
 import glob
+import multiprocessing
 import subprocess
 
-input_directory = "/data/VHECR/LORAtriggered/data"
+max_threads = 12 # maximum number of threads to use
+max_attempts = 2 # maximum number of times to attempt processing a file before adding to failed
+input_dir = "/data/VHECR/LORAtriggered/data"
 processed_files = "/data/VHECR/LORAtriggered/processed.log"
 failed_files = "/data/VHECR/LORAtriggered/failed.log"
 log_dir = "/data/VHECR/LORAtriggered/log"
+
+def call_pipeline(filename):
+    """Function that actually calls the subprocess for each file.
+    """
+    with open(log_dir+"/"+filename.split("/")[-1].rstrip(".h5")+".logtemp", "w") as f:
+
+        status = 1
+        attempt = 1
+
+        while (attempt <= max_attempts and status != 0):
+            print filename
+            status = subprocess.call("/opt/lofarsoft/src/PyCRTools/pipelines/cr_event.py "+filename+" --datadir=/data/VHECR/LORAtriggered/data --outputdir=/data/VHECR/LORAtriggered/results --loradir /data/VHECR/LORAtriggered/LORA --lora_logfile LORAtime4 --max_data_length=12289024 --min_data_length=1 --search_window_width=5000 --nsigma=3 -R", stdout=f, stderr=subprocess.STDOUT, shell=True)
+            attempt += 1
+
+    if status == 0:
+        with open(processed_files, "a", 1) as processed:
+            processed.write(filename+"\n")
+    else:
+        with open(failed_files, "a", 1) as failed:
+            failed.write(filename+"\n")
 
 # Read already processed files from list
 done = []
@@ -19,28 +42,17 @@ if os.path.isfile(processed_files):
     done = [l.strip() for l in f.readlines()]
     f.close()
 
-# Open processed files list for appending
-processed = open(processed_files, "a", 1)
+# Check wich files in the data directory have not been processed yet
+files = [f for f in glob.glob(input_dir +"/*.h5") if f not in done]
 
-# Open failed files list for appending
-failed = open(failed_files, "a", 1)
+# Check how many CPU's we have
+count = multiprocessing.cpu_count()
 
-for file in glob.glob(input_directory+"/*.h5"):
+print "Using", min(max_threads, count), "out of", count, "available CPU's."
 
-    if file not in done:
-        print "processing", file
-        with open(log_dir+"/"+file.split("/")[-1].rstrip(".h5")+".log", "w") as f:
-            status = subprocess.call("/opt/lofarsoft/src/PyCRTools/pipelines/cr_event.py "+file+" --datadir=/data/VHECR/LORAtriggered/data --outputdir=/data/VHECR/LORAtriggered/results --loradir /data/VHECR/LORAtriggered/LORA --lora_logfile LORAtime4 --max_data_length=12289024 --min_data_length=1 --search_window_width=5000 --nsigma=3 -R", stdout=f, stderr=subprocess.STDOUT, shell=True)
+# Create a thread pool
+pool = multiprocessing.Pool(processes=min(max_threads,count))
 
-        if status == 0:
-            processed.write(file + "\n")
-        else:
-            failed.write(file + "\n")
-
-    else:
-        print "skipping already processed", file
-
-# Close files
-processed.close()
-failed.close()
+# Execute the pipeline for each file
+pool.map(call_pipeline, files)
 
