474096099ef345d052d36f24e96d2517fef2dab5
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-02-13 08:54:24 +0000

    Fixes to script.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9537 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/rms_siderial.py
+++ src/PyCRTools/scripts/rms_siderial.py
@@ -1,3 +1,4 @@
+import glob
 import pycrtools as cr
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,10 +6,11 @@ import matplotlib.pyplot as plt
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-a", "--antenna", help="Antenna calculate RMS from.")
+parser.add_option("-a", "--antenna", help="Antenna number.", default=0)
+parser.add_option("-s", "--station", help="Station name (CS002, ...).", default="CS002")
 
 (options, args) = parser.parse_args()
 
-for file in args:
-    print file
+for filename in glob.iglob(options.directory+"*"+options.station+"*.h5"):
+    print filename
 
