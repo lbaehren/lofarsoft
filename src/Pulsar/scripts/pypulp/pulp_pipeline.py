5f2b34cba78fa3a0b232e4410c307363d16a8ea0
kondratiev <kondratiev@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-03-03 08:06:13 +0000

    time logging for waiting process improved
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9688 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/pypulp/pulp_pipeline.py
+++ src/Pulsar/scripts/pypulp/pulp_pipeline.py
@@ -423,7 +423,7 @@ class PipeUnit:
 			self.log.exception("Oops... job has crashed!\n%s\nStatus=%s" % (cmd, self.process.returncode))
 			sys.exit(1)
 
-	def waiting(self, prg, popen):
+	def waiting(self, prg, popen, job_start):
 		"""
 		Waiting for process to finish
 		"""
@@ -433,7 +433,10 @@ class PipeUnit:
 			# we pipe serr to sout, so no need to log serr
 #			self.log.stdout2log(sout)
 			self.log.info(sout)
-			self.log.info("Process pid=%d (%s) has finished, status=%d" % (popen.pid, prg, popen.returncode))
+			job_end = time.time()
+			job_total_time = job_end - job_start
+			self.log.info("Process pid=%d (%s) has finished at UTC %s, status=%d, Total runnung time: %.1f s (%.2f hrs)" % \
+				(popen.pid, prg, time.asctime(time.gmtime()), popen.returncode, job_total_time, job_total_time/3600.))
 			self.log.info("")
 			# if job is not successful
 			if popen.poll() != 0:
@@ -551,14 +554,16 @@ class PipeUnit:
 					zapstr="-zapchan 0:%d:%d" % (total_chan-1, self.nrChanPerSub)
 				self.log.info("Creating RFI mask...")
 				cmd="rfifind -o %s -psrfits -noclip -blocks 16 %s %s.fits" % (self.output_prefix, zapstr, self.output_prefix)
+				rfifind_start_time = time.time()
 				rfifind_popen = self.start_and_go(cmd, workdir=self.curdir)
 				self.log.info("Producing RFI report...")
 				samples_to_average=int(10000. / self.sampling) # 10 s worth of data
 				cmd="python %s/release/share/pulsar/bin/subdyn.py --psrfits --saveonly -n %d %s.fits" % (cep2.lofarsoft, samples_to_average, self.output_prefix)
+				subdyn_start_time = time.time()
 				subdyn_popen = self.start_and_go(cmd, workdir=self.curdir)
 
 				# waiting for rfifind to finish
-				self.waiting("rfifind", rfifind_popen)
+				self.waiting("rfifind", rfifind_popen, rfifind_start_time)
 
 			# running prepfold with and without mask
 			if total_chan >= 256 and total_chan <= 6000:
@@ -681,7 +686,7 @@ class PipeUnit:
 				self.execute(cmd, workdir=self.outdir)
 
 			# waiting for subdyn to finish
-			self.waiting("subdyn.py", subdyn_popen)
+			self.waiting("subdyn.py", subdyn_popen, subdyn_start_time)
 
 			# waiting for pdmp to finish
 			if not cmdline.opts.is_nopdmp and not cmdline.opts.is_nofold: 
