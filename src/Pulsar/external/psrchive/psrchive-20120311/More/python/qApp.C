0dc1cc789b19cece9b263e4f5f1f346e924a4483
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-03-17 20:22:58 +0000

    deleting this version since it has bug with dspsr and psrfits data
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9824 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/external/psrchive/psrchive-20120311/More/python/qApp.C
+++
@@ -1,9 +0,0 @@
-/***************************************************************************
- *
- *   Copyright (C) 2005 by Willem van Straten
- *   Licensed under the Academic Free License version 2.1
- *
- ***************************************************************************/
-class QApplication;
-QApplication *qApp = 0;
-
