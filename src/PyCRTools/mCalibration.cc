1021051665e50705f04dc25a35dacbecffb2ddc1
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-03-18 14:16:35 +0000

    Add functions and task to project polarizations on x,y,z frame and calculate Stokes I, Q, U and V.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9826 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/mCalibration.cc
+++ src/PyCRTools/mCalibration.cc
@@ -559,3 +559,161 @@ void HFPP_FUNC_NAME (const CIter pol0, const CIter pol0_end,
 }
 //$COPY_TO HFILE: #include "hfppnew-generatewrappers.def"
 
+//$DOCSTRING: Project on-sky polarizations onto x,y,z.
+//$COPY_TO HFILE START --------------------------------------------------
+#define HFPP_FUNC_NAME hProjectPolarizations
+//-----------------------------------------------------------------------
+#define HFPP_FUNCDEF  (HFPP_VOID)(HFPP_FUNC_NAME)("$DOCSTRING")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+#define HFPP_PARDEF_0 (HNumber)(polx)()("Polarization X.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_1 (HNumber)(poly)()("Polarization Y.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_2 (HNumber)(polz)()("Polarization Z.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_3 (HNumber)(pol0)()("Polarization 0.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_4 (HNumber)(pol1)()("Polarization 1.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_5 (HNumber)(az)()("Azimuth with respect to antenna frame.")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+#define HFPP_PARDEF_6 (HNumber)(el)()("Elevation with respect to antenna frame.")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+//$COPY_TO END --------------------------------------------------
+/*!
+  \brief $DOCSTRING
+  $PARDOCSTRING
+
+*/
+
+template <class NIter>
+void HFPP_FUNC_NAME (const NIter polx, const NIter polx_end,
+    const NIter poly, const NIter poly_end,
+    const NIter polz, const NIter polz_end,
+    const NIter pol0, const NIter pol0_end,
+    const NIter pol1, const NIter pol1_end,
+    const HNumber az, const HNumber el)
+{
+  // Get length of output vector
+  const int N = std::distance(pol0, pol0_end);
+
+  // Sanity checks
+  if (N != std::distance(pol1, pol1_end) || N != std::distance(polx, polx_end) || N != std::distance(poly, poly_end) || N != std::distance(polz, polz_end))
+  {
+    throw PyCR::ValueError("[hProjectPolarizations] input vectors have incompatible sizes.");
+  }
+
+  // Direction in spherical coordinates with +x direction along N-S
+  const double theta = (M_PI / 2) - el;
+  const double phi = az - (M_PI / 4);
+
+  // Get iterators
+  NIter pol0_it = pol0;
+  NIter pol1_it = pol1;
+  NIter polx_it = polx;
+  NIter poly_it = poly;
+  NIter polz_it = polz;
+
+  for (int i=0; i<N; i++)
+  {
+    // Project onto new coordinate frame
+    *polx_it = cos(theta) * cos(phi) * *pol0_it - sin(phi) * *pol1_it;
+    *poly_it = cos(theta) * sin(phi) * *pol0_it + cos(phi) * *pol1_it;
+    *polz_it = sin(theta) * *pol0_it;
+
+    ++pol0_it;
+    ++pol1_it;
+    ++polx_it;
+    ++poly_it;
+    ++polz_it;
+  }
+}
+//$COPY_TO HFILE: #include "hfppnew-generatewrappers.def"
+
+//$DOCSTRING: Calculate Stokes parameters I, Q, U and V
+//$COPY_TO HFILE START --------------------------------------------------
+#define HFPP_FUNC_NAME hStokesParameters
+//-----------------------------------------------------------------------
+#define HFPP_FUNCDEF  (HFPP_VOID)(HFPP_FUNC_NAME)("$DOCSTRING")(HFPP_PAR_IS_SCALAR)()(HFPP_PASS_AS_VALUE)
+#define HFPP_PARDEF_0 (HNumber)(S)()("Stokes parameters I, Q, U and V.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_1 (HNumber)(polx)()("Polarization X.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_2 (HNumber)(poly)()("Polarization Y.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_3 (HNumber)(polz)()("Polarization Z.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_4 (HNumber)(polxh)()("Hilbert transform of polarization X.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_5 (HNumber)(polyh)()("Hilbert transform of polarization Y.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+#define HFPP_PARDEF_6 (HNumber)(polzh)()("Hilbert transform of polarization Z.")(HFPP_PAR_IS_VECTOR)(STDIT)(HFPP_PASS_AS_REFERENCE)
+//$COPY_TO END --------------------------------------------------
+/*!
+  \brief $DOCSTRING
+  $PARDOCSTRING
+
+  The definitions of the Stokes parameters in the horizontal plane are,
+
+  \begin{align}
+  S_0 &= I = \frac{1}{n} \sum_{i=1}^{n} x_i^2 + \hat{x}_i^2 + y_i^2 + \hat{y}_i^2\\
+  S_1 &= Q = \frac{1}{n} \sum_{i=1}^{n} x_i^2 + \hat{x}_i^2 - y_i^2 - \hat{y}_i^2\\
+  S_2 &= U = \frac{2}{n} \sum_{i=1}^{n} x_i y_i + \hat{x}_i \hat{y}_i\\
+  S_3 &= V = \frac{2}{n} \sum_{i=1}^{n} \hat{x}_i y_i - x_i \hat{y}_i
+  \end{align}
+
+  in which $x_i$ gives the time-dependent electric field in the east-west direction, while $y_i$ corresponds to the north-south direction. The $\hat{x}_i$ and $\hat{y}_i are the complex propagation of the time-series obtained by a Hilbert transformation.
+*/
+
+template <class NIter>
+void HFPP_FUNC_NAME (const NIter S, const NIter S_end,
+    const NIter polx, const NIter polx_end,
+    const NIter poly, const NIter poly_end,
+    const NIter polz, const NIter polz_end,
+    const NIter polxh, const NIter polxh_end,
+    const NIter polyh, const NIter polyh_end,
+    const NIter polzh, const NIter polzh_end)
+{
+  double temp = 0;
+
+  // Get length of output vector
+  const int N = std::distance(polx, polx_end);
+
+  // Sanity checks
+  if (std::distance(S, S_end) != 4 || N != std::distance(poly, poly_end) || N != std::distance(polz, polz_end) ||
+    N != std::distance(polxh, polxh_end) || N != std::distance(polyh, polyh_end) || N != std::distance(polzh, polzh_end))
+  {
+    throw PyCR::ValueError("[hStokesParameters] input vectors have incompatible sizes.");
+  }
+
+  // Get iterators
+  NIter I = S;
+  NIter Q = S+1;
+  NIter U = S+2;
+  NIter V = S+3;
+  NIter polx_it = polx;
+  NIter poly_it = poly;
+  NIter polz_it = polz;
+  NIter polxh_it = polxh;
+  NIter polyh_it = polyh;
+  NIter polzh_it = polzh;
+
+  // Calculate Stokes parameters
+  *I = 0; *Q = 0; *U = 0; *V = 0;
+
+  for (int i=0; i<N; i++)
+  {
+    *I += *polx_it * *polx_it + *polxh_it * *polxh_it;
+    *Q -= *poly_it * *poly_it + *polyh_it * *polyh_it;
+    *U += *polx_it * *poly_it + *polxh_it * *polyh_it;
+    *V += *polxh_it * *poly_it - *polx_it * *polyh_it;
+
+    ++polx_it;
+    ++poly_it;
+    ++polz_it;
+    ++polxh_it;
+    ++polyh_it;
+    ++polzh_it;
+  }
+
+  /* I = 1/N \sum x^2 + y^2 + xh^2 + yh^2 and Q = 1/N \sum x^2 + y^2 - xh^2 - yh^2
+   * but after the loop I contains x^2 + y^2 and Q contains -xh^2 -yh^2
+   * so we may combine them to get the correct value using fewer calculations in the inner loop */
+  temp = *I;
+  *I -= *Q;
+  *Q += temp;
+
+  // Normalize
+  *I = *I / N;
+  *Q = *Q / N;
+  *U = 2 * *U / N;
+  *Q = 2 * *Q / N;
+}
+//$COPY_TO HFILE: #include "hfppnew-generatewrappers.def"
+
---
+++ src/PyCRTools/modules/tasks/stokesparameters.py
@@ -0,0 +1,70 @@
+"""
+Module documentation
+====================
+
+"""
+
+from pycrtools.tasks import Task
+import pycrtools as cr
+
+class StokesParameters(Task):
+    """Calculate Stokes parameters I, Q, U and V and the polarization angle.
+    """
+
+    parameters = dict(
+        timeseries_data = dict( default = None,
+            doc = "Timeseries data." ),
+        nantennas = dict( default = lambda self : self.timeseries_data.shape()[0] / 3,
+            doc = "Number of antennas." ),
+        pulse_start = dict( default = 0,
+            doc = "Start of pulse window." ),
+        pulse_end = dict( default = lambda self : self.timeseries_data.shape[1],
+            doc = "End of pulse window." ),
+        pulse_width = dict( default = lambda self : self.pulse_end - self.pulse_start,
+            doc = "Width of pulse window." ),
+        resample_factor = dict( default = 1,
+            doc = "Resample factor." ),
+        pulse_width_resampled = dict( default = lambda self : self.pulse_width * self.resample_factor,
+            doc = "Width of pulse window after resampling." ),
+        timeseries_data_resampled = dict( default = lambda self : cr.hArray(float, dimensions=(self.nantennas, self.pulse_width_resampled)),
+            doc = "Resampled timeseries data." ),
+        nfreq = dict( default = lambda self : self.pulse_width_resampled / 2 + 1,
+            doc = "Number of frequencies." ),
+        fft_data = dict( default = lambda self : cr.hArray(complex, dimensions=(self.nantennas, self.nfreq)),
+            doc = "Fourier transform of timeseries_data_resampled." ),
+        hilbertt = dict( default = lambda self : self.timeseries_data_resampled.new(), workarray = True,
+            doc = "Hilbert transform of *fft_data*." ),
+        stokes = dict( default = lambda self : cr.hArray(float, dimensions=(self.nantennas, 4)),
+            doc = "Stokes parameters I, Q, U and V for each antenna."),
+        polarization_angle = dict( default = lambda self : cr.hArray(float, self.nantennas),
+            doc = "Polarization angle ..math::`\phi=\atan(U/Q)` for each antenna."),
+        fftwplan = dict( default = lambda self : cr.FFTWPlanManyDftR2c(self.pulse_width_resampled, 1, 1, 1, 1, 1, cr.fftw_flags.ESTIMATE) ),
+        ifftwplan = dict( default = lambda self : cr.FFTWPlanManyDftC2r(self.pulse_width_resampled, 1, 1, 1, 1, 1, cr.fftw_flags.ESTIMATE) ),
+    )
+
+    def run(self):
+        """Run the task.
+        """
+
+        # Resample singal
+        cr.hFFTWResample(self.timeseries_data_resampled[...], self.timeseries_data[..., self.pulse_start:self.pulse_end])
+
+        # Compute FFT
+        cr.hFFTWExecutePlan(self.fft_data[...], self.timeseries_data_resampled[...], self.fftwplan)
+
+        # Apply Hilbert transform
+        cr.hApplyHilbertTransform(self.fft_data[...])
+
+        # Get inverse FFT
+        cr.hFFTWExecutePlan(self.hilbertt[...], self.fft_data[...], self.ifftwplan)
+        self.hilbertt /= self.pulse_width_resampled
+
+        # Calculate Stokes parameters
+        cr.hStokesParameters(self.stokes[...], self.hilbertt[0:3*self.nantennas:3,...], self.hilbertt[1:3*self.nantennas:3,...], self.hilbertt[2:3*self.nantennas:3,...], self.hilbertt[0:3*self.nantennas:3,...], self.hilbertt[1:3*self.nantennas:3,...], self.hilbertt[2:3*self.nantennas:3,...]) 
+
+        # Calculate polarization angle
+        self.polarization_angle[...].copy(self.stokes[1:self.nantennas:4, ...])
+        self.polarization_angle[...].div(self.stokes[2:self.nantennas:4, ...])
+        self.polarization_angle.atan()
+        self.polarization_angle /= 2
+
