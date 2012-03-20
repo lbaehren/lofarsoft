24d7dd2c6613f94d1c1bb018a0ae2b4b709e24c0
kondratiev <kondratiev@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-03-20 20:19:45 +0000

    pav -B -> pav -J
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9850 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/dspsr_ar_plots.sh
+++ src/Pulsar/scripts/dspsr_ar_plots.sh
@@ -6,5 +6,5 @@ nch=$2
 pav -DFTp -g ${stem}_DFTp.ps/cps $stem.ar
 pav -GTpf$nch -g ${stem}_GTpf$nch.ps/cps $stem.ar
 pav -YFp -g ${stem}_YFp.ps/cps $stem.ar
-pav -B -g ${stem}_B.ps/cps $stem.ar
+pav -J -g ${stem}_B.ps/cps $stem.ar
 convert \( ${stem}_GTpf$nch.ps ${stem}_B.ps +append \) \( ${stem}_DFTp.ps ${stem}_YFp.ps +append \) -append -rotate 90 -background white -flatten ${stem}_diag.png
