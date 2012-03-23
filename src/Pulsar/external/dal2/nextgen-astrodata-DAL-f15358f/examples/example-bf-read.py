7b3456a500ef2cbcbcf696e273d364558291ff9b
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-03-23 14:59:54 +0000

    adding the dal2 with Sven's tools
    git-svn-id: http://usg.lofar.org/svn/code/trunk@9911 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/Pulsar/external/dal2/nextgen-astrodata-DAL-f15358f/examples/example-bf-read.py
@@ -0,0 +1,61 @@
+import DAL
+import numpy
+from itertools import count
+
+# open the file for reading
+f = DAL.BF_File("test.h5")
+
+# functions like f.observationID() return attribute objects, which support the following
+# methods:
+#       get()    retrieve the attribute (but bail if it does not exist)
+#       set(x)   set the attribute to value x (creating the attribute if needed)
+#       exists() check for the existence of the attribute
+#       remove() delete the attribute from the group
+#       name()   the name of the attribute in the HDF5 file (f.e. "OBSERVATION_ID")
+print "Observation %s ran from %s UTC to %s UTC" % (f.observationID().get(), f.observationStartUTC().get(), f.observationEndUTC().get())
+
+for sapnr in count():
+  # functions like f.subArrayPointing(x) return group objects, which have their own attributes.
+  # A group is only really opened only if its attributes/datasets/subgroups are accessed, which
+  # allows the following functionality first:
+  #
+  #     create()         create the group
+  #     exists()         check for the existence of the group
+
+  sap = f.subArrayPointing(sapnr)
+
+  if not sap.exists():
+    break
+
+  print "SAP %u points to (%.2f, %.2f)" % (sapnr, sap.pointRA().get(), sap.pointDEC().get())
+
+  for beamnr in count():
+    beam = sap.beam(beamnr)
+
+    if not beam.exists():
+      break
+
+    print "    Beam %u points to (%.2f, %.2f)" % (beamnr, beam.pointRA().get(), beam.pointDEC().get())
+
+    for stokesnr in [0,1,2,3]:
+      # any stokes [0..3] can exist
+
+      stokes = beam.stokes(stokesnr)
+
+      if not stokes.exists():
+        continue
+
+      print "        Stokes %s has %u subbands and %u samples" % (stokes.stokesComponent().get(), stokes.nofSubbands().get(), stokes.nofSamples().get())
+
+      # data sets can get and set slices of data in the form of numpy arrays.
+      # The dimensions of the provided numpy arrays determine the amount of data
+      # that has to be read or written. The dimensionality must be the same as of
+      # the dataset. Care must be taken that the numpy array
+      # is of the correct type (stokes.dtype == numpy.float32 in this case).
+      x = numpy.array([[0,0],[0,0]], dtype=stokes.dtype)
+
+      # get2D(pos,array) reads `array` at position `pos`.
+      stokes.get2D([0,0],x);
+
+      print "            First two samples of the first two channels are:\n%s" % (x,)
+
---
+++ src/Pulsar/external/dal2/nextgen-astrodata-DAL-f15358f/examples/example-custom-write.py
@@ -0,0 +1,26 @@
+import DAL
+
+# create the file
+f = DAL.HDF5FileBase("foo.h5", DAL.HDF5FileBase.CREATE)
+
+# create an attribute
+a = DAL.AttributeString(f.group(), "MY_ATTRIBUTE")
+a.set("hello world!")
+
+# create and destroy a second attribute
+b = DAL.AttributeString(f.group(), "FAULTY_ATTRIBUTE")
+b.set("hello world!")
+b.remove()
+
+# create a dataset
+d = DAL.HDF5DatasetBaseComplexFloat(f.group(), "MY_DATASET")
+d.create([4,4],[4,4])
+
+# insert some data points
+d.setScalar([0,0],1+2j)
+d.setScalar([1,0],2+3j)
+d.setScalar([0,1],3+4j)
+
+# flushing is not needed, but can be done explicitly
+f.flush()
+
