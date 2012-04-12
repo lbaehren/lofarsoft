e1e9a632035ddfe32afaa89b7856bf4c19f792dc
enriquez <enriquez@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-04-12 10:30:31 +0000

    Testing 3 different ways to go from RA-DEC to AZEL. Do we really need this many?
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10059 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/test/testequatorial2horizontal.py
+++ src/PyCRTools/test/testequatorial2horizontal.py
@@ -7,6 +7,9 @@ from pytmf import *
 import pycrtools as cr
 import math
 
+deg=math.pi/180.
+pi2=math.pi/2.
+
 old_test=0
 
 if old_test:
@@ -17,9 +20,12 @@ if old_test:
     
     utc = gregorian2jd(1987, 4, 10. + ((19. + 21. / 60.) / 24.)) # Obstime 
 
-else: # EE
+else: # By EE
     alpha = hms2rad(11, 30, 02.74); # Right assention  (Mars)
     delta = dms2rad(07, 42, 48.6); # Declination    
+#    alpha = hms2rad(03,32,59.37 ); # Right assention  (Pulsar)
+#    delta = dms2rad(54,34,44.9); # Declination    
+
     phi = deg2rad(52.915122495)  #(LOFAR Superterp)
     L = deg2rad(6.869837540)
     
@@ -38,23 +44,23 @@ horizontal = equatorial.new()
 cr.hEquatorial2Horizontal(horizontal, equatorial, utc, 0., L, phi)
 
 horizontal2 = radec2azel(alpha,delta,utc,0.,L,phi)
-#horizontal3 = equatorial2horizontal(alpha,delta,utc,0.,L,phi)   # need other parameters not the ones given here (but note the name discrepansies of the functions!!!  ie. radec2azel = hEquatorial2Horizontal != equatorial2horizontal)
+horizontal3 = equatorial2horizontal(last(utc,utc+delta_tt_utc(utc),L)-alpha,delta,phi)   #  note the name discrepansies of the functions' inputs/results!!!  ie. radec2azel = hEquatorial2Horizontal != equatorial2horizontal)
 
+#if horizontal[0] < 0:  horizontal[0] = 90*deg-horizontal[0] #2.*math.pi
 if horizontal[0] < 0:  horizontal[0] += 2.*math.pi
+
 
 # Print the result
 print '-------------------------'
-print 'horizontal2'
-print "A", rad2deg(horizontal2[0])+360, "h", rad2deg(horizontal2[1])
-print ''
-#print 'horizontal3'
-#print "A", rad2deg(horizontal3[0])+360, "h", rad2deg(horizontal3[1])
-#print ''
-print 'horizontal2='
+print 'horizontal='
 print "A", rad2deg(horizontal[0]), "h", rad2deg(horizontal[1])
+print 'horizontal2='
+print "A", rad2deg(horizontal2[0]), "h", rad2deg(horizontal2[1])
+print 'horizontal3='
+print "A", rad2deg(horizontal3[0]), "h", rad2deg(horizontal3[1])
 if old_test:
     print "Expected result: A 68.034 h 15.125"
 else:
-    print "Expected result: A 149.9026 h 41.2025" # From HORIZONS (NASA/JPL webtool for ephemerides.)
+    print "-------- \n Expected result for Mars at 2012:02:14.00 UT, from LOFAR-Superterp:\n A 149.9026 h 41.2025 \n --------" # From HORIZONS (NASA/JPL webtool for ephemerides.), as well as Stellarium (V.0.10.6)
 
   
