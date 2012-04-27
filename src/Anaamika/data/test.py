0e1a2d65a48d3ab0534eb0f5151a321b101a5910
rafferty <rafferty@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-04-27 11:43:31 +0000

    PyBDSM code clean up. Renamed project from Anaamika to PyBDSM and removed non-PyBDSM code.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10147 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Anaamika/data/test.py
+++
@@ -1,5 +0,0 @@
-"""Simple script to test basic functionality of PyBDSM"""
-import bdsm
-
-img = bdsm.execute(bdsm.fits_chain,{'fits_name': "WN65341H.fits", 'beam': (.0165515, .01500, 0.0), 'collapse_mode' : 'average', 'collapse_wt' : 'rms', 'thresh':"hard", 'polarisation_do': False, 'spectralindex_do': False, 'shapelet_do': False, 'psf_vary_do': False})
-
