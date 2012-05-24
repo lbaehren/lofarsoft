dfad597fdb7d0799429a1d9885b2c2f2f12197b4
alexov <alexov@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-05-24 09:13:57 +0000

    adding LD cmake find script for Daily Build, in order to build dspsr with DAL and autoconf
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10247 e3a606d5-b720-0410-b6ad-cb58f335abf8

---
+++ src/Pulsar/cmake/FindLdLibraryPath.cmake
@@ -0,0 +1,61 @@
+
+## ==============================================================================
+##
+##  Check for the presence and value of the LD_LIBRARY_PATH variable
+##
+## ==============================================================================
+
+set (PULSAR_LD_LIBRARY_PATH "")
+
+##____________________________________________________________________
+## Step 1: Is the LD_LIBRARY_PATH defined via an environment variable?
+
+find_path (PULSAR_LD_LIBRARY_PATH_ENV
+  NAMES libdal.so
+  $ENV{LD_LIBRARY_PATH}
+  )
+
+if (PULSAR_LD_LIBRARY_PATH_ENV)
+  list (APPEND PULSAR_LD_LIBRARY_PATH ${PULSAR_LD_LIBRARY_PATH_ENV})
+endif (PULSAR_LD_LIBRARY_PATH_ENV)
+
+##____________________________________________________________________
+## Step 2: LD_LIBRARY_PATH extension for LUS Python modules
+
+find_path (PULSAR_LD_LIBRARY_PATH_LUS
+  NAMES
+  PATHS
+  ${CMAKE_INSTALL_PREFIX}
+  ${CMAKE_INSTALL_PREFIX}/..
+  ${CMAKE_INSTALL_PREFIX}/../..
+  ${CMAKE_INSTALL_PREFIX}/../../lib
+  PATH_SUFFIXES
+  lib
+  share/pulsar/lib
+  )
+
+if (NOT PULSAR_LD_LIBRARY_PATH_LUS)
+  ## Reconstruct addition to LD path from LUS installation prefix
+  if (LUS_INSTALL_PREFIX)
+    set (PULSAR_LD_LIBRARY_PATH_LUS "")
+    string(REPLACE "share/pulsar" "lib" PULSAR_LD_LIBRARY_PATH_LUS ${LUS_INSTALL_PREFIX})
+  endif (LUS_INSTALL_PREFIX)
+endif (NOT PULSAR_LD_LIBRARY_PATH_LUS)
+
+if (PULSAR_LD_LIBRARY_PATH_LUS)
+  list (APPEND PULSAR_LD_LIBRARY_PATH ${PULSAR_LD_LIBRARY_PATH_LUS})
+endif (PULSAR_LD_LIBRARY_PATH_LUS)
+
+##____________________________________________________________________
+## Step 3: Add Pulsar-Tools specific extensions to LD_LIBRARY_PATH
+
+list (APPEND PULSAR_LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib)
+list (APPEND PULSAR_LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/../../lib)
+
+##____________________________________________________________________
+## Clean up the list: remove duplicate entries and export back to
+## environment variable.
+
+list (REMOVE_DUPLICATES PULSAR_LD_LIBRARY_PATH)
+
+set( ENV{LD_LIBRARY_PATH} ${PULSAR_LD_LIBRARY_PATH} )
