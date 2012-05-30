RFI phases test script: ongoing work. Plot phase stability across blocks. Apply calibration delays. Fix averaging over phases which can be ambiguous close to +/- pi. Etc.
git-svn-id: http://usg.lofar.org/svn/code/trunk@10290 e3a606d5-b720-0410-b6ad-cb58f335abf8

src/PyCRTools/scripts/testRFIphases.py
@@ -3,12 +3,16 @@
 from pycrtools import *
 from pycrtools import srcfind as sf
 
-deg2rad = 2*np.pi / 360.0
-f = open('/Users/acorstanje/triggering/CR/L29590_D20110714T174749.986Z_CS003_R000_tbb.h5')
+twopi = 2 * np.pi
+deg2rad = twopi / 360.0
+f = open('/Users/acorstanje/triggering/CR/L29590_D20110714T174749.986Z_CS005_R000_tbb.h5')
 
 #f = open('/Users/acorstanje/triggering/CR/L28318_D20110613T051515.914Z_CS003_R000_tbb.h5')
 #f = open('/Users/acorstanje/triggering/CR/L44792_D20120204T002716.906Z_CS003_R000_tbb.h5')
 f["BLOCKSIZE"]=65536
+
+# get calibration delays from file
+caldelays = np.array(f["DIPOLE_CALIBRATION_DELAY"]) # f[...] outputs list!
 
 plt.figure()
 ph = np.zeros(14)
@@ -17,10 +21,18 @@ n = 0
 fchannel = 5058
 freqs = f["FREQUENCY_DATA"]
 freq = freqs[fchannel]
-polarisation = 1
-for i in range(14):
+calphases = (twopi * freq) * caldelays
 
-    f["BLOCK"] = i
+polarisation = 0
+nblocks = 14
+measuredphases = np.zeros((nblocks, 96))
+for i in range(nblocks):
+ 
+    if i == 7:
+        f["BLOCK"] = 8
+    else:
+        f["BLOCK"] = i
+    
     
     magspectrum = f["FFT_DATA"]
     spectrum = hArray(copy=magspectrum)
@@ -37,7 +49,6 @@ for i in range(14):
     # s[...].maxpos()
     #f["FREQUENCY_DATA"][5058]
     #f["FREQUENCY_DATA"][4538]
-
     
     print freqs[fchannel]
     print (0.1 / (2*np.pi)) / freqs[fchannel]
@@ -46,18 +57,30 @@ for i in range(14):
 
     y = phases.toNumpy()
     y = np.angle(y)
+    y += calphases # subtract phases from delay calibration metadata
+    
     ph[i] = y[27] # just to print differences across blocks
     y -= y[polarisation] # subtract reference phase y[0] or y[1] (polarisation)
     
     wrapped = y
-    for k in range(len(y)): # must be a better way to do this...
-        wrapped[k] = y[k] - 2*np.pi * round(y[k] / (2*np.pi))
+#    for k in range(len(y)): # must be a better way to do this...
+#        wrapped[k] = y[k] - 2*np.pi * round(y[k] / (2*np.pi))
+    wrapped -= twopi * (y / twopi).round()
+  #  wrapped[12] = 0.0
+    # NB. Cannot average sequence of phases just like that; when having e.g. phi = +/- pi + noise, 
+    # a spurious term -/+ k (2pi / n) will come in, depending on the number of phase wraps k. 
+    # Therefore, force consecutive phases to have abs-difference < pi
+    if n > 0:
+        wrapped -= twopi * ((wrapped - lastwrapped) / twopi).round()
+    measuredphases[i] = np.copy(wrapped)
     
     averagePhasePerAntenna += wrapped[polarisation::2]
     n+=1
     
     if i < 15:
         plt.plot(wrapped[polarisation::2])
+
+    lastwrapped = np.copy(wrapped) # for disambiguating phases around wrapping points +/- pi (+ noise)
 
     delta = y[32] - y[0]
     print delta -  2*np.pi * round(delta / (2*np.pi))
@@ -75,7 +98,7 @@ allpositions = f["ANTENNA_POSITIONS"].toNumpy()
 positions = allpositions[polarisation::2].ravel() # only pol 'polarisation'
 # NB. Indexing to the end is done by thisArray[start::step] !
 
-(fitaz, fitel, minPhaseError) = sf.directionBruteForcePhases(positions, averagePhasePerAntenna, freq, elSteps = 9, showImage=True, verbose=True)
+(fitaz, fitel, minPhaseError) = sf.directionBruteForcePhases(positions, averagePhasePerAntenna, freq, azSteps = 5 * 360,elSteps = 18, showImage=True, verbose=True)
 
 print 'Best fit az = %f, el = %f' % (fitaz / deg2rad, fitel / deg2rad)
 print 'Phase error = %f' % minPhaseError
@@ -89,4 +112,9 @@ phaseDiff = averagePhasePerAntenna - modelphases
 
 plt.figure()
 plt.plot(sf.phaseWrap(phaseDiff))
+
+#plt.figure()
+rms_phase = measuredphases.std(axis=0)
+plt.plot(rms_phase[polarisation::2], 'r')
+plt.plot( - rms_phase[polarisation::2], 'r')
 