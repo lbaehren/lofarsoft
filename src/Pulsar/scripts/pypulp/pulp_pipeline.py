bd6387e11878a134caf2f35a686d95e5b66c2e6f
kondratiev <kondratiev@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-05-31 20:24:25 +0000

    small bug fixed in CV processing with DAL
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10297 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/Pulsar/scripts/pypulp/pulp_pipeline.py
+++ src/Pulsar/scripts/pypulp/pulp_pipeline.py
@@ -1345,9 +1345,10 @@ class CVUnit(PipeUnit):
 						psr2=re.sub(r'[BJ]', '', psr)
 						# loop on frequency splits
 						for ii in range(len(s0_files)):
+							fpart=int(s0_files[ii].split("_P")[-1].split("_")[0])
 							cmd="dspsr -A -L %d %s -fft-bench -E %s/%s.par -O %s_%s_P%d -t %d -U %f %s" % \
 								(cmdline.opts.tsubint, verbose, self.outdir, psr2, \
-								psr, self.output_prefix, ii, cmdline.opts.nthreads, cmdline.opts.maxram, s0_files[ii])
+								psr, self.output_prefix, fpart, cmdline.opts.nthreads, cmdline.opts.maxram, s0_files[ii])
 							self.execute(cmd, workdir=self.curdir)
 
 						# running psradd to add all freq channels together
