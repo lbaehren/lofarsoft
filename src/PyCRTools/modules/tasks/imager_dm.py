5b1a1b561bec85f97aeb23be447ac97f8032fd9c
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-11-13 15:07:59 +0000

    Go back to even older version of the imager.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10929 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/imager_dm.py
+++ src/PyCRTools/modules/tasks/imager_dm.py
@@ -10,100 +10,46 @@ import pycrtools as cr
 import pytmf
 import numpy as np
 import time
-import pyfits
-import os
-
-def savefits(filename, array, overwrite=True, **kwargs):
-    """Save image as standard FITS file.
-    """
-
-    # Convert image to Numpy array if required
-    if cr.ishArray(array):
-        array = array.toNumpy()
-
-    # Create new FITS structure (array needs to be transposed for FITS convention)
-    hdu = pyfits.PrimaryHDU(array.transpose())
-
-    # Create FITS header
-    hdr = hdu.header
-
-    for key in sorted(kwargs.iterkeys()):
-
-        # NAXIS are calculated by pyfits and need to match
-        if not "NAXIS" in key:
-
-            # Correct for ugly bug in casaviewer
-            if "CTYPE" in key and "ALON" in kwargs[key]:
-                hdr.update(key, "??LN-"+kwargs[key][5:])
-            elif "CTYPE" in key and "ALAT" in kwargs[key]:
-                hdr.update(key, "??LT-"+kwargs[key][5:])
-            else:
-                hdr.update(key, kwargs[key])
-
-    # Check if file exists and overwrite if requested
-    if not filename.endswith(".fits"):
-        filename += ".fits"
-    if os.path.isfile(filename) and overwrite:
-        os.remove(filename)
-    hdu.writeto(filename)
-
 
 class ImagerDM(Task):
     """Imager task documentation.
     """
 
-    parameters = dict(
-        data = dict( default = None,
-                     positional = 1 ),
-        image = dict( default = None ),
-        output = dict( default = "out.fits" ),
-        startblock = dict( default = 0 ),
-        nblocks = dict( default = 1 ),
-        ntimesteps = dict( default = lambda self : self.data["MAXIMUM_READ_LENGTH"] / (self.nblocks * self.data["BLOCKSIZE"]) ),
-        intgrfreq = dict( default = False,
-                          doc = "Output frequency integrated image." ),
-        inversefft = dict( default = False ),
-        mask = dict( default = None ),
-        FREQMIN = dict( default = None ),
-        FREQMAX = dict( default = None ),
-        OBSTIME = dict( default = lambda self : self.data["TIME"][0] ),
-        DM = dict( default = None ),
-        OBSLON = dict( default = pytmf.deg2rad(6.869837540),
-                       doc = "Observer longitude in radians" ),
-        OBSLAT = dict( default = pytmf.deg2rad(52.915122495),
-                       doc = "Observer latitude in radians" ),
-        NAXIS = dict( default = 4 ),
-        NAXIS1 = dict( default = 90 ),
-        NAXIS2 = dict( default = 90 ),
-        NAXIS3 = dict( default = 1 ),
-        NAXIS4 = dict( default = 1 ),
-        LONPOLE = dict( default = 0. ),
-        LATPOLE = dict( default = 90. ),
-        CTYPE1 = dict( default = 'ALON-STG' ),
-        CTYPE2 = dict( default = 'ALAT-STG' ),
-        CTYPE3 = dict( default = 'FREQ' ),
-        CTYPE4 = dict( default = 'TIME' ),
-        CRVAL1 = dict( default = 180. ),
-        CRVAL2 = dict( default = 90. ),
-        CRVAL3 = dict( default = 0. ),
-        CRVAL4 = dict( default = 0. ),
-        CRPIX1 = dict( default = lambda self : float(self.NAXIS1) / 2 ),
-        CRPIX2 = dict( default = lambda self : float(self.NAXIS2) / 2 ),
-        CRPIX3 = dict( default = 0. ),
-        CRPIX4 = dict( default = 0. ),
-        CDELT1 = dict( default = 2.566666603088E+00 ),
-        CDELT2 = dict( default = 2.566666603088E+00 ),
-        CDELT3 = dict( default = lambda self : self.data["FREQUENCY_INTERVAL"][0] ),
-        CDELT4 = dict( default = lambda self : self.nblocks * self.data["BLOCKSIZE"] * self.data["SAMPLE_INTERVAL"][0] ),
-        CUNIT1 = dict( default = 'deg' ),
-        CUNIT2 = dict( default = 'deg' ),
-        CUNIT3 = dict( default = 'Hz' ),
-        CUNIT4 = dict( default = 's' ),
-        PC001001 = dict( default = 1.000000000000E+00 ),
-        PC002001 = dict( default = 0.000000000000E+00 ),
-        PC001002 = dict( default = 0.000000000000E+00 ),
-        PC002002 = dict( default = 1.000000000000E+00 )
-    )
+    parameters = {
+        'image' : { "default" : None, "positional" : 1 },
+        'data' : { "default" : None, "positional" : 2 },
+        'mask' : { "default" : None, "positional" : 3 },
+        'startblock' : { "default" : 0 },
+        'nblocks' : { "default" : 16 },
+        'ntimesteps' : { "default" : 1 },
+        'nfmin' : { "default" : None },
+        'nfmax' : { "default" : None },
+        'obstime' : { "default" : 0 },
+        'L' : { "default" : pytmf.deg2rad(6.869837540) },
+        'phi' : { "default" : pytmf.deg2rad(52.915122495) },
+        'NAXIS' : { "default" : 2 },
+        'NAXIS1' : { "default" : 90 },
+        'NAXIS2' : { "default" : 90 },
+        'CTYPE1' : { "default" : 'ALON-STG' },
+        'CTYPE2' : { "default" : 'ALAT-STG' },
+        'LONPOLE' : { "default" : 0. },
+        'LATPOLE' : { "default" : 90. },
+        'CRVAL1' : { "default" : 180. },
+        'CRVAL2' : { "default" : 90. },
+        'CRPIX1' : { "default" : 45.5 },
+        'CRPIX2' : { "default" : 45.5 },
+        'CDELT1' : { "default" : 2.566666603088E+00 },
+        'CDELT2' : { "default" : 2.566666603088E+00 },
+        'CUNIT1' : { "default" : 'deg' },
+        'CUNIT2' : { "default" : 'deg' },
+        'PC001001' : { "default" : 1.000000000000E+00 },
+        'PC002001' : { "default" : 0.000000000000E+00 },
+        'PC001002' : { "default" : 0.000000000000E+00 },
+        'PC002002' : { "default" : 1.000000000000E+00 },
+        'DM' : { "default" : None },
+        'dt' : { "default" : None },
+        'inversefft' : { "default" : False }
+    }
 
     def init(self):
         """Initialize imager.
@@ -111,10 +57,9 @@ class ImagerDM(Task):
 
         # Generate coordinate grid
         print "Generating grid"
-
-        self.grid=CoordinateGrid(obstime=self.OBSTIME,
-                                 L=self.OBSLON,
-                                 phi=self.OBSLAT,
+        self.grid=CoordinateGrid(obstime=self.obstime,
+                                 L=self.L,
+                                 phi=self.phi,
                                  NAXIS=self.NAXIS,
                                  NAXIS1=self.NAXIS1,
                                  NAXIS2=self.NAXIS2,
@@ -136,15 +81,12 @@ class ImagerDM(Task):
                                  PC002002=self.PC002002)
         print "Grid generation finished"
         print self.grid
-         
+
         # Get frequencies
         self.frequencies=self.data.getFrequencies()
 
-        self.frequency_slice = None
-        if self.FREQMIN != None and self.FREQMAX != None:
-            frequency_slice = cr.hArray(int, 2)
-            cr.hFindSequenceBetweenOrEqual(frequency_slice, self.frequencies, self.FREQMIN, self.FREQMAX, 0, 0)
-            self.frequencies=self.frequencies[frequency_slice[0]:frequency_slice[1]]
+        if self.nfmin != None and self.nfmax != None:
+            self.frequencies=self.frequencies[range(self.nfmin, self.nfmax)]
 
         self.nfreq = len(self.frequencies)
 
@@ -153,18 +95,13 @@ class ImagerDM(Task):
         # Calculate dispersion measure shifts if requested
         if self.DM:
             self.dispersion_shifts = cr.hArray(int, self.nfreq)
-            cr.hDedispersionShifts(self.dispersion_shifts, self.frequencies, cr.hMin(self.frequencies).val(), self.DM, self.CDELT4)
+            cr.hDedispersionShifts(self.dispersion_shifts, self.frequencies, cr.hMin(self.frequencies).val(), self.DM, self.dt)
 
             print "Dedispersion shifts", self.dispersion_shifts
 
-        # Get steps corresponding to mask if given
-        if self.mask != None:
-            self.step = cr.hArray(int, cr.hCountZero(self.mask))
-            cr.hMaskToStep(self.step, self.mask)
-
         # Get antenna positions
         self.antpos=self.data.getRelativeAntennaPositions()
-        self.nantennas=int(self.antpos.shape()[0])
+        self.nantennas=int(self.antpos.getDim()[0])
 
         # Calculate geometric delays for all sky positions for all antennas
         self.delays = cr.hArray(float, dimensions=(self.NAXIS1, self.NAXIS2, self.nantennas))
@@ -172,7 +109,7 @@ class ImagerDM(Task):
 
         # Create plan for inverse FFT if needed
         if self.inversefft:
-            self.blocksize = (self.nfreq - 1) * 2
+            self.blocksize = (self.nfreq - 1) * 2#self.data["BLOCKSIZE"]
             self.plan = cr.FFTWPlanManyDftC2r(self.blocksize, self.NAXIS1 * self.NAXIS2, 1, self.nfreq, 1, self.blocksize, cr.fftw_flags.ESTIMATE)
             print "created inverse fft plan"
             self.t_image2=cr.hArray(float, dimensions=(self.NAXIS1, self.NAXIS2, self.blocksize), fill=0.)
@@ -180,155 +117,53 @@ class ImagerDM(Task):
         # Initialize empty arrays
         self.scratchfft = self.data.empty("FFT_DATA")
         self.fftdata=cr.hArray(complex, dimensions=(self.nantennas, self.nfreq))
-        if not self.intgrfreq:
-            self.t_image=cr.hArray(complex, dimensions=(self.NAXIS1, self.NAXIS2, self.nfreq), fill=0.)
-
-        # Create image array if none is given as input
-        if self.image==None:
-            if self.intgrfreq:
-                self.image = np.zeros(shape=(self.ntimesteps, self.NAXIS1, self.NAXIS2), dtype=float)
-            elif self.inversefft:
-                self.image = np.zeros(shape=(self.ntimesteps, self.blocksize, self.NAXIS1, self.NAXIS2), dtype=float)
-            else:
-                self.image = np.zeros(shape=(self.ntimesteps, self.NAXIS1, self.NAXIS2, self.nfreq), dtype=float)
-
+        self.t_image=cr.hArray(complex, dimensions=(self.NAXIS1, self.NAXIS2, self.nfreq), fill=0.)
 
     def run(self):
         """Run the imager.
         """
 
+        # Get steps corresponding to mask
+        mask = self.mask
+        step = cr.hArray(int, cr.hCountZero(mask))
+        cr.hMaskToStep(step, mask)
+
         start = time.time()
         for tstep in range(self.ntimesteps):
-            print "processing timestep: %d/%d" % (tstep, self.ntimesteps)
-
             for block in range(self.startblock, self.startblock+self.nblocks):
 
-                if self.frequency_slice != None:
+                print "processing block:", block
+
+                if self.nfmin != None and self.nfmax != None:
                     self.data.getFFTData(self.scratchfft, block)
 
-                    self.fftdata[...].copy(self.scratchfft[..., self.frequency_slice[0]:self.frequency_slice[1]])
+                    self.fftdata[...].copy(self.scratchfft[..., self.nfmin:self.nfmax])
                 else:
                     self.data.getFFTData(self.fftdata, block)
 
                 cr.hFFTConvert(self.fftdata[...])
+                np.save("indata", self.fftdata.toNumpy())
 
                 print "reading done"
 
-                print "beamforming started"
+                self.t_image.fill(0.)
 
-                if self.intgrfreq:
-                    cr.hBeamformImageAndIntegrate(self.image[tstep], self.fftdata, self.frequencies, self.delays)
-                elif self.DM:
-                    self.t_image.fill(0.)
-                    cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.delays)
+                print "beamforming started"
+                cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.delays)
+#                cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.antpos, self.grid.cartesian)
+#                cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.antpos, self.grid.cartesian, step)
+                print "beamforming done"
+
+                if self.DM:
                     cr.hShiftedAbsSquareAdd(self.image, self.t_image, self.dispersion_shifts + tstep)
                 elif self.inversefft:
-                    self.t_image.fill(0.)
-                    cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.delays)
                     cr.hFFTWExecutePlan(self.t_image2, self.t_image, self.plan)
-                    cr.hSquareAddTransposed(self.image[tstep], self.t_image2, self.blocksize)
+                    cr.hSquareAdd(self.image[tstep], self.t_image2)
                 else:
-                    self.t_image.fill(0.)
-                    cr.hBeamformImage(self.t_image, self.fftdata, self.frequencies, self.delays)
                     cr.hAbsSquareAdd(self.image[tstep], self.t_image)
-
-                print "beamforming done"
 
             self.startblock += self.nblocks
 
         end = time.time()
         print "total runtime:", end-start, "s"
-
-        # Save image to disk
-
-        if self.inversefft:
-            self.image = self.image.reshape((self.ntimesteps * self.blocksize, self.NAXIS1, self.NAXIS2))
-            self.image = np.rollaxis(self.image, 0, 3)
-        elif self.intgrfreq:
-            self.image = np.rollaxis(self.image, 0, 3)
-        else:
-            self.image = np.rollaxis(self.image, 0, 4)
-
-        if self.inversefft:
-            savefits(self.output, self.image,
-                        OBSLON=self.OBSLON,
-                        OBSLAT=self.OBSLAT,
-                        CTYPE1=self.CTYPE1,
-                        CTYPE2=self.CTYPE2,
-                        CTYPE3="TIME",
-                        LONPOLE=self.LONPOLE,
-                        LATPOLE=self.LATPOLE,
-                        CRVAL1=self.CRVAL1,
-                        CRVAL2=self.CRVAL2,
-                        CRVAL3=0.,
-                        CRPIX1=self.CRPIX1,
-                        CRPIX2=self.CRPIX2,
-                        CRPIX3=0.,
-                        CDELT1=self.CDELT1,
-                        CDELT2=self.CDELT2,
-                        CDELT3=self.data["SAMPLE_INTERVAL"][0],
-                        CUNIT1=self.CUNIT1,
-                        CUNIT2=self.CUNIT2,
-                        CUNIT3="s",
-                        PC001001=self.PC001001,
-                        PC002001=self.PC002001,
-                        PC001002=self.PC001002,
-                        PC002002=self.PC002002)
-
-        elif self.intgrfreq:
-            savefits(self.output, self.image,
-                        OBSLON=self.OBSLON,
-                        OBSLAT=self.OBSLAT,
-                        CTYPE1=self.CTYPE1,
-                        CTYPE2=self.CTYPE2,
-                        CTYPE3="TIME",
-                        LONPOLE=self.LONPOLE,
-                        LATPOLE=self.LATPOLE,
-                        CRVAL1=self.CRVAL1,
-                        CRVAL2=self.CRVAL2,
-                        CRVAL3=0.,
-                        CRPIX1=self.CRPIX1,
-                        CRPIX2=self.CRPIX2,
-                        CRPIX3=0.,
-                        CDELT1=self.CDELT1,
-                        CDELT2=self.CDELT2,
-                        CDELT3=self.CDELT4,
-                        CUNIT1=self.CUNIT1,
-                        CUNIT2=self.CUNIT2,
-                        CUNIT3="s",
-                        PC001001=self.PC001001,
-                        PC002001=self.PC002001,
-                        PC001002=self.PC001002,
-                        PC002002=self.PC002002)
-
-        else:
-            savefits(self.output, self.image,
-                        OBSLON=self.OBSLON,
-                        OBSLAT=self.OBSLAT,
-                        CTYPE1=self.CTYPE1,
-                        CTYPE2=self.CTYPE2,
-                        CTYPE3=self.CTYPE3,
-                        CTYPE4=self.CTYPE4,
-                        LONPOLE=self.LONPOLE,
-                        LATPOLE=self.LATPOLE,
-                        CRVAL1=self.CRVAL1,
-                        CRVAL2=self.CRVAL2,
-                        CRVAL3=self.CRVAL3,
-                        CRVAL4=self.CRVAL4,
-                        CRPIX1=self.CRPIX1,
-                        CRPIX2=self.CRPIX2,
-                        CRPIX3=self.CRPIX3,
-                        CRPIX4=self.CRPIX4,
-                        CDELT1=self.CDELT1,
-                        CDELT2=self.CDELT2,
-                        CDELT3=self.CDELT3,
-                        CDELT4=self.CDELT4,
-                        CUNIT1=self.CUNIT1,
-                        CUNIT2=self.CUNIT2,
-                        CUNIT3=self.CUNIT3,
-                        CUNIT4=self.CUNIT4,
-                        PC001001=self.PC001001,
-                        PC002001=self.PC002001,
-                        PC001002=self.PC001002,
-                        PC002002=self.PC002002)
 
