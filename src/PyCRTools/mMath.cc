b81d054fe3e884c08b58ae0ccef7e0a4d0a4fd0d
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-11-15 10:34:27 +0000

    Long long.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10961 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/mMath.cc
+++ src/PyCRTools/mMath.cc
@@ -6008,21 +6008,21 @@ void HFPP_FUNC_NAME (const NIter snr, const NIter snr_end,
   if (signal_start < 0)
   {
     char error_message[256];
-    sprintf(error_message, "[hMaxSNR] signal_start[=%ld] < 0", signal_start);
+    sprintf(error_message, "[hMaxSNR] signal_start[=%lld] < 0", signal_start);
     throw PyCR::ValueError(error_message);
   }
 
   if (signal_end < signal_start || signal_end > n)
   {
     char error_message[256];
-    sprintf(error_message, "[hMaxSNR] signal_end[=%ld] < signal_start[=%ld]", signal_end, signal_start);
+    sprintf(error_message, "[hMaxSNR] signal_end[=%lld] < signal_start[=%lld]", signal_end, signal_start);
     throw PyCR::ValueError(error_message);
   }
 
   if (signal_end > n)
   {
     char error_message[256];
-    sprintf(error_message, "[hMaxSNR] signal_end[=%ld] > n=[%ld]", signal_end, n);
+    sprintf(error_message, "[hMaxSNR] signal_end[=%lld] > n=[%lld]", signal_end, n);
     throw PyCR::ValueError(error_message);
   }
 
