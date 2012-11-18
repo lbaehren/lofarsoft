095165634cff3329b65879ef0531f791e82a6fcf
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-11-18 10:38:37 +0000

    Add alt_status option to crdb_update.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@10986 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/extras/crdb_update.py
+++ src/PyCRTools/extras/crdb_update.py
@@ -8,10 +8,11 @@ from optparse import OptionParser
 
 # Parse commandline options
 parser = OptionParser()
-parser.add_option("-a", "--all-events", action="store_true", default=False, help = "apply action to all events in database")
+parser.add_option("-e", "--every-event", action="store_true", default=False, help = "apply action to all events in database")
 parser.add_option("-s", "--event-status", default = "NEW", help = "update event status to given value")
 parser.add_option("-o", "--old-event-status", default = "NEW", help = "update all events with this status to new status")
 parser.add_option("-d", "--database", default = "cr.db", help = "filename of database")
+parser.add_option("-a", "--alt-status", action = "store_true", default = False, doc = "use alternate status for all actions.")
 
 (options, args) = parser.parse_args()
 
@@ -28,14 +29,22 @@ with con:
 
         events = [e[0] for e in cur.fetchall()]
     elif options.old_event_status != "NEW":
-        cur.execute("SELECT eventID FROM events WHERE status=?", (options.old_event_status, ))
+        if options.alt_status:
+            cur.execute("SELECT eventID FROM events WHERE alt_status=?", (options.old_event_status, ))
+        else:
+            cur.execute("SELECT eventID FROM events WHERE status=?", (options.old_event_status, ))
 
         events = [e[0] for e in cur.fetchall()]
     else:
         events = args
 
 
-    for eid in events:
-        print "setting status of event", eid, "to", options.event_status
-        cur.execute('UPDATE events SET status=? WHERE eventID=?', (options.event_status, eid))
+    if options.alt_status:
+        for eid in events:
+            print "setting alt_status of event", eid, "to", options.event_alt_status
+            cur.execute('UPDATE events SET alt_status=? WHERE eventID=?', (options.event_alt_status, eid))
+    else:
+        for eid in events:
+            print "setting status of event", eid, "to", options.event_status
+            cur.execute('UPDATE events SET status=? WHERE eventID=?', (options.event_status, eid))
 
