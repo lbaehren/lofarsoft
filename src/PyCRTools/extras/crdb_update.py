998ca95cf3ae2f29df98c31150bb08d84f15cf8c
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-11-26 13:14:03 +0000

    Add PostgreSQL option to crdb_update.py
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11112 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/extras/crdb_update.py
+++ src/PyCRTools/extras/crdb_update.py
@@ -4,6 +4,12 @@
 import sys
 import sqlite3
 
+try:
+    import psycopg2
+    have_psycopg2 = True
+except ImportError:
+    have_psycopg2 = False
+
 from optparse import OptionParser
 
 # Parse commandline options
@@ -11,13 +17,24 @@ parser = OptionParser()
 parser.add_option("-e", "--every-event", action="store_true", default=False, help = "apply action to all events in database")
 parser.add_option("-s", "--event-status", default = "NEW", help = "update event status to given value")
 parser.add_option("-o", "--old-event-status", default = "NEW", help = "update all events with this status to new status")
-parser.add_option("-d", "--database", default = "cr.db", help = "filename of database")
 parser.add_option("-a", "--alt-status", action = "store_true", default = False, help = "use alternate status for all actions.")
+parser.add_option("-d", "--database", default = "crdb.sqlite", help = "filename of database")
+parser.add_option("--host", default=None, help="PostgreSQL host.")
+parser.add_option("--user", default=None, help="PostgreSQL user.")
+parser.add_option("--password", default=None, help="PostgreSQL password.")
+parser.add_option("--dbname", default=None, help="PostgreSQL dbname.")
 
 (options, args) = parser.parse_args()
 
 # Connect to database
-con = sqlite3.connect(options.database)
+if have_psycopg2 and options.host:
+    # Open PostgreSQL database
+    print "Opening connection to PostgreSQL database"
+    con = psycopg2.connect(host=options.host, user=options.user, password=options.password, dbname=options.dbname)
+else:
+    # Open SQLite database
+    print "Opening Sqlite database"
+    con = sqlite3.connect(options.database, timeout=60.0)
 
 with con:
 
