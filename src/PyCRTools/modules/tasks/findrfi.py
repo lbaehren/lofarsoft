e6acd09c00e9500d74dd5f4fed91062586a0d7e5
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-12-12 10:42:33 +0000

    Part of the object.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11262 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/findrfi.py
+++ src/PyCRTools/modules/tasks/findrfi.py
@@ -274,9 +274,9 @@ class FindRFI(Task):
 
 #        calcbaseline1 = cr.trerun("CalcBaseline",1, avgspectrum,pardict = pardict,invert=False,HanningUp=False,normalize=False,doplot=0)
 #        amplitudes=hArray(copy=calcbaseline1.baseline)
-        amplitude_spectrum = self.cleaned_spectrum.new()
-        amplitude_spectrum.copy(self.cleaned_spectrum)
-        amplitude_spectrum.sqrt()
+        self.amplitude_spectrum = self.cleaned_spectrum.new()
+        self.amplitude_spectrum.copy(self.cleaned_spectrum)
+        self.amplitude_spectrum.sqrt()
         
         self.antennas_cleaned_sum_amplitudes =  2 * cr.hArray(self.amplitude_spectrum[...].sum())
         # Sum up power for avg spectrum
