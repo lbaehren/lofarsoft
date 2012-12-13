c8cb150a87325c01326913aee7a55cba85256b07
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-12-13 10:02:51 +0000

    Remove debug and fix problem.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11287 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/shower.py
+++ src/PyCRTools/modules/tasks/shower.py
@@ -197,11 +197,6 @@ class Shower(Task):
     def run(self):
         """Run the task.
         """
-
-        print "DEBUG:"
-        print self.positions
-        print self.signals
-        print self.timelags
         
         
         # ---------------------  LDF  ------------------------------ #
@@ -336,11 +331,10 @@ class Shower(Task):
                     self.signals = self.signals[scaling_check]
                     self.positions = self.positions[scaling_check]
                     self.timelags = self.timelags[scaling_check]
-                    print "DEBUG after scaling:"
-                    print self.positions
-                    print self.signals
-                    print self.timelags
-                    
+                    if self.signals.shape[0] == 0:
+                        print "When scaling to superterp no stations left. Abort. Draw without scaling to see station."
+                        return
+                                            
                 if self.timelags is None:
                    self.scolors = "blue"
                    print "WARNING, footprint does not represent the time, only the signal strength"
