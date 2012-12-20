e9521e2103cad0b5b04196525f16b3d982f0b664
corstanje <corstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2012-12-20 13:24:08 +0000

    The art of removing outliers from a dataset... Working solution for arrivaltimes in Wavefront task.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11384 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/wavefront.py
+++ src/PyCRTools/modules/tasks/wavefront.py
@@ -45,13 +45,26 @@ def mseMinimizer(direction, pos, times, outlierThreshold=0, allowOutlierCount=0)
 
     return mse
 
+def flaggedIndicesForOutliers(inarray, k_sigma = 6):
+    # Remove outliers beyond k-sigma (above and below)
+    # Use robust estimators, i.e. median and percentile-based sigma.
+    n = len(inarray)
+    sortedArray = np.sort(inarray)
+    median = np.median(sortedArray)
+    noise = 0.5 * (sortedArray[0.7*n] - sortedArray[0.3*n]) # ~ sigma estimator, robust against 30% bad data
+
+    goodIndices = np.where( abs(inarray - median) < k_sigma * noise )[0] # indices in original array!
+    flaggedIndices = np.array( [i for i in range(n) if i not in goodIndices] ) # inversion
+
+    return (flaggedIndices, goodIndices)
+
 
 def pulseTimeFromLORA(lora_dir, datafile):
     print "---> Reading pulse timing information from LORA"
     tbb_starttime = datafile["TIME"][0]
     tbb_samplenumber = max(datafile["SAMPLE_NUMBER"])
     blocksize = datafile["BLOCKSIZE"]
-    
+
     lora_event_info = 0 # to check with 'if lora_event_info:'
     lora_logfile = os.path.join(lora_dir,"LORAtime4")
     if os.path.exists(lora_logfile):
@@ -62,7 +75,7 @@ def pulseTimeFromLORA(lora_dir, datafile):
                 (block_number_lora,sample_number_lora)=lora.loraTimestampToBlocknumber(tbb_starttime_sec,tbb_starttime_nsec,tbb_starttime,tbb_samplenumber,blocksize=blocksize)
             except ValueError:
                 raise ValueError("#ERROR - LORA trigger information not found")
-                
+
             print "---> Estimated block number from LORA: block =",block_number_lora,"sample =",sample_number_lora
             block_number = block_number_lora
             sample_number = sample_number_lora
@@ -75,7 +88,7 @@ def pulseTimeFromLORA(lora_dir, datafile):
         raise ValueError( "WARNING: No LORA logfile found - " + str(lora_logfile) )
 
     return (block_number, sample_number)
-       
+
 
 class Wavefront(Task):
     """
@@ -108,7 +121,7 @@ class Wavefront(Task):
         #    doc = "Forward plan for FFTW, by default use a plan that works over all antennas at once and is measured for speed because this is applied many times." ),
         arrivalTimes = dict( default = None, doc = "Pulse arrival times per antenna, for antennas in self.f['SELECTED_DIPOLES']", output = True),
         pointsourceArrivalTimes = dict( default = None, doc = "Arrival times from best-fit point source approximation", output = True),
-        planewaveArrivalTimes = dict( default = None, doc = "Arrival times from best-fit plane-wave approximation.", output = True),  
+        planewaveArrivalTimes = dict( default = None, doc = "Arrival times from best-fit plane-wave approximation.", output = True),
         fitPlaneWave = dict( default = None, doc = "Fit results (az, el, mse) for planar fit.", output = True ),
         fitPointSource = dict( default = None, doc = "Fit results (az, el, R, mse) for point source simplex search.", output = True ),
         bad_antennas = dict( default = [],
@@ -154,10 +167,10 @@ class Wavefront(Task):
             self.f["SELECTED_DIPOLES"] = selected_dipoles
 
         self.blocksize = self.f["BLOCKSIZE"]
-        
+
         # Get the pulse location in the data from LORA timing: block and sample number
         (block, pulse_samplenr) = pulseTimeFromLORA(self.loradir, self.f)
-                    
+
         # assume LORA-LOFAR delay and file shifts are of the order ~ 200 samples << blocksize for cut-out timeseries
 
         antennaPositions = self.f["ANTENNA_POSITIONS"]
@@ -171,14 +184,14 @@ class Wavefront(Task):
         start = pulse_samplenr - cutoutSize / 2
         end = pulse_samplenr + cutoutSize / 2
         cutoutTimeseries[...].copy(timeseries[..., start:end])
-        
+
         # Get reference antenna, take the one with the highest maximum.
         y = cutoutTimeseries.toNumpy()
         refant = self.refant
         if not self.refant:
             refant = int(np.argmax(np.max(y, axis=1)))
         print 'Taking channel %d as reference antenna' % refant
-        
+
         #import pdb; pdb.set_trace()
         plt.plot(y[0])
         plt.plot(y[140])
@@ -212,7 +225,7 @@ class Wavefront(Task):
         # Assuming ordering CS002, 3, 4, 5, 7 for this event (sorted alphabetically)
         if not type(self.interStationDelays) == type(None): # cannot do if self.interStationDelays, apparently...
             subsampleOffsets -= self.interStationDelays
-            
+
 #        subsampleOffsets[1] -= 0.11e-9
 #        subsampleOffsets[2] -= -1.30e-9
 #        subsampleOffsets[3] -= -1.32e-9
@@ -236,7 +249,7 @@ class Wavefront(Task):
         signals = np.copy(times)
         signals.fill(2.71) # signal power not used here; do not give all 1.0 as the log is taken.
         fptask = cr.trerun("Shower", "1", positions=positions2D, signals=signals, timelags=times, footprint_colormap='jet', footprint_enable=True, footprint_shower_enable=False)
-        
+
         plt.title('Footprint using crosscorrelated arrival times')
         # Do plane-wave direction fit on full arrivaltimes
         # Fit pulse direction
@@ -251,27 +264,28 @@ class Wavefront(Task):
         self.planewaveArrivalTimes = bestfitDelays
 
         # Flag outlying residues. Determine valid range for residues, and set the zero-offset value
-        n = len(residu)
-        sortedResidues = np.sort(residu)
-        median = np.median(sortedResidues)
-        noise = 0.5 * (sortedResidues[0.7*n] - sortedResidues[0.3*n]) # ~ sigma estimator, robust against 30% bad data
-        trimmedResidues = sortedResidues[np.where( abs(sortedResidues - median) < 6 * noise )]
-        rangemin = min(trimmedResidues)
-        rangemax = max(trimmedResidues)
-        import pdb; pdb.set_trace()
+        (flaggedIndices, goodIndices) = flaggedIndicesForOutliers(residu)
+        print 'Flagged channels: '
+        print flaggedIndices
 
-#        residu[np.where(abs(residu) > 100e-9)] = 0.0  
+        goodResidues = residu[goodIndices]
+        goodPositions2D = positions2D[goodIndices]
+        goodPositions = goodPositions2D.ravel()
+        goodTimes = times[goodIndices]
+        goodSignals = signals[goodIndices]
+        goodResidues -= min(goodResidues)
+#        residu[np.where(abs(residu) > 100e-9)] = 0.0
 #        residu -= residu.mean()
 #        residu[np.where(abs(residu) > 15e-9)] = 0.0  # hack
 
 #        residu[np.argmax(residu)] = 0.0
 #        residu[np.argmin(residu)] = 0.0
-        residu -= rangemin
-        rangemax -= rangemin
+#        residu -= rangemin
+#        rangemax -= rangemin
         # now the good one: difference between measured arrival times and plane wave fit!
 #        import pdb; pdb.set_trace()
-        fptask_delta = cr.trerun("Shower", "3", positions=positions2D, signals=signals, timelags=residu, footprint_colormap='jet', footprint_enable=True, footprint_shower_enable=False)
-        plt.clim(0.0, rangemax)
+        fptask_delta = cr.trerun("Shower", "3", positions=goodPositions2D, signals=goodSignals, timelags=goodResidues, footprint_colormap='jet', footprint_enable=True, footprint_shower_enable=False)
+#        plt.clim(0.0, rangemax)
         plt.title('Footprint of residual delays w.r.t. planewave fit')
 
         # Simplex fit point source...
@@ -279,7 +293,7 @@ class Wavefront(Task):
         startPosition = (az, el, 1.0)  # 1.0 means R = 1000.0 ...
         print 'Doing simplex search for minimum MSE...'
         times -= times[0]
-        optimum = fmin(mseMinimizer, startPosition, (positions, times, 0, 4), xtol=1e-5, ftol=1e-5, full_output=1)
+        optimum = fmin(mseMinimizer, startPosition, (goodPositions, goodTimes, 0, 4), xtol=1e-5, ftol=1e-5, full_output=1)
         # raw_input('Done simplex search.')
 
         simplexDirection = optimum[0]
@@ -290,29 +304,29 @@ class Wavefront(Task):
         print '-----'
         print 'Simplex search: '
         print 'Best MSE: %f; best direction = (%f, %f, %f)' % (simplexMSE, simplexAz / deg2rad, simplexEl / deg2rad, simplexR)
-        msePlanar = sf.mseWithDistance(simplexAz, simplexEl, 1.0e6, positions, times, allowOutlierCount=4)
+        msePlanar = sf.mseWithDistance(simplexAz, simplexEl, 1.0e6, goodPositions, goodTimes, allowOutlierCount=4)
         print 'Best MSE for R = 10^6 (approx. planar): %f' % msePlanar
         print '-----'
         # get the calculated delays according to this plane wave
         # simplexDirection[2] = 4000.0
-        bestfitDelays = sf.timeDelaysFromDirectionAndDistance(positions, simplexDirection)
+        bestfitDelays = sf.timeDelaysFromDirectionAndDistance(goodPositions, simplexDirection)
         self.pointsourceArrivalTimes = bestfitDelays
-        
-        residu = times - bestfitDelays
+
+        residu = goodTimes - bestfitDelays
         # residu[np.where(abs(residu) > 100e-9)] = 0.0 # hack
         residu -= residu.mean()
         # residu[np.where(abs(residu) > 15e-9)] = np.float('nan') # hack
 
-        residu[np.argmax(residu)] = 0.0
-        residu[np.argmin(residu)] = 0.0
-        residu[np.argmax(residu)] = 0.0
-        residu[np.argmin(residu)] = 0.0
+#        residu[np.argmax(residu)] = 0.0
+#        residu[np.argmin(residu)] = 0.0
+#        residu[np.argmax(residu)] = 0.0
+#        residu[np.argmin(residu)] = 0.0
         # residu -= min(residu)
 
         # Difference between measured arrival times and point source fit. Check plot for fit errors!
- 
-        fptask_delta_pointsource = cr.trerun("Shower", "4", positions=positions2D, signals=signals, timelags=residu, footprint_colormap='jet', footprint_enable=True, footprint_shower_enable=False)
-        
+
+        fptask_delta_pointsource = cr.trerun("Shower", "4", positions=goodPositions2D, signals=goodSignals, timelags=residu, footprint_colormap='jet', footprint_enable=True, footprint_shower_enable=False)
+
         plt.title('Footprint of residual delays w.r.t. point source fit')
 
         """
