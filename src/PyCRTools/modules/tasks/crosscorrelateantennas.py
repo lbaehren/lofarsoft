ee5b0f32cd38b2ed1ec8da8a47e7b2976c04448b
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-01-07 16:20:53 +0000

    debug
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11450 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/crosscorrelateantennas.py
+++ src/PyCRTools/modules/tasks/crosscorrelateantennas.py
@@ -126,8 +126,8 @@ class FindPulseDelay(Task):
         temp = self.trace.new()
         temp[...].copy(self.trace[...])
         temp.abs()
-        print temp[0].max(), temp[0].maxpos()
-        print temp[1].max(), temp[1].maxpos()
+        print temp[0].max(), temp[0].maxpos(), temp[0, temp[0].maxpos()]
+        print temp[1].max(), temp[1].maxpos(), temp[1, temp[1].maxpos()]
 
         # Calculate position
         self.maxpos[...] = temp[...].maxpos()
