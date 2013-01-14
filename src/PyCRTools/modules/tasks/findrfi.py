0c9ac397f84cf07f503573ceacc7b5c797b7ef47
corstanje <corstanje@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-01-14 13:27:29 +0000

    FindRFI: Remove 1st harmonic after all... Power estimate appears to be better. But what is supposed to be in this 1st harmonic?? It depends on Hanning filter yes/no. Some polishing.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11538 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/findrfi.py
+++ src/PyCRTools/modules/tasks/findrfi.py
@@ -76,13 +76,10 @@ class FindRFI(Task):
         nofblocks=dict(default=-1, doc="Number of data blocks to process. Set to -1 for entire file."),
         startblock=dict(default=0, doc="Start processing file at this block nr."),
         refant=dict(default=None, doc="Optional parameter to set reference antenna number."),
-#        timeseries_data = dict( default = lambda self : cr.hArray(float, dimensions=(self.nantennas, self.blocksize)),
-#            doc = "Timeseries data." ),
+
         freq_range=dict(default=None, doc="Optional frequency range to consider; everything outside the range is flagged as 'bad'. Give as tuple, e.g. (30, 80)"),
         fft_data=dict(default=lambda self: cr.hArray(complex, dimensions=(self.nantennas, self.nfreq)),
             doc="Fourier transform of timeseries_data_resampled."),
-        # fftwplan = dict( default = lambda self : cr.FFTWPlanManyDftR2c(self.blocksize, self.nantennas, 1, 1, 1, 1, cr.fftw_flags.MEASURE),
-        #    doc = "Forward plan for FFTW, by default use a plan that works over all antennas at once and is measured for speed because this is applied many times." ),
         bad_antennas = dict(default=[],
             doc="Antennas found to be bad.", output=True),
         good_antennas = dict(default=lambda self: [name for name in self.f["SELECTED_DIPOLES"] if name not in self.bad_antennas],
@@ -152,13 +149,13 @@ class FindRFI(Task):
             # But no window makes the cleaning less effective... :(
             spectrum = self.fft_data / self.f["BLOCKSIZE"]  # normalize back to ADC units
             spectrum[..., 0] = 0.0  # reject DC component
+            spectrum[..., 1] = 0.0  # reject 1st harmonic. NB! May be wrong for getting power estimates !!!
             # test hack
 #            print spectrum.shape()
 #            if i == 0:
 #                spectrum.fill(0.0)
             # for j in range(spectrum.shape()[1]):
             #    spectrum[20, j] = 0.0
-#            import pdb; pdb.set_trace()
             # end test hack
             magspectrum.copy(spectrum)
             magspectrum.abs()
@@ -167,15 +164,14 @@ class FindRFI(Task):
                 channel_power = 2 * cr.hArray(magspectrum[...].sum())
                 refant = np.argsort(channel_power.toNumpy())[self.nantennas / 2]
                 refant = int(refant)  # numpy.int64 causes problems...
-                print 'Taking antenna %d as reference antenna' % refant
+                print 'Taking channel %d as reference antenna' % refant
                 # the index halfway in the sorted array is the channel with median power
                 magspectrum.sqrt()
 #            magspectrum += 1e-9
 
             incphase.copy(spectrum)
             incphase /= (magspectrum + 1.0e-9)  # divide abs-magnitude out to get exp(i phi). Avoid div by 0
-            incphaseRef = cr.hArray(incphase[refant].vec())  # improve? i.e. w/o memory leak. Phases of channel 0 for all freqs
-            # Note that this assumes that antenna 0 has valid data...! What to do when it doesn't?
+            incphaseRef = cr.hArray(incphase[refant].vec())  # improve? i.e. w/o memory leak. Phases of refant channel for all freqs
             incphase /= incphaseRef
             incphase[..., 0] = 1.0  # reject DC component and first harmonic.
             incphase[..., 1] = 1.0
@@ -238,65 +234,42 @@ class FindRFI(Task):
             alldirtychannels = [int(ch) for ch in range(len(freqs)) if (ch in dirty_channels) or not (self.freq_range[0] < freqs[ch] < self.freq_range[1])]
         else:
             alldirtychannels = [int(ch) for ch in dirty_channels]
-#            alldirtychannels = []
-#            for i in range(len(freqs)):
-#                if (i in dirty_channels) or not (self.freq_range[0] < freqs[i] < self.freq_range[1]):
-#                    alldirtychannels.append(i)
-                # cleaner solution?
-#            dirty_channels = alldirtychannels
+
         self.dirty_channels = alldirtychannels
         # Get cleaned spectrum per antenna
         cleanedspectrum = cr.hArray(copy=avgspectrum)
-#        import pdb; pdb.set_trace()
         cleanedspectrum[..., cr.hArray(self.dirty_channels)] = 0.0  # seems not to work with hArrays as cleanedspectrum[..., cr.hArray(self.dirty_channels)] = 0.0
 #        plt.figure()
 #        plt.plot(np.log(cleanedspectrum.toNumpy()[0]))
 #        plt.plot(np.log(cleanedspectrum.toNumpy()[20]))
         self.cleaned_spectrum = cleanedspectrum
         # cleanedspectrum[self.dirty_channels] = np.float('nan')
-#        self.cleaned_spectrum = cr.hArray(cleanedspectrum)
 
         # Get median cleaned spectrum (over all antennas)
         median_cleaned_spectrum = np.copy(y)
         median_cleaned_spectrum[self.dirty_channels] = 0.0
         self.median_cleaned_spectrum = cr.hArray(median_cleaned_spectrum)
-        # Compute FFT
-        # cr.hFFTWExecutePlan(self.fft_data, self.timeseries_data_resampled, self.fftwplan)
 
-        # Get baseline per antenna of the average spectrum
-#        pardict = dict(plot_finish = Pause,
-#                       FitBaseline = dict(ncoeffs=80,numin=30,numax=85,fittype="POLY",splineorder=3),
-#                       CalcBaseline = dict(baseline=False) ) # Make sure baseline is recreated when the task is run a second time
-
-#        print "---> Fit a baseline to the average spectrum"
-#        avgspectrum.xvalues = self.f["FREQUENCY_DATA"]
-#        fitbaseline = cr.trerun("FitBaseline","",avgspectrum, extendfit=0.5,pardict=pardict, frequency = avgspectrum.xvalues, doplot=0)
-
-#        calcbaseline1 = cr.trerun("CalcBaseline",1, avgspectrum,pardict = pardict,invert=False,HanningUp=False,normalize=False,doplot=0)
-#        amplitudes=hArray(copy=calcbaseline1.baseline)
         self.amplitude_spectrum = self.cleaned_spectrum.new()
         self.amplitude_spectrum.copy(self.cleaned_spectrum)
         self.amplitude_spectrum.sqrt()
-        
+
         self.antennas_cleaned_sum_amplitudes =  2 * cr.hArray(self.amplitude_spectrum[...].sum())
         # Sum up power for avg spectrum
-        # Subtract dirty channels
-        cleaned_power = 2 * cr.hArray(self.cleaned_spectrum[...].sum())
+        # Factor 2 because of real-FFT.
+        self.antennas_cleaned_power = 2 * cr.hArray(self.cleaned_spectrum[...].sum())
 #        dirty_power = 2 * cr.hArray(avgspectrum[...].sum() ) - cleaned_power
 #        total_power = 2 * cr.hArray(avgspectrum[...].sum() )
-        
-        
-        self.antennas_cleaned_power = cleaned_power
 
         # Get bad antennas from outliers in power
         # Instead of the non-robust k-sigma method, take the range to be from 1/2 to 2 times median power.
         # This has the advantage that it will always catch 0-power channels
         # and that it is robust for up to 50% - 1 outliers!
-        median_power = cleaned_power.median()[0]
+        median_power = self.antennas_cleaned_power.median()[0]
         channel_ids = self.f["SELECTED_DIPOLES"]
-        self.good_antennas = [channel_ids[i] for i in range(self.nantennas) if (0.5 * median_power < cleaned_power[i] < 2.0 * median_power)]
+        self.good_antennas = [channel_ids[i] for i in range(self.nantennas) if (0.5 * median_power < self.antennas_cleaned_power[i] < 2.0 * median_power)]
         self.bad_antennas = [id for id in channel_ids if id not in self.good_antennas]
-        print 'There are %d bad channels: ' % len(self.bad_antennas)
+        print 'There are %d bad channels (outliers in cleaned-power): ' % len(self.bad_antennas)
         print self.bad_antennas
 
         if self.save_plots:
