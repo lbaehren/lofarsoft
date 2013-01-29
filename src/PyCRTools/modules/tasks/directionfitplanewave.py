86a963d140ee730590a2b56e7c653f09ff66510e
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-01-29 13:36:48 +0000

    Maybe this fixes it now.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11643 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/modules/tasks/directionfitplanewave.py
+++ src/PyCRTools/modules/tasks/directionfitplanewave.py
@@ -180,15 +180,13 @@ class DirectionFitPlaneWave(tasks.Task):
                 indicesOfGoodAntennas = indicesOfGoodAntennas[goodSubset]
 
         cartesianDirection = [cos(el) * sin(az), cos(el) * cos(az), sin(el)]
-        print "direction input", cartesianDirection
         self.meandirection.copy(cr.hArray(cartesianDirection))
-        print "Input for spherical", self.meandirection
-        print "should be result:", pytmf.cartesian2spherical(self.meandirection[0], self.meandirection[1], self.meandirection[2])
-        print "it is before:", self.meandirection_spherical
-        # NB! Have to update the dependent parameters, or the previous value will come out.
-        self.ws.updateParameter("meandirection_spherical", forced=True)
-        self.ws.updateParameter("meandirection_azel_deg", forced=True)
-        self.ws.updateParameter("meandirection_azel", forced=True)
+        
+        if self.failed_fit != True:
+            # NB! Have to update the dependent parameters, or the previous value will come out.
+            self.ws.updateParameter("meandirection_spherical", forced=True)
+            self.ws.updateParameter("meandirection_azel_deg", forced=True)
+            self.ws.updateParameter("meandirection_azel", forced=True)
 
         # now redo arrays for full antenna set
         expectedDelays = srcfind.timeDelaysFromDirection(positions.ravel(), (az, el))  # need positions as flat 1-D array
