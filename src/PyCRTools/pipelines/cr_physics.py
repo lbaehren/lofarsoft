c5055a3de5338a9b2d179b179675724a0303184d
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-02-11 17:37:41 +0000

    Store crp_median_cleaned_spectrum.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11786 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/pipelines/cr_physics.py
+++ src/PyCRTools/pipelines/cr_physics.py
@@ -350,6 +350,8 @@ with process_event(crdb.Event(db=db, id=options.id)) as event:
                 # Select antennas which are marked good for both polarization
                 dipole_names = f["DIPOLE_NAMES"]
 
+                station["crp_median_cleaned_spectrum"] = findrfi.median_cleaned_spectrum
+
                 selected_dipoles = []
                 for i in range(len(dipole_names) / 2):
                     if dipole_names[2 * i] in findrfi.good_antennas and dipole_names[2 * i + 1] in findrfi.good_antennas:
