5bebdc75d3de5a8c858d5c3578373b820d8253f2
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-02-14 16:42:55 +0000

    Stupid storing of empty string.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11800 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/extras/crserver/crserver.py
+++ src/PyCRTools/extras/crserver/crserver.py
@@ -325,7 +325,7 @@ def events_handler():
             lora.attrib['good_reconstruction'] = "true"
 
         SubElement(lora, "energy").text = str(energy)
-        SubElement(lora, "log_energy").text = str(type(energy))
+        SubElement(lora, "log_energy").text = str(math.log10(energy)) if isinstance(energy, float) else ""
         SubElement(lora, "core_x").text = str(core_x)
         SubElement(lora, "core_y").text = str(core_y)
         SubElement(lora, "azimuth").text = str(azimuth)
