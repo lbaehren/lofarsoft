b35598c10361d85f7a66606433434dc30ec885f9
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-03-28 10:57:12 +0000

    Part of class.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@11990 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/pipelines/cr_physics.py
+++ src/PyCRTools/pipelines/cr_physics.py
@@ -349,7 +349,7 @@ with process_event(crdb.Event(db=db, id=options.id)) as event:
                 except ZeroDivisionError as e:
                     raise StationError("findrfi reports NaN in file {0}".format(e.message))
 
-                print "blocks_with_dataloss", blocks_with_dataloss
+                print "blocks_with_dataloss", findrfi.blocks_with_dataloss
 
                 for plot in findrfi.plotlist:
                     station["crp_plotfiles"].append(plot)
