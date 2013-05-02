1f4fb2ea9686c4f77aecc670988acd24e9b10b15
pschella <pschella@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-05-02 15:32:50 +0000

    Bandwidth = 100MHz
    git-svn-id: http://usg.lofar.org/svn/code/trunk@12145 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/pipelines/cr_physics.py
+++ src/PyCRTools/pipelines/cr_physics.py
@@ -325,11 +325,11 @@ with process_event(crdb.Event(db=db, id=options.id)) as event:
 
             # Get bandpass filter
             nf = f["BLOCKSIZE"] / 2 + 1
-            edge_width=10
+            edge_width=10.
             print "nf", nf
             print "width", int(edge_width * nf / f["CLOCK_FREQUENCY"])
             gaussian_weights = cr.hArray(cr.hGaussianWeights(int(edge_width * nf / f["CLOCK_FREQUENCY"])))
-            bandpass_filter[int(nf * 20. / 200.):int(nf * 90. / 200.)] = 1.0
+            bandpass_filter[int(nf * 20. / 100.):int(nf * 90. / 100.)] = 1.0
             cr.hRunningAverage(bandpass_filter, gaussian_weights)
 
             np.save("bandpass_filter.npy", bandpass_filter.toNumpy())
