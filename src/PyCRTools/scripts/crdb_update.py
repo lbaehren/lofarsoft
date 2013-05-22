80c8dae116241a1c68a46f6908fb2501148a1848
anelles <anelles@e3a606d5-b720-0410-b6ad-cb58f335abf8>
2013-05-22 13:37:31 +0000

    Adding functionality to update all events of a type.
    git-svn-id: http://usg.lofar.org/svn/code/trunk@12219 e3a606d5-b720-0410-b6ad-cb58f335abf8

--- src/PyCRTools/scripts/crdb_update.py
+++ src/PyCRTools/scripts/crdb_update.py
@@ -19,6 +19,7 @@ parser.add_option("-s", "--event-status", default = "NEW", help = "update event 
 parser.add_option("-o", "--old-event-status", default = "NEW", help = "update all events with this status to new status")
 parser.add_option("-a", "--alt-status", action = "store_true", default = False, help = "use alternate status for all actions.")
 parser.add_option("-d", "--database", default = "crdb.sqlite", help = "filename of database")
+parser.add_option("-t", "--event-type",default=None, help="Update all events of this type to NEW")
 parser.add_option("--host", default=None, help="PostgreSQL host.")
 parser.add_option("--user", default=None, help="PostgreSQL user.")
 parser.add_option("--password", default=None, help="PostgreSQL password.")
@@ -62,6 +63,14 @@ else:
     for eid in events:
         print "setting status of event", eid, "to", options.event_status
         cur.execute("UPDATE events SET status='{0}' WHERE eventID='{1}'".format(options.event_status, eid))
+        
+if options.event_type == 'HBA':
+    cur.execute("UPDATE events SET status='NEW' WHERE ((antennaset='HBA_DUAL') OR (antennaset='HBA_JOINED') OR (antennaset='HBA_DUAL_INNER') OR(antennaset='HBA_ZERO') OR (antennaset='HBA_ONE'))")
+    
+elif options.event-type == 'LBA':
+    cur.execute("UPDATE events SET status='NEW' WHERE (antennaset='LBA_INNER' OR antennaset='LBA_OUTER')")  
+
+    cur.execute("UPDATE events SET status='NEW' WHERE (antennaset='{0}'".format(options.event-type))")           
 
 con.commit()
 
