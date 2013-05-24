Fixed sign (hopefully)
git-svn-id: http://usg.lofar.org/svn/code/trunk@12239 e3a606d5-b720-0410-b6ad-cb58f335abf8

src/PyCRTools/modules/tasks/pulseenvelope.py
@@ -201,7 +201,9 @@ class PulseEnvelope(Task):
             self.polarization_angle /= 2
 
         s = self.timeseries_data_resampled.toNumpy()
-        self.sign = s[:, self.maxpos[i] + (self.pulse_start - self.window_start) * int(self.resample_factor)]
+        self.sign = np.zeros(s.shape[0])
+        for i in range(s.shape[0]):
+            self.sign[i] = s[i, self.maxpos[i] + (self.pulse_start - self.window_start) * int(self.resample_factor)]
         self.sign /= np.abs(self.sign)
 
         if self.save_plots: